c765c67a054214d5ee35bb4c416b1d9710270ca2
Sébastien Nana <sebastien.nana@maarch.org>
2009-09-28 11:41:17 +0000

    Création rep entreprise

---
+++ notifications/trunk/class/class_modules_tools.php
@@ -0,0 +1,110 @@
+<?php 
+/**
+* modules tools Class for physical archives
+*
+*  Contains all the functions to  modules tables for physical archives
+*
+* @package  maarch
+* @version 3.0
+* @since 10/2005
+* @license GPL v3
+* @author  Loic Vinet  <dev@maarch.org>
+* 
+*/
+
+class modules_notification
+{
+
+/**
+	* Build Maarch module tables into sessions vars with a xml configuration file
+	*/
+	function build_config($conf)
+	{
+		
+		$xmlconfig = simplexml_load_file($conf);
+		foreach($xmlconfig->CONFIG as $CONFIG)
+		{
+				$_SESSION['config']['databaseserver'] = utf8_decode((string) $CONFIG->databaseserver);
+				$_SESSION['config']['databaseserverport'] = utf8_decode((string) $CONFIG->databaseserverport);
+				$_SESSION['config']['databasename'] = utf8_decode((string) $CONFIG->databasename);
+				$_SESSION['config']['databaseuser'] = utf8_decode((string) $CONFIG->databaseuser);
+				$_SESSION['config']['databasetype'] = utf8_decode((string) $CONFIG->databasetype);
+				$_SESSION['config']['databasepassword'] = utf8_decode((string) $CONFIG->databasepassword);
+			
+				$_SESSION['config']['businessapppath'] = (string) $CONFIG->businessapppath;
+				$_SESSION['config']['businessappurl'] = (string) $CONFIG->businessappurl;
+				$_SESSION['config']['MaarchDirectory'] = utf8_decode((string) $CONFIG->MaarchDirectory);
+				
+				$_SESSION['config']['lang'] = utf8_decode((string) $CONFIG->lang);
+				$_SESSION['config']['mail'] = utf8_decode((string) $CONFIG->mailfrom);
+				$_SESSION['config']['debug'] = utf8_decode((string) $CONFIG->debug);
+				$_SESSION['config']['MaarchURL'] = utf8_decode((string) $CONFIG->MaarchURL);
+				$_SESSION['config']['tag_when_send'] = utf8_decode((string) $CONFIG->tag_when_send);
+		}
+
+		$i=0;
+
+		
+		foreach($xmlconfig->COLLECTION as $COLLECTION)
+		{
+			$_SESSION['collection'][$i] = array(
+												"id" => utf8_decode((string) $COLLECTION->id),
+												"label" => utf8_decode((string) $COLLECTION->label),
+												"table" => utf8_decode((string) $COLLECTION->table),
+												"view" => utf8_decode((string) $COLLECTION->view)
+												);
+			$j=0;
+			foreach($COLLECTION->extensions as $EXTENSIONS)
+			{
+					$_SESSION['collection'][$i]['extensions'][$j] = utf8_decode((string) $EXTENSIONS->table);
+					$j++;
+			}
+			$i++;
+		}
+		
+		
+		//Use this value par default for Maarch LetterBox v3
+		$_SESSION['used_table'] = $_SESSION['collection'][0]['table'];
+		
+		$_SESSION['ressources']['letterbox_view'] = $_SESSION['collection'][0]['view'];
+		
+		foreach($xmlconfig->TABLENAME as $TABLENAME)
+		{
+			$_SESSION['tablename']['doctypes'] = utf8_decode((string) $TABLENAME->doctypes);
+			$_SESSION['tablename']['history'] = utf8_decode((string) $TABLENAME->history);
+			$_SESSION['tablename']['listinstance'] = utf8_decode((string) $TABLENAME->listinstance);
+			$_SESSION['tablename']['users'] = utf8_decode((string) $TABLENAME->users);
+			$_SESSION['tablename']['user_abs'] = utf8_decode((string) $TABLENAME->user_abs);
+			$_SESSION['tablename']['entities'] = utf8_decode((string) $TABLENAME->entities);
+			$_SESSION['tablename']['ent_entities'] = utf8_decode((string) $TABLENAME->entities);
+			$_SESSION['tablename']['ent_users_entities'] = utf8_decode((string) $TABLENAME->users_entities);
+			$_SESSION['tablename']['contacts'] = utf8_decode((string) $TABLENAME->contacts);
+		}
+		
+		foreach($xmlconfig->TEMPLATES as $TEMPLATE)
+		{
+			$_SESSION['templates_directory'] = utf8_decode((string) $TEMPLATE->templates_directory);
+			$_SESSION['templates']['notif'] = utf8_decode((string) $TEMPLATE->notif);
+			$_SESSION['templates']['notif_copy'] = utf8_decode((string) $TEMPLATE->notif_copy);
+			$_SESSION['templates']['alarm1'] = utf8_decode((string) $TEMPLATE->alarm1);
+			$_SESSION['templates']['alarm1_copy'] = utf8_decode((string) $TEMPLATE->alarm1_copy);
+			$_SESSION['templates']['alarm2'] = utf8_decode((string) $TEMPLATE->alarm2);
+			$_SESSION['templates']['alarm2_copy'] = utf8_decode((string) $TEMPLATE->alarm2_copy);
+			
+		}
+		
+		foreach($xmlconfig->FEATURES as $FEATURES)
+		{
+			$_SESSION['features']['copy_for_notif'] = utf8_decode((string) $FEATURES->copy_for_notif);
+			$_SESSION['features']['copy_for_alarm1'] = utf8_decode((string) $FEATURES->copy_for_alarm1);
+			$_SESSION['features']['copy_for_alarm2'] = utf8_decode((string) $FEATURES->copy_for_alarm2);
+		}
+		foreach($xmlconfig->DEBUG as $DEBUG)
+		{
+			$_SESSION['debug']['send_mail'] = utf8_decode((string) $DEBUG->send_mail);
+			$_SESSION['debug']['tag_when_send'] = utf8_decode((string) $DEBUG->tag_when_send);
+			$_SESSION['debug']['console'] = utf8_decode((string) $DEBUG->console);
+			
+		}
+	}
+}
---
+++ notifications/trunk/lang/fr.php
@@ -0,0 +1,58 @@
+<?php //communs
+define('_MAIL_TO_PROCESS', 'Courriers &agrave; traiter');
+define('_HELLO','Bonjour');
+define('_THE_MAIL_NUM', 'le courrier n&deg;');
+define('_OF_TYPE', 'de type');
+define('_OBJECT', 'objet');
+define('_RECEIVED_THE', 're&ccedil;u le');
+define('_SEND_BY', 'adress&eacute; par');
+define('_MUST_BE_PROCESSED_BEFORE', 'est &agrave; traiter avant le');
+define('_ACCESS_TO_MAIL_TO_PROCESS', 'Acc&eacute;der au courrier pour le traiter');
+define('_PROCESS_MAIL', 'Traitement courrier');
+define('_USER', 'Utilisateur');
+define('_MAIL', 'Courrier');
+define('_WAS_SENT_TO', 'a &eacute;t&eacute; transmis &agrave;');
+define('_SEE_MAIL', 'Voir ce courrier');
+define('_NO_SENDED',' L&rsquo;email n\'a pas  &eacute;t&eacute; envoy&eacute;');
+// notifs.php
+define('_MUST_SPECIFY_CONFIG_FILE', 'Vous devez sp&eacute;cifier le fichier de configuration');
+define('_DU', 'du');
+define('_OF', 'de');
+define('_TO', 'de la part de ');
+define('_FOR', 'd&eacute;stin&eacute; &agrave;');
+define('_LIST_OF_MAIL_TO_PROCESS', 'voici la liste des nouveaux courriers &agrave; traiter');
+define('_MAIL_COPIES_TO', 'Courriers pour information du');
+define('_MAIL_COPIES_LIST', 'voici la liste des copies des courriers dont vous &ecirc;tes destinataire');
+define('_WHO_MUST_PROCESS_BEFORE', 'qui doit le traiter avant le');
+define('_ORIGINAL_PAPERS_ALREADY_SEND', 'Les originaux papier vous ont &eacute;t&eacute; adress&eacute;s');
+define('_WARNING', 'Attention');
+define('_YOU_MUST_BE_LOGGED', 'vous devez &ecirc;tre identifi&eacute; sur le logiciel avant de tenter d\'acc&eacute;der au courrier');
+define('_MAIL_TO_PROCESS_LIST','Liste des courriers à traiter');
+define('_COPIES_MAIL_LIST', 'Liste des courriers pour information');
+define('_BY', 'par');
+define('_DEPARTMENT', 'le service');
+define('_','');
+
+
+//relance1.php
+define('_FIRST_WARNING', 'Premi&egrave;re relance');
+define('_FIRST_WARNING_TITLE', 'Relance sur les courriers');
+define('_THIS_IS_FIRST_WARNING', 'Ceci est la premi&egrave;re relance pour les courriers suivants');
+define('_MUST_BE_ADDED_TO_PRIORITY', 'Merci d&rsquo;ajouter ce ou ces courriers &agrave; vos traitements prioritaires');
+define('_TO_PROCESS', '&agrave; traiter');
+//relance2.php
+define('_SECOND_WARNING', 'Relance sur mes courriers');
+define('_LATE_MAIL_TO_PROCESS', 'Courriers en retard &agrave; traiter');
+define('_YOU_ARE_LATE', 'Vous avez du retard dans le traitement des courriers suivants');
+define('_WAS_TO_PROCESS_BEFORE', '&eacute;tait &agrave; traiter avant le');
+define('_PROCESS_THIS_MAIL_QUICKLY', 'Merci de r&eacute;diger une r&eacute;ponse sous 48 heures');
+define('_LATE', 'Retard');
+define('_MAIL_TO_CC', 'courriers en copies');
+define('_FOLLOWING_MAIL_ARE_LATE', 'Le traitement des courriers suivants a pris du retard');
+define('_WHO_MUST_BE_PROCESSED_BEFORE', 'qui devait le traiter avant le');
+define('_COPY_TITLE', 'En copie');
+
+
+//notifications engine
+define('_WRONG_FUNCTION_OR_WRONG_PARAMETERS','_WRONG_FUNCTION_OR_WRONG_PARAMETERS')
+?>
