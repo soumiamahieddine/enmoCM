f7ce36be918548a087444b86702fc0d3891b34f5
Sébastien Nana <sebastien.nana@maarch.org>
2009-10-09 15:16:32 +0000

    Modification dossier

--- folder/trunk/class/class_admin_foldertypes.php
+++ folder/trunk/class/class_admin_foldertypes.php
@@ -143,7 +143,8 @@ class foldertype extends dbquery
 							?>
 							<p>
 								<label><?php  echo _COLLECTION; ?> : </label>
-								<input name="collection" id="collection" type="text" value="<?php  echo $func->show($sec->retrieve_coll_label_from_coll_id($_SESSION['m_admin']['foldertype']['COLL_ID'])); ?>" readonly />
+								<input name="collection_show" id="collection_show" type="text" value="<?php  echo $func->show($sec->retrieve_coll_label_from_coll_id($_SESSION['m_admin']['foldertype']['COLL_ID'])); ?>" readonly="readonly" />
+								<input name="collection" id="collection" type="hidden" value="<?php  echo $_SESSION['m_admin']['foldertype']['COLL_ID']; ?>" />
 							</p>
 							<p align="center">
 								<?php
