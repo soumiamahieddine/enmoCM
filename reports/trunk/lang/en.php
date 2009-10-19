fd468e51a0b20a9d772768f6b0aeec071ce6f737
Laurent Giovannoni <laurent.giovannoni@maarch.org>
2009-10-19 08:13:28 +0000

    fix: maj en.php

--- reports/trunk/lang/en.php
+++ reports/trunk/lang/en.php
@@ -1,7 +1,7 @@
 <?php
 /*
  *
- *    Copyright 2008,2009 Maarch    
+ *    Copyright 2008,2009 Maarch
  *
  *  This file is part of Maarch Framework.
  *
@@ -19,4 +19,36 @@
  *    along with Maarch Framework.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/*************************** Report management *****************/
+define('_ADMIN_REPORTS','Manage reports');
+define('_ADMIN_REPORTS_DESC','Manage reports');
+define('_REPORTS','Reports');
+define('_REPORTS_COMMENT','Reports');
+define('_OTHER_REPORTS','Other reports');
+define('_REPORTS_DESC','Edit reports');
+define('_HAVE_TO_SELECT_GROUP', 'You must choose a group');
+define('_AVAILABLE_REPORTS', 'Report available for');
+define('_GROUP_REPORTS_ADDED', 'Reports for the group');
+define('_CLICK_LINE_BELOW_TO_SEE_REPORT', 'Click on a line to see the report');
+define('_CLICK_LINE_BELOW_TO_RETURN_TO_REPORTS', 'Back to the list of reports');
+define('_NO_REPORTS_FOR_USER', 'No report available for this user');
+define('_PRINT_NO_RESULT', 'Nothing to display');
+
+
+//Global
+define('_DOCUMENTS_LIST','Document list');
+define('_DOCUMENTS_LIST_DESC','Documents list');
+
+/*************************** Module label in report *****************/
+define('_TITLE_STATS_DU','from');
+define('_TITLE_STATS_AU','to');
+
+
+define('_NB_TOTAL_DOC', 'Total number of documents');
+define('_NB_TOTAL_FOLDER', 'Total number of files');
+
+define('_NO_DATA_MESSAGE', 'Not enough data to generate the report');
+define('_REPORT', 'Report');
+define('_ERROR_REPORT_TYPE', 'The type of report is not correct');
+define('_ERROR_PERIOD_TYPE', 'The type of period is not correct');
 ?>
