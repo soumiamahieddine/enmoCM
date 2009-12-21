3fae39ef124976e304bea5671c34f4a9f50ca91d
Laurent Giovannoni <laurent.giovannoni@maarch.org>
2009-12-21 17:08:23 +0000

    fix : correction du compteur non affiché de la corbeille en copie

--- basket/trunk/display_basket_list.php
+++ basket/trunk/display_basket_list.php
@@ -70,7 +70,8 @@ $db->connect();
 			if(preg_match('/^CopyMailBasket/', $_SESSION['user']['baskets'][$i]['id']) && !empty($_SESSION['user']['baskets'][$i]['view']))
 			{
 				//$db->query('select r.RES_ID from '.$_SESSION['user']['baskets'][$i]['view']." r, ".$_SESSION['tablename']['ent_listinstance']." l where ".$_SESSION['user']['baskets'][$i]['clause']);
-				//$nb = $db->nb_result();
+				$db->query('select RES_ID from '.$_SESSION['user']['baskets'][$i]['view']." where ".$_SESSION['user']['baskets'][$i]['clause'], true);
+				$nb = $db->nb_result();
 			}
 			elseif(!empty($_SESSION['user']['baskets'][$i]['table']))
 			{
