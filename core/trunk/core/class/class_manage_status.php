e6b04236b108e0a59144c006bb6bf667f3f68d05
Laurent Giovannoni <laurent.giovannoni@maarch.org>
2010-02-10 15:58:56 +0000

    evol : load of status arr with class constructor

--- core/trunk/core/class/class_manage_status.php
+++ core/trunk/core/class/class_manage_status.php
@@ -14,9 +14,13 @@
 
 class manage_status extends dbquery
 {
+	public $statusArr;
+
 	function __construct()
 	{
 		parent::__construct();
+		$this->statusArr = array();
+		$this->get_status_data_array();
 	}
 
 	public function get_searchable_status()
@@ -42,8 +46,57 @@ class manage_status extends dbquery
 		}
 		return $status;
 	}
-
-	public function get_status_data($id_status,$extension = '')
+	
+	public function get_status_data_array()
+	{
+		$this->connect();
+		$this->query("select * from ".$_SESSION['tablename']['status']."");
+		while($res = $this->fetch_object())
+		{
+			$id_status = $res->id;
+			$status_txt = $this->show_string($res->label_status);
+			$maarch_module = $res->maarch_module;
+			$img_name = $res->img_filename;
+			if(!empty($img_name))
+			{
+				$temp_explode = explode( ".", $img_name);
+				$temp_explode[0] = $temp_explode[0].$extension;
+				$img_name = implode(".", $temp_explode);
+			}
+			if($maarch_module == 'apps' && isset($img_name) && !empty($img_name))
+			{
+				$img_path = $_SESSION['config']['businessappurl'].'static.php?filename='.$img_name;
+			}
+			else if(!empty($maarch_module) && isset($maarch_module)&& isset($img_name) && !empty($img_name))
+			{
+				$img_path = $_SESSION['config']['businessappurl'].'static.php?filename='.$img_name."&module=".$maarch_module;
+			}
+			else
+			{
+				$img_path = $_SESSION['config']['businessappurl'].'static.php?filename=default_status'.$extension.'.gif';
+			}
+			if(empty($status_txt) || !isset($status_txt))
+			{
+				$status_txt = $id_status;
+			}
+			array_push($this->statusArr, array('ID' => $id_status, 'LABEL' => $status_txt, 'IMG_SRC' => $img_path));
+		}
+	}
+	
+	public function get_status_data($id_status, $extension = '')
+	{
+		for($cptStatusArr=0;$cptStatusArr<count($this->statusArr);$cptStatusArr++)
+		{
+			if($id_status == $this->statusArr[$cptStatusArr]['ID'])
+			{
+				$status_txt = $this->statusArr[$cptStatusArr]['LABEL'];
+				$img_path = $this->statusArr[$cptStatusArr]['IMG_SRC'];
+			}
+		}
+		return array('ID'=> $id_status, 'LABEL'=> $status_txt, 'IMG_SRC' => $img_path);
+	}
+	
+	/*public function get_status_data($id_status,$extension = '')
 	{
 		$this->connect();
 		$this->query("select label_status, maarch_module, img_filename from ".$_SESSION['tablename']['status']." where id = '".$id_status."'");
@@ -57,21 +110,16 @@ class manage_status extends dbquery
 			$temp_explode[0] = $temp_explode[0].$extension;
 			$img_name = implode(".", $temp_explode);
 		}
-		
-
 		if($maarch_module == 'apps' && isset($img_name) && !empty($img_name))
 		{
-			//$img_path = $_SESSION['config']['businessappurl'].'img/'.$img_name;
 			$img_path = $_SESSION['config']['businessappurl'].'static.php?filename='.$img_name;
 		}
 		else if(!empty($maarch_module) && isset($maarch_module)&& isset($img_name) && !empty($img_name))
 		{
-			//$img_path = $_SESSION['urltomodules'].$maarch_module.'img/'.$img_name;
 			$img_path = $_SESSION['config']['businessappurl'].'static.php?filename='.$img_name."&module=".$maarch_module;
 		}
 		else
 		{
-			//$img_path = $_SESSION['config']['businessappurl'].'img/default_status'.$extension.'.gif';
 			$img_path = $_SESSION['config']['businessappurl'].'static.php?filename=default_status'.$extension.'.gif';
 		}
 
@@ -81,7 +129,7 @@ class manage_status extends dbquery
 		}
 
 		return array('ID'=> $id_status, 'LABEL'=> $status_txt, 'IMG_SRC' => $img_path);
-	}
+	}*/
 
 	public function can_be_modified($id_status)
 	{
