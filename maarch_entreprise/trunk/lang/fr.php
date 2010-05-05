eafbca9241e7e1b249693ef9ef9f8bdc3c2e1445
Laurent Giovannoni <laurent.giovannoni@maarch.org>
2010-05-05 18:46:20 +0000

    evol : add constant to update status of document

--- maarch_entreprise/trunk/lang/fr.php
+++ maarch_entreprise/trunk/lang/fr.php
@@ -853,6 +853,7 @@ if (!defined('_TRY_AGAIN')) define( '_TRY_AGAIN', 'Veuillez r&eacute;essayer');
 if (!defined('_DOCTYPE_MANDATORY')) define( '_DOCTYPE_MANDATORY', 'Le type de pi&egrave;ce est obligatoire');
 if (!defined('_INDEX_UPDATED')) define( '_INDEX_UPDATED', 'Index mis &agrave; jour');
 if (!defined('_DOC_DELETED')) define( '_DOC_DELETED', 'Document supprimé');
+if (!defined('_UPDATE_DOC_STATUS')) define( '_UPDATE_DOC_STATUS', 'Statut du document mis à jour');
 
 if (!defined('_QUICKLAUNCH')) define( '_QUICKLAUNCH', 'Raccourcis');
 if (!defined('_SHOW_DETAILS_DOC')) define( '_SHOW_DETAILS_DOC', 'Voir les d&eacute;tails du document');
