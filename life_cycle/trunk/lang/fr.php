7d67c7766f937059419625b8dce8eea2a2ed07c0
Laurent Giovannoni <laurent.giovannoni@maarch.org>
2010-07-13 08:14:03 +0000

    evol : structure of the module

---
+++ life_cycle/trunk/lang/fr.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ *
+ *   Copyright 2010 Maarch
+ *
+ *   This file is part of Maarch Framework.
+ *
+ *   Maarch Framework is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Maarch Framework is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Maarch Framework.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*********************** SERVICES ***********************************/
+if (!defined('_LIFE_CYCLE'))  define('_LIFE_CYCLE', 'Cycle de vie');
+if (!defined('_ADMIN_LIFE_CYCLE'))  define('_ADMIN_LIFE_CYCLE', 'Cycle de vie');
+if (!defined('_ADMIN_LIFE_CYCLE_DESC'))  define('_ADMIN_LIFE_CYCLE_DESC', 'D&eacute;finir les cycles de vie.');
+
+?>
