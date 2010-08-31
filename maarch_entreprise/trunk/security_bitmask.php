b8048c509d0e586d5e7b2519d33fd21a4397c282
Jean-Louis Ercolani <jeanlouis.ercolani@maarch.org>
2010-08-31 13:47:13 +0000

    Bug fixed: security_management.php and security_bitmask.php namings (RECORD). Bug fixed: relative classification code incrementation (mr_aggregation_controler.php).

--- maarch_entreprise/trunk/security_bitmask.php
+++ maarch_entreprise/trunk/security_bitmask.php
@@ -1,22 +1,22 @@
 <?php
 
-define('ADD_DOCUMENT', 1);
-define('CREATE_SERIE', 2);
-define('CREATE_OTHER_AGREGATE', 4);
+define('ADD_RECORD', 1);
+define('CREATE_CLASS', 2);
+define('CREATE_OTHER_AGREGATION', 4);
 define('DATA_MODIFICATION', 8);
-define('DELETE_DOCUMENT', 16);
-define('DELETE_SERIE', 32);
-define('DELETE_OTHER_AGREGATE', 64);
+define('DELETE_RECORD', 16);
+define('DELETE_CLASS', 32);
+define('DELETE_OTHER_AGREGATION', 64);
 define('VIEW_LOG', 128);
 
 $_ENV['security_bitmask'] = array();
-array_push($_ENV['security_bitmask'], array('ID' => ADD_DOCUMENT, 'LABEL' => _ADD_DOCUMENT_LABEL));
-array_push($_ENV['security_bitmask'], array('ID' => CREATE_SERIE, 'LABEL' => _CREATE_SERIE_LABEL));
-array_push($_ENV['security_bitmask'], array('ID' => CREATE_OTHER_AGREGATE, 'LABEL' => _CREATE_OTHER_AGREGATE_LABEL));
+array_push($_ENV['security_bitmask'], array('ID' => ADD_RECORD, 'LABEL' => _ADD_RECORD_LABEL));
+array_push($_ENV['security_bitmask'], array('ID' => CREATE_CLASS, 'LABEL' => _CREATE_CLASS_LABEL));
+array_push($_ENV['security_bitmask'], array('ID' => CREATE_OTHER_AGREGATION, 'LABEL' => _CREATE_OTHER_AGREGATION_LABEL));
 array_push($_ENV['security_bitmask'], array('ID' => DATA_MODIFICATION, 'LABEL' => _DATA_MODIFICATION_LABEL));
-array_push($_ENV['security_bitmask'], array('ID' => DELETE_DOCUMENT, 'LABEL' => _DELETE_DOCUMENT_LABEL));
+array_push($_ENV['security_bitmask'], array('ID' => DELETE_RECORD, 'LABEL' => _DELETE_RECORD_LABEL));
 array_push($_ENV['security_bitmask'], array('ID' => DELETE_SERIE, 'LABEL' => _DELETE_SERIE_LABEL));
-array_push($_ENV['security_bitmask'], array('ID' => DELETE_OTHER_AGREGATE, 'LABEL' => _DELETE_OTHER_AGREGATE_LABEL));
+array_push($_ENV['security_bitmask'], array('ID' => DELETE_OTHER_AGREGATION, 'LABEL' => _DELETE_OTHER_AGREGATION_LABEL));
 array_push($_ENV['security_bitmask'], array('ID' => VIEW_LOG, 'LABEL' => _VIEW_LOG_LABEL));
 
 function get_task_label($id_task, $tasks_array)
