f01bd3a9c70898e6a82683665feeeb31c44164b7
Laurent Giovannoni <laurent.giovannoni@maarch.org>
2010-09-22 12:15:42 +0000

    fix : pb with illegal chars

--- maarch_entreprise/trunk/tools/Zend/Search/Lucene/Analysis/Analyzer/Common/Text.php
+++ maarch_entreprise/trunk/tools/Zend/Search/Lucene/Analysis/Analyzer/Common/Text.php
@@ -55,7 +55,7 @@ class Zend_Search_Lucene_Analysis_Analyzer_Common_Text extends Zend_Search_Lucen
         // convert input into ascii
         if (PHP_OS != 'AIX') {
             //$this->_input = iconv($this->_encoding, 'ASCII//TRANSLIT', $this->_input);
-            $this->_input = iconv('UTF-8', 'ASCII//TRANSLIT', $this->_input);
+            $this->_input = iconv('ISO-8859-1', 'ASCII//TRANSLIT//IGNORE', $this->_input);
         }
         $this->_encoding = 'ASCII';
     }
