a62d3d7a9c9b848672a97c622dbba1ce908c0855
Laurent Giovannoni <laurent.giovannoni@maarch.org>
2010-10-26 21:32:44 +0000

    evol : signature of ws

---
+++ basket/trunk/class/ws.php
@@ -0,0 +1,21 @@
+<?php 
+global $SOAP_dispatch_map;
+global $XMLRPC_dispatch_map;
+global $SOAP_typedef;
+
+$XMLRPC_dispatch_map['basketSample'] = Array(
+                            'function' => 'basketSample',
+                            'signature' => array(array('string','string')),
+                            'docstring' => '',
+                            'method' => "modules/basket#basket::save"
+                            );
+
+$SOAP_dispatch_map['basketSample'] = Array(
+                                     'in'  => Array('in' => 'string'),
+                                     'out' => Array('out' => 'string'),
+                                     'method' => "modules/basket#basket::save"
+                                     );
+function basketSample($in) {
+    return $in;
+}
+?>
