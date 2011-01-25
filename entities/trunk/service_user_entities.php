fe0dcf8efe68ae86b52487e77eea9ed0116bf0af
Sébastien Nana <sebastien.nana@maarch.org>
2011-01-25 10:09:17 +0000

    Fix : in validation page, if a diffusion list already exists, the model was loaded instead

--- entities/trunk/service_user_entities.php
+++ entities/trunk/service_user_entities.php
@@ -2,17 +2,17 @@
 require_once('modules/entities/class/EntityControler.php');
 if($_SESSION['service_tag'] == 'user_init')
 {
-	$_SESSION['m_admin']['nbentities'] = EntityControler::getEntitiesCount();
-	
-	$tmp_array = EntityControler::getUsersEntities($_SESSION['m_admin']['users']['user_id']);
-	for($i=0; $i<count($tmp_array);$i++)
-	{
-		$ent = EntityControler::get($tmp_array[$i]['ENTITY_ID']);
-		$tmp_array[$i]['LABEL'] = $ent->__get('entity_label');
-		$tmp_array[$i]['SHORT_LABEL'] = $ent->__get('short_label');
-	}
-	$_SESSION['m_admin']['entity']['entities'] = $tmp_array;
-	unset($tmp_array);
+    $_SESSION['m_admin']['nbentities'] = EntityControler::getEntitiesCount();
+
+    $tmp_array = EntityControler::getUsersEntities($_SESSION['m_admin']['users']['user_id']);
+    for($i=0; $i<count($tmp_array);$i++)
+    {
+        $ent = EntityControler::get($tmp_array[$i]['ENTITY_ID']);
+        $tmp_array[$i]['LABEL'] = $ent->__get('entity_label');
+        $tmp_array[$i]['SHORT_LABEL'] = $ent->__get('short_label');
+    }
+    $_SESSION['m_admin']['entity']['entities'] = $tmp_array;
+    unset($tmp_array);
 }
 elseif($_SESSION['service_tag'] == 'formuser')
 {
@@ -26,32 +26,31 @@ elseif($_SESSION['service_tag'] == 'formuser')
 }
 elseif($_SESSION['service_tag'] == 'users_list_init')
 {
-	$_SESSION['m_admin']['load_entities'] = true;
+    $_SESSION['m_admin']['load_entities'] = true;
 
 }
 elseif($_SESSION['service_tag'] == 'user_check')
 {
-	$primary_set = false;
-	if(!empty($_SESSION['m_admin']['entity']['entities'])   )
-	{
-		for($i=0; $i < count($_SESSION['m_admin']['entity']['entities']); $i++)
-		{
-			if($_SESSION['m_admin']['entity']['entities'][$i]['PRIMARY'] == 'Y')
-			{
-				$primary_set = true;
-				break;
-			}
-		}
-
-		if ($primary_set == false)
-		{
-			$_SESSION['error'] = _NO_PRIMARY_ENTITY;
-		}
-	}
+    $primary_set = false;
+    if(isset($_SESSION['m_admin']['entity']['entities']) && !empty($_SESSION['m_admin']['entity']['entities'])   )
+    {
+        for($i=0; $i < count($_SESSION['m_admin']['entity']['entities']); $i++)
+        {
+            if($_SESSION['m_admin']['entity']['entities'][$i]['PRIMARY'] == 'Y')
+            {
+                $primary_set = true;
+                break;
+            }
+        }
+    }
+    if($primary_set == false)
+    {
+        $_SESSION['error'] .= _NO_PRIMARY_ENTITY;
+    }
 }
 elseif($_SESSION['service_tag'] == 'user_add' || $_SESSION['service_tag'] == 'user_up')
 {
-	EntityControler::cleanUsersentities($_SESSION['m_admin']['users']['user_id'], 'user_id');
-	EntityControler::loadDbUsersentities($_SESSION['m_admin']['users']['user_id'], $_SESSION['m_admin']['entity']['entities']);
+    EntityControler::cleanUsersentities($_SESSION['m_admin']['users']['user_id'], 'user_id');
+    EntityControler::loadDbUsersentities($_SESSION['m_admin']['users']['user_id'], $_SESSION['m_admin']['entity']['entities']);
 }
 ?>
