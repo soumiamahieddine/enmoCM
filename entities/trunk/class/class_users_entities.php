1793e9d1f99ea9eedb96114d07c6d6ba4173c419
Sébastien Nana <sebastien.nana@maarch.org>
2011-01-31 10:07:53 +0000

    Fix : there was a show() function in class_functions et class_db(), the show functions in class_db becomes show_str()

--- entities/trunk/class/class_users_entities.php
+++ entities/trunk/class/class_users_entities.php
@@ -242,17 +242,17 @@ class users_entities extends dbquery
                 <form name="frmuserent" method="post" action="<?php  if($mode == "up") { echo $_SESSION['config']['businessappurl'].'index.php?display=true&module=entities&page=users_entities_up_db'; }  ?>" class="forms addforms" >
                                 <p>
                     <label for="UserId"><?php  echo _ID; ?> :</label>
-                    <?php  if($mode == "up") { echo $func->show($_SESSION['m_admin']['entity']['user_UserId']); } ?>
+                    <?php  if($mode == "up") { echo $func->show_str($_SESSION['m_admin']['entity']['user_UserId']); } ?>
                     <input type="hidden"  name="id" id="id" value="<?php  echo $id; ?>" />
                 </p>
 
                 <p>
                     <label for="LastName"><?php  echo _LASTNAME; ?> :</label>
-                    <?php  echo $func->show($_SESSION['m_admin']['entity']['user_LastName']); ?>
+                    <?php  echo $func->show_str($_SESSION['m_admin']['entity']['user_LastName']); ?>
                 </p>
                 <p>
                     <label for="FirstName"><?php  echo _FIRSTNAME; ?> :</label>
-                    <?php  echo $func->show($_SESSION['m_admin']['entity']['user_FirstName']); ?>
+                    <?php  echo $func->show_str($_SESSION['m_admin']['entity']['user_FirstName']); ?>
                 </p>
                 <p>
                     <label for="Phone"><?php  echo _PHONE_NUMBER; ?> :</label>
