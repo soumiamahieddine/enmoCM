6d4b311595481c8335a11af28c1209f7eb0dbcf3
Laurent Giovannoni <laurent.giovannoni@maarch.org>
2011-01-31 17:54:37 +0000

    fix : debug with e_strict

--- notifications/trunk/lang/fr.php
+++ notifications/trunk/lang/fr.php
@@ -1,60 +1,75 @@
-<?php //communs
-define('_MAIL_TO_PROCESS', 'Courriers pour traitement');
-define('_HELLO','Bonjour');
-define('_THE_MAIL_NUM', 'le courrier n&deg;');
-define('_OF_TYPE', 'de type');
-define('_OBJECT', 'objet');
-define('_RECEIVED_THE', 're&ccedil;u le');
-define('_SEND_BY', 'adress&eacute; par');
-define('_MUST_BE_PROCESSED_BEFORE', 'est &agrave; traiter avant le');
-define('_ACCESS_TO_MAIL_TO_PROCESS', 'Acc&eacute;der au courrier pour le traiter');
-define('_PROCESS_MAIL', 'Traitement courrier');
-define('_USER', 'Utilisateur');
-define('_MAIL', 'Courrier');
-define('_WAS_SENT_TO', 'a &eacute;t&eacute; transmis &agrave;');
-define('_SEE_MAIL', 'Voir ce courrier');
-define('_NO_SENDED',' L&rsquo;email n\'a pas  &eacute;t&eacute; envoy&eacute;');
+<?php
+/*
+ *
+ *    Copyright 2008,2009 Maarch
+ *
+ *  This file is part of Maarch Framework.
+ *
+ *   Maarch Framework is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Maarch Framework is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *    along with Maarch Framework.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+//communs
+if (!defined('_MAIL_TO_PROCESS')) define('_MAIL_TO_PROCESS', 'Courriers pour traitement');
+if (!defined('_HELLO')) define('_HELLO','Bonjour');
+if (!defined('_THE_MAIL_NUM')) define('_THE_MAIL_NUM', 'le courrier n&deg;');
+if (!defined('_OF_TYPE')) define('_OF_TYPE', 'de type');
+if (!defined('_OBJECT')) define('_OBJECT', 'objet');
+if (!defined('_RECEIVED_THE')) define('_RECEIVED_THE', 're&ccedil;u le');
+if (!defined('_SEND_BY')) define('_SEND_BY', 'adress&eacute; par');
+if (!defined('_MUST_BE_PROCESSED_BEFORE')) define('_MUST_BE_PROCESSED_BEFORE', 'est &agrave; traiter avant le');
+if (!defined('_ACCESS_TO_MAIL_TO_PROCESS')) define('_ACCESS_TO_MAIL_TO_PROCESS', 'Acc&eacute;der au courrier pour le traiter');
+if (!defined('_PROCESS_MAIL')) define('_PROCESS_MAIL', 'Traitement courrier');
+if (!defined('_USER')) define('_USER', 'Utilisateur');
+if (!defined('_MAIL')) define('_MAIL', 'Courrier');
+if (!defined('_WAS_SENT_TO')) define('_WAS_SENT_TO', 'a &eacute;t&eacute; transmis &agrave;');
+if (!defined('_SEE_MAIL')) define('_SEE_MAIL', 'Voir ce courrier');
+if (!defined('_NO_SENDED')) define('_NO_SENDED',' L&rsquo;email n\'a pas  &eacute;t&eacute; envoy&eacute;');
 // notifs.php
-define('_MUST_SPECIFY_CONFIG_FILE', 'Vous devez sp&eacute;cifier le fichier de configuration');
-define('_DU', 'du');
-define('_OF', 'de');
-define('_TO', 'de la part de ');
-define('_FOR', 'd&eacute;stin&eacute; &agrave;');
-define('_LIST_OF_MAIL_TO_PROCESS', 'voici la liste des nouveaux courriers &agrave; traiter');
-define('_MAIL_COPIES_TO', 'Courriers pour information du');
-define('_MAIL_COPIES_LIST', 'voici la liste des copies des courriers dont vous &ecirc;tes destinataire');
-define('_WHO_MUST_PROCESS_BEFORE', 'qui doit le traiter avant le');
-define('_ORIGINAL_PAPERS_ALREADY_SEND', 'Les originaux papier vous ont &eacute;t&eacute; adress&eacute;s');
-define('_WARNING', 'Attention');
-define('_YOU_MUST_BE_LOGGED', 'vous devez &ecirc;tre identifi&eacute; sur le logiciel avant de tenter d\'acc&eacute;der au courrier');
-
-define('_MAIL_TO_PROCESS_LIST','Liste des courriers pour traitement');
-
-define('_COPIES_MAIL_LIST', 'Liste des courriers pour information');
-define('_BY', 'par');
-define('_DEPARTMENT', 'le service');
-define('_','');
-
-
+if (!defined('_MUST_SPECIFY_CONFIG_FILE')) define('_MUST_SPECIFY_CONFIG_FILE', 'Vous devez sp&eacute;cifier le fichier de configuration');
+if (!defined('_DU')) define('_DU', 'du');
+if (!defined('_OF')) define('_OF', 'de');
+if (!defined('_TO')) define('_TO', 'de la part de ');
+if (!defined('_FOR')) define('_FOR', 'd&eacute;stin&eacute; &agrave;');
+if (!defined('_LIST_OF_MAIL_TO_PROCESS')) define('_LIST_OF_MAIL_TO_PROCESS', 'voici la liste des nouveaux courriers &agrave; traiter');
+if (!defined('_MAIL_COPIES_TO')) define('_MAIL_COPIES_TO', 'Courriers pour information du');
+if (!defined('_MAIL_COPIES_LIST')) define('_MAIL_COPIES_LIST', 'voici la liste des copies des courriers dont vous &ecirc;tes destinataire');
+if (!defined('_WHO_MUST_PROCESS_BEFORE')) define('_WHO_MUST_PROCESS_BEFORE', 'qui doit le traiter avant le');
+if (!defined('_ORIGINAL_PAPERS_ALREADY_SEND')) define('_ORIGINAL_PAPERS_ALREADY_SEND', 'Les originaux papier vous ont &eacute;t&eacute; adress&eacute;s');
+if (!defined('_WARNING')) define('_WARNING', 'Attention');
+if (!defined('_YOU_MUST_BE_LOGGED')) define('_YOU_MUST_BE_LOGGED', 'vous devez &ecirc;tre identifi&eacute; sur le logiciel avant de tenter d\'acc&eacute;der au courrier');
+if (!defined('_MAIL_TO_PROCESS_LIST')) define('_MAIL_TO_PROCESS_LIST','Liste des courriers pour traitement');
+if (!defined('_COPIES_MAIL_LIST')) define('_COPIES_MAIL_LIST', 'Liste des courriers pour information');
+if (!defined('_BY')) define('_BY', 'par');
+if (!defined('_DEPARTMENT')) define('_DEPARTMENT', 'le service');
+if (!defined('_')) define('_','');
 //relance1.php
-define('_FIRST_WARNING', 'Premi&egrave;re relance');
-define('_FIRST_WARNING_TITLE', 'Relance sur les courriers');
-define('_THIS_IS_FIRST_WARNING', 'Ceci est la premi&egrave;re relance pour les courriers suivants');
-define('_MUST_BE_ADDED_TO_PRIORITY', 'Merci d&rsquo;ajouter ce ou ces courriers &agrave; vos traitements prioritaires');
-define('_TO_PROCESS', '&agrave; traiter');
+if (!defined('_FIRST_WARNING')) define('_FIRST_WARNING', 'Premi&egrave;re relance');
+if (!defined('_FIRST_WARNING_TITLE')) define('_FIRST_WARNING_TITLE', 'Relance sur les courriers');
+if (!defined('_THIS_IS_FIRST_WARNING')) define('_THIS_IS_FIRST_WARNING', 'Ceci est la premi&egrave;re relance pour les courriers suivants');
+if (!defined('_MUST_BE_ADDED_TO_PRIORITY')) define('_MUST_BE_ADDED_TO_PRIORITY', 'Merci d&rsquo;ajouter ce ou ces courriers &agrave; vos traitements prioritaires');
+if (!defined('_TO_PROCESS')) define('_TO_PROCESS', '&agrave; traiter');
 //relance2.php
-define('_SECOND_WARNING', 'Relance sur mes courriers');
-define('_LATE_MAIL_TO_PROCESS', 'Courriers en retard &agrave; traiter');
-define('_YOU_ARE_LATE', 'Vous avez du retard dans le traitement des courriers suivants');
-define('_WAS_TO_PROCESS_BEFORE', '&eacute;tait &agrave; traiter avant le');
-define('_PROCESS_THIS_MAIL_QUICKLY', 'Merci de r&eacute;diger une r&eacute;ponse sous 48 heures');
-define('_LATE', 'Retard');
-define('_MAIL_TO_CC', 'courriers en copies');
-define('_FOLLOWING_MAIL_ARE_LATE', 'Le traitement des courriers suivants a pris du retard');
-define('_WHO_MUST_BE_PROCESSED_BEFORE', 'qui devait le traiter avant le');
-define('_COPY_TITLE', 'En copie');
-
-
+if (!defined('_SECOND_WARNING')) define('_SECOND_WARNING', 'Relance sur mes courriers');
+if (!defined('_LATE_MAIL_TO_PROCESS')) define('_LATE_MAIL_TO_PROCESS', 'Courriers en retard &agrave; traiter');
+if (!defined('_YOU_ARE_LATE')) define('_YOU_ARE_LATE', 'Vous avez du retard dans le traitement des courriers suivants');
+if (!defined('_WAS_TO_PROCESS_BEFORE')) define('_WAS_TO_PROCESS_BEFORE', '&eacute;tait &agrave; traiter avant le');
+if (!defined('_PROCESS_THIS_MAIL_QUICKLY')) define('_PROCESS_THIS_MAIL_QUICKLY', 'Merci de r&eacute;diger une r&eacute;ponse sous 48 heures');
+if (!defined('_LATE')) define('_LATE', 'Retard');
+if (!defined('_MAIL_TO_CC')) define('_MAIL_TO_CC', 'courriers en copies');
+if (!defined('_FOLLOWING_MAIL_ARE_LATE')) define('_FOLLOWING_MAIL_ARE_LATE', 'Le traitement des courriers suivants a pris du retard');
+if (!defined('_WHO_MUST_BE_PROCESSED_BEFORE')) define('_WHO_MUST_BE_PROCESSED_BEFORE', 'qui devait le traiter avant le');
+if (!defined('_COPY_TITLE')) define('_COPY_TITLE', 'En copie');
 //notifications engine
-define('_WRONG_FUNCTION_OR_WRONG_PARAMETERS','_WRONG_FUNCTION_OR_WRONG_PARAMETERS')
+if (!defined('_WRONG_FUNCTION_OR_WRONG_PARAMETERS')) define('_WRONG_FUNCTION_OR_WRONG_PARAMETERS','Mauvaise fonction ou mauvais param&egrave;tre')
 ?>
