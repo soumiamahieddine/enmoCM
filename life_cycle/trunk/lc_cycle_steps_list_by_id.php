85d0fad33126a58226c8c656b1cefa8a86acc46b
Laurent Giovannoni <laurent.giovannoni@maarch.org>
2011-02-04 16:45:24 +0000

    evol : autocompletion

---
+++ life_cycle/trunk/lc_cycle_steps_list_by_id.php
@@ -0,0 +1,61 @@
+<?php
+/*
+*    Copyright 2008-2011 Maarch
+*
+*  This file is part of Maarch Framework.
+*
+*   Maarch Framework is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   Maarch Framework is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*    along with Maarch Framework.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/**
+* @brief List of steps for autocompletion
+*
+*
+* @file
+* @author  Laurent Giovannoni <dev@maarch.org>
+* @date $date$
+* @version $Revision$
+* @ingroup life_cycle
+*/
+
+require_once ("modules/life_cycle/life_cycle_tables_definition.php");
+
+$db = new dbquery();
+$db->connect();
+if ($_SESSION['config']['databasetype'] == "POSTGRESQL") {
+    $db->query("select cycle_step_id as tag from " . _LC_CYCLE_STEPS_TABLE_NAME . " where cycle_step_id ilike '" . $_REQUEST['what'] . "%' order by cycle_step_id");
+} else {
+    $db->query("select cycle_step_id as tag from " . _LC_CYCLE_STEPS_TABLE_NAME . " where cycle_step_id like '" . $_REQUEST['what'] . "%' order by cycle_step_id");
+}
+$listArray = array();
+while ($line = $db->fetch_object()) {
+    array_push($listArray, $line->tag);
+}
+echo "<ul>\n";
+$authViewList = 0;
+$flagAuthView = false;
+foreach ($listArray as $what) {
+    if (isset($authViewList) && $authViewList>= 10) {
+        $flagAuthView = true;
+    }
+    if (stripos($what, $_REQUEST['what']) === 0) {
+        echo "<li>".$what."</li>\n";
+        if ($flagAuthView) {
+            echo "<li>...</li>\n";
+            break;
+        }
+        $authViewList++;
+    }
+}
+echo "</ul>";
