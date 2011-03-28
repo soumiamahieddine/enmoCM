f67cbe32a95b0c4431d0e93cf98fb4663d547f3f
Sébastien Nana <sebastien.nana@maarch.org>
2011-03-28 09:50:24 +0000

    Fix : #117

--- full_text/trunk/class/class_modules_tools.php
+++ full_text/trunk/class/class_modules_tools.php
@@ -6,15 +6,11 @@ class full_text extends dbquery
 	{
 		parent::__construct();
 	}
-	
+
 	public function build_modules_tables()
 	{
-	
+
 	}
-	
-	public function load_module_var_session()
-	{
-		
-	}
+
 }
-?>
+
