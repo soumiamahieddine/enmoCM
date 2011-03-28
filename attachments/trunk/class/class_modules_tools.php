2808dea87c94ff552063735f5da349d3656b614c
Sébastien Nana <sebastien.nana@maarch.org>
2011-03-28 09:50:11 +0000

    Fix : #117

--- attachments/trunk/class/class_modules_tools.php
+++ attachments/trunk/class/class_modules_tools.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /**
 * modules tools Class for attachments
 *
@@ -9,18 +9,18 @@
 * @since 10/2005
 * @license GPL v3
 * @author  Claire Figueras  <dev@maarch.org>
-* 
+*
 */
 
 class attachments
 {
-	
+
 	/**
 	* Build Maarch module tables into sessions vars with a xml configuration file
 	*/
 	public function build_modules_tables()
 	{
-		if(file_exists($_SESSION['config']['corepath'].'custom'.DIRECTORY_SEPARATOR.$_SESSION['custom_override_id'].DIRECTORY_SEPARATOR."modules".DIRECTORY_SEPARATOR."attachments".DIRECTORY_SEPARATOR."xml".DIRECTORY_SEPARATOR."config.xml"))
+		if (file_exists($_SESSION['config']['corepath'].'custom'.DIRECTORY_SEPARATOR.$_SESSION['custom_override_id'].DIRECTORY_SEPARATOR."modules".DIRECTORY_SEPARATOR."attachments".DIRECTORY_SEPARATOR."xml".DIRECTORY_SEPARATOR."config.xml"))
 		{
 			$path_config = $_SESSION['config']['corepath'].'custom'.DIRECTORY_SEPARATOR.$_SESSION['custom_override_id'].DIRECTORY_SEPARATOR."modules".DIRECTORY_SEPARATOR."attachments".DIRECTORY_SEPARATOR."xml".DIRECTORY_SEPARATOR."config.xml";
 		}
@@ -39,10 +39,4 @@ class attachments
 		$_SESSION['history']['attachdel'] = (string) $HISTORY->attachdel;
 		$_SESSION['history']['attachview'] = (string) $HISTORY->attachview;
 	}
-	
-	public function load_module_var_session()
-	{
-	
-	}
 }
-?>
