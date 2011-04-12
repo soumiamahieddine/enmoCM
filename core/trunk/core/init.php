b89576fceab7f3cfd4853e9689e60682c1dc7306
Laurent Giovannoni <laurent.giovannoni@maarch.org>
2011-04-12 15:53:04 +0000

    fix : pb with include path

--- core/trunk/core/init.php
+++ core/trunk/core/init.php
@@ -2,7 +2,7 @@
 session_name('maarch_entreprise_trunk');
 session_start();
 if (isset($_SESSION['config']['default_timezone']) 
-	&& ! empty($_SESSION['config']['default_timezone'])
+    && ! empty($_SESSION['config']['default_timezone'])
 ) {
     ini_set('date.timezone', $_SESSION['config']['default_timezone']);
     date_default_timezone_set($_SESSION['config']['default_timezone']);
@@ -12,27 +12,27 @@ if (isset($_SESSION['config']['default_timezone'])
 }
 
 if (isset($_SESSION['config']['corepath']) 
-	&& ! empty($_SESSION['config']['corepath'])
+    && ! empty($_SESSION['config']['corepath'])
 ) {
     chdir($_SESSION['config']['corepath']);
 }
 //ini_set('error_reporting', E_ALL);
 if (isset($_SESSION['custom_override_id']) 
-	&& ! empty($_SESSION['custom_override_id']) 
-	&& isset($_SESSION['config']['corepath']) 
-	&& ! empty($_SESSION['config']['corepath'])
+    && ! empty($_SESSION['custom_override_id']) 
+    && isset($_SESSION['config']['corepath']) 
+    && ! empty($_SESSION['config']['corepath'])
 ) {
     $path = $_SESSION['config']['corepath'] . 'custom' . DIRECTORY_SEPARATOR
-    	  . $_SESSION['custom_override_id'] . DIRECTORY_SEPARATOR;
+        . $_SESSION['custom_override_id'] . DIRECTORY_SEPARATOR;
     //echo $path;
     set_include_path(
-    	get_include_path() . PATH_SEPARATOR . $path . PATH_SEPARATOR
-    	. $_SESSION['config']['corepath']
+        $path . PATH_SEPARATOR . $_SESSION['config']['corepath']
+        . PATH_SEPARATOR . get_include_path()
     );
 } else if (isset($_SESSION['config']['corepath']) 
 	&& ! empty($_SESSION['config']['corepath'])
 ) {
     set_include_path(
-    	get_include_path() . PATH_SEPARATOR . $_SESSION['config']['corepath']
+        $_SESSION['config']['corepath'] . PATH_SEPARATOR . get_include_path()
     );
 }
