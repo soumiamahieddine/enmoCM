ef72776cb3cbcd296279cc6d90cb561ddabc2708
Sébastien Nana <sebastien.nana@maarch.org>
2011-04-14 14:58:21 +0000

    Fix : notice error

--- cases/trunk/case_extend_list.php
+++ cases/trunk/case_extend_list.php
@@ -25,9 +25,8 @@
 * @author Loic Vinet <dev@maarch.org>
 * @date $date$
 * @version $Revision$
-* @ingroup indexing_searching_mlb
+* @ingroup cases
 */
-
 $case_id = $result[$theline][0]['case_id'];
 
 
@@ -37,31 +36,28 @@ $db_external->connect();
 $status_obj = new manage_status();
 $status = $status_obj->get_not_searchable_status();
 $sec = new security();
-$func= new functions();
+$func = new functions();
 $request = new request();
 $status_str = '';
-for($i=0; $i<count($status);$i++)
-{
+$extension_icon = '';
+for ($i = 0; $i < count($status); $i ++) {
 	$status_str .=	"'".$status[$i]['ID']."',";
 }
 $status_str = preg_replace('/,$/', '', $status_str);
-$where_request.= "  status not in (".$status_str.") ";
+$where_request = "  status not in (".$status_str.") ";
 //$where_clause = $sec->get_where_clause_from_coll_id($_SESSION['collection_id_choice']);
 $where_clause =" case_id = '".$case_id."' ";
 
 
-if(!empty($where_request))
-{
-	if($_SESSION['searching']['where_clause_bis'] <> "")
-	{
+if (! empty($where_request)) {
+	if (isset($_SESSION['searching']['where_clause_bis'])
+	    && $_SESSION['searching']['where_clause_bis'] <> ""
+	) {
 		$where_clause = "((".$where_clause.") or (".$_SESSION['searching']['where_clause_bis']."))";
 	}
 	$where_request = '('.$where_request.') and ('.$where_clause.')';
-}
-else
-{
-	if($_SESSION['searching']['where_clause_bis'] <> "")
-	{
+} else {
+	if ($_SESSION['searching']['where_clause_bis'] <> "") {
 		$where_clause = "((".$where_clause.") or (".$_SESSION['searching']['where_clause_bis']."))";
 	}
 	$where_request = $where_clause;
@@ -92,9 +88,9 @@ if($db_external->nb_result() >0)
 		}
 		$external .='<td width="8%" >&nbsp;</td>';
 		$external .='<td width="40px"><img src="'.$res_status['IMG_SRC'].'" alt = "'.$res_status['LABEL'].'" title = "'.$res_status['LABEL'].'"></td>';
-		$external .='<td width="40px"><p><img src="'. get_img_cat($ext_result->category_id,$extension_icon).'" title="'.$_SESSION['mail_categories'][$ext_result->category_id].'" alt="'.$_SESSION['mail_categories'][$ext_result->category_id].'"></p></td>';
+		$external .='<td width="40px"><p><img src="'. get_img_cat($ext_result->category_id, $extension_icon).'" title="'.$_SESSION['mail_categories'][$ext_result->category_id].'" alt="'.$_SESSION['mail_categories'][$ext_result->category_id].'"></p></td>';
 		$external .='<td width="40px" ><b><p align="center" title="'._GED_NUM.' : '.$ext_result->res_id.'" alt="'._GED_NUM.' : '.$ext_result->res_id.'">'.$func->cut_string($ext_result->res_id,50).'</td></b></p>';
-		$external .='<td ><p title="'._SUBJECT.' : '.$request->show_string($ext_result->subject).'" alt="'.SUBJECT.' : '.$request->show_string($ext_result->subject).'">'.$func->cut_string($request->show_string($ext_result->subject),70).'</p></td>';
+		$external .='<td ><p title="'. _SUBJECT . ' : '.$request->show_string($ext_result->subject).'" alt="'._SUBJECT.' : '.$request->show_string($ext_result->subject).'">'.$func->cut_string($request->show_string($ext_result->subject),70).'</p></td>';
 		//$external .='<td width="100px"><p>'.$ext_result->dest_user.'</td></p>';
 		$contactInfo = $contact->get_contact_information_from_view($ext_result->category_id, $ext_result->contact_lastname, $ext_result->contact_firstname, $ext_result->contact_society, $ext_result->user_lastname, $ext_result->user_firstname);
 		$external .='<td  ><p title="'._CONTACT.' : '.$request->show_string($contactInfo).'" alt="'._CONTACT.' : '.$request->show_string($contactInfo).'">'.$request->show_string($contactInfo).'</p></td>';
