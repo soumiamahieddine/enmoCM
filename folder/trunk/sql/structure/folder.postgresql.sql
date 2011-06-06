3ef4a58b06c1dba92269c80288759c47d59bb6ae
Laurent Giovannoni <laurent.giovannoni@maarch.org>
2011-06-06 08:22:10 +0000

    structure

--- folder/trunk/sql/structure/folder.postgresql.sql
+++ folder/trunk/sql/structure/folder.postgresql.sql
@@ -20,7 +20,7 @@ CREATE TABLE folders
   folder_level smallint DEFAULT (1)::smallint,
   creation_date timestamp without time zone NOT NULL,
   folder_out_id bigint,
-  video_status character varying(10) DEFAULT NULL;
+  video_status character varying(10) DEFAULT NULL,
   video_user character varying(32) DEFAULT NULL;
   is_frozen character(1) NOT NULL DEFAULT 'N',
   custom_t1 character varying(255) DEFAULT NULL::character varying,
