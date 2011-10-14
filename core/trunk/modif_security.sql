ba3ff75e285917fb7e5d4eb1ac34821f84657369
Sébastien Nana <sebastien.nana@maarch.org>
2011-10-14 22:09:59 +0000

    delete file

--- core/trunk/modif_security.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE "security" ADD COLUMN rights_bitmask integer;
-ALTER TABLE "security" ADD COLUMN mr_start_date timestamp without time zone;
-ALTER TABLE "security" ADD COLUMN mr_stop_date timestamp without time zone;
-ALTER TABLE "security" ADD COLUMN where_target character varying(15) NOT NULL DEFAULT 'DOC';
-ALTER TABLE "security" DROP CONSTRAINT security_pkey;
-ALTER TABLE "security" ADD COLUMN security_id serial NOT NULL;
-ALTER TABLE "security" ADD PRIMARY KEY (security_id);
