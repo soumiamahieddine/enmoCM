4adba7c7a268dcdba94b8b3bb1b479fd36eb1623
Jean-Louis Ercolani <jeanlouis.ercolani@maarch.org>
2012-02-28 14:18:42 +0000

    modif chemins

--- core/trunk/svnupdateall.bat
+++ core/trunk/svnupdateall.bat
@@ -1,1 +1,10 @@
-svn update . apps\maarch_entreprise modules\*
+cd core
+echo "=" >> C:\xampp\htdocs\maarch_entreprise_trunk\update.log
+echo date >> C:\xampp\htdocs\maarch_entreprise_trunk\update.log
+echo time >> C:\xampp\htdocs\maarch_entreprise_trunk\update.log
+svn update >> C:\xampp\htdocs\maarch_entreprise_trunk\update.log
+cd ..\apps\maarch_entreprise
+svn update >> C:\xampp\htdocs\maarch_entreprise_trunk\update.log
+cd ..\..\modules
+svn update *  >> C:\xampp\htdocs\maarch_entreprise_trunk\update.log
+pause
