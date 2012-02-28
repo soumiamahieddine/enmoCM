fa6b9a95395421156b6bb17b4ce79f3350bceeea
Cyril Vazquez <cyril.vazquez@maarch.org>
2012-02-28 11:17:40 +0000

    Notifications: table names

--- core/trunk/structure.sql
+++ core/trunk/structure.sql
@@ -1179,38 +1179,38 @@ WITH (OIDS=FALSE);
 
 -- modules/notes/sql/structure/notifications.postgresql.sql
 
-CREATE SEQUENCE event_stack_seq
+CREATE SEQUENCE notif_event_stack_seq
   INCREMENT 1
   MINVALUE 1
   MAXVALUE 9223372036854775807
   START 1
   CACHE 1;
 
-CREATE TABLE event_stack
+CREATE TABLE notif_event_stack
 (
-  system_id bigint NOT NULL DEFAULT nextval('event_stack_seq'::regclass),
+  system_id bigint NOT NULL DEFAULT nextval('notif_event_stack_seq'::regclass),
   ta_sid bigint NOT NULL,
   table_name character varying(32) NOT NULL,
   record_id character varying(255) NOT NULL,
   event_date timestamp without time zone NOT NULL,
   exec_date timestamp without time zone NOT NULL,
   exec_result character varying(50) NOT NULL,
-  CONSTRAINT event_stack_pkey PRIMARY KEY (system_id)
+  CONSTRAINT notif_event_stack_pkey PRIMARY KEY (system_id)
 )
 WITH (
   OIDS=FALSE
 );
 
-CREATE SEQUENCE email_stack_seq
+CREATE SEQUENCE notif_email_stack_seq
   INCREMENT 1
   MINVALUE 1
   MAXVALUE 9223372036854775807
   START 1
   CACHE 1;
 
-CREATE TABLE email_stack
+CREATE TABLE notif_email_stack
 (
-  system_id bigint NOT NULL DEFAULT nextval('email_stack_seq'::regclass),
+  system_id bigint NOT NULL DEFAULT nextval('notif_email_stack_seq'::regclass),
   sender character varying(255) NOT NULL,
   reply_to character varying(255),
   recipient character varying(2000) NOT NULL,
@@ -1220,9 +1220,11 @@ CREATE TABLE email_stack
   html_body text,
   text_body text,
   charset character varying(50) NOT NULL,
+  attachments character varying(2000),
+  module character varying(50) NOT NULL,
   exec_date timestamp without time zone,
   exec_result character varying(50),
-  CONSTRAINT email_stack_pkey PRIMARY KEY (system_id)
+  CONSTRAINT notif_email_stack_pkey PRIMARY KEY (system_id)
 )
 WITH (
   OIDS=FALSE
