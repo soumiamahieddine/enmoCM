2fc3eaa8ce4821d1380e1c46fb87149b951e0a53
Jean-Louis Ercolani <jeanlouis.ercolani@maarch.org>
2012-03-05 16:44:29 +0000

    process_all batch

--- notifications/trunk/batch/process_all.bat
+++ notifications/trunk/batch/process_all.bat
@@ -1,2 +1,4 @@
-C:\xampp\htdocs\maarch_entreprise_trunk\modules\notifications\batch\process_event_stack_VAL.bat
-C:\xampp\htdocs\maarch_entreprise_trunk\modules\notifications\batch\process_email_stack.bat
+cd C:\xampp\htdocs\maarch_entreprise_trunk\modules\notifications\batch
+process_event_stack_VAL.bat
+process_email_stack.bat
+pause
