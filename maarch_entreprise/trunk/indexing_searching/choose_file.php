980aa16286e65b123d1606ea599065e9a3255693
Laurent Giovannoni <laurent.giovannoni@maarch.org>
2012-03-11 18:22:42 +0000

    fix: pb with loading file

--- maarch_entreprise/trunk/indexing_searching/choose_file.php
+++ maarch_entreprise/trunk/indexing_searching/choose_file.php
@@ -1,9 +1,9 @@
 <?php
 
 /*
-*    Copyright 2008,2009 Maarch
+*   Copyright 2008-2012 Maarch
 *
-*  This file is part of Maarch Framework.
+*   This file is part of Maarch Framework.
 *
 *   Maarch Framework is free software: you can redistribute it and/or modify
 *   it under the terms of the GNU General Public License as published by
@@ -16,7 +16,7 @@
 *   GNU General Public License for more details.
 *
 *   You should have received a copy of the GNU General Public License
-*    along with Maarch Framework.  If not, see <http://www.gnu.org/licenses/>.
+*   along with Maarch Framework.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 /**
@@ -48,7 +48,9 @@ $core_tools->load_header('', true, false);
                 var test = window.top.document.getElementById('file_iframe');
                 if (test != null)
                 {
-                    test.src = '<?php echo $_SESSION['config']['businessappurl'];?>index.php?display=true&dir=indexing_searching&page=file_iframe&#navpanes=0';
+                    test.src = '<?php 
+                        echo $_SESSION['config']['businessappurl'];
+                        ?>index.php?display=true&dir=indexing_searching&page=file_iframe&#navpanes=0';
                 }
             </script>
             <?php
@@ -62,9 +64,15 @@ $core_tools->load_header('', true, false);
         $filePathOnTmp = $_SESSION['config']['tmppath'] . $fileNameOnTmp;
         //$md5 = md5_file($_FILES['file']['tmp_name']);
         if (!is_uploaded_file($_FILES['file']['tmp_name'])) {
-                $_SESSION['error'] = _FILE_NOT_SEND.". "._TRY_AGAIN.". "._MORE_INFOS." (<a href=\"mailto:".$_SESSION['config']['adminmail']."\">".$_SESSION['config']['adminname']."</a>)";
+                $_SESSION['error'] = _FILE_NOT_SEND . ". " . _TRY_AGAIN 
+                    . ". " . _MORE_INFOS . " (<a href=\"mailto:" 
+                    . $_SESSION['config']['adminmail'] . "\">" 
+                    . $_SESSION['config']['adminname'] . "</a>)";
         } elseif (!@move_uploaded_file($_FILES['file']['tmp_name'], $filePathOnTmp)) {
-            $_SESSION['error'] = _FILE_NOT_SEND.". "._TRY_AGAIN.". "._MORE_INFOS." (<a href=\"mailto:".$_SESSION['config']['adminmail']."\">".$_SESSION['config']['adminname']."</a>)";
+            $_SESSION['error'] = _FILE_NOT_SEND . ". " . _TRY_AGAIN . ". " 
+                . _MORE_INFOS . " (<a href=\"mailto:" 
+                . $_SESSION['config']['adminmail'] . "\">" 
+                . $_SESSION['config']['adminname'] . "</a>)";
         } else {
             $_SESSION['upfile']['size'] = $_FILES['file']['size'];
             $_SESSION['upfile']['mime'] = $_FILES['file']['type'];
@@ -83,11 +91,15 @@ $core_tools->load_header('', true, false);
         ?>
         <script language="javascript" type="text/javascript">
             function refreshFrame(frameId) {
-                frameId.src = '<?php echo $_SESSION['config']['businessappurl'];?>index.php?display=true&dir=indexing_searching&page=file_iframe';
+                frameId.src = '<?php 
+                    echo $_SESSION['config']['businessappurl'];
+                    ?>index.php?display=true&dir=indexing_searching&page=file_iframe';
             }
 
             var test = window.top.document.getElementById('file_iframe');
-            if (test.src == '<?php echo $_SESSION['config']['businessappurl'];?>index.php?display=true&dir=indexing_searching&page=file_iframe&#navpanes=0') {
+            if (test.src == '<?php 
+                echo $_SESSION['config']['businessappurl'];
+                ?>index.php?display=true&dir=indexing_searching&page=file_iframe&#navpanes=0') {
                 //test.location.refresh();
                 //test.src = '';
                 refreshFrame(test);
@@ -95,24 +107,42 @@ $core_tools->load_header('', true, false);
 
             if (test != null) {
                 //fix pb with toolbar of pdf
-                test.src = '<?php echo $_SESSION['config']['businessappurl'];?>index.php?display=true&dir=indexing_searching&page=file_iframe&#navpanes=0';
+                test.src = '<?php 
+                    echo $_SESSION['config']['businessappurl'];
+                    ?>index.php?display=true&dir=indexing_searching&page=file_iframe&#navpanes=0';
             }
         </script>
         <?php
     }
     ?>
-    <form name="select_file_form" id="select_file_form" method="get" enctype="multipart/form-data" action="<?php  echo $_SESSION['config']['businessappurl'];?>index.php?display=true&dir=indexing_searching&page=choose_file" class="forms">
+    <form name="select_file_form" id="select_file_form" method="get" enctype="multipart/form-data" action="<?php 
+        echo $_SESSION['config']['businessappurl'];
+        ?>index.php?display=true&dir=indexing_searching&page=choose_file" class="forms">
         <input type="hidden" name="display" value="true" />
         <input type="hidden" name="dir" value="indexing_searching" />
         <input type="hidden" name="page" value="choose_file" />
         <p>
             <label for="file" ><?php  echo _CHOOSE_FILE; ?> </label>
-            <input type="file" name="file" id="file"  onchange="this.form.method = 'post';this.form.submit();" value="<?php if (isset($_SESSION['file_path'])){ echo $_SESSION['file_path'];} ?>" style="width:200px;margin-left:33px;" /><?php
-            if (!empty($_SESSION['upfile']['local_path']) && empty($_SESSION['error'])) {
-                ?><img src="<?php  echo $_SESSION['config']['businessappurl'];?>static.php?filename=picto_stat_enabled.gif" alt="" class="img_upload_doc" /><?php
-                echo "<br/><center><small>"._DOWNLOADED_FILE." : ".$_SESSION['upfile']['name'];"</small></center><br/>";
+            <input type="file" name="file" id="file" onchange="this.form.method = 'post';this.form.submit();" value="<?php 
+                if (isset($_SESSION['file_path'])) { 
+                    echo $_SESSION['file_path'];
+                } ?>" style="width:200px;margin-left:33px;" />
+                <?php
+            if (
+                !empty($_SESSION['upfile']['local_path']) 
+                && empty($_SESSION['error'])
+            ) {
+                ?><img src="<?php 
+                    echo $_SESSION['config']['businessappurl'];
+                    ?>static.php?filename=picto_stat_enabled.gif" alt="" class="img_upload_doc" />
+                <?php
+                echo "<br/><center><small>" . _DOWNLOADED_FILE 
+                    . " : " . $_SESSION['upfile']['name'];"</small></center><br/>";
             } else {
-                ?><img src="<?php  echo $_SESSION['config']['businessappurl'];?>static.php?filename=picto_stat_disabled.gif" class="img_upload_doc" alt=""/><?php
+                ?><img src="<?php 
+                    echo $_SESSION['config']['businessappurl'];
+                    ?>static.php?filename=picto_stat_disabled.gif" class="img_upload_doc" alt=""/>
+                    <?php
             }
             ?>
         </p>
