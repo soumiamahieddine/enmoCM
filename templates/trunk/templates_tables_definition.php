43b8b4bf46304ab1e9645fcd674813f81dc44c68
Laurent Giovannoni <laurent.giovannoni@maarch.org>
2012-03-29 23:23:19 +0000

    evo: mail merge with DOCX format

--- templates/trunk/templates_tables_definition.php
+++ templates/trunk/templates_tables_definition.php
@@ -1,5 +1,13 @@
 <?php
 
-if (!defined('_TEMPLATES_ASSOCIATION_TABLE')) {
-    define('_TEMPLATES_ASSOCIATION_TABLE', 'templates_association');
+if (!defined('_TEMPLATES_TABLE_NAME')) {
+    define('_TEMPLATES_TABLE_NAME', 'templates');
+}
+
+if (!defined('_TEMPLATES_ASSOCIATION_TABLE_NAME')) {
+    define('_TEMPLATES_ASSOCIATION_TABLE_NAME', 'templates_association');
+}
+
+if (!defined('_TEMPLATES_DOCTYPES_EXT_TABLE_NAME')) {
+    define('_TEMPLATES_DOCTYPES_EXT_TABLE_NAME', 'templates_doctype_ext');
 }
