bb552b4bed1792463ccdade1fbd98e8f515455fd
Laurent Giovannoni <laurent.giovannoni@maarch.org>
2012-04-01 17:17:22 +0000

    fix: indentation

--- attachments/trunk/class/class_modules_tools.php
+++ attachments/trunk/class/class_modules_tools.php
@@ -15,37 +15,37 @@
 class attachments
 {
 
-	/**
-	* Build Maarch module tables into sessions vars with a xml configuration
-	* file
-	*/
-	public function build_modules_tables()
-	{
-		if (file_exists(
-		    $_SESSION['config']['corepath'] . 'custom' . DIRECTORY_SEPARATOR
-		    . $_SESSION['custom_override_id'] . DIRECTORY_SEPARATOR . "modules"
-		    . DIRECTORY_SEPARATOR . "attachments" . DIRECTORY_SEPARATOR . "xml"
-		    . DIRECTORY_SEPARATOR . "config.xml"
-		)
-		) {
-			$configPath = $_SESSION['config']['corepath'] . 'custom'
-			            . DIRECTORY_SEPARATOR . $_SESSION['custom_override_id']
-			            . DIRECTORY_SEPARATOR . "modules" . DIRECTORY_SEPARATOR
-			            . "attachments" . DIRECTORY_SEPARATOR . "xml"
-			            . DIRECTORY_SEPARATOR . "config.xml";
-		} else {
-			$configPath = "modules" . DIRECTORY_SEPARATOR . "attachments"
-			            . DIRECTORY_SEPARATOR . "xml" . DIRECTORY_SEPARATOR
-			            . "config.xml";
-		}
- 		$xmlconfig = simplexml_load_file($configPath);
-		foreach ($xmlconfig->TABLENAME as $tableName) {
-			$_SESSION['tablename']['attach_res_attachments'] = (string) $tableName->attach_res_attachments;
-		}
-		$hist = $xmlconfig->HISTORY;
-		$_SESSION['history']['attachadd'] = (string) $hist->attachadd;
-		$_SESSION['history']['attachup'] = (string) $hist->attachup;
-		$_SESSION['history']['attachdel'] = (string) $hist->attachdel;
-		$_SESSION['history']['attachview'] = (string) $hist->attachview;
-	}
+    /**
+    * Build Maarch module tables into sessions vars with a xml configuration
+    * file
+    */
+    public function build_modules_tables()
+    {
+        if (file_exists(
+            $_SESSION['config']['corepath'] . 'custom' . DIRECTORY_SEPARATOR
+            . $_SESSION['custom_override_id'] . DIRECTORY_SEPARATOR . "modules"
+            . DIRECTORY_SEPARATOR . "attachments" . DIRECTORY_SEPARATOR . "xml"
+            . DIRECTORY_SEPARATOR . "config.xml"
+        )
+        ) {
+            $configPath = $_SESSION['config']['corepath'] . 'custom'
+                        . DIRECTORY_SEPARATOR . $_SESSION['custom_override_id']
+                        . DIRECTORY_SEPARATOR . "modules" . DIRECTORY_SEPARATOR
+                        . "attachments" . DIRECTORY_SEPARATOR . "xml"
+                        . DIRECTORY_SEPARATOR . "config.xml";
+        } else {
+            $configPath = "modules" . DIRECTORY_SEPARATOR . "attachments"
+                        . DIRECTORY_SEPARATOR . "xml" . DIRECTORY_SEPARATOR
+                        . "config.xml";
+        }
+        $xmlconfig = simplexml_load_file($configPath);
+        foreach ($xmlconfig->TABLENAME as $tableName) {
+            $_SESSION['tablename']['attach_res_attachments'] = (string) $tableName->attach_res_attachments;
+        }
+        $hist = $xmlconfig->HISTORY;
+        $_SESSION['history']['attachadd'] = (string) $hist->attachadd;
+        $_SESSION['history']['attachup'] = (string) $hist->attachup;
+        $_SESSION['history']['attachdel'] = (string) $hist->attachdel;
+        $_SESSION['history']['attachview'] = (string) $hist->attachview;
+    }
 }
