ba859314d953fe398ad00159921e9372fc27ea8f
Laurent Giovannoni <laurent.giovannoni@maarch.org>
2012-04-04 15:46:53 +0000

    evo: new process script

--- notes/trunk/frame_notes_doc.php
+++ notes/trunk/frame_notes_doc.php
@@ -18,7 +18,7 @@ $func = new functions();
 //$db = new dbquery();
 //$db->connect();
 if (empty($_SESSION['collection_id_choice'])) {
-	$_SESSION['collection_id_choice'] = $_SESSION['user']['collections'][0];
+    $_SESSION['collection_id_choice'] = $_SESSION['user']['collections'][0];
 }
 
 require_once "core" . DIRECTORY_SEPARATOR . "class" . DIRECTORY_SEPARATOR
@@ -50,98 +50,98 @@ $indNotes1d = '';
 
 
 if (isset($_GET['size']) && $_GET['size'] == "full") {
-	$sizeMedium = "15";
-	$sizeSmall = "15";
-	$sizeFull = "70";
-	$css = "listing spec detailtabricatordebug";
-	$body = "";
-	$cutString = 100;
-	$extendUrl = "&size=full";
+    $sizeMedium = "15";
+    $sizeSmall = "15";
+    $sizeFull = "70";
+    $css = "listing spec detailtabricatordebug";
+    $body = "";
+    $cutString = 100;
+    $extendUrl = "&size=full";
 } else {
-	$sizeMedium = "18";
-	$sizeSmall = "10";
-	$sizeFull = "30";
-	$css = "listingsmall";
-	$body = "iframe";
-	$cutString = 20;
-	$extendUrl = "";
+    $sizeMedium = "18";
+    $sizeSmall = "10";
+    $sizeFull = "30";
+    $css = "listingsmall";
+    $body = "iframe";
+    $cutString = 20;
+    $extendUrl = "";
 }
 
 for ($indNotes1 = 0; $indNotes1 < count($tabNotes); $indNotes1 ++ ) {
-	for ($indNotes2 = 0; $indNotes2 < count($tabNotes[$indNotes1]); $indNotes2 ++) {
-		foreach (array_keys($tabNotes[$indNotes1][$indNotes2]) as $value) {
-			if ($tabNotes[$indNotes1][$indNotes2][$value] == "id") {
-				$tabNotes[$indNotes1][$indNotes2]["id"] = $tabNotes[$indNotes1][$indNotes2]['value'];
-				$tabNotes[$indNotes1][$indNotes2]["label"] = 'ID';
-				$tabNotes[$indNotes1][$indNotes2]["size"] = $sizeSmall;
-				$tabNotes[$indNotes1][$indNotes2]["label_align"] = "left";
-				$tabNotes[$indNotes1][$indNotes2]["align"] = "left";
-				$tabNotes[$indNotes1][$indNotes2]["valign"] = "bottom";
-				$tabNotes[$indNotes1][$indNotes2]["show"] = true;
-				$indNotes1d = $tabNotes[$indNotes1][$indNotes2]['value'];
-			}
-			if ($tabNotes[$indNotes1][$indNotes2][$value] == "user_id") {
-				$tabNotes[$indNotes1][$indNotes2]["user_id"] = $tabNotes[$indNotes1][$indNotes2]['value'];
-				$tabNotes[$indNotes1][$indNotes2]["label"] = _ID;
-				$tabNotes[$indNotes1][$indNotes2]["size"] = $sizeSmall;
-				$tabNotes[$indNotes1][$indNotes2]["label_align"] = "left";
-				$tabNotes[$indNotes1][$indNotes2]["align"] = "left";
-				$tabNotes[$indNotes1][$indNotes2]["valign"] = "bottom";
-				$tabNotes[$indNotes1][$indNotes2]["show"] = false;
-			}
-			if ($tabNotes[$indNotes1][$indNotes2][$value] == "lastname") {
-				$tabNotes[$indNotes1][$indNotes2]['value'] = $request->show_string(
-				    $tabNotes[$indNotes1][$indNotes2]['value']
-				);
-				$tabNotes[$indNotes1][$indNotes2]["lastname"] = $tabNotes[$indNotes1][$indNotes2]['value'];
-				$tabNotes[$indNotes1][$indNotes2]["label"] = _LASTNAME;
-				$tabNotes[$indNotes1][$indNotes2]["size"] = $sizeSmall ;
-				$tabNotes[$indNotes1][$indNotes2]["label_align"] = "left";
-				$tabNotes[$indNotes1][$indNotes2]["align"] = "left";
-				$tabNotes[$indNotes1][$indNotes2]["valign"] = "bottom";
-				$tabNotes[$indNotes1][$indNotes2]["show"] = true;
-			}
-			if ($tabNotes[$indNotes1][$indNotes2][$value] == "date_note") {
-				$tabNotes[$indNotes1][$indNotes2]["date_note"] = $tabNotes[$indNotes1][$indNotes2]['value'];
-				$tabNotes[$indNotes1][$indNotes2]["label"] = _DATE;
-				$tabNotes[$indNotes1][$indNotes2]["size"] = $sizeSmall;
-				$tabNotes[$indNotes1][$indNotes2]["label_align"] = "left";
-				$tabNotes[$indNotes1][$indNotes2]["align"] = "left";
-				$tabNotes[$indNotes1][$indNotes2]["valign"] = "bottom";
-				$tabNotes[$indNotes1][$indNotes2]["show"] = true;
-			}
-			if ($tabNotes[$indNotes1][$indNotes2][$value] == "firstname") {
-				$tabNotes[$indNotes1][$indNotes2]["firstname"] = $tabNotes[$indNotes1][$indNotes2]['value'];
-				$tabNotes[$indNotes1][$indNotes2]["label"] = _FIRSTNAME;
-				$tabNotes[$indNotes1][$indNotes2]["size"] = $sizeSmall;
-				$tabNotes[$indNotes1][$indNotes2]["label_align"] = "center";
-				$tabNotes[$indNotes1][$indNotes2]["align"] = "center";
-				$tabNotes[$indNotes1][$indNotes2]["valign"] = "bottom";
-				$tabNotes[$indNotes1][$indNotes2]["show"] = true;
-			}
-			if ($tabNotes[$indNotes1][$indNotes2][$value] == "note_text") {
-				$tabNotes[$indNotes1][$indNotes2]['value'] = '<a href="javascript://"'
-					. ' onclick="ouvreFenetre(\''
-					. $_SESSION['config']['businessappurl']
-					. 'index.php?display=true&module=notes&page=note_details&id='
-					. $indNotes1d . '&amp;resid=' . $_SESSION['doc_id']
-					. '&amp;coll_id=' . $_SESSION['collection_id_choice']
-					. $extendUrl . '\', 450, 300)">'
-					. $func->cut_string(
-					    $request->show_string(
-					        $tabNotes[$indNotes1][$indNotes2]['value']
-					    ), $cutString
-					) . '<span class="sstit"> > ' . _READ . '</span>';
-				$tabNotes[$indNotes1][$indNotes2]["note_text"] = $tabNotes[$indNotes1][$indNotes2]['value'];
-				$tabNotes[$indNotes1][$indNotes2]["label"] = _NOTES;
-				$tabNotes[$indNotes1][$indNotes2]["size"] = $sizeFull;
-				$tabNotes[$indNotes1][$indNotes2]["label_align"] = "center";
-				$tabNotes[$indNotes1][$indNotes2]["align"] = "center";
-				$tabNotes[$indNotes1][$indNotes2]["valign"] = "bottom";
-				$tabNotes[$indNotes1][$indNotes2]["show"] = true;
-			}
-		}
-	}
+    for ($indNotes2 = 0; $indNotes2 < count($tabNotes[$indNotes1]); $indNotes2 ++) {
+        foreach (array_keys($tabNotes[$indNotes1][$indNotes2]) as $value) {
+            if ($tabNotes[$indNotes1][$indNotes2][$value] == "id") {
+                $tabNotes[$indNotes1][$indNotes2]["id"] = $tabNotes[$indNotes1][$indNotes2]['value'];
+                $tabNotes[$indNotes1][$indNotes2]["label"] = 'ID';
+                $tabNotes[$indNotes1][$indNotes2]["size"] = $sizeSmall;
+                $tabNotes[$indNotes1][$indNotes2]["label_align"] = "left";
+                $tabNotes[$indNotes1][$indNotes2]["align"] = "left";
+                $tabNotes[$indNotes1][$indNotes2]["valign"] = "bottom";
+                $tabNotes[$indNotes1][$indNotes2]["show"] = true;
+                $indNotes1d = $tabNotes[$indNotes1][$indNotes2]['value'];
+            }
+            if ($tabNotes[$indNotes1][$indNotes2][$value] == "user_id") {
+                $tabNotes[$indNotes1][$indNotes2]["user_id"] = $tabNotes[$indNotes1][$indNotes2]['value'];
+                $tabNotes[$indNotes1][$indNotes2]["label"] = _ID;
+                $tabNotes[$indNotes1][$indNotes2]["size"] = $sizeSmall;
+                $tabNotes[$indNotes1][$indNotes2]["label_align"] = "left";
+                $tabNotes[$indNotes1][$indNotes2]["align"] = "left";
+                $tabNotes[$indNotes1][$indNotes2]["valign"] = "bottom";
+                $tabNotes[$indNotes1][$indNotes2]["show"] = false;
+            }
+            if ($tabNotes[$indNotes1][$indNotes2][$value] == "lastname") {
+                $tabNotes[$indNotes1][$indNotes2]['value'] = $request->show_string(
+                    $tabNotes[$indNotes1][$indNotes2]['value']
+                );
+                $tabNotes[$indNotes1][$indNotes2]["lastname"] = $tabNotes[$indNotes1][$indNotes2]['value'];
+                $tabNotes[$indNotes1][$indNotes2]["label"] = _LASTNAME;
+                $tabNotes[$indNotes1][$indNotes2]["size"] = $sizeSmall ;
+                $tabNotes[$indNotes1][$indNotes2]["label_align"] = "left";
+                $tabNotes[$indNotes1][$indNotes2]["align"] = "left";
+                $tabNotes[$indNotes1][$indNotes2]["valign"] = "bottom";
+                $tabNotes[$indNotes1][$indNotes2]["show"] = true;
+            }
+            if ($tabNotes[$indNotes1][$indNotes2][$value] == "date_note") {
+                $tabNotes[$indNotes1][$indNotes2]["date_note"] = $tabNotes[$indNotes1][$indNotes2]['value'];
+                $tabNotes[$indNotes1][$indNotes2]["label"] = _DATE;
+                $tabNotes[$indNotes1][$indNotes2]["size"] = $sizeSmall;
+                $tabNotes[$indNotes1][$indNotes2]["label_align"] = "left";
+                $tabNotes[$indNotes1][$indNotes2]["align"] = "left";
+                $tabNotes[$indNotes1][$indNotes2]["valign"] = "bottom";
+                $tabNotes[$indNotes1][$indNotes2]["show"] = true;
+            }
+            if ($tabNotes[$indNotes1][$indNotes2][$value] == "firstname") {
+                $tabNotes[$indNotes1][$indNotes2]["firstname"] = $tabNotes[$indNotes1][$indNotes2]['value'];
+                $tabNotes[$indNotes1][$indNotes2]["label"] = _FIRSTNAME;
+                $tabNotes[$indNotes1][$indNotes2]["size"] = $sizeSmall;
+                $tabNotes[$indNotes1][$indNotes2]["label_align"] = "center";
+                $tabNotes[$indNotes1][$indNotes2]["align"] = "center";
+                $tabNotes[$indNotes1][$indNotes2]["valign"] = "bottom";
+                $tabNotes[$indNotes1][$indNotes2]["show"] = true;
+            }
+            if ($tabNotes[$indNotes1][$indNotes2][$value] == "note_text") {
+                $tabNotes[$indNotes1][$indNotes2]['value'] = '<a href="javascript://"'
+                    . ' onclick="ouvreFenetre(\''
+                    . $_SESSION['config']['businessappurl']
+                    . 'index.php?display=true&module=notes&page=note_details&id='
+                    . $indNotes1d . '&amp;resid=' . $_SESSION['doc_id']
+                    . '&amp;coll_id=' . $_SESSION['collection_id_choice']
+                    . $extendUrl . '\', 450, 300)">'
+                    . $func->cut_string(
+                        $request->show_string(
+                            $tabNotes[$indNotes1][$indNotes2]['value']
+                        ), $cutString
+                    ) . '<span class="sstit"> > ' . _READ . '</span>';
+                $tabNotes[$indNotes1][$indNotes2]["note_text"] = $tabNotes[$indNotes1][$indNotes2]['value'];
+                $tabNotes[$indNotes1][$indNotes2]["label"] = _NOTES;
+                $tabNotes[$indNotes1][$indNotes2]["size"] = $sizeFull;
+                $tabNotes[$indNotes1][$indNotes2]["label_align"] = "center";
+                $tabNotes[$indNotes1][$indNotes2]["align"] = "center";
+                $tabNotes[$indNotes1][$indNotes2]["valign"] = "bottom";
+                $tabNotes[$indNotes1][$indNotes2]["show"] = true;
+            }
+        }
+    }
 }
 //$request->show_array($tabNotes);
 $core->load_html();
--- notes/trunk/note_add.php
+++ notes/trunk/note_add.php
@@ -31,21 +31,21 @@ $collId = '';
 $extendUrl = '';
 $extendUrlValue = '';
 if (isset($_REQUEST['size']) && $_REQUEST['size'] == "full") {
-	$extendUrl = "&size=full";
-	$extendUrlValue = $_REQUEST['size'];
+    $extendUrl = "&size=full";
+    $extendUrlValue = $_REQUEST['size'];
 }
 
 if (isset($_REQUEST['identifier']) && ! empty($_REQUEST['identifier'])) {
-	$identifier = trim($_REQUEST['identifier']);
+    $identifier = trim($_REQUEST['identifier']);
 }
 if (isset($_REQUEST['coll_id']) && ! empty($_REQUEST['coll_id'])) {
-	$collId = trim($_REQUEST['coll_id']);
-	$view = $sec->retrieve_view_from_coll_id($collId);
-	$table = $sec->retrieve_table_from_coll($collId);
+    $collId = trim($_REQUEST['coll_id']);
+    $view = $sec->retrieve_view_from_coll_id($collId);
+    $table = $sec->retrieve_table_from_coll($collId);
 }
 
 if (isset($_REQUEST['table']) && ! empty($_REQUEST['table'])) {
-	$table = trim($_REQUEST['table']);
+    $table = trim($_REQUEST['table']);
 }
 ?>
 <body id="pop_up" onload="resizeTo(450, 350);setTimeout(window.close, <?php
@@ -54,53 +54,53 @@ echo $time;
 <?php
 
 if (isset($_REQUEST['notes']) && ! empty($_REQUEST['notes'])) {
-	$date = $db->current_datetime();
+    $date = $db->current_datetime();
 
-	$db->query(
-		"INSERT INTO " . NOTES_TABLE . "(identifier, note_text, date_note, "
-	    . "user_id, coll_id, tablename) VALUES"
-		. " (".$identifier . ", '" . $db->protect_string_db($_REQUEST['notes'])
-		. "', " . $date . ", '"
-		. $db->protect_string_db($_SESSION['user']['UserId']) . "', '"
-		. $db->protect_string_db($collId) . "', '"
-		. $db->protect_string_db($table) . "')"
+    $db->query(
+        "INSERT INTO " . NOTES_TABLE . "(identifier, note_text, date_note, "
+        . "user_id, coll_id, tablename) VALUES"
+        . " (".$identifier . ", '" . $db->protect_string_db($_REQUEST['notes'])
+        . "', " . $date . ", '"
+        . $db->protect_string_db($_SESSION['user']['UserId']) . "', '"
+        . $db->protect_string_db($collId) . "', '"
+        . $db->protect_string_db($table) . "')"
     );
-	if ($_SESSION['history']['noteadd']) {
-	    $hist = new history();
-		$db->query(
-			"SELECT id FROM " . NOTES_TABLE . " WHERE date_note = " . $date
-		    . " and identifier = " . $identifier . " and user_id = '"
-		    . $_SESSION['user']['UserId'] . "' and coll_id = '" . $collId . "' "
-		);
-		$res = $db->fetch_object();
-		$id = $res->id;
-		if (isset($_SESSION['origin']) && $_SESSION['origin'] == "show_folder") {
-			$hist->add(
-			    $table, $identifier, "ADD", 'noteadd', _ADDITION_NOTE . _ON_FOLDER_NUM
-			    . $identifier . ' (' . $id . ') : "' . substr($db->protect_string_db($_REQUEST['notes']), 0, 254) .'"',
-			    $_SESSION['config']['databasetype'], 'notes'
-			);
-		} else {
-			$hist->add(
-			    $view, $identifier, "ADD", 'noteadd',  _ADDITION_NOTE . _ON_DOC_NUM
-			    . $identifier . ' (' . $id . ') : "' . substr($db->protect_string_db($_REQUEST['notes']), 0, 254) .'"',
-			    $_SESSION['config']['databasetype'], 'notes'
-			);
-		}
+    if ($_SESSION['history']['noteadd']) {
+        $hist = new history();
+        $db->query(
+            "SELECT id FROM " . NOTES_TABLE . " WHERE date_note = " . $date
+            . " and identifier = " . $identifier . " and user_id = '"
+            . $_SESSION['user']['UserId'] . "' and coll_id = '" . $collId . "' "
+        );
+        $res = $db->fetch_object();
+        $id = $res->id;
+        if (isset($_SESSION['origin']) && $_SESSION['origin'] == "show_folder") {
+            $hist->add(
+                $table, $identifier, "ADD", 'noteadd', _ADDITION_NOTE . _ON_FOLDER_NUM
+                . $identifier . ' (' . $id . ') : "' . substr($db->protect_string_db($_REQUEST['notes']), 0, 254) .'"',
+                $_SESSION['config']['databasetype'], 'notes'
+            );
+        } else {
+            $hist->add(
+                $view, $identifier, "ADD", 'noteadd',  _ADDITION_NOTE . _ON_DOC_NUM
+                . $identifier . ' (' . $id . ') : "' . substr($db->protect_string_db($_REQUEST['notes']), 0, 254) .'"',
+                $_SESSION['config']['databasetype'], 'notes'
+            );
+        }
 
-		$hist->add(
-		    NOTES_TABLE, $id, "ADD", 'noteadd', _NOTES_ADDED . ' (' . $id . ')',
-		    $_SESSION['config']['databasetype'], 'notes'
-		);
-	}
+        $hist->add(
+            NOTES_TABLE, $id, "ADD", 'noteadd', _NOTES_ADDED . ' (' . $id . ')',
+            $_SESSION['config']['databasetype'], 'notes'
+        );
+    }
 
-	if (isset($_REQUEST['origin']) && $_SESSION['origin'] <> 'valid'
-	    && $_SESSION['origin'] <> 'qualify'
-	) {
-		//$_SESSION['error'] = _ADDITION_NOTE;
-	}
-	?>
-		<script type="text/javascript">
+    if (isset($_REQUEST['origin']) && $_SESSION['origin'] <> 'valid'
+        && $_SESSION['origin'] <> 'qualify'
+    ) {
+        //$_SESSION['error'] = _ADDITION_NOTE;
+    }
+    ?>
+        <script type="text/javascript">
         <?php
     if (isset($_SESSION['origin']) && $_SESSION['origin'] == "process") {
         ?>
@@ -135,14 +135,14 @@ if (isset($_REQUEST['notes']) && ! empty($_REQUEST['notes'])) {
         ?>
         eleframe1.src = '<?php
         echo $_SESSION['config']['businessappurl'];
-        ?>index.php?display=true&module=notes&page=frame_notes_doc<?php
+        ?>index.php?display=true&module=notes&size=full&page=frame_notes_doc<?php
         echo $extendUrl;
         ?>';
         <?php
     }
     ?>
     window.top.close();
-	</script>
+    </script>
     <?php
 } else {
     ?>
@@ -152,7 +152,7 @@ if (isset($_REQUEST['notes']) && ! empty($_REQUEST['notes'])) {
     echo _ADD_NOTE;
     ?> </h2>
 
-	<div class="block" style="padding:10px;">
+    <div class="block" style="padding:10px;">
       <form name="form1" method="post" action="<?php
     echo $_SESSION['config']['businessappurl'];
     ?>index.php?display=true&module=notes&page=note_add&identifier=<?php
@@ -161,24 +161,24 @@ if (isset($_REQUEST['notes']) && ! empty($_REQUEST['notes'])) {
     echo $table;
     ?>&coll_id=<?php
     echo $collId;?>" >
-		<input type="hidden" name="display" value="true" />
-		<input type="hidden" name="modules" value="notes" />
-		<input type="hidden" name="page" value="note_add" />
-		<input type="hidden" value="<?php
+        <input type="hidden" name="display" value="true" />
+        <input type="hidden" name="modules" value="notes" />
+        <input type="hidden" name="page" value="note_add" />
+        <input type="hidden" value="<?php
     echo $identifier;?>" name="identifier" id="identifier">
-		<input type="hidden" value="<?php
+        <input type="hidden" value="<?php
     echo $extendUrlValue;?>" name="size" id="size">
-		<textarea  cols="65" rows="10"  name="notes"  id="notes" ></textarea>
-	   <br/>
+        <textarea  cols="65" rows="10"  name="notes"  id="notes" ></textarea>
+       <br/>
         <p class="buttons">
-			<input type="submit" name="Submit" value="<?php
+            <input type="submit" name="Submit" value="<?php
     echo _ADD_NOTE;?>" class="button"/>
              <input type="submit" name="Submit2" value="<?php
     echo _CANCEL;?>" onclick="javascript:self.close();" class="button"/>
         </p>
       </form>
-	</div>
-	<div class="block_end">&nbsp;</div>
+    </div>
+    <div class="block_end">&nbsp;</div>
  <?php
 }
 $core->load_js();
