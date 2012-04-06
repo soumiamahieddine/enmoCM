d057a8e2276752809e905963d7f4c48c673f0b05
Laurent Giovannoni <laurent.giovannoni@maarch.org>
2012-04-06 09:54:24 +0000

    fix: name of the class

--- maarch_entreprise/trunk/load_statuses_session.php
+++ maarch_entreprise/trunk/load_statuses_session.php
@@ -1,10 +1,6 @@
 <?php
-require_once 'core' 
-	. DIRECTORY_SEPARATOR . 'class'
-	. DIRECTORY_SEPARATOR . 'statusControler.php';
-	
+require_once 'core/class/StatusControler.php';
+
 $sts = new Maarch_Core_Class_StatusControler();
 $_SESSION['m_admin']['statuses'] = array();
-$_SESSION['m_admin']['statuses'] = $sts->getAllInfos(); 
-
-?>
+$_SESSION['m_admin']['statuses'] = $sts->getAllInfos();
