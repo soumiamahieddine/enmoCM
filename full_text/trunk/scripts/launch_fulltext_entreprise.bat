1c301df76db87d70ed3c44324121cd3c46cf93f5
Jean-Louis Ercolani <jeanlouis.ercolani@maarch.org>
2012-04-10 20:06:44 +0000

    change conf file for config.xl

--- full_text/trunk/scripts/launch_fulltext_entreprise.bat
+++ full_text/trunk/scripts/launch_fulltext_entreprise.bat
@@ -1,3 +1,3 @@
-"C:\xampp\php\php.exe" C:\xampp\htdocs\maarch_entreprise\modules\full_text\lucene_full_text_engine.php  C:\xampp\htdocs\maarch_entreprise\modules\full_text\xml\config_entreprise.xml
+"C:\xampp\php\php.exe" C:\xampp\htdocs\maarch_entreprise\modules\full_text\lucene_full_text_engine.php  C:\xampp\htdocs\maarch_entreprise\modules\full_text\xml\config.xml
 c:
 pause
