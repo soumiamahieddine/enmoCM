21cfd5cb37b175adfd330d9381936bf775ef7391
Laurent Giovannoni <laurent.giovannoni@maarch.org>
2012-04-12 12:55:28 +0000

    evo: ajax return

--- content_management/trunk/js/functions.js
+++ content_management/trunk/js/functions.js
@@ -16,19 +16,38 @@ function sendAppletMsg(theMsg)
 //destroy the modal of the applet and launch an ajax script
 function endOfApplet(objectType)
 {
+    //window.alert(objectType);
     if (objectType == 'template' || objectType == 'templateStyle') {
         endTemplate();
     } else if (objectType == 'resource') {
         endResource();
+    } else if (objectType == 'attachmentFromTemplate') {
+        endAttachmentFromTemplate();
+    } else if (objectType == 'attachment') {
+        endAttachment();
     }
     destroyModal('CMApplet');
 }
 
-function endTemplate()
+function endAttachmentFromTemplate()
 {
-    window.alert('template ?');
+    //window.alert('template ?');
+    if($('list_attach')) {
+        $('list_attach').src = $('list_attach').src;
+    }
 }
 
+function endAttachment()
+{
+    window.close();
+}
+
+function endTemplate()
+{
+    //window.alert('template ?');
+}
+
+//reload the list div and the document if necessary
 function endResource()
 {
     //window.alert('resource ?');
@@ -38,6 +57,9 @@ function endResource()
         'createVersion', 
         '../../modules/content_management/list_versions.php'
     );
+    if($('viewframe')) {
+        $('viewframe').src = $('viewframe').src;
+    }
 }
 
 function showDiv(divName, spanNb, divCreate, path_manage_script)
