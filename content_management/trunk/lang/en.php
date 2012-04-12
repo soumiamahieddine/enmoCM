cc9afb1d484d681a636be2714736c693975eec40
Sébastien Nana <sebastien.nana@maarch.org>
2012-04-12 08:40:07 +0000

    Add content management translation

---
+++ content_management/trunk/lang/en.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ *
+ *    Copyright 2008,2009 Maarch
+ *
+ *  This file is part of Maarch Framework.
+ *
+ *   Maarch Framework is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Maarch Framework is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *    along with Maarch Framework.  If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
+ /******************** Content management  ************/
+
+
+if (!defined('_TEMPLATE_LABEL')) define( '_TEMPLATE_LABEL', 'Template name');
+if (!defined('_TEMPLATE_COMMENT')) define( '_TEMPLATE_COMMENT', 'Desciption');
+if (!defined('_TEMPLATE_TYPE')) define( '_TEMPLATE_TYPE', 'Template type');
+if (!defined('_TEMPLATE_STYLE')) define( '_TEMPLATE_STYLE', 'Template association');
+if (!defined('_EDIT_TEMPLATE')) define( '_EDIT_TEMPLATE', 'Edit template');
+if (!defined('_TEMPLATE_ID')) define( '_TEMPLATE_ID', 'Template ID');
