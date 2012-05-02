b594feaa3dbe551fe5e8abcf027c7b38843e41a8
Cyril Vazquez <cyril.vazquez@maarch.org>
2012-05-02 08:04:06 +0000

    Template content moved to templates

--- core/trunk/structure.sql
+++ core/trunk/structure.sql
@@ -1361,7 +1361,6 @@ CREATE TABLE templates_association
   description character varying(255),
   diffusion_type character varying(50),
   diffusion_properties character varying(255),
-  diffusion_content character varying(255),
   is_attached character varying(1) DEFAULT 'N'::character varying,
   CONSTRAINT templates_association_pkey PRIMARY KEY (system_id)
 )
