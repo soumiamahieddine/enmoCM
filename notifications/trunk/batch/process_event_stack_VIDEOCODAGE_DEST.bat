f6564e47ed2ce7c6955e2eb3ddf0e807de46d35a
Cyril Vazquez <cyril.vazquez@maarch.org>
2012-05-07 15:54:11 +0000

    MLB index to dest user and copy list

---
+++ notifications/trunk/batch/process_event_stack_VIDEOCODAGE_DEST.bat
@@ -0,0 +1,2 @@
+"C:\xampp\php\php.exe" "C:\xampp\htdocs\maarch_trunk\modules\notifications\batch\process_event_stack.php" -c "C:\xampp\htdocs\maarch_trunk\modules\notifications\batch\config\config.xml" -n INDEX_DEST
+pause
