26d6e20cd016c5c67c0d3a272208b1a9279c34ad
Laurent Giovannoni <laurent.giovannoni@maarch.org>
2012-05-10 12:48:06 +0000

    evo: add tags table

--- core/trunk/structure.sql
+++ core/trunk/structure.sql
@@ -1508,6 +1508,15 @@ WITH (
   OIDS=FALSE
 );
 
+CREATE TABLE tags
+(
+  tag_label character varying(50) NOT NULL,
+  coll_id character varying(50) NOT NULL,
+  res_id bigint NOT NULL,
+  CONSTRAINT tagsjoin_pkey PRIMARY KEY (tag_label, coll_id, res_id )
+)
+WITH (OIDS=FALSE);
+
 CREATE SEQUENCE res_id_seq
   INCREMENT 1
   MINVALUE 1
