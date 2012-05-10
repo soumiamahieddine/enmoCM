3adb2bc71ad326b4f0bc185a2d21f551081d903e
Laurent Giovannoni <laurent.giovannoni@maarch.org>
2012-05-10 13:53:11 +0000

    fix: translate

--- attachments/trunk/lang/en.php
+++ attachments/trunk/lang/en.php
@@ -1,17 +1,23 @@
 <?php
-/**
-* File : fr.php
-*
-* French language file
-*
-* @package  Maarch LetterBox 1.0
-* @version 1.0
-* @since 06/2007
-* @license GPL
-* @author  Claire Figueras  <dev@maarch.org>
-*/
-/*********************** communs ***********************************/
-
+/*
+ *
+ *   Copyright 2008-2012 Maarch
+ *
+ *   This file is part of Maarch Framework.
+ *
+ *   Maarch Framework is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Maarch Framework is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Maarch Framework.  If not, see <http://www.gnu.org/licenses/>.
+ */
 if (!defined('_TITLE')) define('_TITLE', 'Title');
 if (!defined('_ATTACHMENTS')) define('_ATTACHMENTS', 'Attachments');
 if (!defined('_ATTACHMENTS_COMMENT')) define('_ATTACHMENTS_COMMENT', 'Manage attachments');
@@ -25,4 +31,15 @@ if (!defined('_ATTACHED_DOC')) define('_ATTACHED_DOC', 'Attachments');
 if (!defined('_INDEX_ATTACHMENT'))  define('_INDEX_ATTACHMENT', 'Add an attachment');
 if (!defined('_INDEX_ATTACHMENT_DESC'))  define('_INDEX_ATTACHMENT_DESC', 'Add an attachment');
 if (!defined('_ATTACH_TO_DOC'))  define('_ATTACH_TO_DOC', 'Add as answer to an existing document');
-?>
+if (!defined('_ATTACH_TO_DOC_NUM'))  define('_ATTACH_TO_DOC_NUM', 'Attach to document number');
+
+if (!defined('_VIEW_ATTACHMENTS')) {
+	define('_VIEW_ATTACHMENTS', 'The attachments');
+}
+if (!defined('_VIEW_ATTACHMENTS_DESC')) {
+	define('_VIEW_ATTACHMENTS_DESC', 'View attachments');
+}
+if (!defined('_MANAGE_ATTACHMENTS'))  define('_MANAGE_ATTACHMENTS', 'Manage attachments');
+
+if (!defined('_GENERATE_OFFICE_ATTACHMENT'))  define('_GENERATE_OFFICE_ATTACHMENT', 'Generate Office Attachment');
+if (!defined('_GENERATE_HTML_ATTACHMENT'))  define('_GENERATE_HTML_ATTACHMENT', 'Generate HTML Attachment');
--- attachments/trunk/lang/fr.php
+++ attachments/trunk/lang/fr.php
@@ -1,15 +1,23 @@
 <?php
-/**
-* File : fr.php
-*
-* French language file
-*
-* @package  Maarch workflow 1.0
-* @version 1.0
-* @since 06/2007
-* @license GPL
-* @author  Claire Figueras <dev@maarch.org>
-*/
+/*
+ *
+ *   Copyright 2008-2012 Maarch
+ *
+ *   This file is part of Maarch Framework.
+ *
+ *   Maarch Framework is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Maarch Framework is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Maarch Framework.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 if (!defined('_TITLE'))  define('_TITLE', 'Titre');
 if (!defined('_ATTACHMENTS'))  define('_ATTACHMENTS', 'Pi&egrave;ces jointes');
@@ -33,4 +41,7 @@ if (!defined('_VIEW_ATTACHMENTS_DESC')) {
 	define('_VIEW_ATTACHMENTS_DESC', 'Voir les pi&eagrave;ces jointes');
 }
 if (!defined('_MANAGE_ATTACHMENTS'))  define('_MANAGE_ATTACHMENTS', 'G&eacute;rer les pi&eagrave;ces jointes');
+
+if (!defined('_GENERATE_OFFICE_ATTACHMENT'))  define('_GENERATE_OFFICE_ATTACHMENT', 'G&eacute;n&eacute;rer OFFICE');
+if (!defined('_GENERATE_HTML_ATTACHMENT'))  define('_GENERATE_HTML_ATTACHMENT', 'G&eacute;n&eacute;rer HTML');
 
