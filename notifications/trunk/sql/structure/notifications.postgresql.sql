4ef615a8f2ba45ae1aab60464932587fb66c4552
Cyril Vazquez <cyril.vazquez@maarch.org>
2012-05-11 13:15:25 +0000

    Move notif management from templates_association to new table/object "notifications"

--- notifications/trunk/sql/structure/notifications.postgresql.sql
+++ notifications/trunk/sql/structure/notifications.postgresql.sql
@@ -1,35 +1,63 @@
-CREATE SEQUENCE event_stack_seq
+CREATE SEQUENCE notifications_seq
   INCREMENT 1
   MINVALUE 1
   MAXVALUE 9223372036854775807
   START 1
   CACHE 1;
 
-CREATE TABLE event_stack
+CREATE TABLE notifications
 (
-  system_id bigint NOT NULL DEFAULT nextval('event_stack_seq'::regclass),
-  ta_sid bigint NOT NULL,
-  table_name character varying(32) NOT NULL,
-  record_id character varying(255) NOT NULL,
-  event_date timestamp without time zone NOT NULL,
-  exec_date timestamp without time zone NOT NULL,
-  exec_result character varying(50) NOT NULL,
-  CONSTRAINT event_stack_pkey PRIMARY KEY (system_id)
+  notification_sid bigint NOT NULL DEFAULT nextval('notifications_seq'::regclass),
+  notification_id character varying(50) NOT NULL,
+  description character varying(255),
+  event_id character varying(255) NOT NULL,
+  notification_mode character varying(30) NOT NULL,
+  template_id bigint,
+  diffusion_type character varying(50) NOT NULL,
+  diffusion_properties character varying(255),
+  attachfor_type character varying(50),
+  attachfor_properties character varying(255),
+  CONSTRAINT notifications_pkey PRIMARY KEY (notification_sid)
 )
 WITH (
   OIDS=FALSE
 );
 
-CREATE SEQUENCE email_stack_seq
+
+CREATE SEQUENCE notif_event_stack_seq
   INCREMENT 1
   MINVALUE 1
   MAXVALUE 9223372036854775807
   START 1
   CACHE 1;
 
-CREATE TABLE email_stack
+CREATE TABLE notif_event_stack
 (
-  system_id bigint NOT NULL DEFAULT nextval('email_stack_seq'::regclass),
+  event_stack_sid bigint NOT NULL DEFAULT nextval('notif_event_stack_seq'::regclass),
+  notif_sid bigint NOT NULL,
+  table_name character varying(50) NOT NULL,
+  record_id character varying(50) NOT NULL,
+  user_id character varying(128) NOT NULL,
+  event_info character varying(255) NOT NULL,
+  event_date timestamp without time zone NOT NULL,
+  exec_date timestamp without time zone,
+  exec_result character varying(50),
+  CONSTRAINT notif_event_stack_pkey PRIMARY KEY (event_stack_sid)
+)
+WITH (
+  OIDS=FALSE
+);
+
+CREATE SEQUENCE notif_email_stack_seq
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+
+CREATE TABLE notif_email_stack
+(
+  email_stack_sid bigint NOT NULL DEFAULT nextval('notif_email_stack_seq'::regclass),
   sender character varying(255) NOT NULL,
   reply_to character varying(255),
   recipient character varying(2000) NOT NULL,
@@ -39,9 +67,11 @@ CREATE TABLE email_stack
   html_body text,
   text_body text,
   charset character varying(50) NOT NULL,
+  attachments character varying(2000),
+  module character varying(50) NOT NULL,
   exec_date timestamp without time zone,
   exec_result character varying(50),
-  CONSTRAINT email_stack_pkey PRIMARY KEY (system_id)
+  CONSTRAINT notif_email_stack_pkey PRIMARY KEY (email_stack_sid)
 )
 WITH (
   OIDS=FALSE
