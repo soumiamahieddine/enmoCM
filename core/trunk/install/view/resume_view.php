2b5fc83644856976b2aed1be03bf2529f01f03c5
Laurent Giovannoni <laurent.giovannoni@maarch.org>
2012-05-14 11:16:30 +0000

    evo: resume details

--- core/trunk/install/view/resume_view.php
+++ core/trunk/install/view/resume_view.php
@@ -6,7 +6,11 @@
     </div>
     <div class="contentBlock" id="resume">
         <p>
-
+            <?php echo _RESUME_DETAILS; ?>
+        </p>
+        <p>
+            <?php echo _INSTALL_SUCCESS; ?>
+            <iframe src="http://www.facebook.com/plugins/like.php?href=53918706268&layout=standard&show_faces=false&width=450&action=like&colorscheme=light" scrolling="no" frameborder="0" allowTransparency="true" style="border:none; overflow:hidden; width:450px; height:25px"></iframe>
         </p>
     </div>
 </div>
