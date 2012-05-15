249f8df0f8752f813f53db95a77242f240ec73ae
Jean-Louis Ercolani <jeanlouis.ercolani@maarch.org>
2012-05-15 16:38:00 +0000

    Fix: GetRecipients query => Item_type = 'user_id'

--- notifications/trunk/diffusion_types/copy_list.php
+++ notifications/trunk/diffusion_types/copy_list.php
@@ -10,7 +10,7 @@ case 'recipients':
 	$query = "SELECT distinct us.* "
 		. " FROM listinstance li LEFT JOIN users us ON li.item_id = us.user_id " 
 		. " WHERE coll_id = 'letterbox_coll' AND res_id = ".$event->record_id
-		. " AND listinstance_type='DOC' AND item_mode = 'cc'";
+		. " AND listinstance_type='DOC' AND item_type='user_id' AND item_mode = 'cc'";
 	$dbRecipients = new dbquery();
 	$dbRecipients->query($query);
 	$dbRecipients->connect();
--- notifications/trunk/diffusion_types/dest_user.php
+++ notifications/trunk/diffusion_types/dest_user.php
@@ -8,7 +8,7 @@ case 'recipients':
 	$query = "SELECT distinct us.* "
 		. " FROM listinstance li LEFT JOIN users us ON li.item_id = us.user_id " 
 		. " WHERE coll_id = 'letterbox_coll' AND res_id = ".$event->record_id
-		. " AND listinstance_type='DOC' AND item_mode = 'dest'";
+		. " AND listinstance_type='DOC' AND item_type='user_id' AND item_mode = 'dest'";
 	$dbRecipients = new dbquery();
 	$dbRecipients->query($query);
 	$dbRecipients->connect();
