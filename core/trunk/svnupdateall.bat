8bd9ef4356b99f1bbb67da9894647452477d3613
Cyril Vazquez <cyril.vazquez@maarch.org>
2012-05-15 13:53:39 +0000

    Add: install

--- core/trunk/svnupdateall.bat
+++ core/trunk/svnupdateall.bat
@@ -3,6 +3,8 @@ echo "Core" >> C:\xampp\htdocs\maarch_entreprise\update.log
 echo date >> C:\xampp\htdocs\maarch_entreprise\update.log
 echo time >> C:\xampp\htdocs\maarch_entreprise\update.log
 svn update >> C:\xampp\htdocs\maarch_entreprise\update.log
+cd ..\install
+svn update >> C:\xampp\htdocs\maarch_entreprise\install.log
 cd ..\apps\maarch_entreprise
 echo "Apps" >> C:\xampp\htdocs\maarch_entreprise\update.log
 svn update >> C:\xampp\htdocs\maarch_entreprise\update.log
