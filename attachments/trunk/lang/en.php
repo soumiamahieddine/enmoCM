da44dd45931d5ee0eab5f1f63d46e149e1878805
Laurent Giovannoni <laurent.giovannoni@maarch.org>
2012-05-16 12:39:26 +0000

    fix: lang

--- attachments/trunk/lang/en.php
+++ attachments/trunk/lang/en.php
@@ -18,30 +18,44 @@
  *   You should have received a copy of the GNU General Public License
  *   along with Maarch Framework.  If not, see <http://www.gnu.org/licenses/>.
  */
-if (!defined('_TITLE')) define('_TITLE', 'Title');
-if (!defined('_ATTACHMENTS')) define('_ATTACHMENTS', 'Attachments');
-if (!defined('_ATTACHMENTS_COMMENT')) define('_ATTACHMENTS_COMMENT', 'Manage attachments');
-if (!defined('_ATTACH_ANSWER')) define('_ATTACH_ANSWER', 'Add an attachment');
-if (!defined('_ATTACH')) define('_ATTACH', 'Add an attachment');
-if (!defined('_PLEASE_SELECT_FILE')) define('_PLEASE_SELECT_FILE', 'Select a file');
-if (!defined('_NEW_ATTACH_ADDED')) define('_NEW_ATTACH_ADDED', 'New attachment');
-if (!defined('_ATTACH_DELETED')) define('_ATTACH_DELETED', 'Attachment deleted');
-if (!defined('_ATTACHED_DOC')) define('_ATTACHED_DOC', 'Attachments');
+if (!defined('_TITLE'))
+    define('_TITLE', 'Title');
+if (!defined('_ATTACHMENTS'))
+    define('_ATTACHMENTS', 'Attachments');
+if (!defined('_ATTACHMENTS_COMMENT'))
+    define('_ATTACHMENTS_COMMENT', 'Manage attachments');
+if (!defined('_ATTACH_ANSWER'))
+    define('_ATTACH_ANSWER', 'Add an attachment');
+if (!defined('_ATTACH'))
+    define('_ATTACH', 'Add an attachment');
+if (!defined('_PLEASE_SELECT_FILE'))
+    define('_PLEASE_SELECT_FILE', 'Select a file');
+if (!defined('_NEW_ATTACH_ADDED'))
+    define('_NEW_ATTACH_ADDED', 'New attachment');
+if (!defined('_ATTACH_DELETED'))
+    define('_ATTACH_DELETED', 'Attachment deleted');
+if (!defined('_ATTACHED_DOC'))
+    define('_ATTACHED_DOC', 'Attachments');
 
-if (!defined('_INDEX_ATTACHMENT'))  define('_INDEX_ATTACHMENT', 'Add an attachment');
-if (!defined('_INDEX_ATTACHMENT_DESC'))  define('_INDEX_ATTACHMENT_DESC', 'Add an attachment');
-if (!defined('_ATTACH_TO_DOC'))  define('_ATTACH_TO_DOC', 'Add as answer to an existing document');
-if (!defined('_ATTACH_TO_DOC_NUM'))  define('_ATTACH_TO_DOC_NUM', 'Attach to document number');
-
-if (!defined('_VIEW_ATTACHMENTS')) {
-	define('_VIEW_ATTACHMENTS', 'The attachments');
-}
-if (!defined('_VIEW_ATTACHMENTS_DESC')) {
-	define('_VIEW_ATTACHMENTS_DESC', 'View attachments');
-}
-if (!defined('_MANAGE_ATTACHMENTS'))  define('_MANAGE_ATTACHMENTS', 'Manage attachments');
-
-if (!defined('_GENERATE_OFFICE_ATTACHMENT'))  define('_GENERATE_OFFICE_ATTACHMENT', 'Generate Office Attachment');
-if (!defined('_GENERATE_HTML_ATTACHMENT'))  define('_GENERATE_HTML_ATTACHMENT', 'Generate HTML Attachment');
-if (!defined('_ATTACH_FROM_HDD'))  define('_ATTACH_FROM_HDD', 'Add an Attachment from your Hard Disk');
-if (!defined('_GENERATE_ATTACHMENT_FROM'))  define('_GENERATE_ATTACHMENT_FROM', 'Generate Attachment from a model with format');
+if (!defined('_INDEX_ATTACHMENT'))
+    define('_INDEX_ATTACHMENT', 'Add an attachment');
+if (!defined('_INDEX_ATTACHMENT_DESC'))
+    define('_INDEX_ATTACHMENT_DESC', 'Add an attachment');
+if (!defined('_ATTACH_TO_DOC'))
+    define('_ATTACH_TO_DOC', 'Add as answer to an existing document');
+if (!defined('_ATTACH_TO_DOC_NUM'))
+    define('_ATTACH_TO_DOC_NUM', 'Attach to document number');
+if (!defined('_VIEW_ATTACHMENTS'))
+    define('_VIEW_ATTACHMENTS', 'The attachments');
+if (!defined('_VIEW_ATTACHMENTS_DESC'))
+    define('_VIEW_ATTACHMENTS_DESC', 'View attachments');
+if (!defined('_MANAGE_ATTACHMENTS'))
+    define('_MANAGE_ATTACHMENTS', 'Manage attachments');
+if (!defined('_GENERATE_OFFICE_ATTACHMENT'))
+    define('_GENERATE_OFFICE_ATTACHMENT', 'Generate Office Attachment');
+if (!defined('_GENERATE_HTML_ATTACHMENT'))
+    define('_GENERATE_HTML_ATTACHMENT', 'Generate HTML Attachment');
+if (!defined('_ATTACH_FROM_HDD'))
+    define('_ATTACH_FROM_HDD', 'Add an Attachment from your Hard Disk');
+if (!defined('_GENERATE_ATTACHMENT_FROM'))
+    define('_GENERATE_ATTACHMENT_FROM', 'Generate Attachment from a model with format');
--- attachments/trunk/lang/fr.php
+++ attachments/trunk/lang/fr.php
@@ -18,30 +18,44 @@
  *   You should have received a copy of the GNU General Public License
  *   along with Maarch Framework.  If not, see <http://www.gnu.org/licenses/>.
  */
-if (!defined('_TITLE'))  define('_TITLE', 'Titre');
-if (!defined('_ATTACHMENTS'))  define('_ATTACHMENTS', 'Pi&egrave;ces jointes');
-if (!defined('_ATTACHMENTS_COMMENT'))  define('_ATTACHMENTS_COMMENT', 'Gestion des pi&egrave;ces jointes');
-if (!defined('_ATTACH_ANSWER'))  define('_ATTACH_ANSWER', 'Attacher une pi&egrave;ce jointe');
-if (!defined('_ATTACH'))  define('_ATTACH', 'Attacher PJ');
-if (!defined('_PLEASE_SELECT_FILE'))  define('_PLEASE_SELECT_FILE', 'Choisissez un fichier');
-if (!defined('_NEW_ATTACH_ADDED'))  define('_NEW_ATTACH_ADDED', 'Nouvelle pi&egrave;ce jointe');
-if (!defined('_ATTACH_DELETED'))  define('_ATTACH_DELETED', 'Pi&egrave;ce jointe supprim&eacute;e');
-if (!defined('_ATTACHED_DOC'))  define('_ATTACHED_DOC', 'Pi&egrave;ces jointes');
+if (!defined('_TITLE'))
+    define('_TITLE', 'Titre');
+if (!defined('_ATTACHMENTS'))
+    define('_ATTACHMENTS', 'Pi&egrave;ces jointes');
+if (!defined('_ATTACHMENTS_COMMENT'))
+    define('_ATTACHMENTS_COMMENT', 'Gestion des pi&egrave;ces jointes');
+if (!defined('_ATTACH_ANSWER'))
+    define('_ATTACH_ANSWER', 'Attacher une pi&egrave;ce jointe');
+if (!defined('_ATTACH'))
+    define('_ATTACH', 'Attacher PJ');
+if (!defined('_PLEASE_SELECT_FILE'))
+    define('_PLEASE_SELECT_FILE', 'Choisissez un fichier');
+if (!defined('_NEW_ATTACH_ADDED'))
+    define('_NEW_ATTACH_ADDED', 'Nouvelle pi&egrave;ce jointe');
+if (!defined('_ATTACH_DELETED'))
+    define('_ATTACH_DELETED', 'Pi&egrave;ce jointe supprim&eacute;e');
+if (!defined('_ATTACHED_DOC'))
+    define('_ATTACHED_DOC', 'Pi&egrave;ces jointes');
 
-if (!defined('_INDEX_ATTACHMENT'))  define('_INDEX_ATTACHMENT', 'Indexer une pi&egrave;ce jointe');
-if (!defined('_INDEX_ATTACHMENT_DESC'))  define('_INDEX_ATTACHMENT_DESC', 'Indexer une pi&egrave;ce jointe');
-if (!defined('_ATTACH_TO_DOC'))  define('_ATTACH_TO_DOC', 'Attacher en r&eacute;ponse &agrave; un courrier existant');
-if (!defined('_ATTACH_TO_DOC_NUM'))  define('_ATTACH_TO_DOC_NUM', 'attach&eacute; au document n&deg;');
-
-if (!defined('_VIEW_ATTACHMENTS')) {
-	define('_VIEW_ATTACHMENTS', 'Voir les pi&eagrave;ces jointes');
-}
-if (!defined('_VIEW_ATTACHMENTS_DESC')) {
-	define('_VIEW_ATTACHMENTS_DESC', 'Voir les pi&eagrave;ces jointes');
-}
-if (!defined('_MANAGE_ATTACHMENTS'))  define('_MANAGE_ATTACHMENTS', 'G&eacute;rer les pi&eagrave;ces jointes');
-
-if (!defined('_GENERATE_OFFICE_ATTACHMENT'))  define('_GENERATE_OFFICE_ATTACHMENT', 'G&eacute;n&eacute;rer OFFICE');
-if (!defined('_GENERATE_HTML_ATTACHMENT'))  define('_GENERATE_HTML_ATTACHMENT', 'G&eacute;n&eacute;rer HTML');
-if (!defined('_ATTACH_FROM_HDD'))  define('_ATTACH_FROM_HDD', 'Ajouter une pi&egrave;ce jointe / r&eacute;ponse depuis votre disque dur');
-if (!defined('_GENERATE_ATTACHMENT_FROM'))  define('_GENERATE_ATTACHMENT_FROM', 'G&eacute;n&eacute;rer une pi&egrave;ce jointe / r&eacute;ponse depuis un mod&egrave;le au format');
+if (!defined('_INDEX_ATTACHMENT'))
+    define('_INDEX_ATTACHMENT', 'Indexer une pi&egrave;ce jointe');
+if (!defined('_INDEX_ATTACHMENT_DESC'))
+    define('_INDEX_ATTACHMENT_DESC', 'Indexer une pi&egrave;ce jointe');
+if (!defined('_ATTACH_TO_DOC'))
+    define('_ATTACH_TO_DOC', 'Attacher en r&eacute;ponse &agrave; un courrier existant');
+if (!defined('_ATTACH_TO_DOC_NUM'))
+    define('_ATTACH_TO_DOC_NUM', 'attach&eacute; au document n&deg;');
+if (!defined('_VIEW_ATTACHMENTS'))
+    define('_VIEW_ATTACHMENTS', 'Voir les pi&eagrave;ces jointes');
+if (!defined('_VIEW_ATTACHMENTS_DESC'))
+    define('_VIEW_ATTACHMENTS_DESC', 'Voir les pi&eagrave;ces jointes');
+if (!defined('_MANAGE_ATTACHMENTS'))
+    define('_MANAGE_ATTACHMENTS', 'G&eacute;rer les pi&eagrave;ces jointes');
+if (!defined('_GENERATE_OFFICE_ATTACHMENT'))
+    define('_GENERATE_OFFICE_ATTACHMENT', 'G&eacute;n&eacute;rer OFFICE');
+if (!defined('_GENERATE_HTML_ATTACHMENT'))
+    define('_GENERATE_HTML_ATTACHMENT', 'G&eacute;n&eacute;rer HTML');
+if (!defined('_ATTACH_FROM_HDD'))
+    define('_ATTACH_FROM_HDD', 'Ajouter une pi&egrave;ce jointe / r&eacute;ponse depuis votre disque dur');
+if (!defined('_GENERATE_ATTACHMENT_FROM'))
+    define('_GENERATE_ATTACHMENT_FROM', 'G&eacute;n&eacute;rer une pi&egrave;ce jointe / r&eacute;ponse depuis un mod&egrave;le au format');
