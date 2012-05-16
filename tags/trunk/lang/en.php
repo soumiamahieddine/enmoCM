bf8243cbed38ffd3493a8379b810fa396186df8f
Laurent Giovannoni <laurent.giovannoni@maarch.org>
2012-05-16 13:50:48 +0000

    fix: lang

--- tags/trunk/lang/en.php
+++ tags/trunk/lang/en.php
@@ -20,7 +20,7 @@
 
 /**
 * Module : Tags
-* 
+*
 * This module is used to store ressources with any keywords
 * V: 1.0
 *
@@ -30,57 +30,82 @@
 * @version $Revision$
 */
 /*********************** TAGS ***********************************/
-if (!defined('_TAG_DEFAULT'))  define('_TAG_DEFAULT', 'Tag');
-if (!defined('_TAGS_DEFAULT'))  define('_TAGS_DEFAULT', 'Tags');
-if (!defined('_TAGS_COMMENT'))  define('_TAGS_COMMENT', 'Tags');
-if (!defined('_TAGS_COMMENT'))  define('_TAGS_COMMENT', 'Tags');
-
-
-if (!defined('_TAGS_LIST'))  define('_TAGS_LIST', 'Tags list');
-if (!defined('_MODIFY_TAG'))  define('_MODIFY_TAG', 'Update this Tag');
-if (!defined('_MANAGE_TAGS'))  define('_MANAGE_TAGS', 'Manage Tags');
-if (!defined('_ADMIN_TAGS'))  define('_ADMIN_TAGS', 'Tags Administration');
-if (!defined('_ADMIN_TAGS_DESC'))  define('_ADMIN_TAGS_DESC', 'Update, delete, add or join Tags');
-if (!defined('_ALL_TAGS'))  define('_ALL_TAGS', 'Tous les '._TAG_DEFAULT);
-if (!defined('_TAG_DELETED'))  define('_TAG_DELETED', _TAG_DEFAULT.' deleted');
-if (!defined('_TAG_ADDED'))  define('_TAG_ADDED', _TAG_DEFAULT.' added');
-if (!defined('_TAG_UPDATED'))  define('_TAG_UPDATED', _TAG_DEFAULT.' updated');
-if (!defined('_TAG_LABEL_IS_EMPTY'))  define('_TAG_LABEL_IS_EMPTY', 'Label is empty');
-
-
- 
-
-if (!defined('_NO_TAG'))  define('_NO_TAG', 'No '._TAG_DEFAULT);
-
-if (!defined('_TAG_VIEW'))  define('_TAG_VIEW', _TAGS_DEFAULT.' view');
-if (!defined('_TAG_VIEW_DESC'))  define('_TAG_VIEW_DESC', 'View '._TAGS_DEFAULT);
-if (!defined('_ADD_TAG'))  define('_ADD_TAG', 'Add a '._TAG_DEFAULT);
-if (!defined('_ADD_TAG_TO_RES'))  define('_ADD_TAG_TO_RES', 'Add '._TAGS_DEFAULT.' for a ressource');
-if (!defined('_ADD_TAG_TO_RES_DESC'))  define('_ADD_TAG_TO_RES_DESC', 'Add '._TAGS_DEFAULT.' for a ressource');
-
-if (!defined('_DELETE_TAG_TO_RES'))  define('_DELETE_TAG_TO_RES', 'Delete '._TAGS_DEFAULT.' for a ressource');
-if (!defined('_DELETE_TAG_TO_RES_DESC'))  define('_DELETE_TAG_TO_RES_DESC', 'Delete '._TAGS_DEFAULT.' for a ressource');
-
-if (!defined('_NEW_TAG_IN_LIBRARY_RIGHTS'))  define('_NEW_TAG_IN_LIBRARY_RIGHTS', 'Create new '._TAGS_DEFAULT.' in Maarch');
-if (!defined('_NEW_TAG_IN_LIBRARY_RIGHTS_DESC'))  define('_NEW_TAG_IN_LIBRARY_RIGHTS_DESC', 'Create new '._TAGS_DEFAULT.' in Maarch');
-if (!defined('_TAG'))  define('_TAG', _TAG_DEFAULT);
-if (!defined('_TAGS'))  define('_TAGS', _TAGS_DEFAULT);
-
-if (!defined('_TAG_SEPARATOR_HELP'))  define('_TAG_SEPARATOR_HELP', '	Separate tags by Enter Key or , key');
-
-if (!defined('_NB_DOCS_FOR_THIS_TAG'))  define('_NB_DOCS_FOR_THIS_TAG', 'Number for this tag');
-if (!defined('_TAG_FUSION_ACTIONLABEL'))  define('_NB_DOCS_FOR_THIS_TAG', 'join this tag with');
-
-if (!defined('_TAGOTHER_OPTIONS'))  define('_TAGOTHER_OPTIONS', 'Other');
-if (!defined('_TAG_FUSION_ACTIONLABEL'))  define('_TAG_FUSION_ACTIONLABEL', 'Join this tag with');
-if (!defined('_TAGFUSION'))  define('_TAGFUSION', 'Fusion');
-if (!defined('_TAGFUSION_GOODRESULT'))  define('_TAGFUSION_GOODRESULT', 'this tags are joined');
-
-if (!defined('_TAG_ALREADY_EXISTS'))  define('_TAG_ALREADY_EXISTS', 'this tag already exists');
-
-if (!defined('_CHOOSE_TAG'))  define('_CHOOSE_TAG', 'Choose tags');
-if (!defined('_TAG_SEARCH'))  define('_TAG_SEARCH', 'Tags');
-if (!defined('_TAGNONE'))  define('_TAGNONE', 'None');
-if (!defined('_ALL_TAG_DELETED_FOR_RES_ID'))  define('_ALL_TAG_DELETED_FOR_RES_ID', 'All tags are deleted for res_id');
-if (!defined('_TAGCLICKTODEL'))  define('_TAGCLICKTODEL', 'delete');
+if (!defined('_TAG_DEFAULT'))
+    define('_TAG_DEFAULT', 'Tag');
+if (!defined('_TAGS_DEFAULT'))
+    define('_TAGS_DEFAULT', 'Tags');
+if (!defined('_TAGS_COMMENT'))
+    define('_TAGS_COMMENT', 'Tags');
+if (!defined('_TAGS_COMMENT'))
+    define('_TAGS_COMMENT', 'Tags');
+if (!defined('_TAGS_LIST'))
+    define('_TAGS_LIST', 'Tags list');
+if (!defined('_MODIFY_TAG'))
+    define('_MODIFY_TAG', 'Update this Tag');
+if (!defined('_MANAGE_TAGS'))
+    define('_MANAGE_TAGS', 'Manage Tags');
+if (!defined('_ADMIN_TAGS'))
+    define('_ADMIN_TAGS', 'Tags Administration');
+if (!defined('_ADMIN_TAGS_DESC'))
+    define('_ADMIN_TAGS_DESC', 'Update, delete, add or join Tags');
+if (!defined('_ALL_TAGS'))
+    define('_ALL_TAGS', 'Tous les '._TAG_DEFAULT);
+if (!defined('_TAG_DELETED'))
+    define('_TAG_DELETED', _TAG_DEFAULT.' deleted');
+if (!defined('_TAG_ADDED'))
+    define('_TAG_ADDED', _TAG_DEFAULT.' added');
+if (!defined('_TAG_UPDATED'))
+    define('_TAG_UPDATED', _TAG_DEFAULT.' updated');
+if (!defined('_TAG_LABEL_IS_EMPTY'))
+    define('_TAG_LABEL_IS_EMPTY', 'Label is empty');
+if (!defined('_NO_TAG'))
+    define('_NO_TAG', 'No '._TAG_DEFAULT);
+if (!defined('_TAG_VIEW'))
+    define('_TAG_VIEW', _TAGS_DEFAULT.' view');
+if (!defined('_TAG_VIEW_DESC'))
+    define('_TAG_VIEW_DESC', 'View '._TAGS_DEFAULT);
+if (!defined('_ADD_TAG'))
+    define('_ADD_TAG', 'Add a '._TAG_DEFAULT);
+if (!defined('_ADD_TAG_TO_RES'))
+    define('_ADD_TAG_TO_RES', 'Add '._TAGS_DEFAULT.' for a ressource');
+if (!defined('_ADD_TAG_TO_RES_DESC'))
+    define('_ADD_TAG_TO_RES_DESC', 'Add '._TAGS_DEFAULT.' for a ressource');
+if (!defined('_DELETE_TAG_TO_RES'))
+    define('_DELETE_TAG_TO_RES', 'Delete '._TAGS_DEFAULT.' for a ressource');
+if (!defined('_DELETE_TAG_TO_RES_DESC'))
+    define('_DELETE_TAG_TO_RES_DESC', 'Delete '._TAGS_DEFAULT.' for a ressource');
+if (!defined('_NEW_TAG_IN_LIBRARY_RIGHTS'))
+    define('_NEW_TAG_IN_LIBRARY_RIGHTS', 'Create new '._TAGS_DEFAULT.' in Maarch');
+if (!defined('_NEW_TAG_IN_LIBRARY_RIGHTS_DESC'))
+    define('_NEW_TAG_IN_LIBRARY_RIGHTS_DESC', 'Create new '._TAGS_DEFAULT.' in Maarch');
+if (!defined('_TAG'))
+    define('_TAG', _TAG_DEFAULT);
+if (!defined('_TAGS'))
+    define('_TAGS', _TAGS_DEFAULT);
+if (!defined('_TAG_SEPARATOR_HELP'))
+    define('_TAG_SEPARATOR_HELP', '   Separate tags by Enter Key or , key');
+if (!defined('_NB_DOCS_FOR_THIS_TAG'))
+    define('_NB_DOCS_FOR_THIS_TAG', 'Number for this tag');
+if (!defined('_TAG_FUSION_ACTIONLABEL'))
+    define('_NB_DOCS_FOR_THIS_TAG', 'join this tag with');
+if (!defined('_TAGOTHER_OPTIONS'))
+    define('_TAGOTHER_OPTIONS', 'Other');
+if (!defined('_TAG_FUSION_ACTIONLABEL'))
+    define('_TAG_FUSION_ACTIONLABEL', 'Join this tag with');
+if (!defined('_TAGFUSION'))
+    define('_TAGFUSION', 'Fusion');
+if (!defined('_TAGFUSION_GOODRESULT'))
+    define('_TAGFUSION_GOODRESULT', 'this tags are joined');
+if (!defined('_TAG_ALREADY_EXISTS'))
+    define('_TAG_ALREADY_EXISTS', 'this tag already exists');
+if (!defined('_CHOOSE_TAG'))
+    define('_CHOOSE_TAG', 'Choose tags');
+if (!defined('_TAG_SEARCH'))
+    define('_TAG_SEARCH', 'Tags');
+if (!defined('_TAGNONE'))
+    define('_TAGNONE', 'None');
+if (!defined('_ALL_TAG_DELETED_FOR_RES_ID'))
+    define('_ALL_TAG_DELETED_FOR_RES_ID', 'All tags are deleted for res_id');
+if (!defined('_TAGCLICKTODEL'))
+    define('_TAGCLICKTODEL', 'delete');
 ?>
--- tags/trunk/lang/fr.php
+++ tags/trunk/lang/fr.php
@@ -20,7 +20,7 @@
 
 /**
 * Module : Tags
-* 
+*
 * This module is used to store ressources with any keywords
 * V: 1.0
 *
@@ -31,57 +31,80 @@
 */
 
 /*********************** TAGS ***********************************/
-if (!defined('_TAG_DEFAULT'))  define('_TAG_DEFAULT', 'Mot-cl&eacute;');
-if (!defined('_TAGS_DEFAULT'))  define('_TAGS_DEFAULT', 'Mots-cl&eacute;s');
-if (!defined('_TAGS_COMMENT'))  define('_TAGS_COMMENT', 'Mots-cl&eacute;s');
-
-
-if (!defined('_TAGS_LIST'))  define('_TAGS_LIST', 'Liste des Mot-cl&eacute;');
-if (!defined('_MODIFY_TAG'))  define('_MODIFY_TAG', 'Modifier le Mot-cl&eacute;');
-if (!defined('_MANAGE_TAGS'))  define('_MANAGE_TAGS', 'G&eacute;rer les Mots-cl&eacute;s');
-if (!defined('_ADMIN_TAGS'))  define('_ADMIN_TAGS', 'Administration des Mots-cl&eacute;s');
-if (!defined('_ADMIN_TAGS_DESC'))  define('_ADMIN_TAGS_DESC', 'Permet de modifier, supprimer, ajouter ou fusionner des Mots-cl&eacute;s');
-if (!defined('_ALL_TAGS'))  define('_ALL_TAGS', 'Tous les '._TAG_DEFAULT);
-if (!defined('_TAG_DELETED'))  define('_TAG_DELETED', _TAG_DEFAULT.' supprim&eacute;');
-if (!defined('_TAG_ADDED'))  define('_TAG_ADDED', _TAG_DEFAULT.' ajout&eacute;');
-if (!defined('_TAG_UPDATED'))  define('_TAG_UPDATED', _TAG_DEFAULT.' modifi&eacute;');
-if (!defined('_TAG_LABEL_IS_EMPTY'))  define('_TAG_LABEL_IS_EMPTY', 'Le libell&eacute; est vide');
-
-
- 
-
-if (!defined('_NO_TAG'))  define('_NO_TAG', 'Pas de '._TAG_DEFAULT);
-
-if (!defined('_TAG_VIEW'))  define('_TAG_VIEW', 'Visualisation des '._TAGS_DEFAULT);
-if (!defined('_TAG_VIEW_DESC'))  define('_TAG_VIEW_DESC', 'Permet de visualiser les '._TAGS_DEFAULT);
-if (!defined('_ADD_TAG'))  define('_ADD_TAG', 'Ajouter un '._TAG_DEFAULT);
-if (!defined('_ADD_TAG_TO_RES'))  define('_ADD_TAG_TO_RES', 'Ajouter des '._TAGS_DEFAULT.' &agrave; une ressource');
-if (!defined('_ADD_TAG_TO_RES_DESC'))  define('_ADD_TAG_TO_RES_DESC', 'Permet d&rsquo;ajouter des '._TAGS_DEFAULT.' &agrave; une ressource');
-
-if (!defined('_DELETE_TAG_TO_RES'))  define('_DELETE_TAG_TO_RES', 'Supprimer des '._TAGS_DEFAULT.' &agrave; une ressource');
-if (!defined('_DELETE_TAG_TO_RES_DESC'))  define('_DELETE_TAG_TO_RES_DESC', 'Permet de supprimer des '._TAGS_DEFAULT.' &agrave; une ressource');
-
-if (!defined('_NEW_TAG_IN_LIBRARY_RIGHTS'))  define('_NEW_TAG_IN_LIBRARY_RIGHTS', 'Cr&eacute;er des nouveaux '._TAGS_DEFAULT.' dans la librairie Maarch');
-if (!defined('_NEW_TAG_IN_LIBRARY_RIGHTS_DESC'))  define('_NEW_TAG_IN_LIBRARY_RIGHTS_DESC', 'En activant ce tag, l&rsquo;utilistateur pourra ajouter de nouveaux '._TAGS_DEFAULT.' dans la librairie de Maarch');
-if (!defined('_TAG'))  define('_TAG', _TAG_DEFAULT);
-if (!defined('_TAGS'))  define('_TAGS', _TAGS_DEFAULT);
-
-if (!defined('_TAG_SEPARATOR_HELP'))  define('_TAG_SEPARATOR_HELP', 'S&eacute;parez les mots-cl&eacute;s en appuyant sur Entrée ou avec des virgules');
-
-if (!defined('_NB_DOCS_FOR_THIS_TAG'))  define('_NB_DOCS_FOR_THIS_TAG', 'Nbre de documents taggu&eacute;s');
-if (!defined('_TAG_FUSION_ACTIONLABEL'))  define('_NB_DOCS_FOR_THIS_TAG', 'Fusionner le tag avec');
-
-if (!defined('_TAGOTHER_OPTIONS'))  define('_TAGOTHER_OPTIONS', 'Autres options');
-if (!defined('_TAG_FUSION_ACTIONLABEL'))  define('_TAG_FUSION_ACTIONLABEL', 'Fusionner le tag avec');
-if (!defined('_TAGFUSION'))  define('_TAGFUSION', 'Fusion');
-if (!defined('_TAGFUSION_GOODRESULT'))  define('_TAGFUSION_GOODRESULT', 'Ces tags sont maintenant fusionn&eacute;s');
-
-if (!defined('_TAG_ALREADY_EXISTS'))  define('_TAG_ALREADY_EXISTS', 'Ce tag existe d&eacute;j&agrave;');
-
-if (!defined('_CHOOSE_TAG'))  define('_CHOOSE_TAG', 'Choix des mots-cl&eacute;s');
-if (!defined('_TAG_SEARCH'))  define('_TAG_SEARCH', 'Mots-cl&eacute;s');
-if (!defined('_TAGNONE'))  define('_TAGNONE', 'Aucun');
-if (!defined('_ALL_TAG_DELETED_FOR_RES_ID'))  define('_ALL_TAG_DELETED_FOR_RES_ID', 'Tous les tags sont supprim&eacute; pour la ressource');
-if (!defined('_TAGCLICKTODEL'))  define('_TAGCLICKTODEL', 'supprimer');
-
+if (!defined('_TAG_DEFAULT'))
+    define('_TAG_DEFAULT', 'Mot-cl&eacute;');
+if (!defined('_TAGS_DEFAULT'))
+    define('_TAGS_DEFAULT', 'Mots-cl&eacute;s');
+if (!defined('_TAGS_COMMENT'))
+    define('_TAGS_COMMENT', 'Mots-cl&eacute;s');
+if (!defined('_TAGS_LIST'))
+    define('_TAGS_LIST', 'Liste des Mot-cl&eacute;');
+if (!defined('_MODIFY_TAG'))
+    define('_MODIFY_TAG', 'Modifier le Mot-cl&eacute;');
+if (!defined('_MANAGE_TAGS'))
+    define('_MANAGE_TAGS', 'G&eacute;rer les Mots-cl&eacute;s');
+if (!defined('_ADMIN_TAGS'))
+    define('_ADMIN_TAGS', 'Administration des Mots-cl&eacute;s');
+if (!defined('_ADMIN_TAGS_DESC'))
+    define('_ADMIN_TAGS_DESC', 'Permet de modifier, supprimer, ajouter ou fusionner des Mots-cl&eacute;s');
+if (!defined('_ALL_TAGS'))
+    define('_ALL_TAGS', 'Tous les '._TAG_DEFAULT);
+if (!defined('_TAG_DELETED'))
+    define('_TAG_DELETED', _TAG_DEFAULT.' supprim&eacute;');
+if (!defined('_TAG_ADDED'))
+    define('_TAG_ADDED', _TAG_DEFAULT.' ajout&eacute;');
+if (!defined('_TAG_UPDATED'))
+    define('_TAG_UPDATED', _TAG_DEFAULT.' modifi&eacute;');
+if (!defined('_TAG_LABEL_IS_EMPTY'))
+    define('_TAG_LABEL_IS_EMPTY', 'Le libell&eacute; est vide');
+if (!defined('_NO_TAG'))
+    define('_NO_TAG', 'Pas de '._TAG_DEFAULT);
+if (!defined('_TAG_VIEW'))
+    define('_TAG_VIEW', 'Visualisation des '._TAGS_DEFAULT);
+if (!defined('_TAG_VIEW_DESC'))
+    define('_TAG_VIEW_DESC', 'Permet de visualiser les '._TAGS_DEFAULT);
+if (!defined('_ADD_TAG'))
+    define('_ADD_TAG', 'Ajouter un '._TAG_DEFAULT);
+if (!defined('_ADD_TAG_TO_RES'))
+    define('_ADD_TAG_TO_RES', 'Ajouter des '._TAGS_DEFAULT.' &agrave; une ressource');
+if (!defined('_ADD_TAG_TO_RES_DESC'))
+    define('_ADD_TAG_TO_RES_DESC', 'Permet d&rsquo;ajouter des '._TAGS_DEFAULT.' &agrave; une ressource');
+if (!defined('_DELETE_TAG_TO_RES'))
+    define('_DELETE_TAG_TO_RES', 'Supprimer des '._TAGS_DEFAULT.' &agrave; une ressource');
+if (!defined('_DELETE_TAG_TO_RES_DESC'))
+    define('_DELETE_TAG_TO_RES_DESC', 'Permet de supprimer des '._TAGS_DEFAULT.' &agrave; une ressource');
+if (!defined('_NEW_TAG_IN_LIBRARY_RIGHTS'))
+    define('_NEW_TAG_IN_LIBRARY_RIGHTS', 'Cr&eacute;er des nouveaux '._TAGS_DEFAULT.' dans la librairie Maarch');
+if (!defined('_NEW_TAG_IN_LIBRARY_RIGHTS_DESC'))
+    define('_NEW_TAG_IN_LIBRARY_RIGHTS_DESC', 'En activant ce tag, l&rsquo;utilistateur pourra ajouter de nouveaux '._TAGS_DEFAULT.' dans la librairie de Maarch');
+if (!defined('_TAG'))
+    define('_TAG', _TAG_DEFAULT);
+if (!defined('_TAGS'))
+    define('_TAGS', _TAGS_DEFAULT);
+if (!defined('_TAG_SEPARATOR_HELP'))
+    define('_TAG_SEPARATOR_HELP', 'S&eacute;parez les mots-cl&eacute;s en appuyant sur Entrée ou avec des virgules');
+if (!defined('_NB_DOCS_FOR_THIS_TAG'))
+    define('_NB_DOCS_FOR_THIS_TAG', 'Nbre de documents taggu&eacute;s');
+if (!defined('_TAG_FUSION_ACTIONLABEL'))
+    define('_NB_DOCS_FOR_THIS_TAG', 'Fusionner le tag avec');
+if (!defined('_TAGOTHER_OPTIONS'))
+    define('_TAGOTHER_OPTIONS', 'Autres options');
+if (!defined('_TAG_FUSION_ACTIONLABEL'))
+    define('_TAG_FUSION_ACTIONLABEL', 'Fusionner le tag avec');
+if (!defined('_TAGFUSION'))
+    define('_TAGFUSION', 'Fusion');
+if (!defined('_TAGFUSION_GOODRESULT'))
+    define('_TAGFUSION_GOODRESULT', 'Ces tags sont maintenant fusionn&eacute;s');
+if (!defined('_TAG_ALREADY_EXISTS'))
+    define('_TAG_ALREADY_EXISTS', 'Ce tag existe d&eacute;j&agrave;');
+if (!defined('_CHOOSE_TAG'))
+    define('_CHOOSE_TAG', 'Choix des mots-cl&eacute;s');
+if (!defined('_TAG_SEARCH'))
+    define('_TAG_SEARCH', 'Mots-cl&eacute;s');
+if (!defined('_TAGNONE'))
+    define('_TAGNONE', 'Aucun');
+if (!defined('_ALL_TAG_DELETED_FOR_RES_ID'))
+    define('_ALL_TAG_DELETED_FOR_RES_ID', 'Tous les tags sont supprim&eacute; pour la ressource');
+if (!defined('_TAGCLICKTODEL'))
+    define('_TAGCLICKTODEL', 'supprimer');
 ?>
