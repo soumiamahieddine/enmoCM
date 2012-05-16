48feaead61f0a71842a814b3c408328cc8db231b
Laurent Giovannoni <laurent.giovannoni@maarch.org>
2012-05-16 08:50:50 +0000

    fix: cases with quotes

--- cases/trunk/create_case.php
+++ cases/trunk/create_case.php
@@ -27,64 +27,64 @@ $db = new dbquery();
 $status_obj = new manage_status();
 if (($core_tools->test_service('join_res_case', 'cases', false) == 1) || ($core_tools->test_service('join_res_case_in_process', 'cases', false) == 1))
 {
-	$case_label = $_POST['case_label'];
-	$case_description = $_POST['case_description'];
-	$case_description = str_replace("\n"," ",$case_description);
-	$case_description = str_replace("\r","",$case_description);
-	$actual_res_id = $_POST['searched_value'];
-	if($case_label <> '' && $actual_res_id <> '')
-	{
-		if (!$cases->create_case($actual_res_id, $case_label, $case_description))
-		{
-			echo 'CASES ATTACHEMENT ERROR';	
-		}
-		else
-		{
-			if($_POST['searched_item'] == 'res_id_in_process')
-			{
-				$case_redemption = new cases();
-				$case_id_newest = $case_redemption->get_case_id($actual_res_id);
-				?>
-				<script type="text/javascript">
-				var case_id = window.opener.$('case_id');
-				var case_label = window.opener.$('case_label');
-				var case_description = window.opener.$('case_description');
-				if(case_id)
-				{
-					case_id.value = '<?php echo $case_id_newest ;?>';
-					case_label.value = '<?php echo $case_label ;?>';
-					case_description.value = '<?php echo $case_description ;?>';
-				}
-				self.close();
-				</script>
-				<?php
-			}
-			else
-			{	
-				$error = _CASE_CREATED;
-				?>
-				<script type="text/javascript">
-				window.opener.top.location.reload();
-				/*var error_div = window.opener.$('main_error');
-				if(error_div)
-				{
-					error_div.update('<?php echo $error ;?>');
-				}*/
-				self.close();
-				</script>
-				<?php
-			}
-		}
-	}
-	else
-	{
-		$_SESSION['cases_error'] = _LABEL_MANDATORY;
-		?>
-		<script type="text/javascript">
-			window.history.back();
-		</script>
-		<?php
-	}
+    $case_label = $_POST['case_label'];
+    $case_description = $_POST['case_description'];
+    $case_description = str_replace("\n"," ",$case_description);
+    $case_description = str_replace("\r","",$case_description);
+    $actual_res_id = $_POST['searched_value'];
+    if($case_label <> '' && $actual_res_id <> '')
+    {
+        if (!$cases->create_case($actual_res_id, $case_label, $case_description))
+        {
+            echo 'CASES ATTACHEMENT ERROR'; 
+        }
+        else
+        {
+            if($_POST['searched_item'] == 'res_id_in_process')
+            {
+                $case_redemption = new cases();
+                $case_id_newest = $case_redemption->get_case_id($actual_res_id);
+                ?>
+                <script type="text/javascript">
+                var case_id = window.opener.$('case_id');
+                var case_label = window.opener.$('case_label');
+                var case_description = window.opener.$('case_description');
+                if(case_id)
+                {
+                    case_id.value = '<?php echo $case_id_newest ;?>';
+                    case_label.value = '<?php echo str_replace('\'', ' ', $case_label);?>';
+                    case_description.value = '<?php echo str_replace('\'', ' ', $case_description);?>';
+                }
+                self.close();
+                </script>
+                <?php
+            }
+            else
+            {   
+                $error = _CASE_CREATED;
+                ?>
+                <script type="text/javascript">
+                window.opener.top.location.reload();
+                /*var error_div = window.opener.$('main_error');
+                if(error_div)
+                {
+                    error_div.update('<?php echo $error ;?>');
+                }*/
+                self.close();
+                </script>
+                <?php
+            }
+        }
+    }
+    else
+    {
+        $_SESSION['cases_error'] = _LABEL_MANDATORY;
+        ?>
+        <script type="text/javascript">
+            window.history.back();
+        </script>
+        <?php
+    }
 }
 $core_tools->load_js();
 ?>
