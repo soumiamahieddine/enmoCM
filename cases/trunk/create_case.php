d18a637d7f33c3d53b48459c349de860f556b617
Cyril Vazquez <cyril.vazquez@maarch.org>
2012-05-16 08:32:01 +0000

    Fix: Quotes doubled in label and description

--- cases/trunk/create_case.php
+++ cases/trunk/create_case.php
@@ -27,11 +27,11 @@ $db = new dbquery();
 $status_obj = new manage_status();
 if (($core_tools->test_service('join_res_case', 'cases', false) == 1) || ($core_tools->test_service('join_res_case_in_process', 'cases', false) == 1))
 {
-	$case_label = $db->protect_string_db($_POST['case_label']);
-	$case_description = $db->protect_string_db($_POST['case_description']);
+	$case_label = $_POST['case_label'];
+	$case_description = $_POST['case_description'];
 	$case_description = str_replace("\n"," ",$case_description);
 	$case_description = str_replace("\r","",$case_description);
-	$actual_res_id = $db->protect_string_db($_POST['searched_value']);
+	$actual_res_id = $_POST['searched_value'];
 	if($case_label <> '' && $actual_res_id <> '')
 	{
 		if (!$cases->create_case($actual_res_id, $case_label, $case_description))
