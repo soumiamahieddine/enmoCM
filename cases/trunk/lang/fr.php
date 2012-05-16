d2188064d31b9133f3c1d162dfa90b1942ba6aad
Laurent Giovannoni <laurent.giovannoni@maarch.org>
2012-05-16 12:58:33 +0000

    fix: lang

--- cases/trunk/lang/fr.php
+++ cases/trunk/lang/fr.php
@@ -20,58 +20,112 @@
  */
 
 /*********************** CASES ***********************************/
-if (!defined('_CASES_COMMENT'))  define('_CASES_COMMENT', 'Affaires');
-if (!defined('_CASE_INFO'))  define('_CASE_INFO', 'Informations sur l&rsquo;affaire');
-if (!defined('_JOIN_RES_CASE'))  define('_JOIN_RES_CASE','Attacher des courriers aux affaires');
-if (!defined('_CASE_NUM'))  define('_CASE_NUM', 'Num Affaire');
-if (!defined('_NUM_CASE'))  define('_NUM_CASE', 'Affaire num&eacute;ro');
-if (!defined('_CASE_DETAIL'))  define('_CASE_DETAIL', 'Fiche d&eacute;taill&eacute;e de l&rsquo;affaire');
-if (!defined('_DETAILS_CASES'))  define('_DETAILS_CASES', 'Fiche d&eacute;taill&eacute;e de l&rsquo;affaire');
-if (!defined('_INDEX_CASES'))  define('_INDEX_CASES', 'Index de l&rsquo;affaire');
-if (!defined('_DOCUMENTS_LIST_IN_THIS_CASE'))  define('_DOCUMENTS_LIST_IN_THIS_CASE', 'Liste des documents de l&rsquo;affaire');
-if (!defined('_NOTES_FOR_THIS_CASES'))  define('_NOTES_FOR_THIS_CASES', 'Annotations de cette affaire');
-if (!defined('_HISTORY_CASES'))  define('_HISTORY_CASES', 'Historique de l&rsquo;affaire');
-if (!defined('_CASE_CLOSED'))  define('_CASE_CLOSED', 'Affaire cl&ocirc;tur&eacute;e');
-if (!defined('_CASE_NUMBER'))  define('_CASE_NUMBER', 'Num&eacute;ro de l&rsquo;affaire');
-if (!defined('_CASE_NUMBER_HELP'))  define('_CASE_NUMBER_HELP', 'Recherche sur une affaire comprenant plusieurs courriers...');
-if (!defined('_N_CASE'))  define('_N_CASE', 'num&eacute;ro de l&rsquo;affaire');
-if (!defined('_CANT_SHOW_THIS_CASE'))  define('_CANT_SHOW_THIS_CASE', 'Impossible d\'afficher cette affaire');
-if (!defined('_CASE_ID'))  define('_CASE_ID','Identifiant de l&rsquo;affaire');
-if (!defined('_CASE_LABEL'))  define('_CASE_LABEL','Libell&eacute; de l&rsquo;affaire');
-if (!defined('_CASE_DESCRIPTION'))  define('_CASE_DESCRIPTION','Description de l&rsquo;affaire');
-if (!defined('_CASE_TYPIST'))  define('_CASE_TYPIST','Cr&eacute;ateur de l&rsquo;affaire');
-if (!defined('_CASE_CREATION_DATE'))  define('_CASE_CREATION_DATE','Date de cr&eacute;ation');
-if (!defined('_CASE_LAST_UPDATE_DATE'))  define('_CASE_LAST_UPDATE_DATE','Derni&egrave;re mise &agrave; jour');
-if (!defined('_CASE_CLOSING_DATE'))  define('_CASE_CLOSING_DATE','Date de cl&ocirc;ture');
-if (!defined('_CASES_INDEXES'))  define('_CASES_INDEXES','Index de l&rsquo;affaire');
-if (!defined('_RESSOURCES_REPORTS'))  define('_RESSOURCES_REPORTS','Contenu de l&rsquo;affaire');
-if (!defined('_UPDATE_CASE'))  define('_UPDATE_CASE','Mise &agrave; jour d&rsquo;affaires');
-if (!defined('_NEW_CASE'))  define('_NEW_CASE','Nouvelle affaire');
-if (!defined('_RES_ATTACH_ON_CASE'))  define('_RES_ATTACH_ON_CASE','Document attach&eacute; &agrave; l&rsquo;affaire');
-if (!defined('_RES_DETTACH_ON_CASE'))  define('_RES_DETTACH_ON_CASE','Document dettach&eacute; &agrave; l&rsquo;affaire');
-if (!defined('_JOIN_CASE'))  define('_JOIN_CASE','Attacher &agrave; une affaire');
-if (!defined('_MODIFY_CASE'))  define('_MODIFY_CASE','Attacher &agrave; une autre affaire');
-if (!defined('_JOIN_RES_ON_THIS_CASE'))  define('_JOIN_RES_ON_THIS_CASE','Attacher un document &agrave; l&rsquo;affaire');
-if (!defined('_RESSOURCES_LINKED'))  define('_RESSOURCES_LINKED','Document attach&eacute;');
-if (!defined('_RESSOURCES_NOT_LINKED'))  define('_RESSOURCES_NOT_LINKED','Ce document est d&eacute;j&agrave; attach&eacute;');
-if (!defined('_CASE'))  define('_CASE','Affaire');
-if (!defined('_SEARCH_A_CASE'))  define('_SEARCH_A_CASE','Rechercher une affaire');
-if (!defined('_CLOSED_CASE'))  define('_CLOSED_CASE','Cl&ocirc;tur&eacute;e');
-if (!defined('_SEARCH_A_RES'))  define('_SEARCH_A_RES','Rechercher un document');
-if (!defined('_CREATE_NEW_CASE'))  define('_CREATE_NEW_CASE','Cr&eacute;er une nouvelle affaire');
-if (!defined('_ADD_CASES'))  define('_ADD_CASES','Cr&eacute;er des affaires');
-if (!defined('_CASE_DESCRIPTION_HELP'))  define('_CASE_DESCRIPTION_HELP','Saisissez les mots cl&eacute;s de la description de l&rsquo;affaire...');
-if (!defined('_CASE_LABEL_HELP'))  define('_CASE_LABEL_HELP','Recherchez sur le libell&eacute; de l&rsquo;affaire...');
-if (!defined('_FOUND_CASE'))  define('_FOUND_CASE','affaire(s) trouv&eacute;e(s)');
-if (!defined('_CREATE_CASE'))  define('_CREATE_CASE','Cr&eacute;er l&rsquo;affaire');
-if (!defined('_LINKED_TO_CASE'))  define('_LINKED_TO_CASE','Attach&eacute; &agrave; l&rsquo;affaire');
-if (!defined('_CLOSE_THIS_CASE'))  define('_CLOSE_THIS_CASE','Cl&ocirc;turer cette affaire');
-if (!defined('_THIS_CASE_IS_CLOSED'))  define('_THIS_CASE_IS_CLOSED','Affaire cl&ocirc;tur&eacute;e');
-if (!defined('_CASE_CREATED'))  define('_CASE_CREATED','Affaire cr&eacute;e');
-if (!defined('_JOIN_RES_CASE_IN_PROCESS'))  define('_JOIN_RES_CASE_IN_PROCESS','Attacher des courriers aux affaires (corbeille de traitement)');
-if (!defined('_CLOSE_CASE'))  define('_CLOSE_CASE','Cl&ocirc;turer des affaires');
-if (!defined('_LINK_TO_CASE'))  define('_LINK_TO_CASE','Attacher &agrave; l&rsquo;affaire');
-if (!defined('_ACCESS_LIST_CASE'))  define('_ACCESS_LIST_CASE','Regroupement par affaire');
-if (!defined('_CLICK_HERE_TO_ACCESS_CASE'))  define('_CLICK_HERE_TO_ACCESS_CASE','Cliquez ici pour acc&eacute;der &agrave; la fiche d&eacute;taill&eacute;e de l&rsquo;affaire');
-if (!defined('_LABEL_MANDATORY'))  define('_LABEL_MANDATORY','libell&eacute; de l&rsquo;affaire obligatoire');
+if (!defined('_CASES_COMMENT'))
+    define('_CASES_COMMENT', 'Affaires');
+if (!defined('_CASE_INFO'))
+    define('_CASE_INFO', 'Informations sur l&rsquo;affaire');
+if (!defined('_JOIN_RES_CASE'))
+    define('_JOIN_RES_CASE','Attacher des courriers aux affaires');
+if (!defined('_CASE_NUM'))
+    define('_CASE_NUM', 'Num Affaire');
+if (!defined('_NUM_CASE'))
+    define('_NUM_CASE', 'Affaire num&eacute;ro');
+if (!defined('_CASE_DETAIL'))
+    define('_CASE_DETAIL', 'Fiche d&eacute;taill&eacute;e de l&rsquo;affaire');
+if (!defined('_DETAILS_CASES'))
+    define('_DETAILS_CASES', 'Fiche d&eacute;taill&eacute;e de l&rsquo;affaire');
+if (!defined('_INDEX_CASES'))
+    define('_INDEX_CASES', 'Index de l&rsquo;affaire');
+if (!defined('_DOCUMENTS_LIST_IN_THIS_CASE'))
+    define('_DOCUMENTS_LIST_IN_THIS_CASE', 'Liste des documents de l&rsquo;affaire');
+if (!defined('_NOTES_FOR_THIS_CASES'))
+    define('_NOTES_FOR_THIS_CASES', 'Annotations de cette affaire');
+if (!defined('_HISTORY_CASES'))
+    define('_HISTORY_CASES', 'Historique de l&rsquo;affaire');
+if (!defined('_CASE_CLOSED'))
+    define('_CASE_CLOSED', 'Affaire cl&ocirc;tur&eacute;e');
+if (!defined('_CASE_NUMBER'))
+    define('_CASE_NUMBER', 'Num&eacute;ro de l&rsquo;affaire');
+if (!defined('_CASE_NUMBER_HELP'))
+    define('_CASE_NUMBER_HELP', 'Recherche sur une affaire comprenant plusieurs courriers...');
+if (!defined('_N_CASE'))
+    define('_N_CASE', 'num&eacute;ro de l&rsquo;affaire');
+if (!defined('_CANT_SHOW_THIS_CASE'))
+    define('_CANT_SHOW_THIS_CASE', 'Impossible d\'afficher cette affaire');
+if (!defined('_CASE_ID'))
+    define('_CASE_ID','Identifiant de l&rsquo;affaire');
+if (!defined('_CASE_LABEL'))
+    define('_CASE_LABEL','Libell&eacute; de l&rsquo;affaire');
+if (!defined('_CASE_DESCRIPTION'))
+    define('_CASE_DESCRIPTION','Description de l&rsquo;affaire');
+if (!defined('_CASE_TYPIST'))
+    define('_CASE_TYPIST','Cr&eacute;ateur de l&rsquo;affaire');
+if (!defined('_CASE_CREATION_DATE'))
+    define('_CASE_CREATION_DATE','Date de cr&eacute;ation');
+if (!defined('_CASE_LAST_UPDATE_DATE'))
+    define('_CASE_LAST_UPDATE_DATE','Derni&egrave;re mise &agrave; jour');
+if (!defined('_CASE_CLOSING_DATE'))
+    define('_CASE_CLOSING_DATE','Date de cl&ocirc;ture');
+if (!defined('_CASES_INDEXES'))
+    define('_CASES_INDEXES','Index de l&rsquo;affaire');
+if (!defined('_RESSOURCES_REPORTS'))
+    define('_RESSOURCES_REPORTS','Contenu de l&rsquo;affaire');
+if (!defined('_UPDATE_CASE'))
+    define('_UPDATE_CASE','Mise &agrave; jour d&rsquo;affaires');
+if (!defined('_NEW_CASE'))
+    define('_NEW_CASE','Nouvelle affaire');
+if (!defined('_RES_ATTACH_ON_CASE'))
+    define('_RES_ATTACH_ON_CASE','Document attach&eacute; &agrave; l&rsquo;affaire');
+if (!defined('_RES_DETTACH_ON_CASE'))
+    define('_RES_DETTACH_ON_CASE','Document dettach&eacute; &agrave; l&rsquo;affaire');
+if (!defined('_JOIN_CASE'))
+    define('_JOIN_CASE','Attacher &agrave; une affaire');
+if (!defined('_MODIFY_CASE'))
+    define('_MODIFY_CASE','Attacher &agrave; une autre affaire');
+if (!defined('_JOIN_RES_ON_THIS_CASE'))
+    define('_JOIN_RES_ON_THIS_CASE','Attacher un document &agrave; l&rsquo;affaire');
+if (!defined('_RESSOURCES_LINKED'))
+    define('_RESSOURCES_LINKED','Document attach&eacute;');
+if (!defined('_RESSOURCES_NOT_LINKED'))
+    define('_RESSOURCES_NOT_LINKED','Ce document est d&eacute;j&agrave; attach&eacute;');
+if (!defined('_CASE'))
+    define('_CASE','Affaire');
+if (!defined('_SEARCH_A_CASE'))
+    define('_SEARCH_A_CASE','Rechercher une affaire');
+if (!defined('_CLOSED_CASE'))
+    define('_CLOSED_CASE','Cl&ocirc;tur&eacute;e');
+if (!defined('_SEARCH_A_RES'))
+    define('_SEARCH_A_RES','Rechercher un document');
+if (!defined('_CREATE_NEW_CASE'))
+    define('_CREATE_NEW_CASE','Cr&eacute;er une nouvelle affaire');
+if (!defined('_ADD_CASES'))
+    define('_ADD_CASES','Cr&eacute;er des affaires');
+if (!defined('_CASE_DESCRIPTION_HELP'))
+    define('_CASE_DESCRIPTION_HELP','Saisissez les mots cl&eacute;s de la description de l&rsquo;affaire...');
+if (!defined('_CASE_LABEL_HELP'))
+    define('_CASE_LABEL_HELP','Recherchez sur le libell&eacute; de l&rsquo;affaire...');
+if (!defined('_FOUND_CASE'))
+    define('_FOUND_CASE','affaire(s) trouv&eacute;e(s)');
+if (!defined('_CREATE_CASE'))
+    define('_CREATE_CASE','Cr&eacute;er l&rsquo;affaire');
+if (!defined('_LINKED_TO_CASE'))
+    define('_LINKED_TO_CASE','Attach&eacute; &agrave; l&rsquo;affaire');
+if (!defined('_CLOSE_THIS_CASE'))
+    define('_CLOSE_THIS_CASE','Cl&ocirc;turer cette affaire');
+if (!defined('_THIS_CASE_IS_CLOSED'))
+    define('_THIS_CASE_IS_CLOSED','Affaire cl&ocirc;tur&eacute;e');
+if (!defined('_CASE_CREATED'))
+    define('_CASE_CREATED','Affaire cr&eacute;e');
+if (!defined('_JOIN_RES_CASE_IN_PROCESS'))
+    define('_JOIN_RES_CASE_IN_PROCESS','Attacher des courriers aux affaires (corbeille de traitement)');
+if (!defined('_CLOSE_CASE'))
+    define('_CLOSE_CASE','Cl&ocirc;turer des affaires');
+if (!defined('_LINK_TO_CASE'))
+    define('_LINK_TO_CASE','Attacher &agrave; l&rsquo;affaire');
+if (!defined('_ACCESS_LIST_CASE'))
+    define('_ACCESS_LIST_CASE','Regroupement par affaire');
+if (!defined('_CLICK_HERE_TO_ACCESS_CASE'))
+    define('_CLICK_HERE_TO_ACCESS_CASE','Cliquez ici pour acc&eacute;der &agrave; la fiche d&eacute;taill&eacute;e de l&rsquo;affaire');
+if (!defined('_LABEL_MANDATORY'))
+    define('_LABEL_MANDATORY','libell&eacute; de l&rsquo;affaire obligatoire');
 ?>
