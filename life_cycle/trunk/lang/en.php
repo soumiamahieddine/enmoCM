d95d5742f487874b0ae45bb6b22dc8ef40a0b488
Laurent Giovannoni <laurent.giovannoni@maarch.org>
2012-05-16 13:27:15 +0000

    fix: lang

--- life_cycle/trunk/lang/en.php
+++ life_cycle/trunk/lang/en.php
@@ -21,253 +21,144 @@
  */
 
 /*********************** ADMIN ***********************************/
-if (!defined('_LIFE_CYCLE')) {
+if (!defined('_LIFE_CYCLE'))
     define('_LIFE_CYCLE', 'Life cycle');
-}
-if (!defined('_LIFE_CYCLE_COMMENT')) {
+if (!defined('_LIFE_CYCLE_COMMENT'))
     define('_LIFE_CYCLE_COMMENT', 'Gestion du cycle de vie');
-}
-if (!defined('_ADMIN_LIFE_CYCLE_SHORT')) {
+if (!defined('_ADMIN_LIFE_CYCLE_SHORT'))
     define('_ADMIN_LIFE_CYCLE_SHORT', ' Life cycle administration');
-}
-if (!defined('_ADMIN_LIFE_CYCLE')) {
-    define(
-        '_ADMIN_LIFE_CYCLE', 
-        ' Administration des cycles de vie des '
-        . 'ressources num&eacute;riques'
-    );
-}
-if (!defined('_ADMIN_LIFE_CYCLE_DESC')) {
-    define(
-        '_ADMIN_LIFE_CYCLE_DESC', 
-        'Administration des cycles de vie des '
-        . 'ressources num&eacute;riques.'
-    );
-}
-if (!defined('_MANAGE_LC_CYCLES')) {
+if (!defined('_ADMIN_LIFE_CYCLE'))
+    define('_ADMIN_LIFE_CYCLE',' Administration des cycles de vie des ressources num&eacute;riques');
+if (!defined('_ADMIN_LIFE_CYCLE_DESC'))
+    define('_ADMIN_LIFE_CYCLE_DESC','Administration des cycles de vie des ressources num&eacute;riques.');
+if (!defined('_MANAGE_LC_CYCLES'))
     define('_MANAGE_LC_CYCLES', 'Life cycle management ("lc_cycles")');
-}
-if (!defined('_MANAGE_LC_CYCLE_STEPS')) {
-    define(
-        '_MANAGE_LC_CYCLE_STEPS', 
-        'life cycle steps management("lc_cycles_steps")'
-    );
-}
-if (!defined('_MANAGE_LC_POLICIES')) {
-    define(
-        '_MANAGE_LC_POLICIES', 
-        'Life cycle policies management ("lc_policies")'
-    );
-}
+if (!defined('_MANAGE_LC_CYCLE_STEPS'))
+    define('_MANAGE_LC_CYCLE_STEPS','life cycle steps management("lc_cycles_steps")');
+if (!defined('_MANAGE_LC_POLICIES'))
+    define('_MANAGE_LC_POLICIES','Life cycle policies management ("lc_policies")');
+
 /*****************CYCLE_STEPS************************************/
-if (!defined('_LC_CYCLE_STEP')) {
+if (!defined('_LC_CYCLE_STEP'))
     define('_LC_CYCLE_STEP', 'Life cycle step');
-}
-if (!defined('_LC_CYCLE_STEP_ID')) {
+if (!defined('_LC_CYCLE_STEP_ID'))
     define('_LC_CYCLE_STEP_ID', 'Life cycle step ID');
-}
-if (!defined('_LC_CYCLE_STEPS_LIST')) {
+if (!defined('_LC_CYCLE_STEPS_LIST'))
     define('_LC_CYCLE_STEPS_LIST', 'Life cycle step list');
-}
-if (!defined('_ALL_LC_CYCLE_STEPS')) {
+if (!defined('_ALL_LC_CYCLE_STEPS'))
     define('_ALL_LC_CYCLE_STEPS', 'View all');
-}
-if (!defined('_POLICY_ID')) {
+if (!defined('_POLICY_ID'))
     define('_POLICY_ID', 'Policy ID');
-}
-if (!defined('_CYCLE_STEP_ID')) {
+if (!defined('_CYCLE_STEP_ID'))
     define('_CYCLE_STEP_ID', 'Life cycle step ID ("lc_cycle_steps")');
-}
-if (!defined('_CYCLE_STEP_DESC')) {
+if (!defined('_CYCLE_STEP_DESC'))
     define('_CYCLE_STEP_DESC', 'Life cycle step description');
-}
-if (!defined('_STEPT_OPERATION')) {
+if (!defined('_STEPT_OPERATION'))
     define('_STEP_OPERATION', 'Action on a life cycle step');
-}
-if (!defined('_IS_ALLOW_FAILURE')) {
+if (!defined('_IS_ALLOW_FAILURE'))
     define('_IS_ALLOW_FAILURE', 'Allow failures');
-}
-if (!defined('_PREPROCESS_SCRIPT')) {
+if (!defined('_PREPROCESS_SCRIPT'))
     define('_PREPROCESS_SCRIPT', 'Before process script');
-}
-if (!defined('_POSTPROCESS_SCRIPT')) {
+if (!defined('_POSTPROCESS_SCRIPT'))
     define('_POSTPROCESS_SCRIPT', 'After process script');
-}
-if (!defined('_IS_MUST_COMPLETE')) {
+if (!defined('_IS_MUST_COMPLETE'))
     define('_IS_MUST_COMPLETE', 'Must complete');
-}
-if (!defined('_LC_CYCLE_STEP_ADDITION')) {
+if (!defined('_LC_CYCLE_STEP_ADDITION'))
     define('_LC_CYCLE_STEP_ADDITION', 'Add a life cycle step');
-}
-if (!defined('_LC_CYCLE_STEP_UPDATED')) {
+if (!defined('_LC_CYCLE_STEP_UPDATED'))
     define('_LC_CYCLE_STEP_UPDATED', 'Life cycle step updated');
-}
-if (!defined('_LC_CYCLE_STEP_ADDED')) {
+if (!defined('_LC_CYCLE_STEP_ADDED'))
     define('_LC_CYCLE_STEP_ADDED', 'Life cycle step added');
-}
-if (!defined('_LC_CYCLE_STEP_DELETED')) {
+if (!defined('_LC_CYCLE_STEP_DELETED'))
     define('_LC_CYCLE_STEP_DELETED', 'Life cycle step deleted');
-}
 
 /****************CYCLES*************************************/
-if (!defined('_CYCLE_ID')) {
+if (!defined('_CYCLE_ID'))
     define('_CYCLE_ID', 'Life cycle ID');
-}
-if (!defined('_LC_CYCLE_ID')) {
+if (!defined('_LC_CYCLE_ID'))
     define('_LC_CYCLE_ID', 'Life cycle ID');
-}
-if (!defined('_SEQUENCE_NUMBER')) {
+if (!defined('_SEQUENCE_NUMBER'))
     define('_SEQUENCE_NUMBER', 'sequence number');
-}
-if (!defined('_LC_CYCLE')) {
+if (!defined('_LC_CYCLE'))
     define('_LC_CYCLE', 'Life cycle');
-}
-if (!defined('_CYCLE_DESC')) {
+if (!defined('_CYCLE_DESC'))
     define('_CYCLE_DESC', 'Life cycle description');
-}
-if (!defined('_VALIDATION_MODE')) {
+if (!defined('_VALIDATION_MODE'))
     define('_VALIDATION_MODE', 'Validation mode');
-}
-if (!defined('_ALL_LC_CYCLES')) {
+if (!defined('_ALL_LC_CYCLES'))
     define('_ALL_LC_CYCLES', 'View all');
-}
-if (!defined('_LC_CYCLES_LIST')) {
+if (!defined('_LC_CYCLES_LIST'))
     define('_LC_CYCLES_LIST', 'Life cycle list');
-}
-if (!defined('_SEQUENCE_NUMBER')) {
+if (!defined('_SEQUENCE_NUMBER'))
     define('_SEQUENCE_NUMBER', 'Sequence number');
-}
-if (!defined('_BREAK_KEY')) {
+if (!defined('_BREAK_KEY'))
     define('_BREAK_KEY', 'Break key');
-}
-if (!defined('_LC_CYCLE_ADDITION')) {
+if (!defined('_LC_CYCLE_ADDITION'))
     define('_LC_CYCLE_ADDITION', 'Add a life cycle');
-}
-if (!defined('_LC_CYCLE_ADDED')) {
+if (!defined('_LC_CYCLE_ADDED'))
     define('_LC_CYCLE_ADDED', 'Life cycle added');
-}
-if (!defined('_LC_CYCLE_UPDATED')) {
+if (!defined('_LC_CYCLE_UPDATED'))
     define('_LC_CYCLE_UPDATED', 'Life cycle updated');
-}
-if (!defined('_LC_CYCLE_DELETED')) {
+if (!defined('_LC_CYCLE_DELETED'))
     define('_LC_CYCLE_DELETED', 'Life cycle deleted');
-}
-if (!defined('_PB_WITH_WHERE_CLAUSE')) {
+if (!defined('_PB_WITH_WHERE_CLAUSE'))
     define('_PB_WITH_WHERE_CLAUSE', 'Pb with where clause');
-}
-if (!defined('_CANNOT_DELETE_CYCLE_ID')) {
+if (!defined('_CANNOT_DELETE_CYCLE_ID'))
     define('_CANNOT_DELETE_CYCLE_ID', 'Cannot delete cycle');
-}
 
 /*************CYCLE POLICIES*************************************/
-if (!defined('_LC_POLICY')) {
+if (!defined('_LC_POLICY'))
     define('_LC_POLICY', 'Life cycle policy');
-}
-if (!defined('_POLICY_NAME')) {
+if (!defined('_POLICY_NAME'))
     define('_POLICY_NAME', 'Policy name');
-}
-if (!defined('_LC_POLICY_ID')) {
+if (!defined('_LC_POLICY_ID'))
     define('_LC_POLICY_ID', 'Policy ID');
-}
-if (!defined('_LC_POLICY_NAME')) {
+if (!defined('_LC_POLICY_NAME'))
     define('_LC_POLICY_NAME', 'Life cycle policy name');
-}
-if (!defined('_POLICY_DESC')) {
+if (!defined('_POLICY_DESC'))
     define('_POLICY_DESC', 'Policy description');
-}
-if (!defined('_LC_POLICIES_LIST')) {
+if (!defined('_LC_POLICIES_LIST'))
     define('_LC_POLICIES_LIST', 'Life cycle policy list');
-}
-if (!defined('_ALL_LC_POLICIES')) {
+if (!defined('_ALL_LC_POLICIES'))
     define('_ALL_LC_POLICIES', 'View all');
-}
-if (!defined('_LC_POLICY_ADDITION')) {
+if (!defined('_LC_POLICY_ADDITION'))
     define('_LC_POLICY_ADDITION', 'Add a life cycle policy');
-}
-if (!defined('_LC_POLICY_UPDATED')) {
+if (!defined('_LC_POLICY_UPDATED'))
     define('_LC_POLICY_UPDATED', 'Life cycle policy updated');
-}
-if (!defined('_LC_POLICY_ADDED')) {
+if (!defined('_LC_POLICY_ADDED'))
     define('_LC_POLICY_ADDED', 'Life cycle policy added');
-}
-if (!defined('_LC_POLICY_DELETED')) {
+if (!defined('_LC_POLICY_DELETED'))
     define('_LC_POLICY_DELETED', 'Life cycle policy deleted');
-}
-
-if (!defined('_MISSING_A_CYCLE_STEP')) {
-    define('_MISSING_A_CYCLE_STEP', 'You have to had a cycle and a step to '
-        . 'finish the setup of the policy');
-}
-
-if (!defined('_MISSING_A_CYCLE_STEP')) {
-    define('_MISSING_A_CYCLE_STEP', 'You have to had a cycle to '
-        . 'finish the setup of the policy');
-}
+if (!defined('_MISSING_A_CYCLE_STEP'))
+    define('_MISSING_A_CYCLE_STEP', 'You have to had a cycle and a step to finish the setup of the policy');
+if (!defined('_MISSING_A_CYCLE_STEP'))
+    define('_MISSING_A_CYCLE_STEP', 'You have to had a cycle to finish the setup of the policy');
 
 /*************BATCH*************************************/
-if (!defined('_PI_COMMENT_ROOT')) {
-    define(
-        '_PI_COMMENT_ROOT', 
-        'Packaging information: Archival Information Package (AIP) content '
-        . 'and usage'
-    );
-}
-if (!defined('_PI_COMMENT_FINGERPRINT')) {
+if (!defined('_PI_COMMENT_ROOT'))
+    define('_PI_COMMENT_ROOT','Packaging information: Archival Information Package (AIP) content and usage');
+if (!defined('_PI_COMMENT_FINGERPRINT'))
     define('_PI_COMMENT_FINGERPRINT', 'Fingerprint for CI');
-}
-if (!defined('_PI_COMMENT_AIU')) {
+if (!defined('_PI_COMMENT_AIU'))
     define('_PI_COMMENT_AIU', 'Number of resources in AIP');
-}
-if (!defined('_PI_COMMENT_CONTENT')) {
-    define(
-        '_PI_COMMENT_CONTENT', 
-        'Digital resources in native format, each having a sequential' 
-        . 'filename+extension'
-    );
-}
-if (!defined('_PI_COMMENT_PDI')) {
-    define(
-        '_PI_COMMENT_PDI', 
-        'Container for Preservation Description Information. This is a catalog '
-        . 'containing descriptors for Provenance, Reference, Fixity, Context,'
-        . 'and Access Rights for each resource in <CONTENT_FILE>. '
-        . 'For convenience, history is stored apart in <HISTORY_FILE>. '
-        .'See pdi.xsd for structure and comments'
-    );
-}
-if (!defined('_HISTORY_COMMENT_ROOT')) {
-    define(
-        '_HISTORY_COMMENT_ROOT', 
-        'Preservation Description Information - History : List of events on' 
-        . 'resource, each event set being identified by resource filename in '
-        . '<CONTENT_FILE>. Ordered by ascending date'
-    );
-}
-if (!defined('_PDI_COMMENT_ROOT')) {
-    define(
-        '_PDI_COMMENT_ROOT', 'Preservation Description Information : List of '
-        .'qualifiers for resources, ordered by Provenance, Reference, Fixity, '
-        . 'Context, and Access Rights. There is one set by resource, each set '
-        . 'being identified by resource filename in <CONTENT_FILE>'
-    );
-}
-if (!defined('_PDI_COMMENT_HISTORY')) {
-    define(
-        '_PDI_COMMENT_HISTORY', 
-        'Preservation Description Information - History : List of events on '
-        . 'resource, each event set being identified by resource filename in '
-        . '<CONTENT_FILE>. Ordered by ascending date.'
-    );
-}
+if (!defined('_PI_COMMENT_CONTENT'))
+    define('_PI_COMMENT_CONTENT','Digital resources in native format, each having a sequentialfilename+extension');
+if (!defined('_PI_COMMENT_PDI'))
+    define('_PI_COMMENT_PDI','Container for Preservation Description Information. This is a catalog containing descriptors for Provenance, Reference, Fixity, Context,and Access Rights for each resource in <CONTENT_FILE>. For convenience, history is stored apart in <HISTORY_FILE>. See pdi.xsd for structure and comments');
+if (!defined('_HISTORY_COMMENT_ROOT'))
+    define('_HISTORY_COMMENT_ROOT','Preservation Description Information - History : List of events onresource, each event set being identified by resource filename in <CONTENT_FILE>. Ordered by ascending date');
+if (!defined('_PDI_COMMENT_ROOT'))
+    define('_PDI_COMMENT_ROOT', 'Preservation Description Information : List of qualifiers for resources, ordered by Provenance, Reference, Fixity, Context, and Access Rights. There is one set by resource, each set being identified by resource filename in <CONTENT_FILE>');
+
+if (!defined('_PDI_COMMENT_HISTORY'))
+    define('_PDI_COMMENT_HISTORY','Preservation Description Information - History : List of events on resource, each event set being identified by resource filename in <CONTENT_FILE>. Ordered by ascending date.');
+
 
 /*************OTHER*************************************/
-if (!defined('_DOCS')) {
+if (!defined('_DOCS'))
     define('_DOCS', 'Documents');
-}
-if (!defined('_LINK_EXISTS')) {
+if (!defined('_LINK_EXISTS'))
     define('_LINK_EXISTS', 'A link exists with an another object');
-}
-if (!defined('_VIEW_GENERAL_PARAMETERS_OF_THE_POLICY')) {
-    define('_VIEW_GENERAL_PARAMETERS_OF_THE_POLICY'
-        , 'View the global setup of the policy');
-}
+if (!defined('_VIEW_GENERAL_PARAMETERS_OF_THE_POLICY'))
+    define('_VIEW_GENERAL_PARAMETERS_OF_THE_POLICY', 'View the global setup of the policy');
+
