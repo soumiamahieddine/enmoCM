b2d17b09620fca282e62cd71e6ae8727d34ec1ed
Laurent Giovannoni <laurent.giovannoni@maarch.org>
2012-06-11 10:02:13 +0000

    fix: show res_child in process

--- attachments/trunk/show_res_id.php
+++ attachments/trunk/show_res_id.php
@@ -1,8 +1,11 @@
 <script type="text/javascript">
-var input_res = window.opener.$('res_id');
+var input_res = window.opener.$('res_id_link');
 //console.log(input_res);
 if (input_res) {
 	input_res.value='<?php echo $_REQUEST['field'];?>';
+}
+else {
+	window.opener.$('res_id').value='<?php echo $_REQUEST['field'];?>';
 }
 self.close();
 </script>
