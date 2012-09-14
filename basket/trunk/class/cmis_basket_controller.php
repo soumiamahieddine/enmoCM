7bf1e64f532764c248661511a3858908b2cb441e
Sébastien Nana <sebastien.nana@maarch.org>
2012-09-14 16:37:24 +0000

    evo: refactoring

--- basket/trunk/class/cmis_basket_controller.php
+++ basket/trunk/class/cmis_basket_controller.php
@@ -5,9 +5,7 @@ require_once 'apps/maarch_entreprise/class/cmis_class_property.php' ;
 require_once 'apps/maarch_entreprise/class/cmis_res_controller.php' ;
 
 class basketCMIS extends folderCMIS
-{
-    var $strReturn = 'THE RESULT IS A BASKET WITH ';
-    
+{    
     private $basketId;
     
   
@@ -109,15 +107,9 @@ class basketCMIS extends folderCMIS
     
     public function entryMethod ($atomFileContent, $requestedResourceId)
     {
-    	//faire des verifications pour savoir si on veut voir la liste des corbeilles ou voir la liste des documents d'une corbeille
-    	//on ne devrait jamais avoir de atomFileContent
-    	//si on a un id alors on consulte la liste des documents de cette corbeille
-    	//si on n'a pas d'id alors on consulte la liste des corbeilles
-    	 
-    	$str = "";
+    	
     	if(!isset($atomFileContent)){
     		if(!isset($requestedResourceId)){
-    			$str = 'show baskets list ';
     			
     			//get list of baskets (no documents attached)
     			$cmisBaskets = $this->loadBaskets();
@@ -127,14 +119,11 @@ class basketCMIS extends folderCMIS
     			$basketDoc->formatOutput = true;
     			//TODO feed
     			
-    			/*foreach($cmisBaskets as $cmisbasket){
-    				$cmisbasket->getAtomXmlEntry($basketDoc);
-    			}*/
     			
     			$title = 'Baskets';
     			$basketDoc = basketCMIS::getFeed($cmisBaskets, $title);
     			
-    			echo '<br />'.$basketDoc->saveXML().'<br /><br />';
+    			echo $basketDoc->saveXML();
     			
     			/**/
     		}
@@ -144,101 +133,86 @@ class basketCMIS extends folderCMIS
     			//get list of baskets (no documents attached)
     			$cmisBaskets = $this->loadBaskets();
     			
-    			//print_r($_SESSION['user']['baskets']);
+    			$found=false;
+    			$i=0;
+    			 
+    			while(!$found && ($basket = $_SESSION['user']['baskets'][$i++])){
+    				$found = (strcmp($basket['id'],$requestedResourceId)==0);
+    			}
     			
-    			//TODO feed
-    			
-    			//get the list of documents in the basket whose id is $requestedResourceId
-    			$documents = $this->getDocumentsInBasket($requestedResourceId);
-    			$title = trim('Documents in '.$requestedResourceId);
-    			$resAtom = resCMIS::getFeed($documents, $title);
-    			echo '<br />'.$resAtom->saveXML().'<br /><br />';
-    			
-    			
-    			/*if($documents = $this->getDocumentsInBasket($requestedResourceId)){
-    				$title = trim('Documents in '.$requestedResourceId);
-    				$resAtom = resCMIS::getFeed($documents, $title);
-	    			echo '<br />'.$resAtom->saveXML().'<br /><br />';
-    			}*/
-    			
+    			if($found){
+	    			//get the list of documents in the basket whose id is $requestedResourceId
+	    			$documents = $this->getDocumentsInBasket($basket);
+	    			$title = trim('Documents in '.$requestedResourceId);
+	    			$resAtom = resCMIS::getFeed($documents, $title);
+	    			
+	    			echo $resAtom->saveXML();
+    			}    			
+    			else{
+    				//TODO throw objectNotFound
+    				echo "<br />ERREUR : objectNotFound.<br />";
+    			}
     		}
     	}
     	else{
         	//TODO throw notSupported or invalidArgument
-    		$str = "Error : atomFileContent should not be set ";
+	    		echo "<br />ERREUR : requete sql non réalisée throw notSupported or invalidArgument.<br />";    		
     	}
     
-    	return $str . $this->strReturn . $requestedResourceId . ' ' . $atomFileContent;
+    	//return $requestedResourceId . ' ' . $atomFileContent;
     }
     
     
-    private function getDocumentsInBasket($requestedResourceId){
+    private function getDocumentsInBasket($basket){
     	require_once('core/class/class_db.php');
     	$db = new dbquery();
     	$db->connect();
-    	$found=false;
-    	$i=0;
-    	
-    	while(!$found && ($basket = $_SESSION['user']['baskets'][$i++])){
-    		$found = strcmp($basket['id'],$requestedResourceId)==0;
-    	}
-    	
     	//$resArray = null
     	
-    	if($found){
-    		$view = $basket['view'];
-    		$clause = $basket['clause'];
-    		$statement = "select * from " . $view . " where " . $clause;
-    		
-	    	$result = $db->query($statement);
-	    	
-	    	//TODO gerer les cas d erreurs
-	    	if($result === false){
-	        	//TODO throw objectNotFound or invalidArgument or runtime
-	    		echo "<br />ERREUR : requete sql non réalisée.<br />";
-	    	}
+    	$view = $basket['view'];
+    	$clause = $basket['clause'];
+    	$statement = "select * from " . $view . " where " . $clause;
     	
+	    $result = $db->query($statement);
+	    
+	    //TODO 
+	    if($result === false){
+	       	//TODO throw objectNotFound or invalidArgument or runtime
+	    	echo "<br />ERREUR : requete sql non réalisée runtime.<br />";
+	    }
+    		    	
+	    $resArray = array();
+	    while ($recordset = $db->fetch_object()) {
+	    	$documentCmis = new resCMIS();
+	    	$documentCmis->DocumentCmis();
+	    	$documentCmis->setRes(
+    			array('res_id' => $recordset->res_id,
+	    			'type_label'=> $recordset->type_label,
+	    			'contact_society' => $recordset->contact_society ,
+	    			'contact_firstname' => $recordset->contact_firstname , 
+	    			'contact_lastname' => $recordset->contact_lastname , 
+	    			'entity_label' => $recordset->entity_label , 
+	    			'dest_user' => $recordset->dest_user , 
+	    			'doc_date' => $recordset->doc_date , 
+	    			'process_limit_date' => $recordset->process_limit_date,
+	    			'author' => $recordset->author
+		   		));
 	    	
-	    	$resArray = array();
-	    	while ($recordset = $db->fetch_object()) {
-	    		$documentCmis = new resCMIS();
-	    		$documentCmis->DocumentCmis();
-	    		$documentCmis->setRes(
-    				array('res_id' => $recordset->res_id,
-	    				'type_label'=> $recordset->type_label,
-	    				'contact_society' => $recordset->contact_society ,
-	    				'contact_firstname' => $recordset->contact_firstname , 
-	    				'contact_lastname' => $recordset->contact_lastname , 
-	    				'entity_label' => $recordset->entity_label , 
-	    				'dest_user' => $recordset->dest_user , 
-	    				'doc_date' => $recordset->doc_date , 
-	    				'process_limit_date' => $recordset->process_limit_date,
-	    				'author' => $recordset->author
-		    		));
 	    		
+	    	$url = $_SESSION['config']['coreurl']."apps/maarch_entreprise/index.php?page=view_baskets&module=basket&directLinkToAction&baskets=". $basket['id'] . "&resid=" . $recordset->res_id; 
+	    	$url = htmlentities($url, ENT_COMPAT | ENT_HTML401 , "utf-8");
 	    		
-	    		$url = $_SESSION['config']['coreurl']."apps/maarch_entreprise/index.php?page=view_baskets&module=basket&directLinkToAction&baskets=". $basket['id'] . "&resid=" . $recordset->res_id; 
-	    		$url = htmlentities($url, ENT_COMPAT | ENT_HTML401 , "utf-8");
-	    		//var_dump($url);
-	    		$documentCmis->setUrl($url);
+	    	$documentCmis->setUrl($url);
 	    		
-	    		array_push($resArray, $documentCmis);
-	    	}
-    	}
-    	else{
-    		//TODO throw objectNotFound
-    	}
+	    	array_push($resArray, $documentCmis);
+	    }
     	
     	return $resArray;
     }
     
     
     
-    
-    
     private function loadBaskets(){
-    	//variable de session des corbeilles de l'utilisateur : tableau $_SESSION['user']['baskets']
-    	//pour charger ce tableau
     	require_once('core/class/class_db.php');
     	$db = new dbquery();
     	$db->connect();
@@ -249,6 +223,7 @@ class basketCMIS extends folderCMIS
     	//TODO gerer les cas d erreurs
     	if($result === false){
         	//TODO throw runtime
+	    	echo "<br />ERREUR : runtime.<br />";
     	}
     	
     	$recordset = $db->fetch_object();
@@ -283,37 +258,5 @@ class basketCMIS extends folderCMIS
     	return $basketCmis;
     }
     
-    
-    //TODO fonction a supprimer est ds objectCMIS
-    public static function _getFeed($baskets, $title=null){
-    	$doc = new DOMDocument('1.0', 'utf-8');
-    	$doc->formatOutput = true;
-    	 
-    	//TODO add xmlns
-    	$root = $doc->createElementNS('http://www.w3.org/2005/Atom', 'feed');
-    	$doc->appendChild($root);
-    	 
-    	$eAuthor = $doc->createElement('author');
-    	$root->appendChild($eAuthor);
-    	$name = $_SESSION['user']['FirstName'].' '.$_SESSION['user']['LastName'];  //'name';
-    	$eName = $doc->createElement('name', $name);
-    	$eAuthor->appendChild($eName);
-    	 
-    	
-    	if(isset($title) && !empty($title)){
-    		$eTitle = $doc->createElement('title', $title);
-    		$root->appendChild($eTitle);
-    	}
-    	 
-    	
-    	$eNumItems = $doc->createElement('cmisra:numItems', count($documents));
-    	$root->appendChild($eNumItems);
-    	 
-    	foreach($baskets as $basket){
-    		$basket->getAtomXmlEntry($doc, $root);
-    	}
-    	 
-    	return $doc;
-    }
 }
 
