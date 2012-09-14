fcf73cdc60d0f0bf89e913f0aaa3ca4089b8a5bb
Sébastien Nana <sebastien.nana@maarch.org>
2012-09-14 09:50:14 +0000

    evo: cmis functions

--- core/trunk/core/class/web_service/cmis_test/test_rest.php
+++ core/trunk/core/class/web_service/cmis_test/test_rest.php
@@ -9,7 +9,8 @@ $curl = curl_init();
 
 //BASIC AUTHENTICATION
 curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-curl_setopt($curl, CURLOPT_USERPWD, 'bblier:maarch');
+//curl_setopt($curl, CURLOPT_USERPWD, 'bblier:maarch');
+curl_setopt($curl, CURLOPT_USERPWD, 'pparker:maarch');
 
 //WS URL
 $url = 'http://127.0.0.1/maarch_entreprise_trunk/ws_server.php?REST';
@@ -21,18 +22,29 @@ if (isset($_REQUEST['idResource']) && !empty($_REQUEST['idResource'])) {
 }
 curl_setopt($curl, CURLOPT_URL, $url . '/');
 
-//POST CONTENT
-$xmlAtomFileContent = base64_encode(file_get_contents('create_folder.atom.xml'));
-curl_setopt($curl, CURLOPT_POSTFIELDS, 'atomFileContent=' . $xmlAtomFileContent);
-//HTTP METHOD
-if ($_REQUEST['method'] == 'post' || !isset($_REQUEST['method'])) {
-    curl_setopt($curl, CURLOPT_POST, 1);
-} else {
-    //GET, PUT, DELETE METHOD
-    curl_setopt($curl, CURLOPT_CUSTOMREQUEST, strtoupper($_REQUEST['method']));
-    //curl_setopt($curl, CURLOPT_HTTPHEADER, array('X-HTTP-Method-Override: ' . $_REQUEST['method']));
+if (($_REQUEST['method'] == 'post' || !isset($_REQUEST['method']))&& isset($_REQUEST['xmlFile'])) {
+	$xmlAtomFileContent = file_get_contents($_REQUEST['xmlFile']);
+	curl_setopt($curl, CURLOPT_POSTFIELDS, 'atomFileContent=' . $xmlAtomFileContent);
+	curl_setopt($curl, CURLOPT_POST, 1);
+}
+else {
+	//GET, PUT, DELETE METHOD
+	curl_setopt($curl, CURLOPT_CUSTOMREQUEST, strtoupper($_REQUEST['method']));
 }
 
+//POST CONTENT
+//$xmlAtomFileContent = base64_encode(file_get_contents('create_folder.atom.xml'));
+//$xmlAtomFileContent = file_get_contents('query.xml');
+//$xmlAtomFileContent = file_get_contents('testcreatefolder.atom.xml');
+//curl_setopt($curl, CURLOPT_POSTFIELDS, 'atomFileContent=' . $xmlAtomFileContent);
+//HTTP METHOD
+//if ($_REQUEST['method'] == 'post' || !isset($_REQUEST['method'])) {
+//    curl_setopt($curl, CURLOPT_POST, 1);
+//} else {
+    //GET, PUT, DELETE METHOD
+    //curl_setopt($curl, CURLOPT_CUSTOMREQUEST, strtoupper($_REQUEST['method']));
+    //curl_setopt($curl, CURLOPT_HTTPHEADER, array('X-HTTP-Method-Override: ' . $_REQUEST['method']));
+//}
 //RESULT
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 $page = curl_exec($curl);
