12058fae229e72eb52e3d8aec55e74acfc852da1
Laurent Giovannoni <laurent.giovannoni@maarch.org>
2012-12-03 14:08:21 +0000

    evo: new organisation

--- notifications/trunk/batch/process_all.bat
+++
@@ -1,6 +0,0 @@
-cd C:\xampp\htdocs\maarch_entreprise_trunk\modules\notifications\batch 
-process_event_stack_VAL.bat 
-
-process_email_stack.bat 
-
-pause
--- notifications/trunk/batch/process_event_stack_VIDEOCODAGE_DEST.bat
+++
@@ -1,2 +0,0 @@
-"C:\xampp\php\php.exe" "C:\xampp\htdocs\maarch_entreprise\modules\notifications\batch\process_event_stack.php" -c "C:\xampp\htdocs\maarch_entreprise\modules\notifications\batch\config\config.xml" -n INDEX_DEST
-pause
