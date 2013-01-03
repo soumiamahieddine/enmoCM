3d6afb2c9b5c6898888caa5926154e5a18b37fd2
Laurent Giovannoni <laurent.giovannoni@maarch.org>
2013-01-03 15:06:32 +0000

    evo: replace [dates] [time] in the indexation template

--- maarch_entreprise/trunk/getIframeTemplateContent.php
+++ maarch_entreprise/trunk/getIframeTemplateContent.php
@@ -2,5 +2,8 @@
 
 $_SESSION['template_content'] = $_REQUEST['template_content'];
 
+$_SESSION['template_content'] = str_replace('[dates]', date('d-m-Y'), $_SESSION['template_content']);
+$_SESSION['template_content'] = str_replace('[time]', date('G:i:s'), $_SESSION['template_content']);
+
 echo "{status : 'OK " . addslashes($_REQUEST['template_content']) . "'}";
 exit;
