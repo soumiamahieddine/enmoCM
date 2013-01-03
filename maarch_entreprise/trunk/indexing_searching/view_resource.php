3c5e57635bd382876d220668869ff7716d40e449
Laurent Giovannoni <laurent.giovannoni@maarch.org>
2013-01-03 14:48:58 +0000

    evo: utf8 encoding for html maarch format

--- maarch_entreprise/trunk/indexing_searching/view_resource.php
+++ maarch_entreprise/trunk/indexing_searching/view_resource.php
@@ -4,7 +4,7 @@
 if ($viewResourceArr['status'] <> 'ko') {
     if (strtolower($viewResourceArr['mime_type']) == 'application/maarch') {
         ?>
-
+        <head><meta content="text/html; charset=UTF-8" http-equiv="Content-Type"></head>
         <!--<body id="validation_page" 
         onload="javascript:moveTo(0,0);">
             <div id="template_content" style="width:100%;">-->
