1636fad6f3901f56d657d84a8336669837ad16b9
Laurent Giovannoni <laurent.giovannoni@maarch.org>
2013-01-16 15:11:22 +0000

    fix: ajax pb for template edition

--- maarch_entreprise/trunk/getIframeTemplateContent.php
+++ maarch_entreprise/trunk/getIframeTemplateContent.php
@@ -1,9 +1,63 @@
 <?php
+$sessionTemplateContent = utf8_encode(html_entity_decode(strip_tags($_SESSION['template_content'])));
+$requestTemplateContent = utf8_encode(html_entity_decode(strip_tags($_REQUEST['template_content'])));
 
-$_SESSION['template_content'] = $_REQUEST['template_content'];
+$sessionTemplateContent = trim(str_replace(
+    "\n", 
+    "",
+    $sessionTemplateContent
+));
+$sessionTemplateContent = trim(str_replace(
+    " ", 
+    "",
+    $sessionTemplateContent
+));
+$sessionTemplateContent = trim(str_replace(
+    "\r", 
+    "",
+    $sessionTemplateContent
+));
+$sessionTemplateContent = trim(str_replace(
+    "\t", 
+    "",
+    $sessionTemplateContent
+));
+$requestTemplateContent = trim(str_replace(
+    "\n", 
+    "", 
+    $requestTemplateContent
+));
+$requestTemplateContent = trim(str_replace(
+    " ", 
+    "", 
+    $requestTemplateContent
+));
+$requestTemplateContent = trim(str_replace(
+    "\r", 
+    "", 
+    $requestTemplateContent
+));
+$requestTemplateContent = trim(str_replace(
+    "\t", 
+    "", 
+    $requestTemplateContent
+));
 
-$_SESSION['template_content'] = str_replace('[dates]', date('d-m-Y'), $_SESSION['template_content']);
-$_SESSION['template_content'] = str_replace('[time]', date('G:i:s'), $_SESSION['template_content']);
 
-echo "{status : 'OK " . addslashes($_REQUEST['template_content']) . "'}";
+/*echo $sessionTemplateContent;
+echo "
+";
+echo $requestTemplateContent;
+exit;*/
+
+if ($sessionTemplateContent == $requestTemplateContent) {
+    $_SESSION['template_content'] = '';
+    echo "{status : '1, responseText : same content ! '}";
+} else {
+    $_SESSION['template_content'] = $_REQUEST['template_content'];
+    $_SESSION['template_content'] = str_replace('[dates]', date('d-m-Y'), $_SESSION['template_content']);
+    $_SESSION['template_content'] = str_replace('[time]', date('G:i:s'), $_SESSION['template_content']);
+    echo "{status : '0, responseText : " . addslashes($_REQUEST['template_content']) . "'}";
+}
+
 exit;
