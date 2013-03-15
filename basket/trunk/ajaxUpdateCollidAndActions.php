33e1da913d16720d8811553650fa073f8a97e722
Laurent Giovannoni <laurent.giovannoni@maarch.org>
2013-03-15 18:15:58 +0000

    evo: #604

---
+++ basket/trunk/ajaxUpdateCollidAndActions.php
@@ -0,0 +1,41 @@
+<?php
+$core_tools = new core_tools();
+$dbActions= new dbquery();
+$dbActions->connect();
+require_once('modules/basket/class/class_admin_basket.php');
+$adminBasket = new admin_basket();
+
+//SELECT ACTIONS
+$_SESSION['m_admin']['basket']['all_actions'] = array();
+if ($_REQUEST['is_reload_groups'] == 'true') {
+    $_SESSION['m_admin']['basket']['groups'] = array();
+}
+$dbActions->query("select id, label_action, keyword, create_id, action_page from " 
+    . $_SESSION['tablename']['actions'] . " where enabled = 'Y' order by label_action"
+);
+
+while ($line = $dbActions->fetch_object()) {
+    if ($core_tools->is_action_defined($line->id) && $line->action_page == '') {
+        array_push(
+            $_SESSION['m_admin']['basket']['all_actions'] ,
+            array(
+                'ID' => $line->id, 
+                'LABEL' => $line->label_action, 
+                'KEYWORD' => $line->keyword, 
+                'CREATE_ID' => $line->create_id
+            )
+        );
+    } elseif ($adminBasket->isAnActionOfMyBasketCollection($line->action_page, $_REQUEST['coll_id'])) {
+        array_push(
+            $_SESSION['m_admin']['basket']['all_actions'] ,
+            array(
+                'ID' => $line->id, 
+                'LABEL' => $line->label_action, 
+                'KEYWORD' => $line->keyword, 
+                'CREATE_ID' => $line->create_id
+            )
+        );
+    }
+}
+
+//var_dump($_SESSION['m_admin']['basket']['all_actions']);
