f3ba620aba0ac8475f2193f608133bcb18c99b06
Laurent Giovannoni <laurent.giovannoni@maarch.org>
2013-03-26 16:17:16 +0000

    evo: quicklaunch buttons are now wrapped by the link

--- maarch_entreprise/trunk/quicklaunch.php
+++ maarch_entreprise/trunk/quicklaunch.php
@@ -114,36 +114,35 @@ $core_tools->test_service('quicklaunch', "apps");
         }
         if ($_SESSION['user']['UserId'] == 'superadmin' && !$tag) {
             $tag = true;
-            echo '<div class="quiclaunch_div bighome_userinfo"><a href="index.php?page=admin&amp;reinit=true"><span>'._ADMIN.'</span></a></div>';
+            echo '<a href="index.php?page=admin&amp;reinit=true"><div class="quiclaunch_div bighome_userinfo"><span>'._ADMIN.'</span></div></a>';
             $nb_max++;
             $displayed_admin = true;
         }
         if ($_SESSION['user']['UserId'] <> 'superadmin' && ($element['id'] == 'index_mlb' && $element['show'] == true &&
             (!isset($displayed_index_mlb) || $displayed_index_mlb <> true))) {
-            echo '<div class="quiclaunch_div bighome_indexing"><a href="index.php?page=view_baskets&amp;module=basket&amp;baskets=IndexingBasket"><span>'
-                ._INDEXING_MLB.'</span></a></div>';
+            echo '<a href="index.php?page=view_baskets&amp;module=basket&amp;baskets=IndexingBasket"><div class="quiclaunch_div bighome_indexing"><span>'
+                ._INDEXING_MLB.'</span></div></a>';
             $nb_max++;
             $displayed_index_mlb = true;
         }
         if ($element['id'] == 'adv_search_mlb' && $element['show'] == true &&
             (!isset($displayed_adv_search_mlb) || isset($displayed_index_mlb) && $displayed_adv_search_mlb <> true)) {
-            echo '<div class="quiclaunch_div bighome_search_adv"><a href="index.php?page=search_adv&amp;dir=indexing_searching&amp;reinit=true"><span>'
-                ._ADV_SEARCH_MLB.'</span></a></div>';
+            echo '<a href="index.php?page=search_adv&amp;dir=indexing_searching&amp;reinit=true"><div class="quiclaunch_div bighome_search_adv"><span>'
+                ._ADV_SEARCH_MLB.'</span></div></a>';
             $nb_max++;
             $displayed_adv_search_mlb = true;
         }
         //business collection
         if ($_SESSION['user']['UserId'] <> 'superadmin' && ($element['id'] == 'index_business' && $element['show'] == true &&
             (!isset($displayed_index_business) || $displayed_index_business <> true))) {
-            echo '<div class="quiclaunch_div bighome_indexing"><a href="index.php?page=view_baskets&amp;module=basket&amp;baskets=businessIndexation"><span>'
-                ._INDEXING_BUSINESS.'</span></a></div>';
+            echo '<a href="index.php?page=view_baskets&amp;module=basket&amp;baskets=businessIndexation"><div class="quiclaunch_div bighome_indexing"><span>'
+                ._INDEXING_BUSINESS.'</span></div></a>';
             $nb_max++;
             $displayed_index_business = true;
         }
         if ($element['id'] == 'adv_search_business' && $element['show'] == true &&
             (!isset($displayed_adv_search_business) || isset($displayed_index_business) && $displayed_adv_search_business <> true)) {
-            echo '<div class="quiclaunch_div bighome_search_adv"><a href="index.php?page=search_adv_business&amp;dir=indexing_searching&amp;reinit=true"><span>'
-                ._ADV_SEARCH_BUSINESS.'</span></a></div>';
+            echo '<a href="index.php?page=search_adv_business&amp;dir=indexing_searching&amp;reinit=true"><div class="quiclaunch_div bighome_search_adv"><span>' ._ADV_SEARCH_BUSINESS.'</span></div></a>';
             $nb_max++;
             $displayed_adv_search_business = true;
         }
@@ -157,15 +156,15 @@ $core_tools->test_service('quicklaunch', "apps");
         }
         if ($element['id'] == 'adv_search_rm' && $element['show'] == true &&
             (!isset($displayed_adv_search_rm) || isset($displayed_index_rm) && $displayed_adv_search_rm <> true)) {
-            echo '<div class="quiclaunch_div bighome_search_adv"><a href="index.php?page=search_adv_rm&amp;module=records_management&amp;reinit=true"><span>'
-            ._ADV_SEARCH_RM.'</span></a></div>';
+            echo '<a href="index.php?page=search_adv_rm&amp;module=records_management&amp;reinit=true"><div class="quiclaunch_div bighome_search_adv"><span>'
+            ._ADV_SEARCH_RM.'</span></div></a>';
             $nb_max++;
             $displayed_adv_search_rm = true;
         }
         //physical archive
         if ($element['id'] == 'physical_archive' && $element['show'] == true && (!isset($displayed_physical_archive) || isset($displayed_physical_archive) && $displayed_physical_archive <> true)) {
-            echo '<div class="quiclaunch_div bighome_physical_archive"><a href="index.php?page=boxes&amp;module=physical_archive&amp;reinit=true"><span>'
-                ._PHYSICAL_ARCHIVE.'</span></a></div>';
+            echo '<a href="index.php?page=boxes&amp;module=physical_archive&amp;reinit=true"><div class="quiclaunch_div bighome_physical_archive"><span>'
+                ._PHYSICAL_ARCHIVE.'</span></div></a>';
             $nb_max++;
             $displayed_physical_archive = true;
         }
