cee8a3032cec9cb739e45e3d2dc8f713ddbfbefc
Laurent Giovannoni <laurent.giovannoni@maarch.org>
2013-04-25 08:12:23 +0000

    evo: no notif for copies if status = INIT or VAL

--- notifications/trunk/diffusion_types/copy_list.php
+++ notifications/trunk/diffusion_types/copy_list.php
@@ -3,8 +3,8 @@
 switch ($request) {
 case 'form_content':
 //Affichage du formulaire/interface dans l'administration des notification => Envoi Ajax
-	$form_content .= '<p class="sstit">' . _NOTIFICATIONS_COPY_LIST_DIFF_TYPE . '</p>';
-	break;
+    $form_content .= '<p class="sstit">' . _NOTIFICATIONS_COPY_LIST_DIFF_TYPE . '</p>';
+    break;
 
 case 'recipients':
     $recipients = array();
@@ -13,7 +13,7 @@ case 'recipients':
     
     // Copy to users
     $select = "SELECT distinct us.*";
-	$from = " FROM listinstance li "
+    $from = " FROM listinstance li "
         . " JOIN users us ON li.item_id = us.user_id";
     $where = " WHERE li.coll_id = 'letterbox_coll' AND li.listinstance_type='DOC' AND li.item_mode = 'cc'"
         . " AND item_type='user_id'";
@@ -21,7 +21,7 @@ case 'recipients':
     switch($event->table_name) {
     case 'notes':
         $from .= " JOIN notes ON notes.coll_id = li.coll_id AND notes.identifier = li.res_id";
-		$where .= " AND notes.id = " . $event->record_id . " AND li.item_id != notes.user_id"
+        $where .= " AND notes.id = " . $event->record_id . " AND li.item_id != notes.user_id"
             . " AND ("
                 . " notes.id not in (SELECT DISTINCT note_id FROM note_entities) "
                 . " OR us.user_id IN (SELECT ue.user_id FROM note_entities ne JOIN users_entities ue ON ne.item_id = ue.entity_id WHERE ne.note_id = " . $event->record_id . ")"
@@ -37,24 +37,25 @@ case 'recipients':
     
     case 'listinstance':
     default:
-        $where .= " AND listinstance_id = " . $event->record_id;
+        $from .= " JOIN res_letterbox lb ON lb.res_id = li.res_id";
+        $where .= " AND listinstance_id = " . $event->record_id . " AND lb.status not in ('INIT', 'AVAL')";
     }
     
     $query = $select . $from . $where;
-    	
+        
     if($GLOBALS['logger']) {
         $GLOBALS['logger']->write($query , 'DEBUG');
     }
     
-	$dbRecipients->query($query);
-	
-	while($recipient = $dbRecipients->fetch_object()) {
-		$recipients[] = $recipient;
-	}
+    $dbRecipients->query($query);
+    
+    while($recipient = $dbRecipients->fetch_object()) {
+        $recipients[] = $recipient;
+    }
     
     // Copy to entities
     $select = "SELECT distinct us.*";
-	$from = " FROM listinstance li "
+    $from = " FROM listinstance li "
         . " LEFT JOIN users_entities ue ON li.item_id = ue.entity_id "
         . " JOIN users us ON ue.user_id = us.user_id";
     $where = " WHERE li.coll_id = 'letterbox_coll' AND li.listinstance_type='DOC' AND li.item_mode = 'cc'"
@@ -63,7 +64,7 @@ case 'recipients':
     switch($event->table_name) {
     case 'notes':
         $from .= " JOIN notes ON notes.coll_id = li.coll_id AND notes.identifier = li.res_id";
-		$where .= " AND notes.id = " . $event->record_id . " AND li.item_id != notes.user_id"
+        $where .= " AND notes.id = " . $event->record_id . " AND li.item_id != notes.user_id"
             . " AND ("
                 . " notes.id not in (SELECT DISTINCT note_id FROM note_entities) "
                 . " OR us.user_id IN (SELECT ue.user_id FROM note_entities ne JOIN users_entities ue ON ne.item_id = ue.entity_id WHERE ne.note_id = " . $event->record_id . ")"
@@ -83,7 +84,7 @@ case 'recipients':
     }
     
     $query = $select . $from . $where;
-    	
+        
     if($GLOBALS['logger']) {
         $GLOBALS['logger']->write($query , 'DEBUG');
     }
@@ -91,13 +92,13 @@ case 'recipients':
     $dbRecipients->query($query);
     
     while($recipient = $dbRecipients->fetch_object()) {
-		$recipients[] = $recipient;
-	}
-	break;
-	
+        $recipients[] = $recipient;
+    }
+    break;
+    
 case 'attach':
-	$attach = false;
-	break;
+    $attach = false;
+    break;
   
 case 'res_id':
     $select = "SELECT li.res_id";
@@ -107,7 +108,7 @@ case 'res_id':
     switch($event->table_name) {
     case 'notes':
         $from .= " JOIN notes ON notes.coll_id = li.coll_id AND notes.identifier = li.res_id";
-		$where .= " AND notes.id = " . $event->record_id . " AND li.item_id != notes.user_id";
+        $where .= " AND notes.id = " . $event->record_id . " AND li.item_id != notes.user_id";
         break;
         
     case 'res_letterbox':
@@ -127,10 +128,10 @@ case 'res_id':
         $GLOBALS['logger']->write($query , 'DEBUG');
     }
     
-	$dbResId = new dbquery();
+    $dbResId = new dbquery();
     $dbResId->connect();
-	$dbResId->query($query);
-	$res_id_record = $dbResId->fetch_object();
+    $dbResId->query($query);
+    $res_id_record = $dbResId->fetch_object();
     $res_id = $res_id_record->res_id;
     break;
 
