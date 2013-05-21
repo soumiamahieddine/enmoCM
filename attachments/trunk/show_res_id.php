9b7a89ccd47543c137efc6c051530a788f3e0c8e
Sébastien Nana <sebastien.nana@maarch.org>
2013-05-21 08:59:17 +0000

    Fix: problem adding link

--- attachments/trunk/show_res_id.php
+++ attachments/trunk/show_res_id.php
@@ -2,10 +2,10 @@
 var input_res = window.opener.$('res_id_link');
 //console.log(input_res);
 if (input_res) {
-	input_res.value='<?php echo $_REQUEST['field'];?>';
+	input_res.value='<?php echo $_REQUEST['field'][0];?>';
 }
 else {
-	window.opener.$('res_id').value='<?php echo $_REQUEST['field'];?>';
+	window.opener.$('res_id').value='<?php echo $_REQUEST['field'][0];?>';
 }
 self.close();
 </script>
