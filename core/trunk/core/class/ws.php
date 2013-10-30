ea87e850cdf938c7d4b7a6981cce37bc2f815f44
Laurent Giovannoni <laurent.giovannoni@maarch.org>
2013-10-30 10:21:15 +0000

    FIX #1033 test commit

--- core/trunk/core/class/ws.php
+++ core/trunk/core/class/ws.php
@@ -3,6 +3,7 @@ global $SOAP_dispatch_map;
 global $XMLRPC_dispatch_map;
 global $SOAP_typedef;
 
+
 //test
 /*$SOAP_dispatch_map['testMethod'] = array(
                                         'in'  => array('myVar' => 'string'),
