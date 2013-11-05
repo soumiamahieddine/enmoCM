926ef75ab046ec0e7b07470d30dec51d10934e1e
Laurent Giovannoni <laurent.giovannoni@maarch.org>
2013-11-05 17:09:07 +0000

    FEAT #1045

--- core/trunk/core/class/class_alert_engine.php
+++ core/trunk/core/class/class_alert_engine.php
@@ -1093,56 +1093,112 @@ class alert_engine extends dbquery
     /**
     *  Allow to know when the next open day come
     *
-    * @param date $Date
+    * @param date $Date in timestamp format
     * @param int $Delta
+    * @param boolean $isMinus
     */
-    public function WhenOpenDay($Date, $Delta)
+    public function WhenOpenDay($Date, $Delta, $isMinus = false)
     {
-        $Date = strtotime ($Date);
         $Hollidays = array (
-        '1_1',
-        '1_5',
-        '8_5',
-        '14_7',
-        '15_8',
-        '1_11',
-        '11_11',
-        '25_12'
+            '1_1',
+            '1_5',
+            '8_5',
+            '14_7',
+            '15_8',
+            '1_11',
+            '11_11',
+            '25_12'
         );
-        if(function_exists ('easter_date'))
-        {
-            $WhenEasterCelebrates = easter_date ((int)date('Y'), $Date);
+        $this->connect();
+        $this->query("select * from parameters where id like 'alert_stop%'");
+        while ($result = $this->fetch_object()) {
+            if ($result->param_value_date <> '') {
+                $compare = $this->compare_date($result->param_value_date, date("d-m-Y"));
+                //take the alert stop only if > now
+                if ($compare == 'date1' || $compare == 'equal') {
+                    $dateExploded = explode("-", str_replace(" 00:00:00", "", $result->param_value_date));
+                    array_push($Hollidays, (int)$dateExploded[2] . "_" . (int)$dateExploded[1]);
+                }
+            }
         }
-        else
-        {
+        //var_dump($Hollidays);
+        
+        if (function_exists ('easter_date')) {
+            $WhenEasterCelebrates = easter_date ((int)date('Y'), $Date);
+        } else {
             $WhenEasterCelebrates = $this->getEaster ((int)date('Y'), $Date);
         }
         $Hollidays[] = date ('j_n', $WhenEasterCelebrates);
         $Hollidays[] = date ('j_n', $WhenEasterCelebrates + (86400*39));
         $Hollidays[] = date ('j_n', $WhenEasterCelebrates + (86400*49));
         $iEnd = $Delta * 86400;
+
         $i = 0;
-        while ($i < $iEnd)
-        {
+        while ($i < $iEnd) {
             $i = strtotime ('+1 day', $i);
-            if (in_array (date ('w', $Date+$i),array (0,6) ) || in_array (date ('j_n', $Date+$i), $Hollidays))
-            {
-                $iEnd = strtotime ('+1 day', $iEnd);
-                $Delta ++;
+            if ($isMinus) {
+                if (in_array(
+                    date('w', $Date-$i),array (0,6)
+                ) || in_array (date ('j_n', $Date-$i), $Hollidays)
+                ) {
+                    $iEnd = strtotime ('+1 day', $iEnd);
+                    $Delta ++;
+                }
+            } else {
+                if (
+                    in_array(
+                        date('w', $Date+$i),array (0,6)
+                    ) || in_array (date ('j_n', $Date+$i), $Hollidays)
+                ) {
+                    $iEnd = strtotime ('+1 day', $iEnd);
+                    $Delta ++;
+                }
             }
         }
-        return date('Y-m-d', $Date + (86400*$Delta));
+        if ($isMinus) {
+            return date('Y-m-d', $Date + (86400*-$Delta));
+        } else {
+            return date('Y-m-d', $Date + (86400*$Delta));
+        }
     }
 
     /**
     *  Allow to know the next date to treat
     *
     * @param int $delay Delay in days
+    * @param boolean $isMinus true if minus
     */
-    public function date_max_treatment($delay)
+    public function date_max_treatment($delay, $isMinus = false)
     {
-        //$result = $this->calculDate(strftime("%Y")."-".strftime("%m")."-".strftime("%d"), $delay);
-        $result = $this->WhenOpenDay(strftime("%Y")."-".strftime("%m")."-".strftime("%d"), $delay);
+        $result = $this->WhenOpenDay(
+             strtotime (strftime("%Y")."-".strftime("%m")."-".strftime("%d")), 
+            $delay, 
+            $isMinus
+        );
         return $result;
+    }
+    
+    /**
+    *  Allow to know the next date to treat
+    *
+    * @param int $delay Delay in days
+    * @param boolean $isMinus true if minus
+    */
+    public function processDelayDate($date, $delay, $isMinus = false)
+    {
+        $date = date('Y-m-d', $date);
+        $result = $this->WhenOpenDay(
+            $date, 
+            $delay, 
+            $isMinus
+        );
+        return $result;
+    }
+    
+    function dateFR2Time($date)
+    {
+        list($day, $month, $year) = explode('/', $date);
+        $timestamp = mktime(0, 0, 0, $month, $day, $year);
+        return $timestamp;
     }
 }
