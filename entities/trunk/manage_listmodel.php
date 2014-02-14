b6b9135e85c776d8affabeb8949db1f4f09c6b62
Laurent Giovannoni <laurent.giovannoni@maarch.org>
2014-02-14 14:48:12 +0000

    FIX #1156

--- entities/trunk/manage_listmodel.php
+++ entities/trunk/manage_listmodel.php
@@ -139,7 +139,7 @@ $users_query =
     . ENT_USERS_ENTITIES . " ue WHERE u.status <> 'DEL' and u.enabled = 'Y' and"
     . " e.entity_id = ue.entity_id and u.user_id = ue.user_id and"
     . " e.enabled = 'Y' and ue.primary_entity='Y' " . $user_expr . $entity_expr
-    . " order by u.user_id asc, u.lastname asc, u.firstname asc, e.entity_label asc";
+    . " order by u.lastname asc, u.firstname asc, u.user_id asc, e.entity_label asc";
     
 $db->query($users_query);
 while ($line = $db->fetch_object()) {
