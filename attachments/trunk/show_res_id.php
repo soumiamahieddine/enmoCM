f820dfce6fdac205693f562aab84be6b386e958d
Laurent Giovannoni <laurent.giovannoni@maarch.org>
2014-04-14 14:31:19 +0000

    FIX #0 compare to 1.4

--- attachments/trunk/show_res_id.php
+++ attachments/trunk/show_res_id.php
@@ -2,10 +2,10 @@
 var input_res = window.opener.$('res_id_link');
 //console.log(input_res);
 if (input_res) {
-	input_res.value='<?php echo $_REQUEST['field'][0];?>';
+	input_res.value='<?php echo $_REQUEST['field'];?>';
 }
 else {
-	window.opener.$('res_id').value='<?php echo $_REQUEST['field'][0];?>';
+	window.opener.$('res_id').value='<?php echo $_REQUEST['field'];?>';
 }
 self.close();
 </script>
