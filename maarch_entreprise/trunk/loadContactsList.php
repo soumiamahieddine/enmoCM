a9ccbbd67b2cfcbb06af9f66d5b8c2a4b230dda4
Florian Azizian <florian.azizian@maarch.org>
2014-05-07 11:56:12 +0000

    FEAT #1348 ergonomie affichage liste de contacts dans liste de resultats

--- maarch_entreprise/trunk/loadContactsList.php
+++ maarch_entreprise/trunk/loadContactsList.php
@@ -14,44 +14,44 @@ if (isset($_REQUEST['res_id'])) {
 
                 $db = new dbquery();
                 $db->connect();
-					  
-				$query = "select c.firstname, c.lastname, c.society, c.address_num, c.address_street, c.address_complement, c.address_town, c.address_postal_code, c.address_country ";
-						$query .= "from contacts c, contacts_res cres  ";
-						$query .= "where cres.coll_id = 'letterbox_coll' AND cres.res_id = ".$_REQUEST['res_id']." AND cast (c.contact_id as varchar) = cres.contact_id";
-						
+                      
+                $query = "select c.firstname, c.lastname, c.society, c.address_num, c.address_street, c.address_complement, c.address_town, c.address_postal_code, c.address_country ";
+                        $query .= "from contacts c, contacts_res cres  ";
+                        $query .= "where cres.coll_id = 'letterbox_coll' AND cres.res_id = ".$_REQUEST['res_id']." AND cast (c.contact_id as varchar) = cres.contact_id";
+                        
                 $db->query($query);
 
                 $fetch = '';
                 while ($res = $db->fetch_object()) {
 
                     $return .= '<tr>';
-                        $return .= '<td style="background: transparent; border: 1px dashed rgb(200, 200, 200);">';
-                            $return .= '<blockquote >';
-                                $return .= '<div style="text-align: left; background-color: rgb(230, 230, 230); padding: 5px;">';
+                        $return .= '<td style="background: transparent; border: 0px dashed rgb(200, 200, 200);">';
+                            
+                                $return .= '<div style="text-align: left; background-color: rgb(230, 230, 230); padding: 3px; margin-left: 20px; margin-top: -6px;">';
                                     $return .= $res->firstname . ' ' . $res->lastname . ' ' . $res->society;
-									
-									if(isset($res->address_num) && !empty($res->address_num)){
-										$return .= ': ' . $res->address_num;
-									}									
-									if(isset($res->address_street) && !empty($res->address_street)){
-										$return .= ', ' . $res->address_street;
-									}									
-									if(isset($res->address_complement) && !empty($res->address_complement)){
-										$return .= ', ' . $res->address_complement;
-									}									
-									if(isset($res->address_postal_code) && !empty($res->address_postal_code)){
-										$return .= ', ' . $res->address_postal_code;
-									}									
-									if(isset($res->address_town) && !empty($res->address_town)){
-										$return .= ' ' . $res->address_town;
-									}									
-									if(isset($res->address_country) && !empty($res->address_country)){
-										$return .= ', ' . $res->address_country;
-									}
-												
+                                    
+                                    if(isset($res->address_num) && !empty($res->address_num)){
+                                        $return .= ': ' . $res->address_num;
+                                    }                                   
+                                    if(isset($res->address_street) && !empty($res->address_street)){
+                                        $return .= ', ' . $res->address_street;
+                                    }                                   
+                                    if(isset($res->address_complement) && !empty($res->address_complement)){
+                                        $return .= ', ' . $res->address_complement;
+                                    }                                   
+                                    if(isset($res->address_postal_code) && !empty($res->address_postal_code)){
+                                        $return .= ', ' . $res->address_postal_code;
+                                    }                                   
+                                    if(isset($res->address_town) && !empty($res->address_town)){
+                                        $return .= ' ' . $res->address_town;
+                                    }                                   
+                                    if(isset($res->address_country) && !empty($res->address_country)){
+                                        $return .= ', ' . $res->address_country;
+                                    }
+                                                
                                 $return .= '</div>';
                                 //$return .= '<br />';
-                            $return .= '</blockquote>';
+                            
                         $return .= '</td>';
                     $return .= '</tr>';
                 }
