48df843b2981d727dc41f6a072e87dcfcf376401
Alex Orluc <alex.orluc@maarch.org>
2014-09-02 08:24:55 +0000

    FEAT #1455 add ajax check contact date exp

---
+++ maarch_entreprise/trunk/indexing_searching/contact_check.php
@@ -0,0 +1,44 @@
+<?php
+/*
+*
+*    Copyright 2013 Maarch
+*
+*  This file is part of Maarch Framework.
+*
+*   Maarch Framework is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   Maarch Framework is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*    along with Maarch Framework.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/**
+* @brief    Script to return ajax result
+*
+* @author   Alex ORLUC <dev@maarch.org>
+* @date     $date$
+* @version  $Revision$
+*/
+
+
+if(empty($_POST['contact_id'])){
+echo "success";
+
+}else{
+$db = new dbquery();
+$db->connect();
+$query="SELECT res_id FROM res_view_letterbox WHERE contact_id = '".$_POST['contact_id']."' AND creation_date >= (select CURRENT_DATE + integer '-5') ORDER by creation_date DESC limit 1";
+$db->query($query);
+if ($db->nb_result() > 0){
+echo "fail";
+}else{
+echo "success";
+}
+}
