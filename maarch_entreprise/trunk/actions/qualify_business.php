4f087def8017ca8a95bdb17b4f41cffe405fb3a8
Laurent Giovannoni <laurent.giovannoni@maarch.org>
2014-09-05 13:31:09 +0000

    FEAT #1424 hide history on qualification

--- maarch_entreprise/trunk/actions/qualify_business.php
+++ maarch_entreprise/trunk/actions/qualify_business.php
@@ -838,14 +838,16 @@ $frm_str .= '</div>';
         }
         
         // HISTORY
-        $frm_str .= '<td>';
-        $frm_str .= '|<span onclick="new Effect.toggle(\'history_div\', \'appear\', {delay:0.2});'
-            . 'whatIsTheDivStatus(\'history_div\', \'divStatus_history_div\');return false;" '
-            . 'onmouseover="this.style.cursor=\'pointer\';" class="categorie" style="width:90%;">';
-        $frm_str .= '<span id="divStatus_history_div" style="color:#1C99C5;"><<</span><b>'
-           . '<small>' . _DOC_HISTORY . '</small>';
-        $frm_str .= '</b></span>|';
-        $frm_str .= '</td>';
+        if ($core_tools->test_service('view_doc_history', 'apps', false)) {
+            $frm_str .= '<td>';
+            $frm_str .= '|<span onclick="new Effect.toggle(\'history_div\', \'appear\', {delay:0.2});'
+                . 'whatIsTheDivStatus(\'history_div\', \'divStatus_history_div\');return false;" '
+                . 'onmouseover="this.style.cursor=\'pointer\';" class="categorie" style="width:90%;">';
+            $frm_str .= '<span id="divStatus_history_div" style="color:#1C99C5;"><<</span><b>'
+               . '<small>' . _DOC_HISTORY . '</small>';
+            $frm_str .= '</b></span>|';
+            $frm_str .= '</td>';
+        }
         
         //NOTE
         if ($core_tools->is_module_loaded('notes')) {
