f979752d6cec391773bf2ee174847ba1fcc5c41f
Florian Azizian <florian.azizian@maarch.org>
2014-09-10 14:10:42 +0000

    FEAT #1541 mise à jour des données

--- maarch_entreprise/trunk/maarch_credits.php
+++ maarch_entreprise/trunk/maarch_credits.php
@@ -41,20 +41,20 @@ $core_tools->manage_location_bar($page_path, $page_label, $page_id, $init, $leve
 
 <h1><img src="<?php 
     echo $_SESSION['config']['businessappurl'];
-    ?>static.php?filename=picto_menu_help.gif" alt="" /><?php echo _MAARCH_CREDITS; ?>
+    ?>static.php?filename=picto_menu_help.gif" alt="" /> <?php echo _MAARCH_CREDITS; ?>
 </h1>
 <div id="inner_content" class="clearfix">
     <div class="maarch_credits_left_box" style="height:520px;">
         <h3><?php  echo _MAARCH_CREDITS; ?></h3>
         <hr/>
-        <p><em>Copyright &copy; 2008-2013 Maarch SAS.</em></p>
+        <p><em>Copyright &copy; 2008-2014 Maarch SAS.</em></p>
         <p><?php echo _MAARCH_LICENCE;?> 
-            <a href="http://www.gnu.org/licenses/gpl-3.0-standalone.html">licence GNU GPLv3</a>.</p>
+            <a target="blank" href="http://www.gnu.org/licenses/gpl-3.0-standalone.html">licence GNU GPLv3</a>.</p>
         <div>
             <ul>
-                <li><?php echo _OFFICIAL_WEBSITE;?> : <a href="http://www.maarch.com">http://www.maarch.com</a></li>
-                <li><?php echo _COMMUNITY;?> : <a href="http://www.maarch.org">http://www.maarch.org</a></li>
-                <li><?php echo _DOCUMENTATION;?> : <a href="http://wiki.maarch.org/Accueil">http://wiki.maarch.org/Accueil</a></li>
+                <li><?php echo _OFFICIAL_WEBSITE;?> : <a target="blank" href="http://www.maarch.com">http://www.maarch.com</a></li>
+                <li><?php echo _COMMUNITY;?> : <a target="blank" href="http://www.maarch.org">http://www.maarch.org</a></li>
+                <li><?php echo _DOCUMENTATION;?> : <a target="blank" href="http://wiki.maarch.org/Accueil">http://wiki.maarch.org/Accueil</a></li>
             </ul>
         </div>
         <p>&nbsp;</p>
@@ -63,18 +63,18 @@ $core_tools->manage_location_bar($page_path, $page_label, $page_id, $init, $leve
         <em><?php echo _THANKS_TO_EXT_DEV;?></em>
         <p>&nbsp;</p>
         <ul>
-            <li><a href="http://www.fpdf.org/">Fpdf</a></li>
-            <li><a href="http://www.setasign.de/products/pdf-php-solutions/fpdi/">fpdi</a></li>
-            <li><a href="http://logging.apache.org/log4php/">log4php</a></li>
-            <li><a href="http://chir.ag/tech/download/pdfb">Pdfb</a></li>
-            <li><a href="http://www.foolabs.com/xpdf/">Pdftotext</a></li>
-            <li><a href="http://www.prototypejs.org/">Prototype</a></li>
-            <li><a href="http://script.aculo.us/">Script.aculo.us</a></li>
-            <li><a href="http://www.cyber-sandbox.com/">Tabricator</a></li>
-            <li><a href="http://tafel.developpez.com">Tafel Tree</a></li>
-            <li><a href="http://www.tinybutstrong.com/">Tiny But Strong</a></li>
-            <li><a href="http://www.tinymce.com/">TinyMCE</a></li>
-            <li><a href="http://framework.zend.com/">Zend Lucene Search</a></li>
+            <li><a target="blank" href="http://www.fpdf.org/">Fpdf</a></li>
+            <li><a target="blank" href="http://www.setasign.de/products/pdf-php-solutions/fpdi/">fpdi</a></li>
+            <li><a target="blank" href="http://logging.apache.org/log4php/">log4php</a></li>
+            <li><a target="blank" href="http://chir.ag/tech/download/pdfb">Pdfb</a></li>
+            <li><a target="blank" href="http://www.foolabs.com/xpdf/">Pdftotext</a></li>
+            <li><a target="blank" href="http://www.prototypejs.org/">Prototype</a></li>
+            <li><a target="blank" href="http://script.aculo.us/">Script.aculo.us</a></li>
+            <li><a target="blank" href="http://www.cyber-sandbox.com/">Tabricator</a></li>
+            <li><a target="blank" href="http://tafel.developpez.com">Tafel Tree</a></li>
+            <li><a target="blank" href="http://www.tinybutstrong.com/">Tiny But Strong</a></li>
+            <li><a target="blank" href="http://www.tinymce.com/">TinyMCE</a></li>
+            <li><a target="blank" href="http://framework.zend.com/">Zend Lucene Search</a></li>
         </ul>
     </div>
 
@@ -82,6 +82,7 @@ $core_tools->manage_location_bar($page_path, $page_label, $page_id, $init, $leve
         <h3>Credits</h3>
         <p>&nbsp;</p>
         <ul>
+            <li>Florian AZIZIAN</li>
             <li>Bruno CARLIN</li>
             <li>Carole COTIN</li>
             <li>Driss DEMIRAY</li>
@@ -90,16 +91,19 @@ $core_tools->manage_location_bar($page_path, $page_label, $page_id, $init, $leve
             <li>Jean-Louis ERCOLANI</li>
             <li>Claire FIGUERAS</li>
             <li>Laurent GIOVANNONI</li>
+            <li>Kader KANE</li>
             <li>Yves-Christian KPAKPO</li>
             <li>S&eacute;bastien NANABONDJA</li>
             <li>Fod&eacute; NDIAYE</li>
             <li>C&eacute;dric NDOUMBA</li>
-            <li>Serge THIERRY-MIEG</li>
+            <li>Alex ORLUC</li>
+            <li>Thomas PENARUIZ</li>
             <li>Alexandre STEFANOVIC</li>
+            <li>Serge THIERRY-MIEG</li>
             <li>Cyril VAZQUEZ</li>
             <li>Arnaud VEBER</li>
             <li>Loic VINET</li>
-            <li><em>Merci &agrave; la Mairie de Saint-Germain-En-Laye pour sa contribution sur la recherche plein-texte.</em></li>
+            <li>&nbsp;</li>
             <li><em><?php echo _THANKS_TO_COMMUNITY;?></em></li>
         </ul>
         <p>&nbsp;</p>
