d522bbc3271c11595a72dfc8518e151952248ff0
Laurent Giovannoni <laurent.giovannoni@maarch.org>
2014-09-22 10:26:53 +0000

    FEAT #1466

--- entities/trunk/add_users_entities.php
+++ entities/trunk/add_users_entities.php
@@ -18,7 +18,7 @@ try{
 core_tools::load_lang();
 //core_tools::test_admin('manage_entities', 'entities');
 
-require_once('modules'.DIRECTORY_SEPARATOR.'entities'.DIRECTORY_SEPARATOR.'class'.DIRECTORY_SEPARATOR.'class_manage_entities.php');
+require_once('modules/entities/class/class_manage_entities.php');;
 $ent = new entity();
 $entity_ctrl = new EntityControler();
 //$except = array();
@@ -32,8 +32,40 @@ for($i = 0; $i < count($_SESSION['m_admin']['entity']['entities']); $i++)
 
 $entities = array();
 //$entities = $entity_ctrl->getAllEntities(); // To do : recup l'arborescence des entités
+$entities = $entity_ctrl->getAllEntities();
+//var_dump($entities);
+$EntitiesIdExclusion = array();
+if ($_SESSION['user']['UserId'] != 'superadmin') {
+    $my_tab_entities_id = $ent->get_all_entities_id_user($_SESSION['user']['entities']);
+    $my_tab_entities_id = array_unique($my_tab_entities_id);
+    //var_dump($my_tab_entities_id);
+    if (count($my_tab_entities_id) > 0) {
+        $countEntities = count($entities);
+        for ($cptAllEnt = 0;$cptAllEnt<$countEntities;$cptAllEnt++) {
+            $result = array_search("'" . $entities[$cptAllEnt]->__get('entity_id') . "'", $my_tab_entities_id);
+            //var_dump($result);
+            if (!is_integer(array_search("'" . $entities[$cptAllEnt]->__get('entity_id') . "'", $my_tab_entities_id))) {
+                //echo "UNSET " . $entities[$cptAllEnt]->__get('entity_id') . "<br>";
+                array_push($EntitiesIdExclusion, $entities[$cptAllEnt]->__get('entity_id'));
+                unset($entities[$cptAllEnt]);
+            }
+        }
+    }
+} else {
+    $entities = $entity_ctrl->getAllEntities();
+}
+$entities = array_values($entities);
 
-if($_SESSION['user']['UserId'] == 'superadmin')
+$allEntitiesTree= array();
+$allEntitiesTree = $ent->getShortEntityTreeAdvanced(
+    $allEntitiesTree, 'all', '', $EntitiesIdExclusion, 'all'
+);
+//var_dump($allEntitiesTree);
+
+//echo 'STOOOOOOOOOOOOOOOOOOOP <br />';
+//var_dump($entities);
+
+/*if($_SESSION['user']['UserId'] == 'superadmin')
 {
 
     $entities = $entity_ctrl->getAllEntities();
@@ -48,7 +80,7 @@ else
     		$entities = $entity_ctrl->sortentities($entities);
 	}
 
-}
+}*/
 /*
 if($_SESSION['user']['UserId'] == 'superadmin')
 {
@@ -68,7 +100,7 @@ function in_session_array($entity_id)
     }
     return false;
 }
-{
+/*{
 
 $tab2 = array();
 if ( count($_SESSION['m_admin']['entity']['entities']) > 0 )
@@ -78,12 +110,12 @@ if ( count($_SESSION['m_admin']['entity']['entities']) > 0 )
         array_push($tab2, array('ID'=> $_SESSION['m_admin']['entity']['entities'][$i]['ENTITY_ID'], 'LABEL' => $_SESSION['m_admin']['entity']['entities'][$i]['LABEL']));
     }
 }
-
 $res = $entities;
 for($j=0; $j < count($entities); $j++)
 {
     for($k=0; $k < count($tab2); $k++)
     {
+    
         if($entities[$j]->__get('entity_id') ==  $tab2[$k]['ID'])
         {
             unset($res[$j]);
@@ -95,16 +127,15 @@ $res = array_values($res);
 unset($tab2);
 unset($entities);
 
-}
+}*/
 ?>
 <div class="popup_content">
 <h2 class="tit"><?php  echo USER_ADD_ENTITY;?></h2>
 <form name="chooseEntity" id="chooseEntity" method="get" action="#" class="forms">
 <p>
     <label for="entity_id"> <?php  echo _CHOOSE_ENTITY;?> : </label>
-    <select name="entity_id" id="entity_id" size="10">
+    <select name="entity_id" id="entity_id" size="30">
     <?php
-   
         /*for($i=0; $i<count($entities);$i++)
         {
             $short_label = $entities[$i]->__get('short_label');
@@ -121,7 +152,22 @@ unset($entities);
 
         }*/
 		
-		for($j=0; $j<count($res); $j++)
+        $countAllEntities = count($allEntitiesTree);
+        for ($cptEntities = 0;$cptEntities < $countAllEntities;$cptEntities++) {
+            if (!$allEntitiesTree[$cptEntities]['KEYWORD']) {
+                $optionStr .= '<option data-object_type="entity_id" value="' . $allEntitiesTree[$cptEntities]['ID'] . '"';
+                if ($allEntitiesTree[$cptEntities]['DISABLED']) {
+                    $optionStr .= ' disabled="disabled" class="disabled_entity"';
+                } else {
+                     //$optionStr .= ' style="font-weight:bold;"';
+                }
+                $optionStr .=  '>' 
+                    .  $ent->show_string($allEntitiesTree[$cptEntities]['SHORT_LABEL']) 
+                    . '</option>';
+            }
+        }
+        echo $optionStr;
+		/*for($j=0; $j<count($res); $j++)
 		{
 			$desc = $res[$j]->__get('short_label');
 			if(isset($res[$j]) && !empty($desc))
@@ -130,7 +176,7 @@ unset($entities);
 			<option value="<?php  echo $res[$j]->__get('entity_id') ?>"><?php   echo $res[$j]->__get('short_label'); ?></option>
 		<?php
 			}
-		}
+		}*/
 		
     ?>
     </select>
