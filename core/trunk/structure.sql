01c6b785fd46bc9d9571bf5f0e81ed308c353169
Laurent Giovannoni <laurent.giovannoni@maarch.org>
2014-10-08 18:03:15 +0000

    FEAT #15655 update contact view

--- core/trunk/structure.sql
+++ core/trunk/structure.sql
@@ -3735,14 +3735,14 @@ CREATE OR REPLACE VIEW fp_view_fileplan AS
 
 --view for contacts_v2
 DROP VIEW IF EXISTS view_contacts;
-CREATE VIEW view_contacts AS
-  SELECT 
-  c.contact_id, c.contact_type, c.is_corporate_person, c.society, c.society_short, c.firstname AS contact_firstname, c.lastname AS contact_lastname, 
-  c.title AS contact_title, c.function AS contact_function, c.other_data AS contact_other_data, c.user_id AS contact_user_id, c.entity_id AS contact_entity_id, 
-  c.creation_date, c.update_date,
-  ca.id AS ca_id, ca.contact_purpose_id, ca.departement, ca.firstname, ca.lastname, ca.title, ca.function, ca.occupancy, ca.address_num,
-  ca.address_street, ca.address_complement, ca.address_town, ca.address_postal_code, ca.address_country, ca.phone, ca.email, 
-  ca.website, ca.salutation_header, ca.salutation_footer, ca.other_data, ca.user_id, ca.entity_id, ca.is_private
-  FROM contacts_v2 c
-  RIGHT JOIN contact_addresses ca
-  ON c.contact_id = ca.contact_id;
+CREATE OR REPLACE VIEW view_contacts AS 
+ SELECT c.contact_id, c.contact_type, c.is_corporate_person, c.society, c.society_short, c.firstname AS contact_firstname
+, c.lastname AS contact_lastname, c.title AS contact_title, c.function AS contact_function, c.other_data AS contact_other_data
+, c.user_id AS contact_user_id, c.entity_id AS contact_entity_id, c.creation_date, c.update_date, ca.id AS ca_id
+, ca.contact_purpose_id, ca.departement, ca.firstname, ca.lastname, ca.title, ca.function, ca.occupancy
+, ca.address_num, ca.address_street, ca.address_complement, ca.address_town, ca.address_postal_code, ca.address_country
+, ca.phone, ca.email, ca.website, ca.salutation_header, ca.salutation_footer, ca.other_data, ca.user_id, ca.entity_id, ca.is_private, cp.label as contact_purpose_label, ct.label as contact_type_label
+   FROM contacts_v2 c
+   RIGHT JOIN contact_addresses ca ON c.contact_id = ca.contact_id
+   RIGHT JOIN contact_purposes cp ON ca.contact_purpose_id = cp.id
+   RIGHT JOIN contact_types ct ON c.contact_type = ct.id;
