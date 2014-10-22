48c0c464f4b0567173bfa7f5f1009aa9d5b3150d
Alex Orluc <alex.orluc@maarch.org>
2014-10-22 13:08:48 +0000

    FIX #1624 Pb with conditions

--- maarch_entreprise/trunk/indexing_searching/change_category_actions.php
+++ maarch_entreprise/trunk/indexing_searching/change_category_actions.php
@@ -9,28 +9,22 @@ $_SESSION['category_id'] = $_REQUEST['category_id'];
 $actions = $b->get_actions_from_current_basket(
     $_REQUEST['resId'], $_REQUEST['collId'], 'PAGE_USE', false
 );
-
 if (count($actions) > 0) {
     $frmStr .= '<b>' . _ACTIONS . ' : </b>';
     $frmStr .= '<select name="chosen_action" id="chosen_action">';
-    if (count($actions) > 1) {
-        $frmStr .= '<option value="" selected="selected">' . _CHOOSE_ACTION . '</option>';
-    } else {
-        $frmStr .= '<option value="">' . _CHOOSE_ACTION . '</option>';
-    }
-    if (count($actions) > 1) {
-        for ($indAct = 1; $indAct < count($actions); $indAct ++) {
-            $frmStr .= '<option value="' . $actions[$indAct]['VALUE'] . '"';
-            /*if ($indAct == 1) {
-                $frmStr .= 'selected="selected"';
-            }*/
-            $frmStr .= '>' . $actions[$indAct]['LABEL'] . '</option>';
-        }
-    } else {
-        $frmStr .= '<option value="' . $actions[0]['VALUE'] . '"';
+
+
+    $frmStr .= '<option value="">' . _CHOOSE_ACTION . '</option>';
+    
+    
+    for ($indAct = 0; $indAct < count($actions); $indAct ++) {
+        $frmStr .= '<option value="' . $actions[$indAct]['VALUE'] . '"';
+        if ($indAct == 0) {
             $frmStr .= 'selected="selected"';
-        $frmStr .= '>' . $actions[0]['LABEL'] . '</option>';
+        }
+        $frmStr .= '>' . $actions[$indAct]['LABEL'] . '</option>';
     }
+    
     $frmStr .= '</select> ';
 } else {
     $frmStr .= _NO_AVAILABLE_ACTIONS_FOR_THIS_BASKET;
