a82f0540d1e012c5fecf6ade83750fa90e209e1c
Alex Orluc <alex.orluc@maarch.org>
2014-10-23 11:07:22 +0000

    FIX #1628 fragment arborescence generation

--- folder/trunk/ajax_get_folder.php
+++ folder/trunk/ajax_get_folder.php
@@ -13,8 +13,81 @@
 
 $db = new dbquery();
 $db->connect();
+$db2 = new dbquery();
+$db2->connect();
+$db3 = new dbquery();
+$db3->connect();
 $core = new core_tools();
 $core->load_lang();
+require_once "core/class/class_security.php";
+$sec = new security();
+$whereClause = $sec->get_where_clause_from_coll_id($_SESSION['collection_id_choice']);
+if($_POST['FOLDER_TREE']){
+	$folders = array();
+	$db->query('select folders_system_id, folder_name, parent_id, folder_level from folders WHERE foldertype_id not in (100) AND parent_id='.$_POST["folders_system_id"].' order by folder_id asc');
+	while($row=$db->fetch_array()){
+
+		$db3->query(
+			"select res_id, subject,doctypes_first_level_label,doctypes_second_level_label from res_view_letterbox WHERE folders_system_id in ('".$_POST['folders_system_id']."') AND (".$whereClause.") AND status NOT IN ('DEL')"
+		);
+	
+		/*while($row3 = $db3->fetch_array()) {
+			$folders_doc[] = array(
+			'subject' => $row3['subject'],
+			'doctypes_first_level_label' => $row3['doctypes_first_level_label'],
+			'res_id' => $row3['res_id'],
+			'doctypes_second_level_label' => $row3['doctypes_second_level_label']
+			);
+		}*/
+		$db2->query(
+				"select count(*) as total from res_view_letterbox WHERE folders_system_id in ('".$row['folders_system_id']."') AND (".$whereClause.") AND status NOT IN ('DEL')"
+				);
+		$row2 = $db2->fetch_array();
+		$folders[] = array(
+			'parent_id' => $row['parent_id'],
+			'folders_system_id' => $row['folders_system_id'],
+			'nom_folder' => $row['folder_name'],
+			'folder_level' => $row['folder_level'],
+			'nb_doc' => $row2['total'],
+			'docs' => $folders_doc
+		);
+	}
+	echo json_encode($folders);
+	exit();
+}else if($_POST['FOLDER_TREE_RESET']){
+	$folders = array();
+	$db->query('select folders_system_id, folder_name, parent_id, folder_level from folders WHERE foldertype_id not in (100) AND folders_system_id='.$_POST["folders_system_id"].' order by folder_id asc');
+	while($row=$db->fetch_array()){
+		$db2->query(
+				"select count(*) as total from res_view_letterbox WHERE folders_system_id in ('".$_POST['folders_system_id']."') AND (".$whereClause.") AND status NOT IN ('DEL')"
+				);
+		$row2 = $db2->fetch_array();
+		$folders[] = array(
+			'parent_id' => $row['parent_id'],
+			'folders_system_id' => $row['folders_system_id'],
+			'nom_folder' => $row['folder_name'],
+			'folder_level' => $row['folder_level'],
+			'nb_doc' => $row2['total']
+		);
+	}
+	echo json_encode($folders);
+	exit();
+}else if($_POST['FOLDER_TREE_DOCS']){
+	$docs = array();
+	$db->query("select res_id, subject,doctypes_first_level_label,doctypes_second_level_label, folder_level from res_view_letterbox WHERE folders_system_id in ('".$_POST['folders_system_id']."') AND (".$whereClause.") AND status NOT IN ('DEL')");
+	while($row=$db->fetch_array()){
+		
+		$docs[] = array(
+			'res_id' => $row['res_id'],
+			'subject' => $row['subject'],
+			'doctypes_first_level_label' => $row['doctypes_first_level_label'],
+			'doctypes_second_level_label' => $row['doctypes_second_level_label'],
+			'folder_level' => $row['folder_level']
+		);
+	}
+	echo json_encode($docs);
+	exit();
+}else{
 
 if(!isset($_REQUEST['id_subfolder']) || empty($_REQUEST['id_subfolder']))
 {
@@ -43,4 +116,5 @@ if($db->nb_result() < 1)
 $res = $db->fetch_object();
 echo "{status : 0, value : '".$db->show_string($res->folder_name).', '.$db->show_string($res->subject).' ('.$db->show_string($res->folders_system_id).')'."'}";
 exit();
+}
 ?>
