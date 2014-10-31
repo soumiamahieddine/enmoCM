bba189f02d383215c6d25ca3daef49f478377fc9
Cyril Vazquez <cyril.vazquez@maarch.org>
2014-10-31 09:29:42 +0000

    FIX: #1641 Also check for html tags with onEvent attribute (onload, on click, etc)

--- maarch_entreprise/trunk/index.php
+++ maarch_entreprise/trunk/index.php
@@ -92,7 +92,7 @@ if (isset($_SESSION['user']['UserId']) && isset($_GET['page'])
 // CV 31 oct 2014 : clean request
 foreach ($_REQUEST as $name => $value) {
     if (is_string($value) && strpos($value, "<") !== false) {
-        $value = preg_replace('/(<\/?script[^>]*>|<\?php|<\?\n)/i', "", $value);
+        $value = preg_replace('/(<\/?script[^>]*>|<\w+[\s\n\r]*on[^>]*>|<\?php|<\?[\s|\n|\r])/i', "", $value);
         $_REQUEST[$name] = $value;
     }
 }
