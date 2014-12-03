c3407f61b6c17b9b8349c77223c4815814af0bf8
Alex Orluc <alex.orluc@maarch.org>
2014-12-03 10:15:13 +0000

    FIX #1729 change cond for result send + change display errors smtp

--- sendmail/trunk/batch/process_emails.php
+++ sendmail/trunk/batch/process_emails.php
@@ -178,11 +178,11 @@ while ($state <> 'END') {
 			//Now send the mail
 			$return = $GLOBALS['mailer']->send($to, (string)$mailerParams->type);
 
-			if($return || $return == 0) {
+			if($return == 1) {
 				$exec_result = 'S';
 			} else {
-				$GLOBALS['logger']->write("Errors when sending message through SMTP :" 
-					. implode(', ', $GLOBALS['mailer']->errors), 'ERROR');
+				//$GLOBALS['logger']->write("Errors when sending message through SMTP :" . implode(', ', $GLOBALS['mailer']->errors), 'ERROR');
+                $GLOBALS['logger']->write("Errors when sending message through SMTP :" . $GLOBALS['mailer']->errors[0].': '.$GLOBALS['mailer']->errors[1], 'ERROR');
 				$exec_result = 'E';
 			}
 			//Update emails table
