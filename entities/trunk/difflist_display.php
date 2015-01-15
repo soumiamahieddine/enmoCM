3d657966d674ee9178421209b7c2d5f0814d9213
Alex Orluc <alex.orluc@maarch.org>
2015-01-15 10:51:27 +0000

    FEAT #1853 add width 100% in tables et add % width in td

--- entities/trunk/difflist_display.php
+++ entities/trunk/difflist_display.php
@@ -14,7 +14,7 @@ foreach($roles as $role_id => $role_label) {
     ) { ?>
         <h3 class="sstit"><?php echo $role_label; ?></h3><?php
     if(count($difflist[$role_id]['users']) > 0) { ?>
-        <table cellpadding="0" cellspacing="0" border="0" class="listingsmall liste_diff spec"><?php
+        <table cellpadding="0" cellspacing="0" border="0" class="listingsmall liste_diff spec" style="width:100%;margin:0;"><?php
         $color = ' class="col"';
         for($i=0, $l=count($difflist[$role_id]['users']);
             $i<$l;
@@ -25,24 +25,24 @@ foreach($roles as $role_id => $role_label) {
             if ($color == ' class="col"') $color = ' ';
             else $color = ' class="col"'; ?>
             <tr <?php echo $color; ?> >
-                <td>
+                <td style="width:15%;text-align:center;">
                     <img src="<?php echo $_SESSION['config']['businessappurl'] ?>static.php?filename=manage_users_entities_b_small.gif&module=entities" alt="<?php echo _USER . " " . $role_label ;?>" title="<?php echo _USER . " " . $role_label ; ?>" />
                 </td>
-                <td><?php
+                <td style="width:10%;"><?php
                 if($user['visible'] == 'Y') { ?>
                     <img src="<?php echo $_SESSION['config']['businessappurl']; ?>static.php?filename=picto_authorize.gif&module=entities" alt="<?php echo _VISIBLE; ?>" title="<?php echo _VISIBLE;?>" /><?php
                 } else {?>
                     <img src="<?php echo $_SESSION['config']['businessappurl']; ?>static.php?filename=picto_delete.gif&module=entities" alt="<?php echo _NOT_VISIBLE; ?>" title="<?php echo _NOT_VISIBLE;?>" /><?php
                 } ?>
                 </td>
-                <td ><?php echo $user['lastname'] . " " . $user['firstname'];?></td>
-                <td><?php echo $user['entity_label']; ?></td>
+                <td style="width:37%;"><?php echo $user['lastname'] . " " . $user['firstname'];?></td>
+                <td style="width:38%;"><?php echo $user['entity_label']; ?></td>
             </tr><?php
         } ?>
         </table><?php
     } 
     if(count($difflist[$role_id]['entities']) > 0) { ?>
-        <table cellpadding="0" cellspacing="0" border="0" class="listingsmall liste_diff spec"><?php
+        <table cellpadding="0" cellspacing="0" border="0" class="listingsmall liste_diff spec" style="width:100%;margin:0;"><?php
         $color = ' class="col"';
         for ($i=0, $l=count($difflist[$role_id]['entities']);
             $i<$l;
@@ -52,18 +52,18 @@ foreach($roles as $role_id => $role_label) {
             if ($color == ' class="col"') $color = '';
             else $color = ' class="col"';?>
             <tr <?php echo $color; ?> >
-                <td>
+                <td style="width:15%;text-align:center;">
                     <img src="<?php echo $_SESSION['config']['businessappurl'] ?>static.php?filename=manage_entities_b_small.gif&module=entities" alt="<?php echo _ENTITY . " " . $role_label ;?>" title="<?php echo _ENTITY . " " . $role_label ; ?>" />
                 </td>
-                <td><?php
+                <td style="width:10%;"><?php
                 if($entity['visible'] == 'Y') { ?>
                     <img src="<?php echo $_SESSION['config']['businessappurl']; ?>static.php?filename=picto_authorize.gif&module=entities" alt="<?php echo _VISIBLE; ?>" title="<?php echo _VISIBLE;?>" /><?php
                 } else {?>
                     <img src="<?php echo $_SESSION['config']['businessappurl']; ?>static.php?filename=picto_delete.gif&module=entities" alt="<?php echo _NOT_VISIBLE; ?>" title="<?php echo _NOT_VISIBLE;?>" /><?php
                 } ?>
                 </td>
-                <td ><?php echo $entity['entity_id']; ?></td>
-                <td ><?php echo $entity['entity_label']; ?></td>
+                <td style="width:37%;"><?php echo $entity['entity_id']; ?></td>
+                <td style="width:38%;"><?php echo $entity['entity_label']; ?></td>
             </tr> <?php
         } ?>
         </table><?php
