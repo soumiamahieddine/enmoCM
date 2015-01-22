3ff6b2465a676a6b8f5a63c2d8ad8947181e09bd
Alex Orluc <alex.orluc@maarch.org>
2015-01-22 11:09:43 +0000

    FEAT #1873 add iframe note add

--- entities/trunk/redirect.php
+++ entities/trunk/redirect.php
@@ -64,9 +64,7 @@ require_once('modules/entities/class/class_manage_entities.php');;
     $frm_str .= '</h2><br/><br/>';
     if(!empty($_SESSION['user']['redirect_groupbasket'][$_SESSION['current_basket']['id']][$id_action]['entities']))
     {
-        /** Note add **/
-        $frm_str .='<div style="text-align:center;"><a href="javascript://" onclick="showNotesForm(\''.$_SESSION['config']['businessappurl'].'index.php?display=true&amp;module=notes&amp;page=notes_ajax_content&amp;mode=add&amp;identifier='.$values_str.'&amp;origin=document&amp;coll_id=' . $coll_id . '&amp;size=medium\')" title="Ajouter une note"><img src="http://localhost/maarch_entreprise_trunk/apps/maarch_entreprise/static.php?filename=tool_note.gif&amp;module=notes" alt="Ajouter une note" border="0" style="margin-bottom:-6px;margin-right:10px;">'._ADD_NOTE.'</a></div>';
-        
+      
         $EntitiesIdExclusion = array();
         $entities = $entity_ctrl->getAllEntities();
         $countEntities = count($entities);
@@ -139,7 +137,10 @@ require_once('modules/entities/class/class_manage_entities.php');;
             $frm_str .='</form>';
         $frm_str .='</div>';
     }
+    /** Note add **/
+    $frm_str .='<iframe src="'.$_SESSION['config']['businessappurl'].'index.php?display=true&module=notes&page=note_add&mode=add&identifier=' . $values_str . '&origin=document&coll_id=' . $coll_id . '&redirect" style="width:100%;border:none;height:67%;"></iframe>';
     $frm_str .='<hr />';
+
     $frm_str .='<div align="center">';
             $frm_str .='<input type="button" name="cancel" id="cancel" class="button"  value="'._CANCEL.'" onclick="pile_actions.action_pop();destroyModal(\'modal_'.$id_action.'\');"/>';
     $frm_str .='</div>';
