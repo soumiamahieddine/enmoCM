41cf0c282bac9fe710519a0afe102354a5b37821
Alex Orluc <alex.orluc@maarch.org>
2015-02-09 16:24:49 +0000

    FEAT #1902 add ajax controller to unlink case

---
+++ cases/trunk/js/functions.js
@@ -0,0 +1,25 @@
+function unlink_case(path_manage_script,case_id,res_id){
+	new Ajax.Request(path_manage_script,
+	{
+		method:'post',
+		parameters: { 
+			case_id : case_id,
+			res_id : res_id,
+		},
+		onSuccess: function(answer){
+			eval('response='+answer.responseText);
+			if(response){
+				window.location.href='<?php
+				echo $_SESSION['config']['businessappurl']
+				. 'index.php?page=details&dir=indexing_searching&id=';
+				?>'+res_id;
+			}else{
+				alert('Something went wrong...');
+			}
+
+		},
+		onFailure: function(){
+			alert('Something went wrong...');
+		}
+	});
+}
