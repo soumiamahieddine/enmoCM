53d209c8c4754dbb58d9e6cba53e5f3f0aaa815f
Jean-Louis Ercolani <jeanlouis.ercolani@maarch.org>
2015-02-20 09:26:04 +0000

    FEAT #1985 improve wsi-compliance

--- life_cycle/trunk/class/ws.php
+++ life_cycle/trunk/class/ws.php
@@ -12,20 +12,20 @@ $SOAP_typedef['lcPolicies'] = array(
 );
 $SOAP_dispatch_map['policySave'] = array(
     'in'  => array(
-        'policy' => '{urn:MySoapServer}lcPolicies',
+        'policy' => '{urn:MaarchSoapServer}lcPolicies',
         'mode' => 'string',
     ),
-    'out' => array('out' => '{urn:MySoapServer}returnArray'),
+    'out' => array('out' => '{urn:MaarchSoapServer}returnArray'),
     'method' => "modules/life_cycle#lc_policies::save",
 );
 $SOAP_dispatch_map['policyDelete'] = array(
-                                        'in'  => array('policy' => '{urn:MySoapServer}lcPolicies'),
-                                        'out' => array('out' => '{urn:MySoapServer}returnArray'),
+                                        'in'  => array('policy' => '{urn:MaarchSoapServer}lcPolicies'),
+                                        'out' => array('out' => '{urn:MaarchSoapServer}returnArray'),
                                         'method' => "modules/life_cycle#lc_policies::delete",
                                         );
 $SOAP_dispatch_map['policyGet'] = array(
                                         'in'  => array('policyId' => 'string'),
-                                        'out' => array('out' => '{urn:MySoapServer}lcPolicies'),
+                                        'out' => array('out' => '{urn:MaarchSoapServer}lcPolicies'),
                                         'method' => "modules/life_cycle#lc_policies::getWs",
                                         );
 /**************************************************************************************************/
@@ -39,18 +39,18 @@ $SOAP_typedef['lcCycles'] = array(      'policy_id' => 'string',
                                         'validation_mode' => 'string',
                                         );
 $SOAP_dispatch_map['cycleSave'] = array(
-                                        'in'  => array('cycle' => '{urn:MySoapServer}lcCycles', 'mode' => 'string'),
-                                        'out' => array('out' => '{urn:MySoapServer}returnArray'),
+                                        'in'  => array('cycle' => '{urn:MaarchSoapServer}lcCycles', 'mode' => 'string'),
+                                        'out' => array('out' => '{urn:MaarchSoapServer}returnArray'),
                                         'method' => "modules/life_cycle#lc_cycles::save",
                                         );
 $SOAP_dispatch_map['cycleDelete'] = array(
-                                        'in'  => array('cycle' => '{urn:MySoapServer}lcCycles'),
-                                        'out' => array('out' => '{urn:MySoapServer}returnArray'),
+                                        'in'  => array('cycle' => '{urn:MaarchSoapServer}lcCycles'),
+                                        'out' => array('out' => '{urn:MaarchSoapServer}returnArray'),
                                         'method' => "modules/life_cycle#lc_cycles::delete",
                                         );
 $SOAP_dispatch_map['cycleGet'] = array(
                                         'in'  => array('cycleId' => 'string'),
-                                        'out' => array('out' => '{urn:MySoapServer}lcCycles'),
+                                        'out' => array('out' => '{urn:MaarchSoapServer}lcCycles'),
                                         'method' => "modules/life_cycle#lc_cycles::getWs",
                                         );
 /**************************************************************************************************/
@@ -68,17 +68,17 @@ $SOAP_typedef['lcCycleSteps'] = array(      'policy_id' => 'string',
                                             'postprocess_script' => 'string',
                                             );
 $SOAP_dispatch_map['cycleStepSave'] = array(
-                                        'in'  => array('cycleStep' => '{urn:MySoapServer}lcCycleSteps', 'mode' => 'string'),
-                                        'out' => array('out' => '{urn:MySoapServer}returnArray'),
+                                        'in'  => array('cycleStep' => '{urn:MaarchSoapServer}lcCycleSteps', 'mode' => 'string'),
+                                        'out' => array('out' => '{urn:MaarchSoapServer}returnArray'),
                                         'method' => "modules/life_cycle#lc_cycle_steps::save",
                                         );
 $SOAP_dispatch_map['cycleStepDelete'] = array(
-                                        'in'  => array('cycleStep' => '{urn:MySoapServer}lcCycleSteps'),
-                                        'out' => array('out' => '{urn:MySoapServer}returnArray'),
+                                        'in'  => array('cycleStep' => '{urn:MaarchSoapServer}lcCycleSteps'),
+                                        'out' => array('out' => '{urn:MaarchSoapServer}returnArray'),
                                         'method' => "modules/life_cycle#lc_cycle_steps::delete",
                                         );
 $SOAP_dispatch_map['cycleStepGet'] = array(
                                         'in'  => array('cycleStepId' => 'string'),
-                                        'out' => array('out' => '{urn:MySoapServer}lcCycleSteps'),
+                                        'out' => array('out' => '{urn:MaarchSoapServer}lcCycleSteps'),
                                         'method' => "modules/life_cycle#lc_cycle_steps::getWs",
                                         );
