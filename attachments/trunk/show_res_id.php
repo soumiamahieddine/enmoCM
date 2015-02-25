992689326f390ba8166a1a201f429ab3b63a1bee
Henri Queneau <henri.queneau@maarch.org>
2015-02-25 16:33:49 +0000

    FEAT #1932 select severals documents in the list of results

--- attachments/trunk/show_res_id.php
+++ attachments/trunk/show_res_id.php
@@ -2,10 +2,11 @@
 var input_res = window.opener.$('res_id_link');
 //console.log(input_res);
 if (input_res) {
-	input_res.value='<?php echo $_REQUEST['field'];?>';
+	input_res.value=<?php echo json_encode($_SESSION['stockCheckbox']);?>;
 }
 else {
-	window.opener.$('res_id').value='<?php echo $_REQUEST['field'];?>';
+	var tab = <?php echo json_encode($_SESSION['stockCheckbox']);?>;
+	window.opener.$('res_id').value=tab;
 }
 self.close();
 </script>
