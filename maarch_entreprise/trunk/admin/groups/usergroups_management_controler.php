6678ed06708320687ef3cda08e821a88d2804898
Alex Orluc <alex.orluc@maarch.org>
2015-03-09 15:58:37 +0000

    FEAT #1928 add alert if users in group del

--- maarch_entreprise/trunk/admin/groups/usergroups_management_controler.php
+++ maarch_entreprise/trunk/admin/groups/usergroups_management_controler.php
@@ -345,6 +345,13 @@ function displayList(){
 function displayDel($groupId)
 {
     $ugc = new usergroups_controler();
+
+    //information users exists in groups
+    if(!empty($ugc->getUsers($groupId))){
+        $usersGroups=implode(",", $ugc->getUsers($groupId));
+        echo '<script type="text/javascript">alert("Pour Information,\n\nLes utilisateurs suivants ont \u00e9t\u00e9 dissoci\u00e9s du groupe :\n'.$usersGroups.'")</script>';
+    }
+
     $group = $ugc->get($groupId);
     if (isset($group) && isset($groupId) && ! empty($groupId)) {
         $control = array();
