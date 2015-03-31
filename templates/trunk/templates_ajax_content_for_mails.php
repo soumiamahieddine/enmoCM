48ea0dc544d03cc9438ebf2dc2dd851ecb4105d9
Sébastien Nana <sebastien.nana@maarch.org>
2015-03-31 10:12:27 +0000

    FEAT #1933[sendmail] Ajout de la fusion des métadonnées

--- templates/trunk/templates_ajax_content_for_mails.php
+++ templates/trunk/templates_ajax_content_for_mails.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once 'modules/templates/class/templates_controler.php';
+require_once 'core/class/class_security.php';
 $templateController = new templates_controler();
 
 if ((! isset($_REQUEST['templateId']) || empty($_REQUEST['templateId']))) {
@@ -9,12 +10,20 @@ if ((! isset($_REQUEST['templateId']) || empty($_REQUEST['templateId']))) {
     exit();
 }
 
-$template = $templateController->get($_REQUEST['templateId']);
+$sec = new security();
+$res_view = $sec->retrieve_view_from_coll_id('letterbox_coll');
 
+$params = array(
+	'res_id' => $_GET['id'],
+	'coll_id'=> "letterbox_coll",
+	'res_view'=> $res_view
+	);
+
+$template = $templateController->get($_REQUEST['templateId']);
+$template->template_content =  $templateController->merge($_REQUEST['templateId'], $params, 'content');
 $template->template_content = str_replace("\r\n", "\n", $template->template_content);
 $template->template_content = str_replace("\r", "\n", $template->template_content);
 $template->template_content = str_replace("\n", "\\n ", $template->template_content);
 
 echo "{status : 0, content : '" . addslashes($template->template_content) . "'}";
 exit();
-
