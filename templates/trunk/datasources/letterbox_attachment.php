f4f48119aecb09f6a06356cdef90b7b02709e681
Alex Orluc <alex.orluc@maarch.org>
2015-04-07 14:27:44 +0000

    FEAT #1688 fix merge contact

--- templates/trunk/datasources/letterbox_attachment.php
+++ templates/trunk/datasources/letterbox_attachment.php
@@ -1,5 +1,4 @@
 <?php
-
 /*********************************************************************************
 ** Get aditionnal data to merge template
 **
@@ -17,26 +16,27 @@ $datasources['res_letterbox'] = array();
 $dbDatasource->query("SELECT * FROM " . $res_view . " WHERE res_id = " . $res_id . "");
 $datasources['res_letterbox'][] = $dbDatasource->fetch_assoc();
 
-// Contact from mail
-if ($datasources['res_letterbox'][0]['exp_contact_id'] <> '') {
-    $datasources['contact'] = array();
-    $dbDatasource->query("SELECT * FROM view_contacts WHERE contact_id = ".$datasources['res_letterbox'][0]['exp_contact_id']." and ca_id = ".$datasources['res_letterbox'][0]['address_id']);
-    $myContact = $dbDatasource->fetch_array();
-	$myContact['title'] = $_SESSION['mail_titles'][$myContact['title']];
-    $myContact['contact_type'] = $contacts->get_label_contact($myContact['contact_type'], $_SESSION['tablename']['contact_types']);
-    $myContact['contact_purpose_id'] = $contacts->get_label_contact($myContact['contact_purpose_id'], $_SESSION['tablename']['contact_purposes']);
-	$datasources['contact'][] = $myContact;
-}
-if ($datasources['res_letterbox'][0]['dest_contact_id'] <> '') {
-    $datasources['contact'] = array();
-    $dbDatasource->query("SELECT * FROM view_contacts WHERE contact_id = ".$datasources['res_letterbox'][0]['dest_contact_id']." and ca_id = ".$datasources['res_letterbox'][0]['address_id']);
-    $myContact = $dbDatasource->fetch_array();
-	$myContact['title'] = $_SESSION['mail_titles'][$myContact['title']];
-    $myContact['contact_type'] = $contacts->get_label_contact($myContact['contact_type'], $_SESSION['tablename']['contact_types']);
-    $myContact['contact_purpose_id'] = $contacts->get_label_contact($myContact['contact_purpose_id'], $_SESSION['tablename']['contact_purposes']);
-	$datasources['contact'][] = $myContact;
-}
 
+//multicontact
+$dbDatasource->query("SELECT * FROM contacts_res WHERE res_id = " . $res_id . " AND contact_id ='".$res_contact_id."'");
+$datasources['res_letterbox_contact'][] = $dbDatasource->fetch_assoc();
+if ($datasources['res_letterbox_contact'][0]['contact_id'] <> '') {
+    $datasources['contact'] = array();
+    $dbDatasource->query("SELECT * FROM view_contacts WHERE contact_id = ".$datasources['res_letterbox_contact'][0]['contact_id']." and ca_id = ".$datasources['res_letterbox_contact'][0]['address_id']);
+    $myContact = $dbDatasource->fetch_array();
+    $myContact['contact_type'] = $contacts->get_label_contact($myContact['contact_type'], $_SESSION['tablename']['contact_types']);
+    $myContact['contact_purpose_id'] = $contacts->get_label_contact($myContact['contact_purpose_id'], $_SESSION['tablename']['contact_purposes']);
+	$datasources['contact'][] = $myContact;
+// single Contact
+}else{
+
+    $datasources['contact'] = array();
+    $dbDatasource->query("SELECT * FROM view_contacts WHERE contact_id = ".$datasources['res_letterbox'][0]['contact_id']." and ca_id = ".$datasources['res_letterbox'][0]['address_id']);
+    $myContact = $dbDatasource->fetch_array();
+    $myContact['contact_type'] = $contacts->get_label_contact($myContact['contact_type'], $_SESSION['tablename']['contact_types']);
+    $myContact['contact_purpose_id'] = $contacts->get_label_contact($myContact['contact_purpose_id'], $_SESSION['tablename']['contact_purposes']);
+    $datasources['contact'][] = $myContact;
+}
 // Notes
 $datasources['notes'] = array();
 $dbDatasource->query("SELECT notes.*, users.firstname, users.lastname FROM notes left join users on notes.user_id = users.user_id WHERE coll_id = '".$coll_id."' AND identifier = ".$res_id."");
