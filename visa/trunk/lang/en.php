2f3b6894053291458fa83718fda0e4d0678b61fc
Henri Queneau <henri.queneau@maarch.org>
2015-04-10 09:06:12 +0000

    FEAT #2166 add the language file for this module

---
+++ visa/trunk/lang/en.php
@@ -0,0 +1,29 @@
+<?php
+// CIRCUIT DE VISA
+if (!defined('_VISA_WORKFLOW'))
+    define('_VISA_WORKFLOW', 'Visa Workflow');
+if (!defined('_VISA_WORKFLOW_COMMENT'))   
+	define('_VISA_WORKFLOW_COMMENT', 'Management of visa workflow');
+if (!defined('_VIEW_VISA_WORKFLOW'))
+    define('_VIEW_VISA_WORKFLOW', 'Visualization of visa workflow');
+if (!defined('_VIEW_VISA_WORKFLOW_DESC'))
+    define('_VIEW_VISA_WORKFLOW_DESC', 'Allow to visualize the visa workflow in the diffusion list zone and in advancement.');
+if (!defined('_CONFIG_VISA_WORKFLOW'))
+    define('_CONFIG_VISA_WORKFLOW', 'Configuration visa workflow');
+if (!defined('_CONFIG_VISA_WORKFLOW_DESC'))
+    define('_CONFIG_VISA_WORKFLOW_DESC', 'Allow to set the visa workflow to be taken by mail.');
+	
+if (!defined('_VISA_ANSWERS'))
+    define('_VISA_ANSWERS', 'target the draft responses');
+	
+if (!defined('_VISA_ANSWERS_DESC'))
+    define('_VISA_ANSWERS_DESC', 'Allow to target the draft responses');
+
+// CIRCUIT D'AVIS
+if (!defined('_AVIS_WORKFLOW'))
+    define('_AVIS_WORKFLOW', 'Avis workflow');
+if (!defined('_CONFIG_AVIS_WORKFLOW'))
+    define('_CONFIG_AVIS_WORKFLOW', 'Configuration of avis workflow');
+if (!defined('_CONFIG_AVIS_WORKFLOW_DESC'))
+    define('_CONFIG_AVIS_WORKFLOW_DESC', 'Allow to set the avis workflow of the mail');
+?>
