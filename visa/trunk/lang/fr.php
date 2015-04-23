df3743ead9510953e18d28cedab66f91fcbff9a4
Nicolas Couture <couture@docimsol.com>
2015-04-23 12:46:54 +0000

    FEAT #2059 Fichier de traduction complété

--- visa/trunk/lang/fr.php
+++ visa/trunk/lang/fr.php
@@ -15,7 +15,8 @@ if (!defined('_CONFIG_VISA_WORKFLOW'))
     define('_CONFIG_VISA_WORKFLOW', 'Configuration du circuit de visa');
 if (!defined('_CONFIG_VISA_WORKFLOW_DESC'))
     define('_CONFIG_VISA_WORKFLOW_DESC', 'Permet de configurer le circuit de visa que devra prendre le courrier');
-	
+if (!defined('_EMPTY_USER_LIST'))
+    define('_EMPTY_USER_LIST', 'La liste des utilisateurs est vide');
 if (!defined('_VISA_ANSWERS'))
     define('_VISA_ANSWERS', 'Viser les projets de réponse');
 	
@@ -32,4 +33,11 @@ if (!defined('_CONFIG_AVIS_WORKFLOW_DESC'))
 
 if (!defined('_THUMBPRINT'))
     define('_THUMBPRINT', 'Empreinte numérique');
+
+// Actions
+if (!defined('_SEND_MAIL'))    define( '_SEND_MAIL', 'Envoi du dossier par courriel');
+if (!defined('_IMPRIM_DOSSIER'))    define( '_IMPRIM_DOSSIER', 'Impression du dossier');
+if (!defined('_PROCEED_WORKFLOW'))    define( '_PROCEED_WORKFLOW', 'Poursuivre le circuit de visa');
+if (!defined('_VISA_MAIL'))    define( '_VISA_MAIL', 'Viser le courrier');
+if (!defined('_PREPARE_VISA'))    define( '_PREPARE_VISA', 'Préparer le circuit de visa');
 ?>
