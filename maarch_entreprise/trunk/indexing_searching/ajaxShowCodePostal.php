5e1df69878816064a1cbb1f278e2c9c1d454fe1c
Sébastien Nana <sebastien.nana@maarch.org>
2015-04-24 13:34:00 +0000

    FIX #2256 [Fichier csv code_postaux] Ajouter les apostrophes + déplacer le fichier dans tools

--- maarch_entreprise/trunk/indexing_searching/ajaxShowCodePostal.php
+++ maarch_entreprise/trunk/indexing_searching/ajaxShowCodePostal.php
@@ -1,15 +1,21 @@
 <?php
+
+require_once("core".DIRECTORY_SEPARATOR."class".DIRECTORY_SEPARATOR."class_request.php");
+
 $row = 1;
-$handle = fopen($_SESSION['config']['businessappurl']."indexing_searching/code_postaux_v201410.csv", "r");
+$handle = fopen($_SESSION['config']['businessappurl']."tools/referentiel/code_postaux_v201410.csv", "r");
+
+$request = new request();
+
 echo "<ul>\n";
 while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) {
     $num = count($data);
     
     
     for ($c=0; $c < $num; $c++) {
-    	$pos = strpos($data[3], $_POST['what']);
+    	$pos = stripos($data[3], $_POST['what']);
     	if ($pos === 0) {
-    		echo "<li id='".$data[2].",".trim($data[3])."'>".$data[2]." - ".trim($data[3])."</li>\n";
+    		echo '<li id="'.$data[2].','. $request->show_string(trim($data[3])) .'">'.$data[2].' - '.$request->show_string(trim($data[3])).'</li>';
    			 break;
 }
     }
