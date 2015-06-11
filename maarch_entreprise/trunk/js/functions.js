4d4f2b867df299d79711b3a2c7d945fec71630cc
Florian Azizian <florian.azizian@maarch.org>
2015-06-11 13:45:53 +0000

    FIX #2464 add functions from life cycle module

--- maarch_entreprise/trunk/js/functions.js
+++ maarch_entreprise/trunk/js/functions.js
@@ -3058,3 +3058,38 @@ function loadAddressAttached(contact_id, select){
         }
     });
 }
+
+function saveSizeInBytes() {
+    if(!isNaN($('size_limit_number').value)) {
+        if($('size_format').value == "MB") {
+            $('size_limit_hidden').value = $('size_limit_number').value * (1000 * 1000);
+        }
+        if($('size_format').value == "GB") {
+            $('size_limit_hidden').value = $('size_limit_number').value * (1000 * 1000 * 1000);
+        }
+        if($('size_format').value == "TB") {
+            $('size_limit_hidden').value = $('size_limit_number').value * (1000 * 1000 * 1000 * 1000);
+        }
+    } else {
+        window.alert('WRONG FORMAT');
+    }   
+}
+
+function convertSize() {
+    if(!isNaN($('size_limit_number').value)) {
+        if($('size_format').value == "MB") {
+            $('size_limit_number').value = $('size_limit_hidden').value / (1000 * 1000);
+            $('actual_size_number').value = $('actual_size_hidden').value / (1000 * 1000);
+        }
+        if($('size_format').value == "GB") {
+            $('size_limit_number').value = $('size_limit_hidden').value / (1000 * 1000 * 1000);
+            $('actual_size_number').value = $('actual_size_hidden').value / (1000 * 1000 * 1000);
+        }
+        if($('size_format').value == "TB") {
+            $('size_limit_number').value = $('size_limit_hidden').value / (1000 * 1000 * 1000 * 1000);
+            $('actual_size_number').value = $('actual_size_hidden').value / (1000 * 1000 * 1000 * 1000);
+        }
+    } else {
+        window.alert('WRONG FORMAT');
+    }
+}
