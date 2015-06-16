507e06bacbaae1538b193492ca6dbe2908eaeadc
Laurent Giovannoni <laurent.giovannoni@maarch.org>
2015-06-16 09:46:20 +0000

    FEAT #2481 nettoyage

--- maarch_entreprise/trunk/admin/maarch_config_tool/generate_map.bat
+++
@@ -1,3 +0,0 @@
-cls
-cd "C:\xampp\php"
-php C:\xampp\htdocs\maarch_trunk\apps\maarch_entreprise\admin\maarch_config_tool\generate_map_of_maarch_config.php C:\xampp\htdocs\maarch_trunk\apps\maarch_entreprise\xml\config.xml C:\xampp\htdocs\maarch_trunk\
