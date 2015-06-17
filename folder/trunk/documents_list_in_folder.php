d93ffd44b55b2877d9db8320ff706b3ab5ba9baa
Florian Azizian <florian.azizian@maarch.org>
2015-06-17 08:56:52 +0000

    FEAT #2488 remove useless function for maarch courrier

--- folder/trunk/documents_list_in_folder.php
+++ folder/trunk/documents_list_in_folder.php
@@ -32,7 +32,7 @@
 require_once "core".DIRECTORY_SEPARATOR."class".DIRECTORY_SEPARATOR."class_request.php";
 require_once "core".DIRECTORY_SEPARATOR."class".DIRECTORY_SEPARATOR."class_security.php";
 require_once "apps".DIRECTORY_SEPARATOR.$_SESSION['config']['app_id'].DIRECTORY_SEPARATOR
-            ."class".DIRECTORY_SEPARATOR."class_contacts.php";
+            ."class".DIRECTORY_SEPARATOR."class_contacts_v2.php";
 require_once "core".DIRECTORY_SEPARATOR."class".DIRECTORY_SEPARATOR."class_manage_status.php";
 require_once "apps".DIRECTORY_SEPARATOR.$_SESSION['config']['app_id'].DIRECTORY_SEPARATOR
             ."class".DIRECTORY_SEPARATOR."class_lists.php";
