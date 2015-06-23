b484131a5d70a5e431e3bb4edc4477af355bc99d
Florian Azizian <florian.azizian@maarch.org>
2015-06-23 09:22:47 +0000

    FIX #2481 useless name appear in menu

--- maarch_entreprise/trunk/admin/contacts/contact_purposes/contact_purposes_up.php
+++ maarch_entreprise/trunk/admin/contacts/contact_purposes/contact_purposes_up.php
@@ -231,8 +231,7 @@ if (isset($_REQUEST['valid'])) {
 		                </p>
 		                <div id="menunav" style="display: none;">
 		                <?php
-		                echo '<div class="header_menu"><div class="user_name_menu">'.functions::xecho($_SESSION['user']['FirstName'])
-		                	.' '.functions::xecho($_SESSION['user']['LastName']).'</div></div>';
+		                echo '<div class="header_menu"></div>';
 		                echo '<div class="header_menu_blank">&nbsp;</div>';?>
 		                <ul  >
 		                    <?php
