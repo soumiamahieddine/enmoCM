c4ff82498cc30862abc13a995f2e80f1433a442b
Laurent Giovannoni <laurent.giovannoni@maarch.org>
2015-06-25 16:54:05 +0000

    FEAT #2508 replace echo

--- sendmail/trunk/lang/fr.php
+++ sendmail/trunk/lang/fr.php
@@ -19,114 +19,114 @@
  *    along with Maarch Framework.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-if (!defined('_SENDMAIL'))
-    define('_SENDMAIL', 'Envoi de courriels');
-if (!defined('_SENDMAIL_COMMENT'))
-    define('_SENDMAIL_COMMENT', 'Envoi de courriels');
-if (!defined('_EMAIL_LIST'))
-    define('_EMAIL_LIST', 'Courriels');
-if (!defined('_EMAIL_LIST_DESC'))
-    define('_EMAIL_LIST_DESC', 'Liste des courriels');
-if (!defined('_SENDED_EMAILS'))
-    define('_SENDED_EMAILS', 'Courriels');
+if (!defined("_SENDMAIL"))
+    define("_SENDMAIL", "Envoi de courriels");
+if (!defined("_SENDMAIL_COMMENT"))
+    define("_SENDMAIL_COMMENT", "Envoi de courriels");
+if (!defined("_EMAIL_LIST"))
+    define("_EMAIL_LIST", "Courriels");
+if (!defined("_EMAIL_LIST_DESC"))
+    define("_EMAIL_LIST_DESC", "Liste des courriels");
+if (!defined("_SENDED_EMAILS"))
+    define("_SENDED_EMAILS", "Courriels");
  
 //STATUS
-if (!defined('_EMAIL_DRAFT'))
-    define('_EMAIL_DRAFT','Brouillon');
-if (!defined('_EMAIL_WAIT'))
-    define('_EMAIL_WAIT','En attente d\'envoi');
-if (!defined('_EMAIL_IN_PROGRESS'))
-    define('_EMAIL_IN_PROGRESS','En cours d\'envoi');
-if (!defined('_EMAIL_SENT'))
-    define('_EMAIL_SENT','Envoy&eacute;');
-if (!defined('_EMAIL_ERROR'))
-    define('_EMAIL_ERROR','Erreur lors de l\'envoi');
+if (!defined("_EMAIL_DRAFT"))
+    define("_EMAIL_DRAFT","Brouillon");
+if (!defined("_EMAIL_WAIT"))
+    define("_EMAIL_WAIT","En attente d'envoi");
+if (!defined("_EMAIL_IN_PROGRESS"))
+    define("_EMAIL_IN_PROGRESS","En cours d'envoi");
+if (!defined("_EMAIL_SENT"))
+    define("_EMAIL_SENT","Envoyé");
+if (!defined("_EMAIL_ERROR"))
+    define("_EMAIL_ERROR","Erreur lors de l'envoi");
     
 //FORM
-if (!defined('_FROM'))
-    define('_FROM','Exp&eacute;diteur');
-if (!defined('_FROM_SHORT'))
-    define('_FROM_SHORT','De');
-if (!defined('_SEND_TO'))
-    define('_SEND_TO','Destinataire');
-if (!defined('_SEND_TO_SHORT'))
-    define('_SEND_TO_SHORT','&Agrave;');
-if (!defined('_COPY_TO'))
-    define('_COPY_TO','En copie');
-if (!defined('_COPY_TO_SHORT'))
-    define('_COPY_TO_SHORT','Cc');
-if (!defined('_COPY_TO_INVISIBLE'))
-    define('_COPY_TO_INVISIBLE','En copie invisible');
-if (!defined('_COPY_TO_INVISIBLE_SHORT'))
-    define('_COPY_TO_INVISIBLE_SHORT','Cci');
-if (!defined('_JOINED_FILES'))
-    define('_JOINED_FILES','Fichiers joints');
-if (!defined('_SHOW_OTHER_COPY_FIELDS'))
-    define('_SHOW_OTHER_COPY_FIELDS','Afficher/masquer les champs Cc et Cci');
-if (!defined('_EMAIL_OBJECT'))
-    define('_EMAIL_OBJECT','Objet');
-if (!defined('_HTML_OR_RAW'))
-    define('_HTML_OR_RAW','Mise en forme avanc&eacute;e / Texte brut');  
-if (!defined('_DEFAULT_BODY'))
-    define('_DEFAULT_BODY','Votre message est pr&ecirc;t &agrave;  &ecirc;tre envoy&eacute; avec les fichiers joints suivants :');
-if (!defined('_NOTES_FILE'))
-    define('_NOTES_FILE', 'Notes du document');
-if (!defined('_EMAIL_WRONG_FORMAT'))
-    define('_EMAIL_WRONG_FORMAT', 'L\'adresse courriel n&rsquo;est pas dans le bon format');
+if (!defined("_FROM"))
+    define("_FROM","Expéditeur");
+if (!defined("_FROM_SHORT"))
+    define("_FROM_SHORT","De");
+if (!defined("_SEND_TO"))
+    define("_SEND_TO","Destinataire");
+if (!defined("_SEND_TO_SHORT"))
+    define("_SEND_TO_SHORT","à");
+if (!defined("_COPY_TO"))
+    define("_COPY_TO","En copie");
+if (!defined("_COPY_TO_SHORT"))
+    define("_COPY_TO_SHORT","Cc");
+if (!defined("_COPY_TO_INVISIBLE"))
+    define("_COPY_TO_INVISIBLE","En copie invisible");
+if (!defined("_COPY_TO_INVISIBLE_SHORT"))
+    define("_COPY_TO_INVISIBLE_SHORT","Cci");
+if (!defined("_JOINED_FILES"))
+    define("_JOINED_FILES","Fichiers joints");
+if (!defined("_SHOW_OTHER_COPY_FIELDS"))
+    define("_SHOW_OTHER_COPY_FIELDS","Afficher/masquer les champs Cc et Cci");
+if (!defined("_EMAIL_OBJECT"))
+    define("_EMAIL_OBJECT","Objet");
+if (!defined("_HTML_OR_RAW"))
+    define("_HTML_OR_RAW","Mise en forme avancée / Texte brut");  
+if (!defined("_DEFAULT_BODY"))
+    define("_DEFAULT_BODY","Votre message est prêt à  être envoyé avec les fichiers joints suivants :");
+if (!defined("_NOTES_FILE"))
+    define("_NOTES_FILE", "Notes du document");
+if (!defined("_EMAIL_WRONG_FORMAT"))
+    define("_EMAIL_WRONG_FORMAT", "L'adresse courriel n'est pas dans le bon format");
     
     
 //ERROR
-if (!defined('_EMAIL_NOT_EXIST'))
-    define('_EMAIL_NOT_EXIST', 'Ce courriel n&rsquo;existe pas');
+if (!defined("_EMAIL_NOT_EXIST"))
+    define("_EMAIL_NOT_EXIST", "Ce courriel n'existe pas");
 
 //ADD
-if (!defined('_NEW_EMAIL'))
-    define('_NEW_EMAIL','Nouveau message');
-if (!defined('_CREATE_EMAIL'))
-    define('_CREATE_EMAIL', 'Cr&eacute;er');
-if (!defined('_EMAIL_ADDED'))
-    define('_EMAIL_ADDED', 'Courriel ajout&eacute;');
+if (!defined("_NEW_EMAIL"))
+    define("_NEW_EMAIL","Nouveau message");
+if (!defined("_CREATE_EMAIL"))
+    define("_CREATE_EMAIL", "Créer");
+if (!defined("_EMAIL_ADDED"))
+    define("_EMAIL_ADDED", "Courriel ajouté");
     
 //SEND
-if (!defined('_SEND_EMAIL'))
-    define('_SEND_EMAIL','Envoyer'); 
-if (!defined('_RESEND_EMAIL'))
-    define('_RESEND_EMAIL','Renvoyer');
+if (!defined("_SEND_EMAIL"))
+    define("_SEND_EMAIL","Envoyer"); 
+if (!defined("_RESEND_EMAIL"))
+    define("_RESEND_EMAIL","Renvoyer");
     
 //SAVE
-if (!defined('_SAVE_EMAIL'))
-    define('_SAVE_EMAIL','Enregistrer');
+if (!defined("_SAVE_EMAIL"))
+    define("_SAVE_EMAIL","Enregistrer");
     
 //READ
-if (!defined('_READ_EMAIL'))
-    define('_READ_EMAIL','Afficher le courriel');
+if (!defined("_READ_EMAIL"))
+    define("_READ_EMAIL","Afficher le courriel");
     
 //TRANSFER
-if (!defined('_TRANSFER_EMAIL'))
-    define('_TRANSFER_EMAIL','Transf&eacute;rer le courriel');
+if (!defined("_TRANSFER_EMAIL"))
+    define("_TRANSFER_EMAIL","Transférer le courriel");
     
 //EDIT    
-if (!defined('_EDIT_EMAIL'))
-    define('_EDIT_EMAIL', 'Modifier le courriel');
-if (!defined('_SAVE_EMAIL'))
-    define('_SAVE_EMAIL', 'Enregistrer');
-if (!defined('_SAVE_COPY_EMAIL'))
-    define('_SAVE_COPY_EMAIL', 'Enregistrer une copie');
-if (!defined('_EMAIL_UPDATED'))
-    define('_EMAIL_UPDATED', 'Email modifi&eacute;');
+if (!defined("_EDIT_EMAIL"))
+    define("_EDIT_EMAIL", "Modifier le courriel");
+if (!defined("_SAVE_EMAIL"))
+    define("_SAVE_EMAIL", "Enregistrer");
+if (!defined("_SAVE_COPY_EMAIL"))
+    define("_SAVE_COPY_EMAIL", "Enregistrer une copie");
+if (!defined("_EMAIL_UPDATED"))
+    define("_EMAIL_UPDATED", "Email modifié");
  
 //REMOVE 
-if (!defined('_REMOVE_EMAIL'))
-    define('_REMOVE_EMAIL', 'Supprimer');
-if (!defined('_REMOVE_EMAILS'))
-    define('_REMOVE_EMAILS', 'Supprimer les courriels');
-if (!defined('_REALLY_REMOVE_EMAIL'))
-    define('_REALLY_REMOVE_EMAIL', 'Voulez-vous supprimez le message');
-if (!defined('_EMAIL_REMOVED'))
-    define('_EMAIL_REMOVED', 'Courriel supprim&eacute;');
+if (!defined("_REMOVE_EMAIL"))
+    define("_REMOVE_EMAIL", "Supprimer");
+if (!defined("_REMOVE_EMAILS"))
+    define("_REMOVE_EMAILS", "Supprimer les courriels");
+if (!defined("_REALLY_REMOVE_EMAIL"))
+    define("_REALLY_REMOVE_EMAIL", "Voulez-vous supprimez le message");
+if (!defined("_EMAIL_REMOVED"))
+    define("_EMAIL_REMOVED", "Courriel supprimé");
 
 
-if (!defined('_Label_ADD_TEMPLATE_MAIL'))
-    define('_Label_ADD_TEMPLATE_MAIL', 'Modèle de courriel');
-if (!defined('_ADD_TEMPLATE_MAIL'))
-    define('_ADD_TEMPLATE_MAIL', 'Sélectionnez le modèle désiré');
+if (!defined("_Label_ADD_TEMPLATE_MAIL"))
+    define("_Label_ADD_TEMPLATE_MAIL", "Modèle de courriel");
+if (!defined("_ADD_TEMPLATE_MAIL"))
+    define("_ADD_TEMPLATE_MAIL", "Sélectionnez le modèle désiré");
