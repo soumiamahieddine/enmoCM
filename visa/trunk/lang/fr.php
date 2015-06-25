914a15b3baced7ab58cff19ecbb42250485088d1
Laurent Giovannoni <laurent.giovannoni@maarch.org>
2015-06-25 16:55:07 +0000

    FEAT #2517 replace echo

--- visa/trunk/lang/fr.php
+++ visa/trunk/lang/fr.php
@@ -1,56 +1,55 @@
 <?php
-if (!defined('_SIGN_DOCS'))
-    define('_SIGN_DOCS', 'Signer les documents');
+if (!defined("_SIGN_DOCS"))
+    define("_SIGN_DOCS", "Signer les documents");
 
 // CIRCUIT DE VISA
-if (!defined('_VISA_WORKFLOW'))
-    define('_VISA_WORKFLOW', 'Circuit de visa');
-if (!defined('_VISA_WORKFLOW_COMMENT'))   
-	define('_VISA_WORKFLOW_COMMENT', 'Gestion du circuit de visa');
-if (!defined('_VIEW_VISA_WORKFLOW'))
-    define('_VIEW_VISA_WORKFLOW', 'Visualisation du circuit de visa');
-if (!defined('_VIEW_VISA_WORKFLOW_DESC'))
-    define('_VIEW_VISA_WORKFLOW_DESC', 'Permet de visualiser le circuit de visa dans les parties de liste de diffusion et dans celles d\'avancement.');
-if (!defined('_CONFIG_VISA_WORKFLOW'))
-    define('_CONFIG_VISA_WORKFLOW', 'Configuration du circuit de visa');
-if (!defined('_CONFIG_VISA_WORKFLOW_DESC'))
-    define('_CONFIG_VISA_WORKFLOW_DESC', 'Permet de configurer le circuit de visa que devra prendre le courrier');
-if (!defined('_EMPTY_USER_LIST'))
-    define('_EMPTY_USER_LIST', 'La liste des utilisateurs est vide');
-if (!defined('_VISA_ANSWERS'))
-    define('_VISA_ANSWERS', 'Viser les projets de réponse');
+if (!defined("_VISA_WORKFLOW"))
+    define("_VISA_WORKFLOW", "Circuit de visa");
+if (!defined("_VISA_WORKFLOW_COMMENT"))   
+	define("_VISA_WORKFLOW_COMMENT", "Gestion du circuit de visa");
+if (!defined("_VIEW_VISA_WORKFLOW"))
+    define("_VIEW_VISA_WORKFLOW", "Visualisation du circuit de visa");
+if (!defined("_VIEW_VISA_WORKFLOW_DESC"))
+    define("_VIEW_VISA_WORKFLOW_DESC", "Permet de visualiser le circuit de visa dans les parties de liste de diffusion et dans celles d\"avancement.");
+if (!defined("_CONFIG_VISA_WORKFLOW"))
+    define("_CONFIG_VISA_WORKFLOW", "Configuration du circuit de visa");
+if (!defined("_CONFIG_VISA_WORKFLOW_DESC"))
+    define("_CONFIG_VISA_WORKFLOW_DESC", "Permet de configurer le circuit de visa que devra prendre le courrier");
+if (!defined("_EMPTY_USER_LIST"))
+    define("_EMPTY_USER_LIST", "La liste des utilisateurs est vide");
+if (!defined("_VISA_ANSWERS"))
+    define("_VISA_ANSWERS", "Viser les projets de réponse");
 	
-if (!defined('_VISA_ANSWERS_DESC'))
-    define('_VISA_ANSWERS_DESC', 'Permet de viser les projets de réponse');
+if (!defined("_VISA_ANSWERS_DESC"))
+    define("_VISA_ANSWERS_DESC", "Permet de viser les projets de réponse");
 
-// CIRCUIT D'AVIS
-if (!defined('_AVIS_WORKFLOW'))
-    define('_AVIS_WORKFLOW', 'Circuit d\'avis');
-if (!defined('_CONFIG_AVIS_WORKFLOW'))
-    define('_CONFIG_AVIS_WORKFLOW', 'Configuration du circuit d\'avis');
-if (!defined('_CONFIG_AVIS_WORKFLOW_DESC'))
-    define('_CONFIG_AVIS_WORKFLOW_DESC', 'Permet de configurer le circuit d\'avis du courrier');
+// CIRCUIT D"AVIS
+if (!defined("_AVIS_WORKFLOW"))
+    define("_AVIS_WORKFLOW", "Circuit d\"avis");
+if (!defined("_CONFIG_AVIS_WORKFLOW"))
+    define("_CONFIG_AVIS_WORKFLOW", "Configuration du circuit d\"avis");
+if (!defined("_CONFIG_AVIS_WORKFLOW_DESC"))
+    define("_CONFIG_AVIS_WORKFLOW_DESC", "Permet de configurer le circuit d\"avis du courrier");
 
-if (!defined('_THUMBPRINT'))
-    define('_THUMBPRINT', 'Empreinte numérique');
+if (!defined("_THUMBPRINT"))
+    define("_THUMBPRINT", "Empreinte numérique");
 
-if (!defined('_DISSMARTCARD_SIGNER_APPLET'))
-    define('_DISSMARTCARD_SIGNER_APPLET', 'Signature électronique en cours...');
-
-
-if (!defined('_IMG_SIGN_MISSING'))
-    define('_IMG_SIGN_MISSING', 'Image de signature manquante');
-
-if (!defined('_THUMBPRINT_MISSING'))
-    define('_THUMBPRINT_MISSING', 'Empreinte numérique manquante');
-
-if (!defined('_SEND_TO_SIGNATURE'))
-    define('_SEND_TO_SIGNATURE', 'Soumettre');
-
-if (!defined('_SUBMIT_COMMENT'))
-    define('_SUBMIT_COMMENT', 'Commentaire de visa (optionnel) ');
+if (!defined("_DISSMARTCARD_SIGNER_APPLET"))
+    define("_DISSMARTCARD_SIGNER_APPLET", "Signature électronique en cours...");
 
 
-if (!defined('_BAD_PIN'))
-    define('_BAD_PIN', 'Code PIN incorrect. Attention, 3 essais maximum !');
-?>
+if (!defined("_IMG_SIGN_MISSING"))
+    define("_IMG_SIGN_MISSING", "Image de signature manquante");
+
+if (!defined("_THUMBPRINT_MISSING"))
+    define("_THUMBPRINT_MISSING", "Empreinte numérique manquante");
+
+if (!defined("_SEND_TO_SIGNATURE"))
+    define("_SEND_TO_SIGNATURE", "Soumettre");
+
+if (!defined("_SUBMIT_COMMENT"))
+    define("_SUBMIT_COMMENT", "Commentaire de visa (optionnel) ");
+
+
+if (!defined("_BAD_PIN"))
+    define("_BAD_PIN", "Code PIN incorrect. Attention, 3 essais maximum !");
