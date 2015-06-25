c8565ad17f83d683ef24765f1c1608077ab2360a
Laurent Giovannoni <laurent.giovannoni@maarch.org>
2015-06-25 16:49:50 +0000

    FEAT #2485 replace echo

--- attachments/trunk/lang/fr.php
+++ attachments/trunk/lang/fr.php
@@ -18,110 +18,110 @@
  *   You should have received a copy of the GNU General Public License
  *   along with Maarch Framework.  If not, see <http://www.gnu.org/licenses/>.
  */
-if (!defined('_TITLE'))
-    define('_TITLE', 'Titre');
-if (!defined('_ATTACHMENTS'))
-    define('_ATTACHMENTS', 'Pi&egrave;ces jointes');
-if (!defined('_ATTACHMENTS_COMMENT'))
-    define('_ATTACHMENTS_COMMENT', 'Gestion des pi&egrave;ces jointes');
-if (!defined('_ATTACH_ANSWER'))
-    define('_ATTACH_ANSWER', 'Attacher une pi&egrave;ce jointe');
-if (!defined('_ATTACH'))
-    define('_ATTACH', 'Attacher PJ');
-if (!defined('_PLEASE_SELECT_FILE'))
-    define('_PLEASE_SELECT_FILE', 'Choisissez un fichier');
-if (!defined('_NEW_ATTACH_ADDED'))
-    define('_NEW_ATTACH_ADDED', 'Nouvelle pi&egrave;ce jointe');
-if (!defined('_ATTACH_DELETED'))
-    define('_ATTACH_DELETED', 'Pi&egrave;ce jointe supprim&eacute;e');
-if (!defined('_ATTACHED_DOC'))
-    define('_ATTACHED_DOC', 'Pi&egrave;ces jointes');
+if (!defined("_TITLE"))
+    define("_TITLE", "Titre");
+if (!defined("_ATTACHMENTS"))
+    define("_ATTACHMENTS", "Pièces jointes");
+if (!defined("_ATTACHMENTS_COMMENT"))
+    define("_ATTACHMENTS_COMMENT", "Gestion des pièces jointes");
+if (!defined("_ATTACH_ANSWER"))
+    define("_ATTACH_ANSWER", "Attacher une pièce jointe");
+if (!defined("_ATTACH"))
+    define("_ATTACH", "Attacher PJ");
+if (!defined("_PLEASE_SELECT_FILE"))
+    define("_PLEASE_SELECT_FILE", "Choisissez un fichier");
+if (!defined("_NEW_ATTACH_ADDED"))
+    define("_NEW_ATTACH_ADDED", "Nouvelle pièce jointe");
+if (!defined("_ATTACH_DELETED"))
+    define("_ATTACH_DELETED", "Pièce jointe supprimée");
+if (!defined("_ATTACHED_DOC"))
+    define("_ATTACHED_DOC", "Pièces jointes");
 
-if (!defined('_INDEX_ATTACHMENT'))
-    define('_INDEX_ATTACHMENT', 'Indexer une pi&egrave;ce jointe');
-if (!defined('_INDEX_ATTACHMENT_DESC'))
-    define('_INDEX_ATTACHMENT_DESC', 'Indexer une pi&egrave;ce jointe');
-if (!defined('_ATTACH_TO_DOC'))
-    define('_ATTACH_TO_DOC', 'Attacher en r&eacute;ponse &agrave; un courrier existant');
-if (!defined('_ATTACH_TO_DOC_NUM'))
-    define('_ATTACH_TO_DOC_NUM', 'attach&eacute; au document n&deg;');
-if (!defined('_VIEW_ATTACHMENTS'))
-    define('_VIEW_ATTACHMENTS', 'Voir les pi&eagrave;ces jointes');
-if (!defined('_VIEW_ATTACHMENTS_DESC'))
-    define('_VIEW_ATTACHMENTS_DESC', 'Voir les pi&eagrave;ces jointes');
-if (!defined('_MANAGE_ATTACHMENTS'))
-    define('_MANAGE_ATTACHMENTS', 'G&eacute;rer les pi&eagrave;ces jointes');
-if (!defined('_GENERATE_OFFICE_ATTACHMENT'))
-    define('_GENERATE_OFFICE_ATTACHMENT', 'G&eacute;n&eacute;rer OFFICE');
-if (!defined('_GENERATE_HTML_ATTACHMENT'))
-    define('_GENERATE_HTML_ATTACHMENT', 'G&eacute;n&eacute;rer HTML');
-if (!defined('_ATTACH_FROM_HDD'))
-    define('_ATTACH_FROM_HDD', 'Ajouter une pi&egrave;ce jointe / r&eacute;ponse depuis votre disque dur');
-if (!defined('_GENERATE_ATTACHMENT_FROM'))
-    define('_GENERATE_ATTACHMENT_FROM', 'G&eacute;n&eacute;rer une pi&egrave;ce jointe / r&eacute;ponse depuis un mod&egrave;le au format');
+if (!defined("_INDEX_ATTACHMENT"))
+    define("_INDEX_ATTACHMENT", "Indexer une pièce jointe");
+if (!defined("_INDEX_ATTACHMENT_DESC"))
+    define("_INDEX_ATTACHMENT_DESC", "Indexer une pièce jointe");
+if (!defined("_ATTACH_TO_DOC"))
+    define("_ATTACH_TO_DOC", "Attacher en réponse à un courrier existant");
+if (!defined("_ATTACH_TO_DOC_NUM"))
+    define("_ATTACH_TO_DOC_NUM", "attaché au document n°");
+if (!defined("_VIEW_ATTACHMENTS"))
+    define("_VIEW_ATTACHMENTS", "Voir les pièces jointes");
+if (!defined("_VIEW_ATTACHMENTS_DESC"))
+    define("_VIEW_ATTACHMENTS_DESC", "Voir les pièces jointes");
+if (!defined("_MANAGE_ATTACHMENTS"))
+    define("_MANAGE_ATTACHMENTS", "Gérer les pièces jointes");
+if (!defined("_GENERATE_OFFICE_ATTACHMENT"))
+    define("_GENERATE_OFFICE_ATTACHMENT", "Générer OFFICE");
+if (!defined("_GENERATE_HTML_ATTACHMENT"))
+    define("_GENERATE_HTML_ATTACHMENT", "Générer HTML");
+if (!defined("_ATTACH_FROM_HDD"))
+    define("_ATTACH_FROM_HDD", "Ajouter une pièce jointe / réponse depuis votre disque dur");
+if (!defined("_GENERATE_ATTACHMENT_FROM"))
+    define("_GENERATE_ATTACHMENT_FROM", "Générer une pièce jointe / réponse depuis un modèle au format");
 
-if (!defined('_EDIT_ATTACHMENTS_FROM_DETAILS'))
-    define('_EDIT_ATTACHMENTS_FROM_DETAILS', 'G&eacute;rer les pi&egrave;ces jointes depuis la fiche d&eacute;tail');
+if (!defined("_EDIT_ATTACHMENTS_FROM_DETAILS"))
+    define("_EDIT_ATTACHMENTS_FROM_DETAILS", "Gérer les pièces jointes depuis la fiche détail");
 
-if (!defined('_CREATED'))
-    define( '_CREATED', 'Cr&eacute;&eacute; le');
-if (!defined('_BBY'))
-    define( '_BBY', 'Par');
-if (!defined('_EXP_DATE'))
-    define( '_EXP_DATE', 'Date de d&eacute;part');
-if (!defined('_CREATE_PJ'))
-    define( '_CREATE_PJ', 'Cr&eacute;er une pi&egrave;ce jointe');
-if (!defined('_ATTACH_FILE'))
-    define( '_ATTACH_FILE', 'Attacher une pi&egrave;ce');
-if (!defined('_MODEL'))
-    define( '_MODEL', 'Mod&egrave;le');
-if (!defined('_CHOOSE_MODEL'))
-    define( '_CHOOSE_MODEL', 'S&eacute;lectionnez le mod&egrave;le');
-if (!defined('_BACK_DATE'))
-    define( '_BACK_DATE', 'Date&nbsp;de&nbsp;retour');
-if (!defined('_ATTACHMENT_TYPES'))
-    define( '_ATTACHMENT_TYPES', 'Type d\'attachement');
-if (!defined('_RESPONSE_PROJECT'))
-    define( '_RESPONSE_PROJECT', 'Projet de r&eacute;ponse');
-if (!defined('_SIGNED_RESPONSE'))
-	define( '_SIGNED_RESPONSE', 'R&eacute;ponse sign&eacute;e');
-if (!defined('_ROUTING'))
-    define( '_ROUTING', 'Fiche de circulation');
-if (!defined('_OUTGOING_MAIL_SIGNED'))
-    define( '_OUTGOING_MAIL_SIGNED', 'Courrier d&eacutepart sign&eacute');
-if (!defined('_A_PJ'))
-    define( '_A_PJ', 'Pi&egrave;ce jointe');
-if (!defined('_CHOOSE_ATTACHMENT_TYPE'))
-    define( '_CHOOSE_ATTACHMENT_TYPE', 'Choisissez un type d\'attachement');
-if (!defined('_UPDATED_DATE'))
-    define( '_UPDATED_DATE', 'Mis&nbsp;&agrave;&nbsp;jour&nbsp;le');
-if (!defined('_FILE_MISSING'))
-    define( '_FILE_MISSING', 'Le fichier est manquant');
-if (!defined('_FILE_EMPTY'))
-    define( '_FILE_EMPTY', 'Le fichier est vide');
-if (!defined('_EDIT_MODEL'))
-    define( '_EDIT_MODEL', 'Editer');
-if (!defined('_ALREADY_MODEL_SELECTED'))
-    define( '_ALREADY_MODEL_SELECTED', 'Mod&egrave;le d&eacute;j&agrave s&eacute;lectionn&eacute;');
-if (!defined('_MODIFY_ANSWER'))
-    define( '_MODIFY_ANSWER', 'Modifier une pi&egrave;ce jointe/un attachement');
-if (!defined('_CREATE_NEW_ATTACHMENT_VERSION'))
-    define( '_CREATE_NEW_ATTACHMENT_VERSION', 'Cr&eacute;er une nouvelle version ?');
-if (!defined('_ATTACH_UPDATED'))
-    define( '_ATTACH_UPDATED', 'Pi&egrave;ce jointe mise &agrave; jour');
-if (!defined('_FINAL_VERSION'))
-    define( '_FINAL_VERSION', 'Version finale');
-if (!defined('_SHOW_PREVIOUS_VERSION'))
-    define( '_SHOW_PREVIOUS_VERSION', 'Voir les versions pr&eacute;c&eacute;dentes');
-if (!defined('_CREATE_ATTACHMENTS_FROM_DETAILS'))
-    define( '_CREATE_ATTACHMENTS_FROM_DETAILS', 'Cr&eacute;er des pi&egrave;ces jointes depuis la fiche d&eacute;tail');
-if (!defined('_DELETE_ATTACHMENTS'))
-    define( '_DELETE_ATTACHMENTS', 'Supprimer les pi&egrave;ces jointes');
-if (!defined('_MODIFY_ATTACHMENTS'))
-    define( '_MODIFY_ATTACHMENTS', 'Modifier les pi&egrave;ces jointes');
-if (!defined('_ALL_ATTACHMENT_TYPES'))
-    define( '_ALL_ATTACHMENT_TYPES', 'Tous les attachements');
-if (!defined('_ATTACHEMENTS'))
-    define( '_ATTACHEMENTS', 'Attachements');
-if (!defined('_WAYBILL'))
-define( '_WAYBILL', 'Bordereau d\'envoi');
+if (!defined("_CREATED"))
+    define( "_CREATED", "Créé le");
+if (!defined("_BBY"))
+    define( "_BBY", "Par");
+if (!defined("_EXP_DATE"))
+    define( "_EXP_DATE", "Date de départ");
+if (!defined("_CREATE_PJ"))
+    define( "_CREATE_PJ", "Créer une pièce jointe");
+if (!defined("_ATTACH_FILE"))
+    define( "_ATTACH_FILE", "Attacher une pièce");
+if (!defined("_MODEL"))
+    define( "_MODEL", "Modèle");
+if (!defined("_CHOOSE_MODEL"))
+    define( "_CHOOSE_MODEL", "Sélectionnez le modèle");
+if (!defined("_BACK_DATE"))
+    define( "_BACK_DATE", "Date de retour");
+if (!defined("_ATTACHMENT_TYPES"))
+    define( "_ATTACHMENT_TYPES", "Type d'attachement");
+if (!defined("_RESPONSE_PROJECT"))
+    define( "_RESPONSE_PROJECT", "Projet de réponse");
+if (!defined("_SIGNED_RESPONSE"))
+	define( "_SIGNED_RESPONSE", "Réponse signée");
+if (!defined("_ROUTING"))
+    define( "_ROUTING", "Fiche de circulation");
+if (!defined("_OUTGOING_MAIL_SIGNED"))
+    define( "_OUTGOING_MAIL_SIGNED", "Courrier départ signé");
+if (!defined("_A_PJ"))
+    define( "_A_PJ", "Pièce jointe");
+if (!defined("_CHOOSE_ATTACHMENT_TYPE"))
+    define( "_CHOOSE_ATTACHMENT_TYPE", "Choisissez un type d'attachement");
+if (!defined("_UPDATED_DATE"))
+    define( "_UPDATED_DATE", "Mis à jour le");
+if (!defined("_FILE_MISSING"))
+    define( "_FILE_MISSING", "Le fichier est manquant");
+if (!defined("_FILE_EMPTY"))
+    define( "_FILE_EMPTY", "Le fichier est vide");
+if (!defined("_EDIT_MODEL"))
+    define( "_EDIT_MODEL", "Editer");
+if (!defined("_ALREADY_MODEL_SELECTED"))
+    define( "_ALREADY_MODEL_SELECTED", "Modèle déjà sélectionné");
+if (!defined("_MODIFY_ANSWER"))
+    define( "_MODIFY_ANSWER", "Modifier une pièce jointe/un attachement");
+if (!defined("_CREATE_NEW_ATTACHMENT_VERSION"))
+    define( "_CREATE_NEW_ATTACHMENT_VERSION", "Créer une nouvelle version ?");
+if (!defined("_ATTACH_UPDATED"))
+    define( "_ATTACH_UPDATED", "Pièce jointe mise à jour");
+if (!defined("_FINAL_VERSION"))
+    define( "_FINAL_VERSION", "Version finale");
+if (!defined("_SHOW_PREVIOUS_VERSION"))
+    define( "_SHOW_PREVIOUS_VERSION", "Voir les versions précédentes");
+if (!defined("_CREATE_ATTACHMENTS_FROM_DETAILS"))
+    define( "_CREATE_ATTACHMENTS_FROM_DETAILS", "Créer des pièces jointes depuis la fiche détail");
+if (!defined("_DELETE_ATTACHMENTS"))
+    define( "_DELETE_ATTACHMENTS", "Supprimer les pièces jointes");
+if (!defined("_MODIFY_ATTACHMENTS"))
+    define( "_MODIFY_ATTACHMENTS", "Modifier les pièces jointes");
+if (!defined("_ALL_ATTACHMENT_TYPES"))
+    define( "_ALL_ATTACHMENT_TYPES", "Tous les attachements");
+if (!defined("_ATTACHEMENTS"))
+    define( "_ATTACHEMENTS", "Attachements");
+if (!defined("_WAYBILL"))
+define( "_WAYBILL", "Bordereau d'envoi");
