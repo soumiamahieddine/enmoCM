8d7bb736a20a531957fb2a7eb53e712cb864f278
Laurent Giovannoni <laurent.giovannoni@maarch.org>
2015-06-25 16:51:59 +0000

    FEAT #2506 replace echo

--- fileplan/trunk/lang/fr.php
+++ fileplan/trunk/lang/fr.php
@@ -19,158 +19,157 @@
  *    along with Maarch Framework.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-if (!defined('_FILEPLAN'))  
-	define('_FILEPLAN','Plan de classement');
-if (!defined('_FILEPLAN_SHORT'))  
-	define('_FILEPLAN_SHORT','Plan de classement');
-if (!defined('_PERSONNAL_FILEPLAN'))  
-	define('_PERSONNAL_FILEPLAN','Plan de classement personnel');
-if (!defined('_FILEPLAN_COMMENT'))  
-	define('_FILEPLAN_COMMENT','Plan de classement organisationnel');
-if (!defined('_PUT_DOC_IN_FILEPLAN'))  
-	define('_PUT_DOC_IN_FILEPLAN','Classer des documents');
-if (!defined('_PUT_DOC_IN_FILEPLAN_COMMENT'))  
-	define('_PUT_DOC_IN_FILEPLAN_COMMENT','Classer des documents dans le plan de classement organisationnel');
-if (!defined('_ADMIN_MODULE_FILEPLAN'))
-    define('_ADMIN_MODULE_FILEPLAN', 'Plan de classement organisationnel');
-if (!defined('_ADMIN_MODULE_FILEPLAN_DESC'))
-    define('_ADMIN_MODULE_FILEPLAN_DESC', 'Gestions du plan de classement organisationnel (Permet de gérer des plans spécifiques à une entité).');
+if (!defined("_FILEPLAN"))  
+	define("_FILEPLAN","Plan de classement");
+if (!defined("_FILEPLAN_SHORT"))  
+	define("_FILEPLAN_SHORT","Plan de classement");
+if (!defined("_PERSONNAL_FILEPLAN"))  
+	define("_PERSONNAL_FILEPLAN","Plan de classement personnel");
+if (!defined("_FILEPLAN_COMMENT"))  
+	define("_FILEPLAN_COMMENT","Plan de classement organisationnel");
+if (!defined("_PUT_DOC_IN_FILEPLAN"))  
+	define("_PUT_DOC_IN_FILEPLAN","Classer des documents");
+if (!defined("_PUT_DOC_IN_FILEPLAN_COMMENT"))  
+	define("_PUT_DOC_IN_FILEPLAN_COMMENT","Classer des documents dans le plan de classement organisationnel");
+if (!defined("_ADMIN_MODULE_FILEPLAN"))
+    define("_ADMIN_MODULE_FILEPLAN", "Plan de classement organisationnel");
+if (!defined("_ADMIN_MODULE_FILEPLAN_DESC"))
+    define("_ADMIN_MODULE_FILEPLAN_DESC", "Gestions du plan de classement organisationnel (Permet de gérer des plans spécifiques à une entité).");
 
 /*FILEPLAN MANAGEMENT*/	
-if (!defined('_ADD_FILEPLAN'))
-    define('_ADD_FILEPLAN','Cr&eacute;er un plan de classement');
-if (!defined('_CREATE_YOUR_PERSONNAL_FILEPLAN'))
-    define('_CREATE_YOUR_PERSONNAL_FILEPLAN','Bienvenue dans la gestion de votre plan de classement personnel.<br />Cr&eacute;ez un plan de classement personnel afin de pouvoir classer et retrouver plus rapidement vos documents.');
-if (!defined('_FILEPLAN_NOT_EXISTS'))
-    define('_FILEPLAN_NOT_EXISTS','Ce plan de classement n\'existe pas');	
-if (!defined('_FILEPLAN_ID'))
-    define('_FILEPLAN_ID','Identifiant du plan de classement');	
-if (!defined('_FILEPLAN_NAME'))
-    define('_FILEPLAN_NAME','Nom du plan de classement');
-if (!defined('_IS_SERIAL_ID'))
-    define('_IS_SERIAL_ID','Identifiant automatique');		
-if (!defined('_CHANGE_DEFAULT_FILEPLAN_NAME'))
-    define('_CHANGE_DEFAULT_FILEPLAN_NAME','Vous pouvez modifier le mom par d&eacute;faut du plan de classement');
-if (!defined('_FILEPLAN_ADDED'))
-    define('_FILEPLAN_ADDED','Plan de classement cr&eacute;e');
-if (!defined('_DUPLICATE_FILEPLAN'))
-    define('_DUPLICATE_FILEPLAN','Dupliquer un plan de classement');
-if (!defined('_MANAGE_FILEPLAN_SHORT'))
-    define('_MANAGE_FILEPLAN_SHORT', 'G&eacute;rer');
-if (!defined('_MANAGE_PERSONNAL_FILEPLAN'))
-    define('_MANAGE_PERSONNAL_FILEPLAN', 'G&eacute;rer mon plan de classement personnel');
-if (!defined('_MANAGE_FILEPLAN'))
-    define('_MANAGE_FILEPLAN', 'G&eacute;rer le plan de classement');
-if (!defined('_VIEW_FILEPLAN'))
-    define('_VIEW_FILEPLAN', 'Afficher le(s) plan(s) de classement');
-if (!defined('_FILEPLAN_POSITIONS'))
-    define('_FILEPLAN_POSITIONS', '&eacute;l&eacute;ment(s)');
-if (!defined('_DOC_IN_FILEPLAN'))
-    define('_DOC_IN_FILEPLAN', 'documents');
-if (!defined('_DELETE_FILEPLAN'))
-    define('_DELETE_FILEPLAN','Supprimer le plan de classement');
-if (!defined('_DELETE_FILEPLAN_SHORT'))
-    define('_DELETE_FILEPLAN_SHORT','Supprimer');	
-if (!defined('_FILEPLAN_DELETED'))
-    define('_FILEPLAN_DELETED', 'Plan de classement supprim&eacute;e');	
-if (!defined('_EDIT_FILEPLAN'))
-    define('_EDIT_FILEPLAN','Modifier le plan de classement');	
-if (!defined('_EDIT_FILEPLAN_SHORT'))
-    define('_EDIT_FILEPLAN_SHORT','Modifier');
-if (!defined('_FILEPLAN_UPDATED'))
-    define('_FILEPLAN_UPDATED', 'Plan de classement modifi&eacute;e');
-if (!defined('_ENABLE_FILEPLAN'))
-    define('_ENABLE_FILEPLAN', 'D&eacute;verouiller');
-if (!defined('_FILEPLAN_ENABLED'))
-    define('_FILEPLAN_ENABLED', 'Plan de classement d&eacute;verouill&eacute;');
-if (!defined('_DISABLE_FILEPLAN'))
-    define('_DISABLE_FILEPLAN', 'Verrouiller');    
-if (!defined('_FILEPLAN_DISABLED'))
-    define('_FILEPLAN_DISABLED', 'Plan de classement verouiller&eacute;');
-if (!defined('_REMOVE_FILEPLAN_INFOS_1'))
-    define('_REMOVE_FILEPLAN_INFOS_1', 'Voulez-vous supprimez plan de classement suivant et retirer les documents li&eacute;s?');
-if (!defined('_REMOVE_FILEPLAN_INFOS_2'))
-    define('_REMOVE_FILEPLAN_INFOS_2', 'Aucun document ne sera supprim&eacute;.');
-if (!defined('_CHOOSE_FILEPLAN'))
-    define('_CHOOSE_FILEPLAN', 'Veuillez s&eacute;lectionner un plan de classement');
+if (!defined("_ADD_FILEPLAN"))
+    define("_ADD_FILEPLAN","Créer un plan de classement");
+if (!defined("_CREATE_YOUR_PERSONNAL_FILEPLAN"))
+    define("_CREATE_YOUR_PERSONNAL_FILEPLAN","Bienvenue dans la gestion de votre plan de classement personnel.<br />Créez un plan de classement personnel afin de pouvoir classer et retrouver plus rapidement vos documents.");
+if (!defined("_FILEPLAN_NOT_EXISTS"))
+    define("_FILEPLAN_NOT_EXISTS","Ce plan de classement n'existe pas");	
+if (!defined("_FILEPLAN_ID"))
+    define("_FILEPLAN_ID","Identifiant du plan de classement");	
+if (!defined("_FILEPLAN_NAME"))
+    define("_FILEPLAN_NAME","Nom du plan de classement");
+if (!defined("_IS_SERIAL_ID"))
+    define("_IS_SERIAL_ID","Identifiant automatique");		
+if (!defined("_CHANGE_DEFAULT_FILEPLAN_NAME"))
+    define("_CHANGE_DEFAULT_FILEPLAN_NAME","Vous pouvez modifier le mom par défaut du plan de classement");
+if (!defined("_FILEPLAN_ADDED"))
+    define("_FILEPLAN_ADDED","Plan de classement crée");
+if (!defined("_DUPLICATE_FILEPLAN"))
+    define("_DUPLICATE_FILEPLAN","Dupliquer un plan de classement");
+if (!defined("_MANAGE_FILEPLAN_SHORT"))
+    define("_MANAGE_FILEPLAN_SHORT", "Gérer");
+if (!defined("_MANAGE_PERSONNAL_FILEPLAN"))
+    define("_MANAGE_PERSONNAL_FILEPLAN", "Gérer mon plan de classement personnel");
+if (!defined("_MANAGE_FILEPLAN"))
+    define("_MANAGE_FILEPLAN", "Gérer le plan de classement");
+if (!defined("_VIEW_FILEPLAN"))
+    define("_VIEW_FILEPLAN", "Afficher le(s) plan(s) de classement");
+if (!defined("_FILEPLAN_POSITIONS"))
+    define("_FILEPLAN_POSITIONS", "élément(s)");
+if (!defined("_DOC_IN_FILEPLAN"))
+    define("_DOC_IN_FILEPLAN", "documents");
+if (!defined("_DELETE_FILEPLAN"))
+    define("_DELETE_FILEPLAN","Supprimer le plan de classement");
+if (!defined("_DELETE_FILEPLAN_SHORT"))
+    define("_DELETE_FILEPLAN_SHORT","Supprimer");	
+if (!defined("_FILEPLAN_DELETED"))
+    define("_FILEPLAN_DELETED", "Plan de classement supprimée");	
+if (!defined("_EDIT_FILEPLAN"))
+    define("_EDIT_FILEPLAN","Modifier le plan de classement");	
+if (!defined("_EDIT_FILEPLAN_SHORT"))
+    define("_EDIT_FILEPLAN_SHORT","Modifier");
+if (!defined("_FILEPLAN_UPDATED"))
+    define("_FILEPLAN_UPDATED", "Plan de classement modifiée");
+if (!defined("_ENABLE_FILEPLAN"))
+    define("_ENABLE_FILEPLAN", "Déverouiller");
+if (!defined("_FILEPLAN_ENABLED"))
+    define("_FILEPLAN_ENABLED", "Plan de classement déverouillé");
+if (!defined("_DISABLE_FILEPLAN"))
+    define("_DISABLE_FILEPLAN", "Verrouiller");    
+if (!defined("_FILEPLAN_DISABLED"))
+    define("_FILEPLAN_DISABLED", "Plan de classement verouilleré");
+if (!defined("_REMOVE_FILEPLAN_INFOS_1"))
+    define("_REMOVE_FILEPLAN_INFOS_1", "Voulez-vous supprimez plan de classement suivant et retirer les documents liés?");
+if (!defined("_REMOVE_FILEPLAN_INFOS_2"))
+    define("_REMOVE_FILEPLAN_INFOS_2", "Aucun document ne sera supprimé.");
+if (!defined("_CHOOSE_FILEPLAN"))
+    define("_CHOOSE_FILEPLAN", "Veuillez sélectionner un plan de classement");
 	
 /*LIST*/
-if (!defined('_POSITION_PATH'))
-    define('_POSITION_PATH', 'Chemin');
-if (!defined('_NO_DOC_IN_POSITION'))
-    define('_NO_DOC_IN_POSITION', 'Aucun documents');
+if (!defined("_POSITION_PATH"))
+    define("_POSITION_PATH", "Chemin");
+if (!defined("_NO_DOC_IN_POSITION"))
+    define("_NO_DOC_IN_POSITION", "Aucun documents");
 	
 /*FORM*/
-if (!defined('_POSITION_ID'))
-    define('_POSITION_ID','Identifiant de position');
-if (!defined('_POSITION_NAME'))
-    define('_POSITION_NAME','Nom de position');
-if (!defined('_NEST_POSITION_UNDER'))
-    define('_NEST_POSITION_UNDER','Positionner sous');
-if (!defined('_POSITION_PARENT'))
-    define('_POSITION_PARENT','Position parente');
-if (!defined('_CHOOSE_ONE_POSITION'))
-    define('_CHOOSE_ONE_POSITION', 'Veuillez s&eacute;lectionner au moins une position');
-if (!defined('_CHOOSE_PARENT_POSITION'))
-    define('_CHOOSE_PARENT_POSITION', 'Veuillez s&eacute;lectionner une position parente');
+if (!defined("_POSITION_ID"))
+    define("_POSITION_ID","Identifiant de position");
+if (!defined("_POSITION_NAME"))
+    define("_POSITION_NAME","Nom de position");
+if (!defined("_NEST_POSITION_UNDER"))
+    define("_NEST_POSITION_UNDER","Positionner sous");
+if (!defined("_POSITION_PARENT"))
+    define("_POSITION_PARENT","Position parente");
+if (!defined("_CHOOSE_ONE_POSITION"))
+    define("_CHOOSE_ONE_POSITION", "Veuillez sélectionner au moins une position");
+if (!defined("_CHOOSE_PARENT_POSITION"))
+    define("_CHOOSE_PARENT_POSITION", "Veuillez sélectionner une position parente");
 	
 /*ACTIONS*/
 	//ADD
-if (!defined('_NEW_POSITION'))
-    define('_NEW_POSITION','Nouvelle position');
-if (!defined('_ADD_POSITION'))
-    define('_ADD_POSITION', 'Cr&eacute;er');
-if (!defined('_POSITION_ADDED'))
-    define('_POSITION_ADDED', 'Position ajout&eacute;e');
+if (!defined("_NEW_POSITION"))
+    define("_NEW_POSITION","Nouvelle position");
+if (!defined("_ADD_POSITION"))
+    define("_ADD_POSITION", "Créer");
+if (!defined("_POSITION_ADDED"))
+    define("_POSITION_ADDED", "Position ajoutée");
 	//EDIT
-if (!defined('_EDIT_POSITION_SHORT'))
-    define('_EDIT_POSITION_SHORT', 'Modifier');
-if (!defined('_EDIT_POSITION'))
-    define('_EDIT_POSITION', 'Modifier la position');
-if (!defined('_SAVE_POSITION'))
-    define('_SAVE_POSITION', 'Enregistrer');
-if (!defined('_POSITION_UPDATED'))
-    define('_POSITION_UPDATED', 'Position modifi&eacute;e');
+if (!defined("_EDIT_POSITION_SHORT"))
+    define("_EDIT_POSITION_SHORT", "Modifier");
+if (!defined("_EDIT_POSITION"))
+    define("_EDIT_POSITION", "Modifier la position");
+if (!defined("_SAVE_POSITION"))
+    define("_SAVE_POSITION", "Enregistrer");
+if (!defined("_POSITION_UPDATED"))
+    define("_POSITION_UPDATED", "Position modifiée");
 	//ENABLE/DISABLE
-if (!defined('_ENABLE_POSITION'))
-    define('_ENABLE_POSITION', 'D&eacute;verouiller');
-if (!defined('_POSITION_ENABLED'))
-    define('_POSITION_ENABLED', 'Position d&eacute;verouill&eacute;e');
-if (!defined('_DISABLE_POSITION'))
-    define('_DISABLE_POSITION', 'Verouiller');    
-if (!defined('_POSITION_DISABLED'))
-    define('_POSITION_DISABLED', 'Position verouiller&eacute;e');
+if (!defined("_ENABLE_POSITION"))
+    define("_ENABLE_POSITION", "Déverouiller");
+if (!defined("_POSITION_ENABLED"))
+    define("_POSITION_ENABLED", "Position déverouillée");
+if (!defined("_DISABLE_POSITION"))
+    define("_DISABLE_POSITION", "Verouiller");    
+if (!defined("_POSITION_DISABLED"))
+    define("_POSITION_DISABLED", "Position verouillerée");
 	//REMOVE
-if (!defined('_REMOVE_POSITION'))
-    define('_REMOVE_POSITION', 'Supprimer la position');
-if (!defined('_REMOVE_POSITIONS'))
-    define('_REMOVE_POSITIONS', 'Supprimer les positions');
-if (!defined('_REALLY_REMOVE_POSITION'))
-    define('_REALLY_REMOVE_POSITION', 'Voulez-vous supprimez la position');
-if (!defined('_REMOVE_POSITIONS_INFOS_1'))
-    define('_REMOVE_POSITIONS_INFOS_1', 'Voulez-vous supprimez les positions suivantes et retirer les documents li&eacute;s?');
-if (!defined('_REMOVE_POSITIONS_INFOS_2'))
-    define('_REMOVE_POSITIONS_INFOS_2', 'Aucun document ne sera supprim&eacute;.');
-if (!defined('_DELETE_POSITION'))
-    define('_DELETE_POSITION', 'Supprimer');
-if (!defined('_POSITION_REMOVED'))
-    define('_POSITION_REMOVED', 'Position supprim&eacute;e');
+if (!defined("_REMOVE_POSITION"))
+    define("_REMOVE_POSITION", "Supprimer la position");
+if (!defined("_REMOVE_POSITIONS"))
+    define("_REMOVE_POSITIONS", "Supprimer les positions");
+if (!defined("_REALLY_REMOVE_POSITION"))
+    define("_REALLY_REMOVE_POSITION", "Voulez-vous supprimez la position");
+if (!defined("_REMOVE_POSITIONS_INFOS_1"))
+    define("_REMOVE_POSITIONS_INFOS_1", "Voulez-vous supprimez les positions suivantes et retirer les documents liés?");
+if (!defined("_REMOVE_POSITIONS_INFOS_2"))
+    define("_REMOVE_POSITIONS_INFOS_2", "Aucun document ne sera supprimé.");
+if (!defined("_DELETE_POSITION"))
+    define("_DELETE_POSITION", "Supprimer");
+if (!defined("_POSITION_REMOVED"))
+    define("_POSITION_REMOVED", "Position supprimée");
 	//SET
-if (!defined('_SET_DOC_TO_POSITION'))
-    define('_SET_DOC_TO_POSITION', 'Classer un/des document(s)');
-if (!defined('_DOC_ADDED_TO_POSITION'))
-    define('_DOC_ADDED_TO_POSITION', 'Document(s) aff&eacute;ct&eacute;(s) à la position');
-if (!defined('_REMOVED_DOC_FROM_POSITION'))
-    define('_REMOVED_DOC_FROM_POSITION', ' Retirer le document de la position');
-if (!defined('_REALLY_REMOVE_DOC_FROM_POSITION'))
-    define('_REALLY_REMOVE_DOC_FROM_POSITION', 'Voulez-vous retirer le document de la position');
-if (!defined('_DOC_REMOVED_FROM_POSITION'))
-    define('_DOC_REMOVED_FROM_POSITION', 'Document retir&eacute; de la position');
+if (!defined("_SET_DOC_TO_POSITION"))
+    define("_SET_DOC_TO_POSITION", "Classer un/des document(s)");
+if (!defined("_DOC_ADDED_TO_POSITION"))
+    define("_DOC_ADDED_TO_POSITION", "Document(s) affécté(s) à la position");
+if (!defined("_REMOVED_DOC_FROM_POSITION"))
+    define("_REMOVED_DOC_FROM_POSITION", " Retirer le document de la position");
+if (!defined("_REALLY_REMOVE_DOC_FROM_POSITION"))
+    define("_REALLY_REMOVE_DOC_FROM_POSITION", "Voulez-vous retirer le document de la position");
+if (!defined("_DOC_REMOVED_FROM_POSITION"))
+    define("_DOC_REMOVED_FROM_POSITION", "Document retiré de la position");
 	//ERROR
-if (!defined('_ERROR_DURING_POSITION_ID_GENERATION'))
-    define('_ERROR_DURING_POSITION_ID_GENERATION', 'Erreur durant le generation de l\'identifiant de la position. (database error)');
-if (!defined('_POSITION_NOT_EXISTS'))
-    define('_POSITION_NOT_EXISTS', 'Cette position n&rsquo;existe pas');
-if (!defined('_POSITION_ALREADY_EXISTS'))
-    define('_POSITION_ALREADY_EXISTS', 'Cet identifiant de position existe d&eacute;j&agrave;');
-?>
+if (!defined("_ERROR_DURING_POSITION_ID_GENERATION"))
+    define("_ERROR_DURING_POSITION_ID_GENERATION", "Erreur durant le generation de l'identifiant de la position. (database error)");
+if (!defined("_POSITION_NOT_EXISTS"))
+    define("_POSITION_NOT_EXISTS", "Cette position n'existe pas");
+if (!defined("_POSITION_ALREADY_EXISTS"))
+    define("_POSITION_ALREADY_EXISTS", "Cet identifiant de position existe déjà");
