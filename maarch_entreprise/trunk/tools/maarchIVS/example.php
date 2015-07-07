807bed458551c6828841b993fa4452ce1302cc37
Laurent Giovannoni <laurent.giovannoni@maarch.org>
2015-07-07 08:32:12 +0000

    FEAT #2619 add maarchIVS tools

---
+++ maarch_entreprise/trunk/tools/maarchIVS/example.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once 'MaarchIVS.php';
+
+$started = MaarchIVS::start(__DIR__ . DIRECTORY_SEPARATOR . 'conf' . DIRECTORY_SEPARATOR . 'conf.xml', 'xml');
+$valid = MaarchIVS::run('silent');
+
+if (!$valid) {
+    var_dump(MaarchIVS::debug());
+} else {
+    echo "Request is valid";
+}
