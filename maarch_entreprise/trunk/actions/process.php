cee356b7032a89b240b9e10ff09b1c683616e86d
Nicolas Couture <couture@docimsol.com>
2015-07-30 06:49:12 +0000

    FEAT #2727 modifs pages action pour courrier spontané (Requetes PDO)

--- maarch_entreprise/trunk/actions/process.php
+++ maarch_entreprise/trunk/actions/process.php
@@ -1138,12 +1138,10 @@ function get_form_txt($values, $path_manage_action,  $id_action, $table, $module
 
     //RESOURCE FRAME
 	if ($data['category_id']['value'] == 'outgoing'){
-		$req2 = new request;
-        $req2->connect();
-        $req2->query("select res_id from "
+		$stmt = $db->query("select res_id from "
             . $_SESSION['tablename']['attach_res_attachments']
-            . " where status <> 'DEL' and res_id_master = " . $res_id . " and coll_id = '" . $coll_id . "' and attachment_type = 'converted_pdf' and type_id = 1");
-		$res_att = $req2->fetch_object();
+            . " where status <> 'DEL' and res_id_master = ? and coll_id = ? and attachment_type = 'converted_pdf' and type_id = 1", array($res_id, $coll_id));
+		$res_att = $stmt->fetchObject();
 		$frm_str .= '<iframe src="' . $_SESSION['config']['businessappurl']
         . 'index.php?display=true&module=attachments&page=view_attachment&res_id_master='
         . $res_id . '&id='.$res_att->res_id.'" name="viewframe" id="viewframe" scrolling="auto" frameborder="0" width="100%" style="width:100% !important;"></iframe>';
