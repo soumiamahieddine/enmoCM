fdbacb20c7286e7722bf1132fe3e3abbf1ef2c46
Florian Azizian <florian.azizian@maarch.org>
2015-08-27 12:30:03 +0000

    FEAT #2408 new version 4.1.10

--- templates/trunk/load_editor.php
+++ templates/trunk/load_editor.php
@@ -1,21 +1,32 @@
 <?php  
-// $theme = "maarch";
-// $show_maarch_list = false;
-/*if($_SESSION['mode_editor'])
-{
-    //$theme = trim($_SESSION['mode_editor']);
-    $show_maarch_list = true;
-}*/
+/*
+*   Copyright 2008-2012 Maarch
+*
+*   This file is part of Maarch Framework.
+*
+*   Maarch Framework is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   Maarch Framework is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with Maarch Framework.  If not, see <http://www.gnu.org/licenses/>.
+*/
 ?>
+
 <!-- tinyMCE -->
 <script  type="text/javascript" src="<?php echo $_SESSION['config']['businessappurl'].'tools/';?>tiny_mce/tinymce.min.js"></script>
 <script type="text/javascript">
     tinymce.init({
-        // theme : "<?php functions::xecho($theme);?>",
+
         selector: "textarea#template_content",
         statusbar : false,
-        // mode : "exact",
-        // elements : "template_content",
+
         language : "fr_FR",
         height : "300",
         plugins: [
@@ -24,38 +35,7 @@
                  "save table contextmenu directionality paste textcolor"
         ],
         toolbar: "undo redo | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | preview fullpage | forecolor backcolor", 
-        //save_callback : "customSave",
-        //apply_source_formatting : true,
-        //convert_newlines_to_brs : true,
-        //preformatted : true,
-/*        convert_fonts_to_spans : false,
-        verify_html : true,
-        //content_css : "example_advanced.css",
-        extended_valid_elements : "a[href|target|name]",
-        plugins : "table,insertdatetime,fullscreen,paste,searchreplace,preview,print,directionality,layer",
-        theme_<?php functions::xecho($theme);?>_buttons1_add : "fontselect,fontsizeselect",
-        theme_<?php functions::xecho($theme);?>_buttons2_add_before : "cut,copy,paste,pastetext,pasteword,separator,search,replace,separator",
-        theme_<?php functions::xecho($theme);?>_buttons2_add : "separator,insertdate,inserttime,preview,separator,forecolor,backcolor",
-        theme_<?php functions::xecho($theme);?>_buttons3_add_before : "tablecontrols,separator",
-        theme_<?php functions::xecho($theme);?>_buttons3_add : "separator,print,separator,ltr,rtl,separator,fullscreen,separator,insertlayer,moveforward,movebackward,absolut",
-        theme_<?php functions::xecho($theme);?>_toolbar_align : "left",
-        theme_advanced_toolbar_location : "top",
-        //invalid_elements : "a",
-        theme_<?php functions::xecho($theme);?>_styles : "Header 1=header1;Header 2=header2;Header 3=header3;Table Row=tableRow1" // Theme specific setting CSS classes*/
-        <?php  
-/*        if($show_maarch_list)
-        {
-            //echo ', mapping_file : "'.$_SESSION['config']['coreurl'].'modules/templates/xml/mapping_file.xml"';
-            if(file_exists($_SESSION['config']['corepath'].'custom'.DIRECTORY_SEPARATOR.$_SESSION['custom_override_id'].DIRECTORY_SEPARATOR."modules".DIRECTORY_SEPARATOR."templates".DIRECTORY_SEPARATOR."xml".DIRECTORY_SEPARATOR."mapping_file.xml"))
-            {
-                echo ', mapping_file : "'.$_SESSION['config']['coreurl'].'custom/'.$_SESSION['custom_override_id'].'/modules/templates/xml/mapping_file.xml"';
-            }
-            else
-            {
-                echo ', mapping_file : "'.$_SESSION['config']['coreurl'].'modules/templates/xml/mapping_file.xml"';
-            }
-        }*/
-        ?>
+
     });
 
     // Custom save callback, gets called when the contents is to be submitted
