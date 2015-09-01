efbde93ce8a62f2b52e111b570153bd8e73dc038
Florian Azizian <florian.azizian@maarch.org>
2015-09-01 13:54:02 +0000

    FEAT #2681 rm xecho

--- maarch_entreprise/trunk/my_contacts/getContacts.php
+++ maarch_entreprise/trunk/my_contacts/getContacts.php
@@ -69,12 +69,12 @@ if ($countsContact == 0) {
 }
  
 for ($cptsContacts = 0;$cptsContacts< $countsContact;$cptsContacts++) {
-	$frmStr .= '<option value="'.functions::xecho($contact_selected[$cptsContacts]['id']).'"';
+	$frmStr .= '<option value="'.functions::xssafe($contact_selected[$cptsContacts]['id']).'"';
 	if ($_REQUEST['mode'] == "view") {
 		$frmStr .= ' disabled ';
 	}
 	$frmStr .= '>'
-	.  functions::xecho($contact_selected[$cptsContacts]['name'])
+	.  functions::xssafe($contact_selected[$cptsContacts]['name'])
 	. '</option>';
 }
 
