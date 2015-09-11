cc539aae838a55f28c3e0395e813696a570fe9e9
Florian Azizian <florian.azizian@maarch.org>
2015-09-11 08:57:27 +0000

    FIX #2726 use chrono of another attachment

--- attachments/trunk/get_other_chrono_attachment.php
+++ attachments/trunk/get_other_chrono_attachment.php
@@ -41,7 +41,7 @@
     $core->test_user();
     $db = new Database();
 
-    $stmt = $db->query("SELECT identifier FROM res_view_attachments WHERE res_id_master = ? and attachment_type = ?", array($_SESSION['doc_id'], $_SESSION['attachment_types_get_chrono'][$_REQUEST['type_id']]));
+    $stmt = $db->query("SELECT distinct identifier FROM res_view_attachments WHERE res_id_master = ? and attachment_type = ? and status <> 'DEL' and status <> 'OBS'", array($_SESSION['doc_id'], $_SESSION['attachment_types_get_chrono'][$_REQUEST['type_id']]));
     
     $listIdentifier = array();
 
