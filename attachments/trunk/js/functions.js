cfb1be1fa33d4f43a72e2aa2ceca979a2f93a61f
Nicolas Couture <couture@docimsol.com>
2015-09-14 14:32:36 +0000

    FIX #2447 Liaison entre le module attachment et les onglets (page de visa)

--- attachments/trunk/js/functions.js
+++ attachments/trunk/js/functions.js
@@ -86,12 +86,14 @@ function ValidAttachmentsForm (path, form_id) {
 				if ($('cur_resId')) var res_id_master = $('cur_resId').value;
 				if ($('cur_rep')) var rep_id = $('cur_rep').value;
 				
-				if($('viewframevalidRep'+num_rep)) {
+				if($('viewframevalidRep'+num_rep+'_'+rep_id)) {
 					if (response.majFrameId > 0){
-						$('viewframevalidRep'+num_rep).src = "index.php?display=true&module=visa&page=view_pdf_attachement&res_id_master="+res_id_master+"&id="+response.majFrameId;	
+						$('viewframevalidRep'+num_rep+'_'+rep_id).src = "index.php?display=true&module=visa&page=view_pdf_attachement&res_id_master="+res_id_master+"&id="+response.majFrameId;	
 						if ($('cur_rep')) $('cur_rep').value = response.majFrameId;
+						$('viewframevalidRep'+num_rep+'_'+rep_id).id = 'viewframevalidRep'+num_rep+'_'+response.majFrameId;
 					}
-					else $('viewframevalidRep'+num_rep).src = $('viewframevalidRep'+num_rep).src;
+					else
+						$('viewframevalidRep'+num_rep+'_'+rep_id).src = $('viewframevalidRep'+num_rep+'_'+rep_id).src;
 				}
 
 				if($('ans_'+num_rep+'_'+rep_id)) {
