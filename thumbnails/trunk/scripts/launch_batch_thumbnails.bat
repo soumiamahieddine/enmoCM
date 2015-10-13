3fe17a07e5a9277a01893d2d51b5aa69540dc2a8
Laurent Giovannoni <laurent.giovannoni@maarch.org>
2015-10-13 10:04:10 +0000

    FIX #2540

--- thumbnails/trunk/scripts/launch_batch_thumbnails.bat
+++ thumbnails/trunk/scripts/launch_batch_thumbnails.bat
@@ -1,3 +1,4 @@
+cd C:\xampp\htdocs\maarch_entreprise\modules\thumbnails\
 "C:\xampp\php\php.exe" C:\xampp\htdocs\maarch_entreprise\modules\thumbnails\create_tnl.php  C:\xampp\htdocs\maarch_entreprise\modules\thumbnails\xml\config_batch_letterbox.xml
 c:
 pause
