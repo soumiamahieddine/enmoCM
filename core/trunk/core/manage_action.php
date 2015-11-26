9005746f4bb24ac74af36f135d2d895d9ca77907
Damien Burel <damien.burel@maarch.org>
2015-11-26 14:20:20 +0000

    FIX #3104 change error message

--- core/trunk/core/manage_action.php
+++ core/trunk/core/manage_action.php
@@ -286,7 +286,10 @@ else if(empty($_POST['values']) || !isset($_POST['action_id']) || empty($_POST['
         }
 		elseif( $_POST['req'] == 'first_request' && $error_visa_response_project == true)
         {
-            echo "{status : 4, error : '".addslashes(_NO_RESPONSE_PROJECT_VISA)."', validate : 'OK', 'action_status' : '".functions::xssafe($status)."'}";
+            if ($visa->fileNameNotConverted)
+                echo "{status : 4, error : '" . addslashes(_NO_CONVERTED_PDF_VISA) . $visa->fileNameNotConverted . "', validate : 'OK', 'action_status' : '".functions::xssafe($status)."'}";
+            else
+                echo "{status : 4, error : '".addslashes(_NO_RESPONSE_PROJECT_VISA)."', validate : 'OK', 'action_status' : '".functions::xssafe($status)."'}";
             exit();
         }
         elseif( $_POST['req'] == 'first_request' && $error_visa_workflow == true)
