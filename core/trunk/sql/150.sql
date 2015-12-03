6f7808bdd7985e1f84b7a2ec4332414e2c4e7012
Alex Orluc <alex.orluc@maarch.org>
2015-12-03 21:08:54 +0000

    FEAT #3166 add service associate_folder in usergroups_services

--- core/trunk/sql/150.sql
+++ core/trunk/sql/150.sql
@@ -794,3 +794,7 @@ UPDATE users SET password = '964a5502faec7a27f63ab5f7bddbe1bd8a685616a90ffcba633
 -- Docservers for thumbnails
 INSERT INTO docserver_types (docserver_type_id, docserver_type_label, enabled, is_container, container_max_number, is_compressed, compression_mode, is_meta, meta_template, is_logged, log_template, is_signed, fingerprint_mode) VALUES ('TNL', 'Thumbnails', 'Y', 'N', 0, 'N', 'NONE', 'N', 'NONE', 'N', 'NONE', 'Y', 'NONE');
 INSERT INTO docservers (docserver_id, docserver_type_id, device_label, is_readonly, enabled, size_limit_number, actual_size_number, path_template, ext_docserver_info, chain_before, chain_after, creation_date, closing_date, coll_id, priority_number, docserver_location_id, adr_priority_number) VALUES ('TNL', 'TNL', 'Server for thumbnails of documents', 'N', 'Y', 50000000000, 0, '/opt/maarch/docservers/thumbnails_mlb/', NULL, NULL, NULL, '2015-03-16 14:47:49.197164', NULL, 'letterbox_coll', 11, 'NANTERRE', 3);
+
+--Add service associate_folder by default
+INSERT INTO usergroups_services(group_id,service_id)
+SELECT group_id,'associate_folder' from usergroups;
