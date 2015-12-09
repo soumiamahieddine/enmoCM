05d542af95baca0a35d3a6793f501952cf89c37b
Damien Burel <damien.burel@maarch.org>
2015-12-09 15:03:37 +0000

    FIX #3182 Change message for password reset

--- maarch_entreprise/trunk/change_pass.php
+++ maarch_entreprise/trunk/change_pass.php
@@ -20,68 +20,76 @@ $time = $core_tools->get_session_time_expire();
 ?>
 
 <body onload="setTimeout(window.close, <?php echo $time;?>*60*1000);">
-<div id="container">
-<div id="content" >
-<div class="error"><?php functions::xecho($_SESSION['error']);  $_SESSION['error'] = "";?></div>
+	<div id="container">
+		<div id="content" >
+			<div class="error">
+				<?php functions::xecho($_SESSION['error']);  $_SESSION['error'] = "";?>
+			</div>
 
-<div id="inner_content" class="clearfix">
-
-<h2 class="tit" ><img src="<?php echo $_SESSION['config']['businessappurl'];?>static.php?filename=default_maarch.gif" ><br/><br/><?php echo _FIRST_CONN;?></h2>
-<div class="block">
-
-<h3> <?php echo _YOUR_FIRST_CONNEXION.", "._PLEASE_CHANGE_PSW.".<br/>"._ASKED_ONLY_ONCE.".";?>  </h3>
-<div class="blank_space">&nbsp;</div>
-<form name="frmuser" method="post"  action="<?php echo $_SESSION['config']['businessappurl'];?>index.php?display=true&page=verif_pass" class="forms" >
-	<input type="hidden" name="display" value="true" />	
-	<input type="hidden" name="page" value="verif_pass" />
-	<p>
-		<label><?php echo _ID;?> : </label>
-		<input type="text" readonly="readonly" class="readonly" value="<?php functions::xecho($_SESSION['user']['UserId']);?>"/>
-	</p>
-	<p>
-		<label><?php echo _PASSWORD;?> : </label>
-		<input name="pass1"  type="password" id="pass1" value="" /><span class="red_asterisk"><i class="fa fa-star"></i></span>
-	</p>
-	<p>
-		<label><?php   echo _REENTER_PSW;?> : </label>
-		<input name="pass2"  type="password" id="pass2" value="" /><span class="red_asterisk"><i class="fa fa-star"></i></span>
-	</p>
-	<p>
-		<label><?php echo _LASTNAME;?> : </label>
-		<input name="LastName"  type="text" id="LastName"  value="<?php functions::xecho($_SESSION['user']['LastName']);?>" /><span class="red_asterisk"><i class="fa fa-star"></i></span>
-	</p>
-	<p>
-		<label><?php echo _FIRSTNAME;?> : </label>
-		<input name="FirstName" type="text" id="FirstName" value="<?php functions::xecho($_SESSION['user']['FirstName']);?>" /><span class="red_asterisk"><i class="fa fa-star"></i></span>
-	</p>
-	<?php if(!$core_tools->is_module_loaded("entities") )
-	{?>
-	<p>
-		<label ><?php echo _DEPARTMENT;?> : </label>
-		<input name="Department"  type="text" id="Department"  value="<?php functions::xecho($_SESSION['user']['department']);?>" />
-	</p>
-	<?php } ?>
-	<p>
-		<label><?php echo _PHONE_NUMBER;?> : </label>
-		<input name="Phone"  type="text" id="Phone" value="<?php functions::xecho($_SESSION['user']['Phone']);?>" />
-	</p>
-	<p>
-		<label ><?php echo _MAIL;?> : </label>
-		<input name="Mail"   type="text" id="Mail"  value="<?php functions::xecho($_SESSION['user']['Mail']);?>" /><span class="red_asterisk"><i class="fa fa-star"></i></span>
-	</p>
-	<p class="buttons">
-		<input type="submit" name="Submit" value="<?php echo _VALIDATE;?>" class="button" />
-        <input type="button" name="cancel" value="<?php echo _CANCEL;?>" class="button" onclick="window.location.href='<?php echo $_SESSION['config']['businessappurl'];?>index.php?display=true&page=login';" />
-	</p>
-
-</form>
-</div>
-<div class="block_end">&nbsp;</div>
-</div>
-
-
-</div>
-<div id="footer">&nbsp;</div>
-</div>
+			<div id="inner_content" class="clearfix">
+				<h2 class="tit" >
+					<img src="<?php echo $_SESSION['config']['businessappurl'];?>static.php?filename=default_maarch.gif" >
+					<br/><br/>
+					<?php
+					if ($_SESSION['user']['cookie_date'])
+						echo _MODIFICATION_PSW;
+					else
+						echo _FIRST_CONN;
+					?>
+				</h2>
+				<div class="block">
+					<?php if (!$_SESSION['user']['cookie_date']) { ?>
+						<h3> <?php echo _YOUR_FIRST_CONNEXION.", "._PLEASE_CHANGE_PSW.".<br/>"._ASKED_ONLY_ONCE.".";?>  </h3>
+					<?php } ?>
+					<div class="blank_space">&nbsp;</div>
+					<form name="frmuser" method="post"  action="<?php echo $_SESSION['config']['businessappurl'];?>index.php?display=true&page=verif_pass" class="forms" >
+						<input type="hidden" name="display" value="true" />
+						<input type="hidden" name="page" value="verif_pass" />
+						<p>
+							<label><?php echo _ID;?> : </label>
+							<input type="text" readonly="readonly" class="readonly" value="<?php functions::xecho($_SESSION['user']['UserId']);?>"/>
+						</p>
+						<p>
+							<label><?php echo _PASSWORD;?> : </label>
+							<input name="pass1"  type="password" id="pass1" value="" /><span class="red_asterisk"><i class="fa fa-star"></i></span>
+						</p>
+						<p>
+							<label><?php   echo _REENTER_PSW;?> : </label>
+							<input name="pass2"  type="password" id="pass2" value="" /><span class="red_asterisk"><i class="fa fa-star"></i></span>
+						</p>
+						<p>
+							<label><?php echo _LASTNAME;?> : </label>
+							<input name="LastName"  type="text" id="LastName"  value="<?php functions::xecho($_SESSION['user']['LastName']);?>" /><span class="red_asterisk"><i class="fa fa-star"></i></span>
+						</p>
+						<p>
+							<label><?php echo _FIRSTNAME;?> : </label>
+							<input name="FirstName" type="text" id="FirstName" value="<?php functions::xecho($_SESSION['user']['FirstName']);?>" /><span class="red_asterisk"><i class="fa fa-star"></i></span>
+						</p>
+						<?php if(!$core_tools->is_module_loaded("entities") )
+						{?>
+						<p>
+							<label ><?php echo _DEPARTMENT;?> : </label>
+							<input name="Department"  type="text" id="Department"  value="<?php functions::xecho($_SESSION['user']['department']);?>" />
+						</p>
+						<?php } ?>
+						<p>
+							<label><?php echo _PHONE_NUMBER;?> : </label>
+							<input name="Phone"  type="text" id="Phone" value="<?php functions::xecho($_SESSION['user']['Phone']);?>" />
+						</p>
+						<p>
+							<label ><?php echo _MAIL;?> : </label>
+							<input name="Mail"   type="text" id="Mail"  value="<?php functions::xecho($_SESSION['user']['Mail']);?>" /><span class="red_asterisk"><i class="fa fa-star"></i></span>
+						</p>
+						<p class="buttons">
+							<input type="submit" name="Submit" value="<?php echo _VALIDATE;?>" class="button" />
+							<input type="button" name="cancel" value="<?php echo _CANCEL;?>" class="button" onclick="window.location.href='<?php echo $_SESSION['config']['businessappurl'];?>index.php?display=true&page=login';" />
+						</p>
+					</form>
+				</div>
+				<div class="block_end">&nbsp;</div>
+			</div>
+		</div>
+		<div id="footer">&nbsp;</div>
+	</div>
 </body>
 </html>
