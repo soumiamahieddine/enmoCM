6da0d5e09952c0232b57819d1ede02b80ed65570
Florian Azizian <florian.azizian@maarch.org>
2016-01-05 14:02:01 +0000

    FEAT #3233 abstract class

--- attachments/trunk/class/class_modules_tools.php
+++ attachments/trunk/class/class_modules_tools.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
-*   Copyright 2008-2015 Maarch
+*   Copyright 2008-2016 Maarch
 *
 *   This file is part of Maarch Framework.
 *
@@ -28,55 +28,13 @@
 * @version 3.0
 * @since 10/2005
 * @license GPL v3
-* @author  Claire Figueras  <dev@maarch.org>
+* @author  <dev@maarch.org>
 *
 */
 
-class attachments
-{
+require_once 'modules/attachments/class/class_modules_tools_Abstract.php';
 
-    /**
-    * Build Maarch module tables into sessions vars with a xml configuration
-    * file
-    */
-    public function build_modules_tables()
-    {
-        if (file_exists(
-            $_SESSION['config']['corepath'] . 'custom' . DIRECTORY_SEPARATOR
-            . $_SESSION['custom_override_id'] . DIRECTORY_SEPARATOR . "modules"
-            . DIRECTORY_SEPARATOR . "attachments" . DIRECTORY_SEPARATOR . "xml"
-            . DIRECTORY_SEPARATOR . "config.xml"
-        )
-        ) {
-            $configPath = $_SESSION['config']['corepath'] . 'custom'
-                        . DIRECTORY_SEPARATOR . $_SESSION['custom_override_id']
-                        . DIRECTORY_SEPARATOR . "modules" . DIRECTORY_SEPARATOR
-                        . "attachments" . DIRECTORY_SEPARATOR . "xml"
-                        . DIRECTORY_SEPARATOR . "config.xml";
-        } else {
-            $configPath = "modules" . DIRECTORY_SEPARATOR . "attachments"
-                        . DIRECTORY_SEPARATOR . "xml" . DIRECTORY_SEPARATOR
-                        . "config.xml";
-        }
-        $xmlconfig = simplexml_load_file($configPath);
-        foreach ($xmlconfig->TABLENAME as $tableName) {
-            $_SESSION['tablename']['attach_res_attachments'] = (string) $tableName->attach_res_attachments;
-        }
-		$conf = $xmlconfig->CONFIG;
-		$_SESSION['modules_loaded']['attachments']['convertPdf'] = (string) $conf->convertPdf;
-		$_SESSION['modules_loaded']['attachments']['vbs_convert_path'] = (string) $conf->vbs_convert_path;
-		$_SESSION['modules_loaded']['attachments']['useExeConvert'] = (string) $conf->useExeConvert;
-        $watermark = $conf->watermark;
-        $_SESSION['modules_loaded']['attachments']['watermark']['enabled'] = (string) $watermark->enabled;
-        $_SESSION['modules_loaded']['attachments']['watermark']['text'] = (string) $watermark->text;
-        $_SESSION['modules_loaded']['attachments']['watermark']['position'] = (string) $watermark->position;
-        $_SESSION['modules_loaded']['attachments']['watermark']['font'] = (string) $watermark->font;
-        $_SESSION['modules_loaded']['attachments']['watermark']['text_color'] = (string) $watermark->text_color;
-		 
-        $hist = $xmlconfig->HISTORY;
-        $_SESSION['history']['attachadd'] = (string) $hist->attachadd;
-        $_SESSION['history']['attachup'] = (string) $hist->attachup;
-        $_SESSION['history']['attachdel'] = (string) $hist->attachdel;
-        $_SESSION['history']['attachview'] = (string) $hist->attachview;
-    }
+class attachments extends attachments_Abstract
+{
+    // custom
 }
