7468ecffd22776de58d2e0da97fde7b5a6ad582d
Laurent Giovannoni <laurent.giovannoni@maarch.org>
2016-01-08 15:35:19 +0000

    FIX #3253 pb with validate button on edit attachments

--- attachments/trunk/attachments_content.php
+++ attachments/trunk/attachments_content.php
@@ -1409,7 +1409,7 @@ if (!isset($_REQUEST['id'])) {
         $content .= _EDIT_MODEL;
         $content .= '" name="edit" id="edit" style="display:none" class="button" '
             .'onclick="window.open(\''. $_SESSION['config']['businessappurl'] . 'index.php?display=true&module=content_management&page=applet_popup_launcher&objectType=attachmentVersion&objectId=\'+$(\'templateOffice\').value+\'&attachType=\'+$(\'attachment_types\').value+\'&objectTable='. $objectTable .'&contactId=\'+$(\'contactidAttach\').value+\'&addressId=\'+$(\'addressidAttach\').value+\'&chronoAttachment=\'+$(\'chrono\').value+\'&titleAttachment=\'+$(\'title\').value+\'&back_date=\'+$(\'back_date\').value+\'&resMaster='.$_SESSION['doc_id']
-            .'\', \'\', \'height=200, width=250,scrollbars=no,resizable=no,directories=no,toolbar=no\');this.hide();"/>';
+            .'\', \'\', \'height=200, width=250,scrollbars=no,resizable=no,directories=no,toolbar=no\');this.hide();$(\'add\').setStyle({display: \'none\'});"/>';
     }
             $content .= '</p>';
     $content .= '<br/>';
@@ -1502,7 +1502,7 @@ if (isset($_REQUEST['id']) && ($data_attachment->status <> 'TMP' || ($data_attac
                 $content .= _EDIT_MODEL;
                 $content .= '" name="editModel" id="editModel" class="button" onclick="$(\'hiddenValidateStatus\').value=\'2\';$(\'edit\').style.visibility=\'visible\';window.open(\''
                     . $_SESSION['config']['businessappurl'] . 'index.php?display=true&module=content_management&page=applet_popup_launcher&objectType=attachmentUpVersion&objectId='.$_REQUEST['id'].'&contactId=\'+$(\'contactidAttach\').value+\'&addressId=\'+$(\'addressidAttach\').value+\'&titleAttachment=\'+$(\'title\').value+\'&back_date=\'+$(\'back_date\').value+\'&objectTable=res_view_attachments&resMaster='.$_SESSION['doc_id']
-                    .'\', \'\', \'height=200, width=250,scrollbars=no,resizable=no,directories=no,toolbar=no\');this.hide();"/>';
+                    .'\', \'\', \'height=200, width=250,scrollbars=no,resizable=no,directories=no,toolbar=no\');this.hide();$(\'edit\').setStyle({display: \'none\'});"/>';
             } /*else {
                                     $content .= '" name="edit" id="edit" style="display:none" class="button" '
                                                 .'onclick="window.open(\''. $_SESSION['config']['businessappurl'] . 'index.php?display=true&module=content_management&page=applet_popup_launcher&objectType=attachmentVersion&objectId=\'+$(\'templateOffice\').value+\'&objectTable='. $objectTable .'&contactId=\'+$(\'contactidAttach\').value+\'&chronoAttachment=\'+$(\'chrono\').value+\'&resMaster='.$_SESSION['doc_id']
