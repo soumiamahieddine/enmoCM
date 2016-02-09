c5a7d6ffb06b107a49e1c8a466597237af9db1fe
Damien Burel <damien.burel@maarch.org>
2016-02-09 10:14:23 +0000

    FEAT #3312 Add cases for valide mails

--- maarch_entreprise/trunk/actions/validate_mail.php
+++ maarch_entreprise/trunk/actions/validate_mail.php
@@ -925,7 +925,27 @@ function get_form_txt($values, $path_manage_action,  $id_action, $table, $module
         
         $frm_str .= '</div>';
         $frm_str .= '</div>';
-        
+
+        //CASES
+        if ($core_tools->is_module_loaded('cases')) {
+            require_once('modules/cases/class/class_modules_tools.php');
+            $cases = new cases();
+            $case_id = $cases->get_case_id($res_id);
+            if ($case_id <> false) {
+                $case_properties = $cases->get_case_info($case_id);
+            } else {
+                $case_properties = array();
+            }
+            $frm_str .= '<br>';
+            $frm_str .= '<h4 onclick="new Effect.toggle(\'cases_div\', \'blind\', {delay:0.2});'
+                . 'whatIsTheDivStatus(\'cases_div\', \'divStatus_cases_div\');hideOtherDiv(\'cases_div\');return false;" '
+                . 'onmouseover="this.style.cursor=\'pointer\';" class="categorie" style="width:90%;">';
+            $frm_str .= ' <span id="divStatus_cases_div" style="color:#1C99C5;"><<</span>&nbsp;<b>' . _CASE . '</b>';
+            $frm_str .= '<span class="lb1-details">&nbsp;</span>';
+            $frm_str .= '</h4>';
+            $frm_str .= '<br>';
+        }
+
         /*** Actions ***/
         $frm_str .= '<hr width="90%" align="center"/>';
         $frm_str .= '<p align="center">';
@@ -1097,7 +1117,68 @@ function get_form_txt($values, $path_manage_action,  $id_action, $table, $module
         $frm_str .= '</div>';
         $frm_str .= '<hr />';
         $frm_str .= '</div>';
-        
+
+        //CASES FRAME
+        if ($core_tools->is_module_loaded('cases')) {
+            if (!isset($case_properties['case_id'])) {
+                $case_properties = array();
+                $case_properties['case_id'] = '';
+                $case_properties['case_label'] = '';
+                $case_properties['case_description'] = '';
+            }
+            $frm_str .= '<div id="cases_div" style="display:none" onmouseover="this.style.cursor=\'pointer\';">';
+            $frm_str .= '<div class="block" style="margin-top:-2px;">';
+            $frm_str .= '<center><h2 onclick="new Effect.toggle(\'cases_div\', \'blind\', {delay:0.2});';
+            $frm_str .= 'whatIsTheDivStatus(\'cases_div\', \'divStatus_cases_div\');';
+            $frm_str .= 'return false;">' . _CASE . '</h2></center>';
+            $frm_str .= '<form name="cases" method="post" id="cases" action="#" class="forms addforms2" style="text-align:center;">';
+            $frm_str .= '<table width="98%" align="center" border="0">';
+            $frm_str .= '<tr>';
+            $frm_str .= '<td><label for="case_id" class="case_label" >' . _CASE . '</label></td>';
+            $frm_str .= '<td>&nbsp;</td>';
+            $frm_str .= '<td><input type="text" readonly="readonly" class="readonly" name="case_id" id="case_id" value="'
+                . $case_properties['case_id'] . '"  onblur=""/>';
+            $frm_str .= '</td>';
+            $frm_str .= '</tr>';
+            $frm_str .= '<tr>';
+            $frm_str .= '<td><label for="case_label" class="case_label" >' . _CASE_LABEL . '</label></td>';
+            $frm_str .= '<td>&nbsp;</td>';
+            $frm_str .= '<td><input type="text" readonly="readonly" class="readonly" name="case_label" '
+                . 'id="case_label" onblur="" value="'.$case_properties['case_label'].'" />';
+            $frm_str .= '</td>';
+            $frm_str .= '</tr>';
+            $frm_str .= '<tr>';
+            $frm_str .= '<td><label for="case_description" class="case_description" >'
+                . _CASE_DESCRIPTION . '</label></td>';
+            $frm_str .= '<td>&nbsp;</td>';
+            $frm_str .= '<td><input type="text" readonly="readonly" class="readonly" '
+                . 'name="case_description" id="case_description" onblur="" value="'
+                . $case_properties['case_description'].'" />';
+            $frm_str .= '</td>';
+            $frm_str .= '</tr>';
+            $frm_str .= '<tr>';
+            if ($core_tools->test_service('join_res_case_in_process', 'cases',false) == 1) {
+                $frm_str .= '<td colspan="3"> <input type="button" class="button" name="search_case" '
+                    . 'id="search_case" value="';
+                if ($case_properties['case_id']<>'') {
+                    $frm_str .= _MODIFY_CASE;
+                } else {
+                    $frm_str .= _JOIN_CASE;
+                }
+                $frm_str .= '" onclick="window.open(\'' . $_SESSION['config']['businessappurl']
+                    . 'index.php?display=true&module=cases&page=search_adv_for_cases'
+                    . '&searched_item=res_id_in_process&searched_value=' . $_SESSION['doc_id']
+                    . '\',\'\', \'scrollbars=yes,menubar=no,toolbar=no,resizable=yes,'
+                    . 'status=no,width=1020,height=710\');"/></td>';
+            }
+            $frm_str .= '</tr>';
+            $frm_str .= '</table>';
+            $frm_str .= '</form>';
+            $frm_str .= '<br>';
+            $frm_str .= '</div>';
+            $frm_str .= '<hr />';
+            $frm_str .= '</div>';
+        }
         //NOTES
         if ($core_tools->is_module_loaded('notes')) {
             //Iframe notes
