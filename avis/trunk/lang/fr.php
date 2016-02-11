63037f8506a24be8f650a5ec6b4102658fa64539
Alex Orluc <alex.orluc@maarch.org>
2016-02-11 14:45:15 +0000

    add content module

---
+++ avis/trunk/lang/fr.php
@@ -0,0 +1,52 @@
+<?php
+// CIRCUIT AVIS
+if (!defined("_AVIS_WORKFLOW"))
+    define("_AVIS_WORKFLOW", "Circuit d'avis");
+
+if (!defined("_AVIS_WORKFLOW_COMMENT"))
+    define("_AVIS_WORKFLOW_COMMENT", "Gestion du circuit d'avis");
+
+if (!defined("_SEND_DOCS_TO_RECOMMENDATION"))
+    define("_SEND_DOCS_TO_RECOMMENDATION", "Transmettre pour avis");
+
+if (!defined("_AVIS_USER"))
+    define("_AVIS_USER", "Pour avis");
+
+if (!defined("_RECOMMENDATION_USER"))
+    define("_RECOMMENDATION_USER", "Personne pour avis");
+
+if (!defined("_RECOMMENDATION_USER"))
+    define("_RECOMMENDATION_USER", "Utilisateurs pour avis");
+
+if (!defined("_RECOMMENDATION_NOTE"))
+    define("_RECOMMENDATION_NOTE", "Motif de l'avis");
+
+if (!defined("_RECOMMENDATION_LIMIT_DATE"))
+    define("_RECOMMENDATION_LIMIT_DATE", "Date limite de l'avis");
+
+if (!defined("_TO_AVIS"))
+    define("_TO_AVIS", "POUR AVIS");
+
+if (!defined("_AVIS_ANSWERS"))
+    define("_AVIS_ANSWERS", "Donne son avis");
+
+if (!defined("_LAST_AVIS"))
+    define("_LAST_AVIS", "Conseiller décideur");
+
+if (!defined("_SEND_TO_AVIS_WF"))
+    define("_SEND_TO_AVIS_WF", "Evoyer au workflow d'avis");
+
+if (!defined("_WAITING_FOR_AVIS"))
+    define("_WAITING_FOR_AVIS", "En attante d'avis");
+
+if (!defined("_PROCEED_WORKFLOW_AVIS"))
+    define("_PROCEED_WORKFLOW_AVIS", "Poursuivre le circuit d'avis");
+
+if (!defined("_EMPTY_AVIS_WORKFLOW"))
+    define("_EMPTY_AVIS_WORKFLOW", "Le circuit d'avis est vide");
+
+if (!defined("_AVIS_SENT"))
+    define("_AVIS_SENT", "Avis donné");
+
+if (!defined("_AVIS_NB"))
+    define("_AVIS_NB", "Avis n°");
