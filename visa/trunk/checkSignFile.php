e045f3176344d6b1145cd17bbdc8aa83ed104180
Alex Orluc <alex.orluc@maarch.org>
2016-04-05 17:29:08 +0000

    FIX #3652 add status <> 'DEL' in sql

--- visa/trunk/checkSignFile.php
+++ visa/trunk/checkSignFile.php
@@ -7,7 +7,7 @@
 	require_once 'modules/attachments/attachments_tables.php';
 	
 	$db = new Database();
-	$stmt = $db->query("SELECT attachment_type from ".RES_ATTACHMENTS_TABLE." where res_id = ?", array($_REQUEST['res_id']));
+	$stmt = $db->query("SELECT attachment_type from ".RES_ATTACHMENTS_TABLE." where res_id = ? and status <> 'DEL'", array($_REQUEST['res_id']));
 	$infos = $stmt->fetchObject();
 	$type = $infos->attachment_type;
 	if ($type == 'signed_response'){
