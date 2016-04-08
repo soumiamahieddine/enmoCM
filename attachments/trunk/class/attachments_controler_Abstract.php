1de3784a431be0ab0b554154e9f106df727f7716
Damien Burel <damien.burel@maarch.org>
2016-04-08 13:25:06 +0000

    FIX #3710 Fix removeTemporaryAttachmentOnDocserver

--- attachments/trunk/class/attachments_controler_Abstract.php
+++ attachments/trunk/class/attachments_controler_Abstract.php
@@ -337,4 +337,55 @@ abstract class attachments_controler_Abstract
         
         return $result;
     }
+
+    /**
+     * Remove temporary attachment file on docserver
+     * @param   bigint $resIdAttachment id of the attachment resource
+     * @param   bigint $resIdMaster id of the master document
+     * @param   string $userId user id who created the temporary attachment
+     * @return  boolean if ok, return true.
+     */
+    public function removeTemporaryAttachmentOnDocserver($resIdAttachment, $resIdMaster, $userId) {
+
+        $db = new Database();
+        $stmt = $db->query(
+            "SELECT docserver_id, path, filename, fingerprint
+                FROM res_view_attachments
+                WHERE (res_id = ? OR res_id_version = ?) AND res_id_master = ? AND status = 'TMP' AND typist = ? ORDER BY relation desc",
+            array($resIdAttachment, $resIdAttachment, $resIdMaster, $userId)
+        );
+
+        if ($stmt->rowCount() == 0) {
+            $_SESSION['error'] = _NO_DOC_OR_NO_RIGHTS;
+            return false;
+        } else {
+            $line           = $stmt->fetchObject();
+            $docserverOld   = $line->docserver_id;
+            $pathOld        = $line->path;
+            $filenameOld    = $line->filename;
+            $fingerprintOld = $line->fingerprint;
+
+            $stmt = $db->query("SELECT path_template FROM " . _DOCSERVERS_TABLE_NAME . " WHERE docserver_id = ?", array($docserverOld) );
+            $lineDoc   = $stmt->fetchObject();
+            $docserver = $lineDoc->path_template;
+            $file      = $docserver . $pathOld . $filenameOld;
+            $file      = str_replace("#", DIRECTORY_SEPARATOR, $file);
+
+            require_once 'core/class/docservers_controler.php';
+            require_once 'core/class/docserver_types_controler.php';
+            $docserverControler = new docservers_controler();
+            $docserverTypeControler = new docserver_types_controler();
+
+            require_once 'core/docservers_tools.php';
+
+            $docserver           = $docserverControler->get($docserverOld);
+            $docserverTypeObject = $docserverTypeControler->get($docserver->docserver_type_id);
+            $fingerprintOldFile  = Ds_doFingerprint($file, $docserverTypeObject->fingerprint_mode);
+            if ($fingerprintOld == $fingerprintOldFile) {
+                unlink($file);
+            }
+            return true;
+        }
+    }
+
 }
