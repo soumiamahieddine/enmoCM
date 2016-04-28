86f076802e1165d63f26c7e228fee60ad42c7b9d
Sébastien Nana <sebastien.nana@maarch.org>
2016-04-28 14:03:59 +0000

    add content of project

---
+++ thesaurus/trunk/js/functions.js
@@ -0,0 +1,174 @@
+function launch_thesaurus_tooltips(trigger, target,thesaurus_name) {
+	var path_to_script = "<?php echo $_SESSION['config']['businessappurl']; ?>index.php?display=true&page=get_thesaurus_info&module=thesaurus";
+	var content='';
+	new Ajax.Request(path_to_script,
+	{
+		method:'post',
+		parameters:
+		{
+			thesaurus_name : thesaurus_name
+		},
+		 onSuccess: function(answer){
+		 	var json = JSON.parse(answer.responseText);
+			//eval("response = "+answer.responseText);
+			if (json.info.thesaurus_parent_id) {
+				content += '<h2 title="terme parent" style="width:300px;">'+json.info.thesaurus_parent_id+'</h2>';
+			}
+			content += '<ul style="padding-left:20px;">';
+			content += '<li style="list-style-type:initial;">';
+			content += '<fieldset style="border: solid 1px;"><legend><b>'+json.info.thesaurus_name+'</b></legend>';
+			if(json.info.thesaurus_description != null){
+				content += '<p style="padding:5px;" title="description">'+json.info.thesaurus_description+'</p>';
+
+			}else{
+				content += '<p style="text-align:center;font-style:italic;padding:5px;color:grey;" title="description">aucune description</p>';		
+			}
+			content += '<hr/>';
+			if(json.info.thesaurus_name_associate != null){
+				content += '<p style="text-align:center;padding:5px;" title="terme(s) associé(s)">'+json.info.thesaurus_name_associate+'</p>';
+
+			}else{
+				content += '<p style="text-align:center;font-style:italic;padding:5px;color:grey;" title="terme(s) associé(s)">aucun terme associé</p>';		
+			}
+			content += '</fieldset>';
+			content += '</li>';
+			content += '</ul>';
+			content += '<ul title="autre(s) terme(s) lié(s) à :  « '+json.info.thesaurus_parent_id+' »" style="margin-top:5px;padding-left:20px;max-height: 200px;overflow: hidden;overflow-y: auto;">';
+			if(json.info_annexe){
+				for (var i = json.info_annexe.length - 1; i >= 0; i--) {
+					if(json.info.thesaurus_name != json.info_annexe[i]){
+						//console.log(document.getElementById('thesaurus_'+json.info_annexe[i].thesaurus_id));
+						if(document.getElementById('thesaurus_'+json.info_annexe[i].thesaurus_id+'').selected == true){
+							content += '<li style="list-style-type:initial;cursor:text;color:grey;font-style:italic;">';
+
+						}else{
+							content += '<li style="list-style-type:initial;cursor:pointer;" onclick="document.getElementById(\'thesaurus_'+json.info_annexe[i].thesaurus_id+'\').selected = true;Event.fire($(\'thesaurus\'), \'chosen:updated\');">';
+						}
+						content += json.info_annexe[i].thesaurus_name;				
+						content += '</li>';
+					}
+				} 
+			}
+			
+			content += '</ul>';
+			document.getElementById("thesaurus_chosen_"+json.info.thesaurus_id).onclick=function(e){toolTipThes(e, content)}; 
+			//console.log(document.getElementById("thesaurus_chosen_"+json.info.thesaurus_id));
+			//console.log(document.getElementById("thesaurus_chosen_"+json.info.thesaurus_id));
+			/*new Opentip(trigger, content, '', {
+			    target: target,
+			    showOn: 'click',
+			    hideTrigger: 'closeButton',
+			    tipJoint: 'left top',
+			    fixed: true });Opentip.lastZIndex = 1500;*/
+			}
+	});
+
+}
+
+function load_specific_thesaurus(thesaurus_id) {
+	var path_to_script = "<?php echo $_SESSION['config']['businessappurl']; ?>index.php?display=true&page=get_thesaurus_childs&module=thesaurus";
+	var content='';
+
+	if(document.getElementById('thesaurus_parent_id').selectedOptions[0].value != ""){
+		new Ajax.Request(path_to_script,
+		{
+			method:'post',
+			parameters:
+			{
+				thesaurus_id : thesaurus_id
+			},
+			 onSuccess: function(answer){
+			 	thesaurus_parent_label=document.getElementById('thesaurus_parent_id').selectedOptions[0].innerHTML.replace(/^\s+/g, '').replace(/\s+$/g, '').replace(/&nbsp;/gi,'');
+
+			 	var json = JSON.parse(answer.responseText);
+				//eval("response = "+answer.responseText);
+				if(json){
+					document.getElementById('thesaurus_name_specific').innerHTML = "Terme(s) spécifique(s) pour :<br/>« " + thesaurus_parent_label + " »";
+					content += '<ul style="text-align:left;padding-left:20px;">';
+					for (var i = json.length - 1; i >= 0; i--) {				
+							content += '<li style="list-style-type:initial;padding:5px;" title="Accéder à la page du terme">';
+							content += "<a href='<?php echo $_SESSION['config']['businessappurl']; ?>index.php?page=manage_thesaurus_list_controller&mode=up&module=thesaurus&id="+json[i].thesaurus_id+"&start=0&order=asc&order_field=&what='>"+json[i].thesaurus_name+"</a>";				
+							content += '</li>';
+
+					} 
+					document.getElementById('thesaurus_list_specific_content').innerHTML = content;
+					content += '</ul>';
+				}else{
+					document.getElementById('thesaurus_name_specific').innerHTML = "Terme(s) spécifique(s) pour :<br/>« " + thesaurus_parent_label + " »";
+					content = '<i style="color:grey;">Aucun terme spécifique</i>';
+					document.getElementById('thesaurus_list_specific_content').innerHTML = content;
+				}
+				
+				}
+		});
+	}else{
+		document.getElementById('thesaurus_name_specific').innerHTML = "Terme(s) spécifique(s) pour :<br/><i>Pas de terme générique</i>";
+		content = '<i style="color:grey;">Aucun terme spécifique</i>';
+		document.getElementById('thesaurus_list_specific_content').innerHTML = content;
+	}
+	
+}
+
+function toolTipThes(e, content){
+    
+    var DocRef;
+    console.log(e);
+    if(e){
+        mouseX = e.pageX;
+        mouseY = e.pageY;
+    }
+    else{
+        mouseX = event.clientX;
+        mouseY = event.clientY;
+
+        if( document.documentElement && document.documentElement.clientWidth) {
+            DocRef = document.documentElement;
+        } else {
+            DocRef = document.body;
+        }
+
+        mouseX += DocRef.scrollLeft;
+        mouseY += DocRef.scrollTop;
+    }
+    var topPosition  = mouseY + 10;
+    var leftPosition = mouseX - 200;
+    
+    var writeHTML = content
+    $('return_previsualise_thes').update(writeHTML);
+    $('return_previsualise_thes').innerHTML;
+
+    var divWidth = $('return_previsualise_thes').getWidth();
+    if (divWidth > 0) {
+        leftPosition = mouseX - (divWidth + 40);
+    }
+	if(leftPosition < 0){
+		leftPosition = - leftPosition;
+	}
+    var divHeight = $('return_previsualise_thes').getHeight();
+    if (divHeight > 0) {
+        topPosition = mouseY - (divHeight - 2);
+    }
+    
+    if (topPosition < 0) {
+        topPosition = 10;
+    }
+    
+    //var scrollY = (document.all ? document.scrollTop : window.pageYOffset);
+    var scrollY = f_filterResults (
+        window.pageYOffset ? window.pageYOffset : 0,
+        document.documentElement ? document.documentElement.scrollTop : 0,
+        document.body ? document.body.scrollTop : 0
+    );
+    
+    if (topPosition < scrollY) {
+        topPosition = scrollY + 10;
+    }
+    
+    $('return_previsualise_thes').style.top=topPosition-10+'px';
+    $('return_previsualise_thes').style.left=mouseX+'px';
+    
+    $('return_previsualise_thes').style.maxWidth='600px';
+    $('return_previsualise_thes').style.maxHeight='600px';
+    $('return_previsualise_thes').style.display='block';
+    
+}
---
+++ thesaurus/trunk/manage_thesaurus_list.php
@@ -0,0 +1,216 @@
+<?php
+/*
+*    Copyright 2008,2016 Maarch
+*
+*  This file is part of Maarch Framework.
+*
+*   Maarch Framework is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   Maarch Framework is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*    along with Maarch Framework.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/**
+* Module : Thesaurus
+* 
+* This module is used to store ressources with any keywords
+* V: 1.0
+*
+* @file
+* @author Loic Vinet
+* @date $date$
+* @version $Revision$
+*/
+
+/* Affichage */
+if ($mode == 'list') {
+    $list = new list_show();
+    $list->admin_list(
+        $thesauruslist['tab'],
+        count($thesauruslist['tab']),
+        $thesauruslist['title'],
+        'thesaurus_name',
+        'manage_thesaurus_list_controller&mode=list',
+        'thesaurus',
+        'thesaurus_id',
+        true,
+        $thesauruslist['page_name_up'],
+        $thesauruslist['page_name_val'],
+        $thesauruslist['page_name_ban'],
+        $thesauruslist['page_name_del'],
+        $thesauruslist['page_name_add'],
+        $thesauruslist['label_add'],
+        false,
+        false,
+        _ALL_THESAURUS,
+        _THESAURUS,
+        'bookmark-o',
+        true,
+        true,
+        false,
+        true,
+        $eventsList['what'],
+        true,
+        $thesauruslist['autoCompletionArray']
+    );
+    echo '<script>
+    $(\'what\').onblur=function(){
+        var str = $(\'what\').value;
+        str = str.split(\'\').reverse().join(\'\');
+        index = str.indexOf("(",-1);
+        str = str.substring(index+2, str.length);
+        str = str.split(\'\').reverse().join(\'\');
+        $(\'what\').value = str;
+    };
+</script>';
+} elseif ($mode == 'up' || $mode == 'add') {
+    //var_dump($_SESSION['m_admin']['thesaurus'])
+    ?><h1><i class="fa fa-bookmark-o fa-2x"> </i>
+        <?php
+        if ($mode == 'up') {
+            echo _MODIFY_THESAURUS;
+        } elseif ($mode == 'add') {
+            echo _ADD_THESAURUS;
+        }?>
+    </h1>
+    <div id="inner_content" class="clearfix" align="center">
+        <br />
+    <?php
+    if ($state == false) {
+        echo '<br /><br /><br /><br />' . _THIS_EVENT . ' ' . _IS_UNKNOWN
+        . '<br /><br /><br /><br />';
+    } else { ?>
+    <form name="frmevent" id="frmevent" method="post" action="<?php
+            echo $_SESSION['config']['businessappurl'] . 'index.php?display=true'
+            . '&amp;module=thesaurus&amp;page=manage_thesaurus_list_controller&amp;mode='
+            . $mode;?>" class="forms addforms" style="width:100%;">
+    <div class="block" style="width:50%;height:300px;float:left;">
+        <div style="width:400px;">
+            <input type="hidden" name="display" value="true" />
+            <input type="hidden" name="admin" value="thesaurus" />
+            <input type="hidden" name="page" value="manage_thesaurus_list_controler" />
+            <input type="hidden" name="mode" value="<?php functions::xecho($mode);?>" />
+
+            <input type="hidden" name="thesaurus_id" id="thesaurus_id" value="<?php functions::xecho($_SESSION['m_admin']['thesaurus']['thesaurus_id']);?>" />
+
+            <input type="hidden" name="order" id="order" value="<?php
+                functions::xecho($_REQUEST['order']);?>" />
+            <input type="hidden" name="order_field" id="order_field" value="<?php
+                functions::xecho($_REQUEST['order_field']);?>" />
+            <input type="hidden" name="what" id="what" value="<?php
+                functions::xecho($_REQUEST['what']);?>" />
+            <input type="hidden" name="start" id="start" value="<?php
+                functions::xecho($_REQUEST['start']);?>" />
+
+            <p>
+                <label for="thesaurus_parent_id"><?php echo _THESAURUS_PARENT_ID;?> : </label>
+                <?php
+                require_once 'modules' . DIRECTORY_SEPARATOR . 'thesaurus'
+                            . DIRECTORY_SEPARATOR . 'class' . DIRECTORY_SEPARATOR
+                            . 'class_modules_tools.php';
+                $thesaurus = new thesaurus();
+                $allThesaurusTree= array();
+                $allThesaurusTree = $thesaurus->getShortThesaurusTreeAdvanced($allThesaurusTree);    
+
+                $countAllThesaurus = count($allThesaurusTree);
+
+                ?>
+                <select id="thesaurus_parent_id" name="thesaurus_parent_id" onchange="load_specific_thesaurus(this.value);">
+                    <?php 
+                        echo '<option value="">-- Aucun Terme générique --</option>';
+                        for ($cptThesaurus = 0;$cptThesaurus < $countAllThesaurus;$cptThesaurus++) {
+                        $label = str_replace("&emsp;", "", $allThesaurusTree[$cptThesaurus]['LABEL']);
+                        echo '<option title="'.functions::show_string($allThesaurusTree[$cptThesaurus]['LABEL']).'" data-object_type="thesaurus_id" value="' . $allThesaurusTree[$cptThesaurus]['ID'] . '"';
+                        if($label == $_SESSION['m_admin']['thesaurus']['thesaurus_parent_id']){
+                            echo 'selected="selected"';
+                        }
+                        echo  '>' 
+                            .  functions::show_string($allThesaurusTree[$cptThesaurus]['LABEL']) 
+                            . '</option>';
+                    } ?>
+                </select>
+                <script type="text/javascript">document.getElementById("thesaurus_parent_id").onchange();</script>
+                <!--<input name="thesaurus_parent_id" type="text"  id="thesaurus_parent_id" value="<?php
+                    echo functions::show_str(
+                        $_SESSION['m_admin']['thesaurus']['thesaurus_parent_id']
+                    );?>"/>-->
+            </p>
+
+            <p>
+                <label for="thesaurus_name"><?php echo _THESAURUS_NAME;?> : </label>
+                <input name="thesaurus_name" type="text"  id="thesaurus_name" value="<?php
+                    echo functions::show_str(
+                        $_SESSION['m_admin']['thesaurus']['thesaurus_name']
+                    );?>"/>
+            </p>
+
+            <p>
+                <label for="thesaurus_description"><?php echo _DESC;?> : </label>
+                <textarea name="thesaurus_description" id="thesaurus_description" style="width:100%;height:150px;"><?php echo $_SESSION['m_admin']['thesaurus']['thesaurus_description']; ?></textarea>
+            </p>
+
+            <p class="buttons">
+                <?php
+                if ($mode == 'up') { ?>
+                    <input class="button" type="submit" name="thesaurus_submit" value=
+                    "<?php echo _MODIFY;?>" />
+                    <?php
+                } elseif ($mode == 'add') { ?>
+                    <input type="submit" class="button"  name="thesaurus_submit" value=
+                    "<?php echo _ADD;?>" />
+                    <?php
+                }
+                ?>
+                <input type="button" class="button"  name="cancel" value="<?php
+                 echo _CANCEL;?>" onclick="javascript:window.location.href='<?php
+                 echo $_SESSION['config']['businessappurl'];
+                 ?>index.php?page=manage_thesaurus_list_controller&amp;mode=list&amp;module=thesaurus'"/>
+            </p>
+            </div>
+        </div>
+        <div class="block" style="margin-left:10px;width:20%;height:300px;float:right;">
+            <h2 id ="thesaurus_name_specific"><?php echo _THESAURUS_LIST_SPECIFIC; ?></h2>
+            <p id="thesaurus_list_specific_content" style="height:240px;overflow:hidden;overflow-y:auto;">
+            <p>
+        </div>
+        <div class="block" style="width:20%;height:300px;float:right;">
+            <h2><?php echo _THESAURUS_NAME_ASSOCIATE; ?></h2>
+            <?php
+            require_once 'modules' . DIRECTORY_SEPARATOR . 'thesaurus'
+                        . DIRECTORY_SEPARATOR . 'class' . DIRECTORY_SEPARATOR
+                        . 'class_modules_tools.php';
+            $thesaurus = new thesaurus();
+
+            $thesaurus_name_associate = explode('/', $_SESSION['m_admin']['thesaurus']['thesaurus_name_associate']);
+
+            ?>
+            <select multiple="multiple" id="thesaurus_name_associate" name="thesaurus_name_associate[]">
+            <?php
+                for ($cptThesaurus = 0;$cptThesaurus < $countAllThesaurus;$cptThesaurus++) {
+                    $label = str_replace("&emsp;", "", $allThesaurusTree[$cptThesaurus]['LABEL']);
+                    echo '<option title="'.functions::show_string($label).'" data-object_type="thesaurus_id" value="' . $allThesaurusTree[$cptThesaurus]['ID'] . '"';
+                        if(in_array($label, $thesaurus_name_associate)){
+                            echo 'selected="selected"';
+                        }
+                    echo  '>';
+                    echo functions::show_string($allThesaurusTree[$cptThesaurus]['LABEL']) 
+                        . '</option>';
+                } ?>
+            </select>
+        </div>
+        </form >
+        <style type="text/css">.search-choice{padding: 5px !important;}</style>
+        <script type="text/javascript">new Chosen($('thesaurus_parent_id'),{disable_search_threshold: 10});new Chosen($('thesaurus_name_associate'),{width: "100%", disable_search_threshold: 10});
+        </script>
+    <?php
+    }
+    ?></div><?php
+}
