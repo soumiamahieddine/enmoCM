b29ec8faad8b701497efb47563fb63335d891955
Alex Orluc <alex.orluc@maarch.org>
2016-05-04 16:47:28 +0000

    FIX #3376 comment function

--- maarch_entreprise/trunk/js/indexing.js
+++ maarch_entreprise/trunk/js/indexing.js
@@ -1311,7 +1311,7 @@ function getIframeContent(path_manage_script)
 {
     if ($('choose_file_div')) {
         var choose_file_div = $('choose_file_div');
-        if (choose_file_div.style.display == 'none') {
+        /*if (choose_file_div.style.display == 'none') {
             var iframe = document.getElementById("file_iframe");
             var iframeContent = iframe.contentDocument;
             var templateContent2 = iframeContent.getElementById("template_content_ifr");
@@ -1337,7 +1337,7 @@ function getIframeContent(path_manage_script)
                     });
                 }
             }
-        }
+        }*/
     }
 }
 
