4b71419b4d32c07bc508438ef9ac0b45f4954a96
Alex Orluc <alex.orluc@maarch.org>
2016-07-26 12:51:35 +0000

    FIX #4354 fix userId checkLoading

--- content_management/trunk/js/functions.js
+++ content_management/trunk/js/functions.js
@@ -196,7 +196,7 @@ function showDiv(divName, spanNb, divCreate, path_manage_script)
 }
 
 function checkEditingDoc(userId) {
-    var lck_name = "applet_<?php echo $_SESSION['user']['UserId'] ?>";
+    //var lck_name = "applet_<?php echo $_SESSION['user']['UserId'] ?>";
     if($('add')){
         var target = $('add');
     }else{
@@ -207,7 +207,7 @@ function checkEditingDoc(userId) {
     {
         method:'post',
         parameters: {
-            lck_name : lck_name},
+            lck_name : userId},
             onSuccess: function(answer){
             eval("response = "+answer.responseText);
             if(response.status == 0) {
