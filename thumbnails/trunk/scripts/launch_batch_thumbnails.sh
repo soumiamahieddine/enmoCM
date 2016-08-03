af9ae1f7968458e548783f93b0762373b04f1f95
Alex Orluc <alex.orluc@maarch.org>
2016-08-03 15:55:02 +0000

    FIX #4474 add thumnails config for attachments

--- thumbnails/trunk/scripts/launch_batch_thumbnails.sh
+++ thumbnails/trunk/scripts/launch_batch_thumbnails.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-cd /var/www/html/maarch_trunk_secure/modules/thumbnails/
-php /var/www/html/maarch_trunk_secure/modules/thumbnails/create_tnl.php /var/www/html/maarch_trunk_secure/modules/thumbnails/xml/config_batch_letterbox.xml
+cd /var/www/html/maarch_courrier_trunk/modules/thumbnails/
+php /var/www/html/maarch_courrier_trunk/modules/thumbnails/create_tnl.php /var/www/html/maarch_courrier_trunk/modules/thumbnails/xml/config_batch_letterbox.xml
+php /var/www/html/maarch_courrier_trunk/modules/thumbnails/create_tnl.php /var/www/html/maarch_courrier_trunk/modules/thumbnails/xml/config_batch_attachments.xml
