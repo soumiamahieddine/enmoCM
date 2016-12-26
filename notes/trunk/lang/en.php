36b21892f0b376c53a3ed6b425f05d600c635b22
Henri Queneau <henri.queneau@maarch.org>
2016-12-26 09:22:53 +0000

    FEAT #5008 add en.php

--- notes/trunk/lang/en.php
+++ notes/trunk/lang/en.php
@@ -19,38 +19,39 @@
  *    along with Maarch Framework.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-if (!defined('_ADD_NOTE'))
-    define('_ADD_NOTE','Add a note');
-if (!defined('_READ'))
-    define('_READ', 'read');
-if (!defined('_ADDITION_NOTE'))
-    define('_ADDITION_NOTE', 'Add a note');
-if (!defined('_NOTES_ADDED'))
-    define('_NOTES_ADDED', 'Note added');
-if (!defined('_NOTES_DELETED'))
-    define('_NOTES_DELETED', 'Note deleted');
-if (!defined('_NOTES_MODIFIED'))
-    define('_NOTES_MODIFIED', 'Note modified');
-if (!defined('_NOTE_UPDATED'))
-    define('_NOTE_UPDATED', 'Note modified');
-if (!defined('_NOTES'))
-    define('_NOTES', 'Notes');
-if (!defined('_NOTES_COMMENT'))
-    define('_NOTES_COMMENT', 'Notes');
-if (!defined('_OF'))
-    define('_OF', 'of');
-if (!defined('_NOTE_DONT_EXIST'))
-    define('_NOTE_DONT_EXIST', 'This note dont exists');
-if (!defined('_THIS_NOTE_IS_VISIBLE_BY'))
-    define('_THIS_NOTE_IS_VISIBLE_BY', 'This note is visible by');
+if (!defined("_ADD_NOTE"))
+    define("_ADD_NOTE","Add a note");
+if (!defined("_READ"))
+    define("_READ", "Read");
+if (!defined("_ADDITION_NOTE"))
+    define("_ADDITION_NOTE", "Note addition");
+if (!defined("_ADDITION_NOTE_PRIVATE"))
+    define("_ADDITION_NOTE_PRIVATE", "Private note addition");
+if (!defined("_NOTES_ADDED"))
+    define("_NOTES_ADDED", "Added note");
+if (!defined("_NOTES_DELETED"))
+    define("_NOTES_DELETED", "Deleted note");
+if (!defined("_NOTES_MODIFIED"))
+    define("_NOTES_MODIFIED", "Modified note");
+if (!defined("_NOTE_UPDATED"))
+    define("_NOTE_UPDATED", "Modified note");
+if (!defined("_NOTES"))
+    define("_NOTES", "Notes");
+if (!defined("_NOTES_COMMENT"))
+    define("_NOTES_COMMENT", "Notes");
+if (!defined("_OF"))
+    define("_OF", "of");
+if (!defined("_NOTE_DONT_EXIST"))
+    define("_NOTE_DONT_EXIST", "This note doesn't exist");
+if (!defined("_THIS_NOTE_IS_VISIBLE_BY"))
+    define("_THIS_NOTE_IS_VISIBLE_BY", "Limit this note to the following departments (if it's empty, the note is public)");
 
 //TEMPLATES FOR NOTES
-if (!defined('_NOTE_TEMPLATE'))
-    define('_NOTE_TEMPLATE', 'Template for note');
-if (!defined('_SELECT_NOTE_TEMPLATE'))
-    define('_SELECT_NOTE_TEMPLATE', 'Select a template for the note');
+if (!defined("_NOTE_TEMPLATE"))
+    define("_NOTE_TEMPLATE", "Note template");
+if (!defined("_SELECT_NOTE_TEMPLATE"))
+    define("_SELECT_NOTE_TEMPLATE", "Select a note template");
 
 //SERVICES FOR NOTES
 if (!defined("_NOTES_RESTRICTION"))
-    define("_NOTES_RESTRICTION", "Restrict notes for your entity");
-
+    define("_NOTES_RESTRICTION", "By default, limit the notes to your department");
