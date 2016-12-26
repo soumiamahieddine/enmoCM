fce71fa8e72530918c933fe269b95f5e72c7a16e
Henri Queneau <henri.queneau@maarch.org>
2016-12-26 09:28:41 +0000

    FEAT #5014 add en.php

---
+++ thesaurus/trunk/lang/en.php
@@ -0,0 +1,110 @@
+<?php
+/*
+*    Copyright 2008,2016 Maarch
+*
+*  This file is part of Maarch Framework.
+*
+*   Maarch Framework is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   Maarch Framework is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*    along with Maarch Framework.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/**
+* Module : Thesaurus
+*
+* This module is used to store ressources with any keywords
+* V: 1.0
+*
+* @file
+* @author Alex ORLUC
+* @date $date$
+* @version $Revision$
+*/
+
+/*********************** THESAURUS ***********************************/
+if (!defined("_THESAURUS"))
+    define("_THESAURUS", "Thesaurus (keywords)");
+
+if (!defined("_ADMIN_THESAURUS"))
+    define("_ADMIN_THESAURUS", "Thesaurus (keywords)");
+
+if (!defined("_ADMIN_THESAURUS_DESC"))
+    define("_ADMIN_THESAURUS_DESC", "The thesaurus is a linguistic tool which allows to connect users' natural language and the language contained in the resources.");
+
+if (!defined("_ADDED_TO_RES"))
+    define("_ADDED_TO_RES", "added to the document");
+
+if (!defined("_RESET_THESAURUS"))
+    define("_RESET_THESAURUS", "Reset of the thesaurus list linked to the document");
+
+if (!defined("_THESAURUS_NAME"))
+    define("_THESAURUS_NAME", "Term");
+
+if (!defined("_THESAURUS_PARENT_ID"))
+    define("_THESAURUS_PARENT_ID", "Common term");
+
+if (!defined("_THESAURUS_NAME_ASSOCIATE"))
+    define("_THESAURUS_NAME_ASSOCIATE", "Associated term(s)");
+
+if (!defined("_THESAURUS_LIST"))
+    define("_THESAURUS_LIST", "Terms list");
+
+if (!defined("_ALL_THESAURUS"))
+    define("_ALL_THESAURUS", "All the terms");
+
+if (!defined("_MANAGE_THESAURUS"))
+    define("_MANAGE_THESAURUS", "Manage the thesaurus");
+
+if (!defined("_THESAURUS_LIST_SPECIFIC"))
+    define("_THESAURUS_LIST_SPECIFIC", "Specific term(s))");
+
+if (!defined("_MODIFY_THESAURUS"))
+    define("_MODIFY_THESAURUS", "Modify the term");
+
+if (!defined("_THESAURUS_NAME_IS_EMPTY"))
+    define("_THESAURUS_NAME_IS_EMPTY", "Term name is empty");
+
+if (!defined("_THESAURUS_ADDED"))
+    define("_THESAURUS_ADDED", "Added term");
+
+if (!defined("_THESAURUS_UPDATED"))
+    define("_THESAURUS_UPDATED", "Updated term");
+
+if (!defined("_THESAURUS_DELETED"))
+    define("_THESAURUS_DELETED", "Deleted term");
+
+if (!defined("_ADD_THESAURUS"))
+    define("_ADD_THESAURUS", "Add a term");
+
+if (!defined("_CHOOSE_THESAURUS"))
+    define("_CHOOSE_THESAURUS", "*Choose a term");
+
+if (!defined("_DISPLAYED"))
+    define("_DISPLAYED", " Displayed");
+
+if (!defined("_THESAURUS_COMMENT"))
+    define("_THESAURUS_COMMENT", "Thesaurus management");
+
+if (!defined("_THESAURUS_VIEW"))
+    define("_THESAURUS_VIEW", "Display the thesaurus in the process pages");
+
+if (!defined("_THESAURUS_VIEW_DESC"))
+    define("_THESAURUS_VIEW_DESC", "Be able to see the term(s) which are associated to different documents");
+
+if (!defined("_ADD_THESAURUS_TO_RES"))
+    define("_ADD_THESAURUS_TO_RES", "Be able to associate terms to different documents");
+
+if (!defined("_ADD_THESAURUS_TO_RES_DESC"))
+    define("_ADD_THESAURUS_TO_RES_DESC", "Allow to use thesaurus 's available terms to refer documents");
+
+if (!defined("_USED_FOR"))
+    define("_USED_FOR", "Used for");
