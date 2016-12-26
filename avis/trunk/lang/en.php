0697e74ceabe08198f8e7cf05259566586e3b82d
Henri Queneau <henri.queneau@maarch.org>
2016-12-26 09:11:11 +0000

    FEAT #4999 add en.php

--- avis/trunk/lang/en.php
+++ avis/trunk/lang/en.php
@@ -1,10 +1,73 @@
 <?php
-// CIRCUIT AVIS
-if (!defined("_AVIS_WORKFLOW"))
-    define("_AVIS_WORKFLOW", "Recommendation workflow");
+if (!defined("_AVIS"))
+    define("_AVIS", "recommendation");
 
 if (!defined("_AVIS_WORKFLOW"))
-    define("_AVIS_WORKFLOW", "Recommendation workflow management");
+    define("_AVIS_WORKFLOW", "recommendation workflow");
+
+if (!defined("_AVIS_WORKFLOW_COMMENT"))
+    define("_AVIS_WORKFLOW_COMMENT", "recommendation workflow management");
 
 if (!defined("_SEND_DOCS_TO_RECOMMENDATION"))
-    define("_SEND_DOCS_TO_RECOMMENDATION", "Send to Recommendation");
+    define("_SEND_DOCS_TO_RECOMMENDATION", "Sent a recommendation request (parallel)");
+
+if (!defined("_VALIDATE_RECOMMENDATION"))
+    define("_VALIDATE_RECOMMENDATION", "Validate the recommendation request(parallel)");
+
+if (!defined("_AVIS_USER"))
+    define("_AVIS_USER", "For recommendation");
+
+if (!defined("_AVIS_USER_COPY"))
+    define("_AVIS_USER_COPY", "On copy (recommendation)");
+
+if (!defined("_AVIS_USER_INFO"))
+    define("_AVIS_USER_INFO", "For information (recommendation)");
+
+if (!defined("_RECOMMENDATION_USER"))
+    define("_RECOMMENDATION_USER", "Person for recommendation");
+
+if (!defined("_RECOMMENDATION_USER"))
+    define("_RECOMMENDATION_USER", "Users for recommendation");
+
+if (!defined("_RECOMMENDATION_NOTE"))
+    define("_RECOMMENDATION_NOTE", "Recommendation pattern");
+
+if (!defined("_RECOMMENDATION_LIMIT_DATE"))
+    define("_RECOMMENDATION_LIMIT_DATE", "Recommendation deadline");
+
+if (!defined("_TO_AVIS"))
+    define("_TO_AVIS", "For recommendation");
+
+if (!defined("_AVIS_ANSWERS"))
+    define("_AVIS_ANSWERS", "Give oneself's opinion");
+
+if (!defined("_AVIS_ANSWERS_DESC"))
+    define("_AVIS_ANSWERS_DESC", "Allows to this group to appear in the counsellor's list (recommendation workflow)");
+
+if (!defined("_CONFIG_AVIS_WORKFLOW_IN_DETAIL"))
+    define("_CONFIG_AVIS_WORKFLOW_IN_DETAIL", "Recommendation workflow setting from the detailed form");
+
+if (!defined("_CONFIG_AVIS_WORKFLOW_IN_DETAIL_DESC"))
+    define("_CONFIG_AVIS_WORKFLOW_IN_DETAIL_DESC", "Allows to define the workflow directly from the mail detailed form");
+
+if (!defined("_LAST_AVIS"))
+    define("_LAST_AVIS", "decision-maker counsellor");
+
+if (!defined("_SEND_TO_AVIS_WF"))
+    define("_SEND_TO_AVIS_WF", "Request for a recommendation (sequential)");
+
+if (!defined("_WAITING_FOR_AVIS"))
+    define("_WAITING_FOR_AVIS", "Recommendation on hold");
+
+if (!defined("_PROCEED_WORKFLOW_AVIS"))
+    define("_PROCEED_WORKFLOW_AVIS", "Issue an opinion (sequential)");
+
+if (!defined("_PROCEED_WORKFLOW_AVIS_SIMPLE"))
+    define("_PROCEED_WORKFLOW_AVIS_SIMPLE", "Issue an opinion (parallel)");
+
+if (!defined("_EMPTY_AVIS_WORKFLOW"))
+    define("_EMPTY_AVIS_WORKFLOW", "The recommendation workflow is empty");
+
+if (!defined("_AVIS_SENT"))
+    define("_AVIS_SENT", "Notice given");
+
