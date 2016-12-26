a7844da1fbbc591a5a4347efe41b86e491786cc3
Henri Queneau <henri.queneau@maarch.org>
2016-12-26 09:25:02 +0000

    FEAT #5010 add en.php

--- reports/trunk/lang/en.php
+++ reports/trunk/lang/en.php
@@ -20,58 +20,64 @@
  */
 
 /*************************** Report management *****************/
-if (!defined('_ADMIN_REPORTS'))
-    define('_ADMIN_REPORTS','Reports');
-if (!defined('_ADMIN_REPORTS_DESC'))
-    define('_ADMIN_REPORTS_DESC','Manage reports');
-if (!defined('_REPORTS'))
-    define('_REPORTS','Reports');
-if (!defined('_REPORTS_COMMENT'))
-    define('_REPORTS_COMMENT','Reports');
-if (!defined('_OTHER_REPORTS'))
-    define('_OTHER_REPORTS','Other reports');
-if (!defined('_REPORTS_DESC'))
-    define('_REPORTS_DESC','Edit reports');
-if (!defined('_HAVE_TO_SELECT_GROUP'))
-    define('_HAVE_TO_SELECT_GROUP', 'You must choose a group');
-if (!defined('_AVAILABLE_REPORTS'))
-    define('_AVAILABLE_REPORTS', 'Report available for');
-if (!defined('_GROUP_REPORTS_ADDED'))
-    define('_GROUP_REPORTS_ADDED', 'Reports for the group');
-if (!defined('_CLICK_LINE_BELOW_TO_SEE_REPORT'))
-    define('_CLICK_LINE_BELOW_TO_SEE_REPORT', 'Click on a line to see the report');
-if (!defined('_CLICK_LINE_BELOW_TO_RETURN_TO_REPORTS'))
-    define('_CLICK_LINE_BELOW_TO_RETURN_TO_REPORTS', 'Back to the list of reports');
-if (!defined('_NO_REPORTS_FOR_USER'))
-    define('_NO_REPORTS_FOR_USER', 'No report available for this user');
-if (!defined('_PRINT_NO_RESULT'))
-    define('_PRINT_NO_RESULT', 'Nothing to display');
+if (!defined("_ADMIN_REPORTS"))
+    define("_ADMIN_REPORTS","States and editions");
+if (!defined("_ADMIN_REPORTS_DESC"))
+    define("_ADMIN_REPORTS_DESC","States and editions administration");
+if (!defined("_REPORTS"))
+    define("_REPORTS","States and editions");
+if (!defined("_REPORTS_COMMENT"))
+    define("_REPORTS_COMMENT","States and editions");
+if (!defined("_OTHER_REPORTS"))
+    define("_OTHER_REPORTS","Others editions");
+if (!defined("_REPORTS_DESC"))
+    define("_REPORTS_DESC","States editions");
+if (!defined("_HAVE_TO_SELECT_GROUP"))
+    define("_HAVE_TO_SELECT_GROUP", "You have to select a group");
+if (!defined("_AVAILABLE_REPORTS"))
+    define("_AVAILABLE_REPORTS", "Available states to");
+if (!defined("_GROUP_REPORTS_ADDED"))
+    define("_GROUP_REPORTS_ADDED", "Affected states to the group");
+if (!defined("_CLICK_LINE_BELOW_TO_SEE_REPORT"))
+    define("_CLICK_LINE_BELOW_TO_SEE_REPORT", "Please click on one of the ligns below to see the related editions");
+if (!defined("_CLICK_LINE_BELOW_TO_RETURN_TO_REPORTS"))
+    define("_CLICK_LINE_BELOW_TO_RETURN_TO_REPORTS", "Click here to return to the available editions list");
+if (!defined("_NO_REPORTS_FOR_USER"))
+    define("_NO_REPORTS_FOR_USER", "No edition is available via the menu for this user");
+if (!defined("_PRINT_NO_RESULT"))
+    define("_PRINT_NO_RESULT", "Nothing to show");
 
 //Global
-if (!defined('_DOCUMENTS_LIST'))
-    define('_DOCUMENTS_LIST','Document list');
-if (!defined('_DOCUMENTS_LIST_DESC'))
-    define('_DOCUMENTS_LIST_DESC','Documents list');
+if (!defined("_DOCUMENTS_LIST"))
+    define("_DOCUMENTS_LIST","Documents list");
+if (!defined("_DOCUMENTS_LIST_DESC"))
+    define("_DOCUMENTS_LIST_DESC","Documents list");
 
 /*************************** Module label in report *****************/
-if (!defined('_TITLE_STATS_DU'))
-    define('_TITLE_STATS_DU','from');
-if (!defined('_TITLE_STATS_AU'))
-    define('_TITLE_STATS_AU','to');
-if (!defined('_NB_TOTAL_DOC'))
-    define('_NB_TOTAL_DOC', 'Total number of documents');
-if (!defined('_NB_TOTAL_FOLDER'))
-    define('_NB_TOTAL_FOLDER', 'Total number of files');
-if (!defined('_NO_DATA_MESSAGE'))
-    define('_NO_DATA_MESSAGE', 'Not enough data to generate the report');
-if (!defined('_REPORT'))
-    define('_REPORT', 'Report');
-if (!defined('_ERROR_REPORT_TYPE'))
-    define('_ERROR_REPORT_TYPE', 'The type of report is not correct');
-if (!defined('_ERROR_PERIOD_TYPE'))
-    define('_ERROR_PERIOD_TYPE', 'The type of period is not correct');
-
+if (!defined("_TITLE_STATS_DU"))
+    define("_TITLE_STATS_DU"," on / from ");
+if (!defined("_TITLE_STATS_AU"))
+    define("_TITLE_STATS_AU"," to ");
+if (!defined("_NB_TOTAL_DOC"))
+    define("_NB_TOTAL_DOC", "Sum of present items");
+if (!defined("_NB_TOTAL_FOLDER"))
+    define("_NB_TOTAL_FOLDER", "Sum of present files");
+if (!defined("_NO_DATA_MESSAGE"))
+    define("_NO_DATA_MESSAGE", "No datas enough to create the graphism or the table");
+if (!defined("_REPORT"))
+    define("_REPORT", "State");
+if (!defined("_ERROR_REPORT_TYPE"))
+    define("_ERROR_REPORT_TYPE", "Error with the kind of state");
+if (!defined("_ERROR_PERIOD_TYPE"))
+    define("_ERROR_PERIOD_TYPE", "Error with the period type");
+if (!defined("_REPORTS_EVO_PROCESS"))
+    define("_REPORTS_EVO_PROCESS", "Evolution of the handling medium time");
+if (!defined("_MONTH"))
+    define("_MONTH", "Month(s)");
+if (!defined("_PROCESS_DELAI_AVG"))
+    define("_PROCESS_DELAI_AVG", "Medium period (days)");
 if (!defined("_PROCESS_DELAY_GENERIC_EVALUATION_REPORT"))
-   define("_PROCESS_DELAY_GENERIC_EVALUATION_REPORT", "Average processing time");
-
+    define("_PROCESS_DELAY_GENERIC_EVALUATION_REPORT", "Handling medium time");
+if (!defined("_PROCESS_DELAY_GENERIC_EVALUATION_REPORT_BY_TYPE"))
+    define("_PROCESS_DELAY_GENERIC_EVALUATION_REPORT_BY_TYPE", "Handling medium time by kind of mail");
 ?>
