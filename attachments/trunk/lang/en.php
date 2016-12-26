277943951718ee51ebef87a955e9f8ec1fe02eb5
Henri Queneau <henri.queneau@maarch.org>
2016-12-26 09:10:14 +0000

    FEAT #4998 add en.php

--- attachments/trunk/lang/en.php
+++ attachments/trunk/lang/en.php
@@ -18,58 +18,57 @@
  *   You should have received a copy of the GNU General Public License
  *   along with Maarch Framework.  If not, see <http://www.gnu.org/licenses/>.
  */
+if (!defined("_TITLE"))
+    define("_TITLE", "Title");
+if (!defined("_ATTACHMENTS"))
+    define("_ATTACHMENTS", "Attachments");
+if (!defined("_ATTACHMENTS_COMMENT"))
+    define("_ATTACHMENTS_COMMENT", "Attachments management");
+if (!defined("_ATTACH_ANSWER"))
+    define("_ATTACH_ANSWER", "Attach an attachment");
+if (!defined("_ATTACH"))
+    define("_ATTACH", "Attach an attachment");
+if (!defined("_PLEASE_SELECT_FILE"))
+    define("_PLEASE_SELECT_FILE", "Select a file");
+if (!defined("_NEW_ATTACH_ADDED"))
+    define("_NEW_ATTACH_ADDED", "New attachment");
+if (!defined("_ATTACH_DELETED"))
+    define("_ATTACH_DELETED", "Deleted attachment");
+if (!defined("_ATTACHED_DOC"))
+    define("_ATTACHED_DOC", "Attachments");
 
-if (!defined('_TITLE'))
-    define('_TITLE', 'Title');
-if (!defined('_ATTACHMENTS'))
-    define('_ATTACHMENTS', 'Attachments');
-if (!defined('_ATTACHMENTS_COMMENT'))
-    define('_ATTACHMENTS_COMMENT', 'Manage attachments');
-if (!defined('_ATTACH_ANSWER'))
-    define('_ATTACH_ANSWER', 'Add an attachment');
-if (!defined('_ATTACH'))
-    define('_ATTACH', 'Add an attachment');
-if (!defined('_PLEASE_SELECT_FILE'))
-    define('_PLEASE_SELECT_FILE', 'Select a file');
-if (!defined('_NEW_ATTACH_ADDED'))
-    define('_NEW_ATTACH_ADDED', 'New attachment');
-if (!defined('_ATTACH_DELETED'))
-    define('_ATTACH_DELETED', 'Attachment deleted');
-if (!defined('_ATTACHED_DOC'))
-    define('_ATTACHED_DOC', 'Attachments');
+if (!defined("_INDEX_ATTACHMENT"))
+    define("_INDEX_ATTACHMENT", "Index an attachment");
+if (!defined("_INDEX_ATTACHMENT_DESC"))
+    define("_INDEX_ATTACHMENT_DESC", "Index an attachment");
+if (!defined("_ATTACH_TO_DOC"))
+    define("_ATTACH_TO_DOC", "Attach as a response to an existing mail");
+if (!defined("_ATTACH_TO_DOC_NUM"))
+    define("_ATTACH_TO_DOC_NUM", "Attached to the document number");
+if (!defined("_VIEW_ATTACHMENTS"))
+    define("_VIEW_ATTACHMENTS", "View attachments");
+if (!defined("_VIEW_ATTACHMENTS_DESC"))
+    define("_VIEW_ATTACHMENTS_DESC", "View attachments");
+if (!defined("_MANAGE_ATTACHMENTS"))
+    define("_MANAGE_ATTACHMENTS", "Manage the attachments");
+if (!defined("_GENERATE_OFFICE_ATTACHMENT"))
+    define("_GENERATE_OFFICE_ATTACHMENT", "Generate OFFICE");
+if (!defined("_GENERATE_HTML_ATTACHMENT"))
+    define("_GENERATE_HTML_ATTACHMENT", "Generate HTML");
+if (!defined("_ATTACH_FROM_HDD"))
+    define("_ATTACH_FROM_HDD", "Add an attachment / response from your hard drive");
+if (!defined("_GENERATE_ATTACHMENT_FROM"))
+    define("_GENERATE_ATTACHMENT_FROM", "Generate an attachment / response from a format model");
 
-if (!defined('_INDEX_ATTACHMENT'))
-    define('_INDEX_ATTACHMENT', 'Add an attachment');
-if (!defined('_INDEX_ATTACHMENT_DESC'))
-    define('_INDEX_ATTACHMENT_DESC', 'Add an attachment');
-if (!defined('_ATTACH_TO_DOC'))
-    define('_ATTACH_TO_DOC', 'Add as answer to an existing document');
-if (!defined('_ATTACH_TO_DOC_NUM'))
-    define('_ATTACH_TO_DOC_NUM', 'Attach to document number');
-if (!defined('_VIEW_ATTACHMENTS'))
-    define('_VIEW_ATTACHMENTS', 'The attachments');
-if (!defined('_VIEW_ATTACHMENTS_DESC'))
-    define('_VIEW_ATTACHMENTS_DESC', 'View attachments');
-if (!defined('_MANAGE_ATTACHMENTS'))
-    define('_MANAGE_ATTACHMENTS', 'Manage attachments');
-if (!defined('_GENERATE_OFFICE_ATTACHMENT'))
-    define('_GENERATE_OFFICE_ATTACHMENT', 'Generate Office Attachment');
-if (!defined('_GENERATE_HTML_ATTACHMENT'))
-    define('_GENERATE_HTML_ATTACHMENT', 'Generate HTML Attachment');
-if (!defined('_ATTACH_FROM_HDD'))
-    define('_ATTACH_FROM_HDD', 'Add an Attachment from your Hard Disk');
-if (!defined('_GENERATE_ATTACHMENT_FROM'))
-    define('_GENERATE_ATTACHMENT_FROM', 'Generate Attachment from a model with format');
-
-if (!defined('_EDIT_ATTACHMENTS_FROM_DETAILS'))
-    define('_EDIT_ATTACHMENTS_FROM_DETAILS', 'Manage attachments from details page');
+if (!defined("_EDIT_ATTACHMENTS_FROM_DETAILS"))
+    define("_EDIT_ATTACHMENTS_FROM_DETAILS", "Manage attachments from detailed card");
 
 if (!defined("_CREATED"))
-    define( "_CREATED", "Created");
+    define( "_CREATED", "Created on");
 if (!defined("_BBY"))
     define( "_BBY", "By");
 if (!defined("_EXP_DATE"))
-    define( "_EXP_DATE", "Date of departure");
+    define( "_EXP_DATE", "Start date");
 if (!defined("_CREATE_PJ"))
     define( "_CREATE_PJ", "Create an attachment");
 if (!defined("_ATTACH_FILE"))
@@ -77,62 +76,68 @@ if (!defined("_ATTACH_FILE"))
 if (!defined("_MODEL"))
     define( "_MODEL", "Model");
 if (!defined("_CHOOSE_MODEL"))
-    define( "_CHOOSE_MODEL", "Choose a model");
+    define( "_CHOOSE_MODEL", "Select the model");
 if (!defined("_BACK_DATE"))
     define( "_BACK_DATE", "Return date");
 if (!defined("_ATTACHMENT_TYPES"))
-    define( "_ATTACHMENT_TYPES", "Attachment types");
+    define( "_ATTACHMENT_TYPES", "Attachment type");
 if (!defined("_RESPONSE_PROJECT"))
     define( "_RESPONSE_PROJECT", "Response project");
 if (!defined("_SIGNED_RESPONSE"))
-    define( "_SIGNED_RESPONSE", "Signed response");
+	define( "_SIGNED_RESPONSE", "Signed response");
 if (!defined("_ROUTING"))
-    define( "_ROUTING", "Record traffic");
+    define( "_ROUTING", "Flow slip");
 if (!defined("_OUTGOING_MAIL_SIGNED"))
-    define( "_OUTGOING_MAIL_SIGNED", "Mail signed departure");
+    define( "_OUTGOING_MAIL_SIGNED", "Signed start date");
 if (!defined("_CONVERTED_PDF"))
-    define( "_CONVERTED_PDF", "PDF converted by the application");
+    define( "_CONVERTED_PDF", "Converted PDF by the solution");
 if (!defined("_PRINT_FOLDER"))
     define( "_PRINT_FOLDER", "Printed folder");
 if (!defined("_OUTGOING_MAIL"))
-    define( "_OUTGOING_MAIL", "Spontaneous outgoing mail");
+    define( "_OUTGOING_MAIL", "Spontaneous start mail");
 if (!defined("_A_PJ"))
     define( "_A_PJ", "Attachment");
+if (!defined("_ENVELOPE"))
+    define( "_ENVELOPE", "Envelope");
 if (!defined("_CHOOSE_ATTACHMENT_TYPE"))
     define( "_CHOOSE_ATTACHMENT_TYPE", "Choose an attachment type");
 if (!defined("_UPDATED_DATE"))
-    define( "_UPDATED_DATE", "Updated");
+    define( "_UPDATED_DATE", "Updated on");
 if (!defined("_FILE_MISSING"))
-    define( "_FILE_MISSING", "File is missing");
+    define( "_FILE_MISSING", "The file is missing");
 if (!defined("_FILE_EMPTY"))
-    define( "_FILE_EMPTY", "File is empty");
+    define( "_FILE_EMPTY", "THe file is empty");
 if (!defined("_EDIT_MODEL"))
     define( "_EDIT_MODEL", "Edit");
 if (!defined("_ALREADY_MODEL_SELECTED"))
-    define( "_ALREADY_MODEL_SELECTED", "Model already choosed");
+    define( "_ALREADY_MODEL_SELECTED", "Model is already selected");
 if (!defined("_MODIFY_ANSWER"))
-    define( "_MODIFY_ANSWER", "Edit an attachment");
+    define( "_MODIFY_ANSWER", "Modify an attachment");
 if (!defined("_CREATE_NEW_ATTACHMENT_VERSION"))
     define( "_CREATE_NEW_ATTACHMENT_VERSION", "Create a new version ?");
 if (!defined("_ATTACH_UPDATED"))
-    define( "_ATTACH_UPDATED", "Attachment updated");
+    define( "_ATTACH_UPDATED", "Updated attachment");
 if (!defined("_FINAL_VERSION"))
     define( "_FINAL_VERSION", "Final version");
 if (!defined("_SHOW_PREVIOUS_VERSION"))
-    define( "_SHOW_PREVIOUS_VERSION", "See previous version");
+    define( "_SHOW_PREVIOUS_VERSION", "See previous versions");
 if (!defined("_CREATE_ATTACHMENTS_FROM_DETAILS"))
-    define( "_CREATE_ATTACHMENTS_FROM_DETAILS", "Create an attachment from detail");
+    define( "_CREATE_ATTACHMENTS_FROM_DETAILS", "Create attachments from details card");
 if (!defined("_DELETE_ATTACHMENTS"))
-    define( "_DELETE_ATTACHMENTS", "Delete attachment");
+    define( "_DELETE_ATTACHMENTS", "Delete attachments");
 if (!defined("_MODIFY_ATTACHMENTS"))
-    define( "_MODIFY_ATTACHMENTS", "Edit attachments");
+    define( "_MODIFY_ATTACHMENTS", "Modify attachments");
 if (!defined("_ALL_ATTACHMENT_TYPES"))
     define( "_ALL_ATTACHMENT_TYPES", "All attachments");
 if (!defined("_ATTACHEMENTS"))
     define( "_ATTACHEMENTS", "Attachments");
 if (!defined("_WAYBILL"))
-    define( "_WAYBILL", "Waybill");
+    define( "_WAYBILL", "Mailing slip");
+
 if (!defined("_VIEW_MAIN_DOC"))
-    define( "_VIEW_MAIN_DOC", "View main mail");
+    define( "_VIEW_MAIN_DOC", "See main mail");
 if (!defined("_RETRIEVE_BACK_UP"))
-    define( "_RETRIEVE_BACK_UP", "Retrieve of the last document being processed");
+    define( "_RETRIEVE_BACK_UP", "The last document was recovered because it was not correctly enclosed");
+
+if (!defined("_DEST_USER_PJ"))
+    define( "_DEST_USER_PJ", "Recipient");
