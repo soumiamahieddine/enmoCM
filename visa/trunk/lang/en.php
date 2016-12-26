40dfd01329feaaa1d3039784bb8f05809629c5db
Henri Queneau <henri.queneau@maarch.org>
2016-12-26 09:30:13 +0000

    FEAT #5016 add en.php

--- visa/trunk/lang/en.php
+++ visa/trunk/lang/en.php
@@ -1,43 +1,85 @@
 <?php
+if (!defined("_SIGN_DOCS"))
+    define("_SIGN_DOCS", "Sign the documents");
+
+if (!defined("_PRINTFOLDER"))
+    define("_PRINTFOLDER", "Print folder");
+
+if (!defined("_PRINT_FOLDER_DOC"))
+    define("_PRINT_FOLDER_DOC", "Print the entire folder");
+
+if (!defined("_NO_PDF_FILE"))
+    define("_NO_PDF_FILE", "No pdf file for this file");
+
 // CIRCUIT DE VISA
-if (!defined('_VISA_WORKFLOW'))
-    define('_VISA_WORKFLOW', 'Visa Workflow');
-if (!defined('_VISA_WORKFLOW_COMMENT'))   
-	define('_VISA_WORKFLOW_COMMENT', 'Management of visa workflow');
-if (!defined('_VIEW_VISA_WORKFLOW'))
-    define('_VIEW_VISA_WORKFLOW', 'Visualization of visa workflow');
-if (!defined('_VIEW_VISA_WORKFLOW_DESC'))
-    define('_VIEW_VISA_WORKFLOW_DESC', 'Allow to visualize the visa workflow in the diffusion list zone and in advancement.');
-if (!defined('_CONFIG_VISA_WORKFLOW'))
-    define('_CONFIG_VISA_WORKFLOW', 'Set visa workflow');
-if (!defined('_CONFIG_VISA_WORKFLOW_DESC'))
-    define('_CONFIG_VISA_WORKFLOW_DESC', 'Allow to set the visa workflow to be taken by mail.');
-
+if (!defined("_VISA_WORKFLOW"))
+    define("_VISA_WORKFLOW", "Visa workflow");
+if (!defined("_INTERRUPT_WORKFLOW"))
+    define("_INTERRUPT_WORKFLOW", "Stop the visa workflow");
+if (!defined("_VISA_WORKFLOW_COMMENT"))   
+	define("_VISA_WORKFLOW_COMMENT", "Visa workflow management");
+if (!defined("_VIEW_VISA_WORKFLOW"))
+    define("_VIEW_VISA_WORKFLOW", "Visa workflow view");
+if (!defined("_VIEW_VISA_WORKFLOW_DESC"))
+    define("_VIEW_VISA_WORKFLOW_DESC", "Allows to view the visa workflow in the parts of diffusion list and in those of progress.");
+if (!defined("_CONFIG_VISA_WORKFLOW"))
+    define("_CONFIG_VISA_WORKFLOW", "Visa workflow setting");
+if (!defined("_CONFIG_VISA_WORKFLOW_DESC"))
+    define("_CONFIG_VISA_WORKFLOW_DESC", "Allows to set up visa flow which the mail will have to follow");
+if (!defined("_EMPTY_USER_LIST"))
+    define("_EMPTY_USER_LIST", "Users list is empty");
 if (!defined("_EMPTY_VISA_WORKFLOW"))
-    define("_EMPTY_VISA_WORKFLOW", "No visa workflow set");
-
-if (!defined('_VISA_ANSWERS'))
-    define('_VISA_ANSWERS', 'target the draft responses');
+    define("_EMPTY_VISA_WORKFLOW", "No set visa flow");
+if (!defined("_VISA_ANSWERS"))
+    define("_VISA_ANSWERS", "Aim the responses projects");
 	
-if (!defined('_VISA_ANSWERS_DESC'))
-    define('_VISA_ANSWERS_DESC', 'Allow to target the draft responses');
+if (!defined("_VISA_ANSWERS_DESC"))
+    define("_VISA_ANSWERS_DESC", "Allows to aim response projects");
 
-// CIRCUIT D'AVIS
-if (!defined('_AVIS_WORKFLOW'))
-    define('_AVIS_WORKFLOW', 'Avis workflow');
-if (!defined('_CONFIG_AVIS_WORKFLOW'))
-    define('_CONFIG_AVIS_WORKFLOW', 'Configuration of avis workflow');
-if (!defined('_CONFIG_AVIS_WORKFLOW_DESC'))
-    define('_CONFIG_AVIS_WORKFLOW_DESC', 'Allow to set the avis workflow of the mail');
-if (!defined('_SEND_TO_SIGNATURE'))
-    define('_SEND_TO_SIGNATURE', 'Send to signature');
+if (!defined("_NO_VISA"))
+    define("_NO_VISA", "No designed person on visa");
+if (!defined("_NO_RESPONSE_PROJECT_VISA"))
+    define("_NO_RESPONSE_PROJECT_VISA", "Please, create one attachment at least and edit it.");
+if (!defined("_NO_CONVERTED_PDF_VISA"))
+    define("_NO_CONVERTED_PDF_VISA", "Please edit your attachment(s)");
+if (!defined("_PLEASE_CONVERT_PDF_VISA"))
+    define("_PLEASE_CONVERT_PDF_VISA", "Please edit the following attachments : ");
 
-if (!defined('_SUBMIT_COMMENT'))
-    define('_SUBMIT_COMMENT', 'Submit visa comment (optional) ');
+// CIRCUIT D"AVIS
+if (!defined("_AVIS_WORKFLOW"))
+    define("_AVIS_WORKFLOW", "Recommendation flow");
+if (!defined("_CONFIG_AVIS_WORKFLOW"))
+    define("_CONFIG_AVIS_WORKFLOW", "Recommendation flow configuration");
+if (!defined("_CONFIG_AVIS_WORKFLOW_DESC"))
+    define("_CONFIG_AVIS_WORKFLOW_DESC", "Allow to set the mail recommendation flow up");
+
+if (!defined("_THUMBPRINT"))
+    define("_THUMBPRINT", "Digital print");
+
+if (!defined("_DISSMARTCARD_SIGNER_APPLET"))
+    define("_DISSMARTCARD_SIGNER_APPLET", "Electronic signature in progress...");
+
+
+if (!defined("_IMG_SIGN_MISSING"))
+    define("_IMG_SIGN_MISSING", "Missing signature picture");
+
+if (!defined("_THUMBPRINT_MISSING"))
+    define("_THUMBPRINT_MISSING", "Missing digital print");
+
+if (!defined("_SEND_TO_SIGNATURE"))
+    define("_SEND_TO_SIGNATURE", "Submit");
+
+if (!defined("_SUBMIT_COMMENT"))
+    define("_SUBMIT_COMMENT", "Visa comments (optional) ");
+
+if (!defined("_NO_FILE_PRINT"))
+    define("_NO_FILE_PRINT", "None of the file to print");
+
+if (!defined("_BAD_PIN"))
+    define("_BAD_PIN", "Wrong PIN code. Warning : Attention, 3 essais maximum !");
 
 if (!defined("_PRINT_DOCUMENT"))
-    define("_PRINT_DOCUMENT", "View and print the document");
-
+    define("_PRINT_DOCUMENT", " Show and print the document");
 
 if (!defined("_VISA_BY"))
     define("_VISA_BY", "Visa by");
@@ -46,58 +88,43 @@ if (!defined("_INSTEAD_OF"))
     define("_INSTEAD_OF", "Instead of");
 
 if (!defined("_CONFIG_VISA_WORKFLOW_IN_DETAIL"))
-    define("_CONFIG_VISA_WORKFLOW_IN_DETAIL", "Set visa workflow from detail");
+    define("_CONFIG_VISA_WORKFLOW_IN_DETAIL", "Configuration du circuit de visa depuis la fiche détaillée");
 
 if (!defined("_CONFIG_VISA_WORKFLOW_DESC"))
-    define("_CONFIG_VISA_WORKFLOW_DESC", "Allow to set the visa workflow from detail");
+    define("_CONFIG_VISA_WORKFLOW_DESC", "Allows to set the visa flow up from the detailed card");
 
 if (!defined("_WAITING_FOR_SIGN"))
-    define("_WAITING_FOR_SIGN", "Waiting for signature");
+    define("_WAITING_FOR_SIGN", "Signature on hold");
 
 if (!defined("_SIGNED"))
     define("_SIGNED", "Signed");
 
 if (!defined("_WAITING_FOR_VISA"))
-    define("_WAITING_FOR_VISA", "Waiting for visa");
+    define("_WAITING_FOR_VISA", "Visa on hold");
 
 if (!defined("_VISED"))
-    define("_VISED", "Checked");
+    define("_VISED", "Aimed ?");
 
 if (!defined("DOWN_USER_WORKFLOW"))
-    define("DOWN_USER_WORKFLOW", "Move down user in the workflow");
+    define("DOWN_USER_WORKFLOW", "move down the user");
 
 if (!defined("UP_USER_WORKFLOW"))
-    define("UP_USER_WORKFLOW", "Move up user in the workflow");
+    define("UP_USER_WORKFLOW", "Move up the user");
 
 if (!defined("ADD_USER_WORKFLOW"))
     define("ADD_USER_WORKFLOW", "Add an user in the workflow");
 
 if (!defined("DEL_USER_WORKFLOW"))
-    define("DEL_USER_WORKFLOW", "Delete user from the workflow");
-
-if (!defined("_NO_VISA"))
-    define("_NO_VISA", "Aucune personne désignée en visa");
-
-if (!defined("_NO_RESPONSE_PROJECT_VISA"))
-    define("_NO_RESPONSE_PROJECT_VISA", "Please edit project response");
+    define("DEL_USER_WORKFLOW", "Remove the user from the flow");
 
 if (!defined("_NO_NEXT_STEP_VISA"))
-    define("_NO_NEXT_STEP_VISA", "C'ant do this action. The workflow doesn't have others steps.");
+    define("_NO_NEXT_STEP_VISA", "Impossible to do this action. The flow doesn't contain more steps.");
 
-if (!defined("_THUMBPRINT"))
-    define("_THUMBPRINT", "Thumprint");
+if (!defined("_VISA_USERS"))
+    define("_VISA_USERS", "Person(s) for visa / signature");
 
-if (!defined("_SIGN_DOCS"))
-    define("_SIGN_DOCS", "Sign documents");
+if (!defined("_TMP_SIGNED_FILE_FAILED"))
+    define("_TMP_SIGNED_FILE_FAILED", "Failure on document generation with signature");
 
-if (!defined("_PRINT_FOLDER_DOC"))
-    define("_PRINT_FOLDER_DOC", "Print folder doc");
-
-if (!defined("_REDIRECTION_VISA_SIGN"))
-    define("_REDIRECTION_VISA_SIGN", "Redirection for signature");
-
-if (!defined("_VISA_MAIL")) 
-    define("_VISA_MAIL", "To stamp a mail");
-
-if (!defined("_PRINTFOLDER"))
-     define("_PRINTFOLDER", "Print folder");
+if (!defined("_ENCRYPTED"))
+    define("_ENCRYPTED", "Encoded");
