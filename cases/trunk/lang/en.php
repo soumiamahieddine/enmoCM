5f0114f7466d14b5913e03fb49d020b29505f810
Henri Queneau <henri.queneau@maarch.org>
2016-12-26 09:14:11 +0000

    FEAT #5001 add en.php

--- cases/trunk/lang/en.php
+++ cases/trunk/lang/en.php
@@ -20,112 +20,113 @@
  */
 
 /*********************** CASES ***********************************/
-if (!defined('_CASES_COMMENT'))
-    define('_CASES_COMMENT', 'Cases');
-if (!defined('_CASE_INFO'))
-    define('_CASE_INFO', 'Information on case');
-if (!defined('_JOIN_RES_CASE'))
-    define('_JOIN_RES_CASE','Attach document to case');
-if (!defined('_CASE_NUM'))
-    define('_CASE_NUM', 'Case id');
-if (!defined('_NUM_CASE'))
-    define('_NUM_CASE', 'Case id');
-if (!defined('_CASE_DETAIL'))
-    define('_CASE_DETAIL', 'Case details');
-if (!defined('_DETAILS_CASES'))
-    define('_DETAILS_CASES', 'Case details');
-if (!defined('_INDEX_CASES'))
-    define('_INDEX_CASES', 'Case qualifiers');
-if (!defined('_DOCUMENTS_LIST_IN_THIS_CASE'))
-    define('_DOCUMENTS_LIST_IN_THIS_CASE', 'List of documents in case');
-if (!defined('_NOTES_FOR_THIS_CASES'))
-    define('_NOTES_FOR_THIS_CASES', 'Notes on this case');
-if (!defined('_HISTORY_CASES'))
-    define('_HISTORY_CASES', 'Case history');
-if (!defined('_CASE_CLOSED'))
-    define('_CASE_CLOSED', 'Case closed');
-if (!defined('_CASE_NUMBER'))
-    define('_CASE_NUMBER', 'Case id');
-if (!defined('_CASE_NUMBER_HELP'))
-    define('_CASE_NUMBER_HELP', 'Search on multiple documents case...');
-if (!defined('_N_CASE'))
-    define('_N_CASE', 'Case id');
-if (!defined('_CANT_SHOW_THIS_CASE'))
-    define('_CANT_SHOW_THIS_CASE', 'Cannot display this case');
-if (!defined('_CASE_ID'))
-    define('_CASE_ID','Case id');
-if (!defined('_CASE_LABEL'))
-    define('_CASE_LABEL','Case label');
-if (!defined('_CASE_DESCRIPTION'))
-    define('_CASE_DESCRIPTION','Case description');
-if (!defined('_CASE_TYPIST'))
-    define('_CASE_TYPIST','Case typist');
-if (!defined('_CASE_CREATION_DATE'))
-    define('_CASE_CREATION_DATE','Case creation date');
-if (!defined('_CASE_LAST_UPDATE_DATE'))
-    define('_CASE_LAST_UPDATE_DATE','Case last update');
-if (!defined('_CASE_CLOSING_DATE'))
-    define('_CASE_CLOSING_DATE','Case closing date');
-if (!defined('_CASES_INDEXES'))
-    define('_CASES_INDEXES','Case qualifiers');
-if (!defined('_RESSOURCES_REPORTS'))
-    define('_RESSOURCES_REPORTS','Case content');
-if (!defined('_UPDATE_CASE'))
-    define('_UPDATE_CASE','Case updated');
-if (!defined('_NEW_CASE'))
-    define('_NEW_CASE','New case');
-if (!defined('_RES_ATTACH_ON_CASE'))
-    define('_RES_ATTACH_ON_CASE','Documents attached to case');
-if (!defined('_RES_DETTACH_ON_CASE'))
-    define('_RES_DETTACH_ON_CASE','Documents detached to case');
-if (!defined('_JOIN_CASE'))
-    define('_JOIN_CASE','Attach a case');
-if (!defined('_MODIFY_CASE'))
-    define('_MODIFY_CASE','Attach another case');
-if (!defined('_JOIN_RES_ON_THIS_CASE'))
-    define('_JOIN_RES_ON_THIS_CASE','Attach a document to this case');
-if (!defined('_RESSOURCES_LINKED'))
-    define('_RESSOURCES_LINKED','Document attached');
-if (!defined('_RESSOURCES_NOT_LINKED'))
-    define('_RESSOURCES_NOT_LINKED','Document is already attached');
-if (!defined('_CASE'))
-    define('_CASE','Case');
-if (!defined('_SEARCH_A_CASE'))
-    define('_SEARCH_A_CASE','Search a case');
-if (!defined('_CLOSED_CASE'))
-    define('_CLOSED_CASE','Closed');
-if (!defined('_SEARCH_A_RES'))
-    define('_SEARCH_A_RES','Search a document');
-if (!defined('_CREATE_NEW_CASE'))
-    define('_CREATE_NEW_CASE','Create a new case');
-if (!defined('_ADD_CASES'))
-    define('_ADD_CASES','Create cases');
-if (!defined('_CASE_DESCRIPTION_HELP'))
-    define('_CASE_DESCRIPTION_HELP','type keywords for the case...');
-if (!defined('_CASE_LABEL_HELP'))
-    define('_CASE_LABEL_HELP','Search on case description...');
-if (!defined('_FOUND_CASE'))
-    define('_FOUND_CASE','case(s) found');
-if (!defined('_CREATE_CASE'))
-    define('_CREATE_CASE','Create new case');
-if (!defined('_LINKED_TO_CASE'))
-    define('_LINKED_TO_CASE','Attach to case');
-if (!defined('_CLOSE_THIS_CASE'))
-    define('_CLOSE_THIS_CASE','Close this case');
-if (!defined('_THIS_CASE_IS_CLOSED'))
-    define('_THIS_CASE_IS_CLOSED','This case is closed');
-if (!defined('_CASE_CREATED'))
-    define('_CASE_CREATED','Case created');
-if (!defined('_JOIN_RES_CASE_IN_PROCESS'))
-    define('_JOIN_RES_CASE_IN_PROCESS','Attach documents to cases (processing basket)');
-if (!defined('_CLOSE_CASE'))
-    define('_CLOSE_CASE','Close case(s)');
-if (!defined('_LINK_TO_CASE'))
-    define('_LINK_TO_CASE','Attach to case');
-if (!defined('_ACCESS_LIST_CASE'))
-    define('_ACCESS_LIST_CASE','Sort by case');
-if (!defined('_CLICK_HERE_TO_ACCESS_CASE'))
-    define('_CLICK_HERE_TO_ACCESS_CASE','View all information for this case');
-if (!defined('_UNLINK_CASE'))
-    define('_UNLINK_CASE','Unlink document to case');
-?>
+if (!defined("_CASES_COMMENT"))
+    define("_CASES_COMMENT", "Cases");
+if (!defined("_CASE_INFO"))
+    define("_CASE_INFO", "Case information");
+if (!defined("_JOIN_RES_CASE"))
+    define("_JOIN_RES_CASE","Attach mails to cases");
+if (!defined("_CASE_NUM"))
+    define("_CASE_NUM", "Case number");
+if (!defined("_NUM_CASE"))
+    define("_NUM_CASE", "Case number");
+if (!defined("_CASE_DETAIL"))
+    define("_CASE_DETAIL", "Detailed card on the case");
+if (!defined("_DETAILS_CASES"))
+    define("_DETAILS_CASES", "Case detailed card");
+if (!defined("_INDEX_CASES"))
+    define("_INDEX_CASES", "Case index");
+if (!defined("_DOCUMENTS_LIST_IN_THIS_CASE"))
+    define("_DOCUMENTS_LIST_IN_THIS_CASE", "Case documents list");
+if (!defined("_NOTES_FOR_THIS_CASES"))
+    define("_NOTES_FOR_THIS_CASES", "Case notes");
+if (!defined("_HISTORY_CASES"))
+    define("_HISTORY_CASES", "Case history");
+if (!defined("_CASE_CLOSED"))
+    define("_CASE_CLOSED", "Enclosed case");
+if (!defined("_CASE_NUMBER"))
+    define("_CASE_NUMBER", "Case number");
+if (!defined("_CASE_NUMBER_HELP"))
+    define("_CASE_NUMBER_HELP", "Search on a case including many mails...");
+if (!defined("_N_CASE"))
+    define("_N_CASE", "Case number");
+if (!defined("_CANT_SHOW_THIS_CASE"))
+    define("_CANT_SHOW_THIS_CASE", "Impossible to display this case");
+if (!defined("_CASE_ID"))
+    define("_CASE_ID","Case ID");
+if (!defined("_CASE_LABEL"))
+    define("_CASE_LABEL","Case wording");
+if (!defined("_CASE_DESCRIPTION"))
+    define("_CASE_DESCRIPTION","Case description");
+if (!defined("_CASE_TYPIST"))
+    define("_CASE_TYPIST","Case author");
+if (!defined("_CASE_CREATION_DATE"))
+    define("_CASE_CREATION_DATE","Creation date");
+if (!defined("_CASE_LAST_UPDATE_DATE"))
+    define("_CASE_LAST_UPDATE_DATE","Last update");
+if (!defined("_CASE_CLOSING_DATE"))
+    define("_CASE_CLOSING_DATE","Enclose date");
+if (!defined("_CASES_INDEXES"))
+    define("_CASES_INDEXES","Case index");
+if (!defined("_RESSOURCES_REPORTS"))
+    define("_RESSOURCES_REPORTS","Case content");
+if (!defined("_UPDATE_CASE"))
+    define("_UPDATE_CASE","Cases updates");
+if (!defined("_NEW_CASE"))
+    define("_NEW_CASE","New case");
+if (!defined("_RES_ATTACH_ON_CASE"))
+    define("_RES_ATTACH_ON_CASE","Document attached to the case");
+if (!defined("_RES_DETTACH_ON_CASE"))
+    define("_RES_DETTACH_ON_CASE","Detached document from the case");
+if (!defined("_JOIN_CASE"))
+    define("_JOIN_CASE","Attach to a case");
+if (!defined("_MODIFY_CASE"))
+    define("_MODIFY_CASE","Attach to an other case");
+if (!defined("_JOIN_RES_ON_THIS_CASE"))
+    define("_JOIN_RES_ON_THIS_CASE","Attach a document to the case");
+if (!defined("_RESSOURCES_LINKED"))
+    define("_RESSOURCES_LINKED","Attached document");
+if (!defined("_RESSOURCES_NOT_LINKED"))
+    define("_RESSOURCES_NOT_LINKED","This document is already attached");
+if (!defined("_CASE"))
+    define("_CASE","Case");
+if (!defined("_SEARCH_A_CASE"))
+    define("_SEARCH_A_CASE","Search a case");
+if (!defined("_CLOSED_CASE"))
+    define("_CLOSED_CASE","Enclosed");
+if (!defined("_SEARCH_A_RES"))
+    define("_SEARCH_A_RES","Search a document");
+if (!defined("_CREATE_NEW_CASE"))
+    define("_CREATE_NEW_CASE","Create a new case");
+if (!defined("_ADD_CASES"))
+    define("_ADD_CASES","Create cases");
+if (!defined("_CASE_DESCRIPTION_HELP"))
+    define("_CASE_DESCRIPTION_HELP","Please enter a name or a name group from the case description...");
+if (!defined("_CASE_LABEL_HELP"))
+    define("_CASE_LABEL_HELP","Please search on the case wording...");
+if (!defined("_FOUND_CASE"))
+    define("_FOUND_CASE"," Found case(s)");
+if (!defined("_CREATE_CASE"))
+    define("_CREATE_CASE","Create the case");
+if (!defined("_LINKED_TO_CASE"))
+    define("_LINKED_TO_CASE","Attached to the case");
+if (!defined("_CLOSE_THIS_CASE"))
+    define("_CLOSE_THIS_CASE","Enclose this case");
+if (!defined("_THIS_CASE_IS_CLOSED"))
+    define("_THIS_CASE_IS_CLOSED","Enclosed case");
+if (!defined("_CASE_CREATED"))
+    define("_CASE_CREATED","Created case");
+if (!defined("_JOIN_RES_CASE_IN_PROCESS"))
+    define("_JOIN_RES_CASE_IN_PROCESS","Link mails to cases (handling basket)");
+if (!defined("_CLOSE_CASE"))
+    define("_CLOSE_CASE","Enclose cases");
+if (!defined("_LINK_TO_CASE"))
+    define("_LINK_TO_CASE","Attach to the case");
+if (!defined("_ACCESS_LIST_CASE"))
+    define("_ACCESS_LIST_CASE","Group by case");
+if (!defined("_CLICK_HERE_TO_ACCESS_CASE"))
+    define("_CLICK_HERE_TO_ACCESS_CASE","See the case detailed card");
+if (!defined("_LABEL_MANDATORY"))
+    define("_LABEL_MANDATORY","Case wording is mandatory");
+if (!defined("_UNLINK_CASE"))
+    define("_UNLINK_CASE","Separate the document from the case");
