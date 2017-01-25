4a280be2797914ca96fbf3c8bf348fc378cd4ea1
Laurent Séguin <laurent.seguin@maarch.org>
2017-01-25 19:42:44 +0100

    Revert "modification des script bash de SVN vers GIT"
    This reverts commit b0f9baa7b79e2c10c9343d78ff82f53ae3e8b1c9

--- gitupdate.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-
-repository="gitlab-public@gitlab.evolix.org:maarch/MaarchCourrier.git"
-
-localFolder="./"
-
-git clone $repository" "$localFolder
---
+++ svnupdateall.sh
@@ -0,0 +1,4 @@
+svn up .
+svn up apps/maarch_entreprise/.
+svn up core/.
+svn up modules/*
