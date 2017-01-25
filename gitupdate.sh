b0f9baa7b79e2c10c9343d78ff82f53ae3e8b1c9
Laurent Séguin <laurent.seguin@maarch.org>
2017-01-25 19:36:35 +0100

    modification des script bash de SVN vers GIT

---
+++ gitupdate.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+
+repository="gitlab-public@gitlab.evolix.org:maarch/MaarchCourrier.git"
+
+localFolder="./"
+
+git clone $repository" "$localFolder
--- svnupdateall.sh
+++
@@ -1,4 +0,0 @@
-svn up .
-svn up apps/maarch_entreprise/.
-svn up core/.
-svn up modules/*
