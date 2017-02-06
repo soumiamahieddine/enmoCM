ec7480f8f5c96aa340615edeb20cbe8966a27493
damien.burel <damien.burel@maarch.org>
2017-02-06 17:59:03 +0100

    Refactor

--- apps/maarch_entreprise/js/functions.js
+++ apps/maarch_entreprise/js/functions.js
@@ -89,6 +89,7 @@ function addNewRowPriority(buttonRow) {
 function delNewRowPriority(buttonRow) {
     var index = buttonRow.rowIndex;
     var indexDiff = index - $("priorityAddField").rowIndex;
+    
     if (indexDiff <= 1) {
         $("priorityAddField").style.display = "none";
         $("minusButton").style.display = "none";
