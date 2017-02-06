416a58216999713bb6854f43518e3f809a04c091
damien <damien@Latitude-E5430>
2017-02-06 17:51:20 +0100

    Refactor

--- apps/maarch_entreprise/js/functions.js
+++ apps/maarch_entreprise/js/functions.js
@@ -73,6 +73,7 @@ function deleteSignature(mailSignaturesJS)
 function addNewRowPriority(buttonRow) {
     var index = buttonRow.rowIndex;
     var indexDiff = index - $("priorityAddField").rowIndex;
+
     if ($("priorityAddField").style.display == "none") {
         $("priorityAddField").style.display = "";
         $("minusButton").style.display = "";
