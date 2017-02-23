5c1ee6f1b0bd00e79cf18666dfc5b7b2aa06a71d
Laurent Giovannoni <laurent.giovannoni@maarch.org>
2017-02-23 11:40:53 +0100

    mv samples for status

---
+++ core/Models/StatusModel.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+* Copyright Maarch since 2008 under licence GPLv3.
+* See LICENCE.txt file at the root folder for more details.
+* This file is part of Maarch software.
+*
+*/
+
+require_once 'core/Models/StatusModelAbstract.php';
+
+class StatusModel extends StatusModelAbstract
+{
+    // Do your stuff in this class
+}
---
+++ core/Models/StatusModelAbstract.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+* Copyright Maarch since 2008 under licence GPLv3.
+* See LICENCE.txt file at the root folder for more details.
+* This file is part of Maarch software.
+*
+*/
+
+require_once 'apps/maarch_entreprise/services/Table.php';
+
+class StatusModelAbstract extends Apps_Table_Service
+{
+
+    public static function getList()
+    {
+        $aReturn = static::select([
+            'select'    => empty($aArgs['select']) ? ['*'] : $aArgs['select'],
+            'table'     => ['status'],
+        ]);
+
+        return $aReturn;
+    }
+
+    public static function getById(array $aArgs = [])
+    {
+        static::checkRequired($aArgs, ['id']);
+        static::checkString($aArgs, ['id']);
+
+        $aReturn = static::select([
+            'select'    => empty($aArgs['select']) ? ['*'] : $aArgs['select'],
+            'table'     => ['status'],
+            'where'     => ['id = ?'],
+            'data'      => [$aArgs['id']]
+        ]);
+
+        return $aReturn;
+    }
+
+    public static function create(array $aArgs = [])
+    {
+        static::checkRequired($aArgs, ['id']);
+        static::checkString($aArgs, ['id']);
+
+        $aReturn = static::insertInto($aArgs, 'status');
+
+        return $aReturn;
+    }
+
+    public static function update(array $aArgs = [])
+    {
+        static::checkRequired($aArgs, ['id']);
+        static::checkString($aArgs, ['id']);
+
+        $where['id'] = $aArgs['id'];
+
+        $aReturn = static::updateTable(
+            $aArgs, 
+            'status',
+            $where
+        );
+
+        return $aReturn;
+    }
+
+    public static function delete(array $aArgs = [])
+    {
+        static::checkRequired($aArgs, ['id']);
+        static::checkString($aArgs, ['id']);
+
+        $aReturn = static::deleteFrom([
+                'table' => 'status',
+                'where' => ['id = ?'],
+                'data'  => [$aArgs['id']]
+            ]);
+
+        return $aReturn;
+    }
+}
