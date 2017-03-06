12c46d7cf6911867d0d3a2d88bcb4e8cc95bbe95
Laurent Giovannoni <laurent.giovannoni@maarch.org>
2017-03-06 11:09:07 +0100

    feat add attachments in progress

--- core/Test/ResControllerTest.php
+++ core/Test/ResControllerTest.php
@@ -118,4 +118,76 @@ class ResControllerTest extends PHPUnit_Framework_TestCase
         
         $this->assertGreaterThanOrEqual(0, $response);
     }
+
+    public function testStoreExtResource()
+    {
+        $action = new \Core\Controllers\ResController();
+
+        $path = $_SESSION['config']['tmppath'] . '/test/';
+
+        if (!is_dir($path)) {
+            mkdir($path);    
+        }
+
+        $fileSource = 'test_source.txt';
+
+        $fp = fopen($path . $fileSource, 'a');
+        fwrite($fp, 'a unit test');
+        fclose($fp);
+
+        $fileContent = file_get_contents($path . $fileSource, FILE_BINARY);
+        $encodedFile = base64_encode($fileContent);
+        
+        $data = [];
+
+        array_push(
+            $data,
+            array(
+                'column' => 'subject',
+                'value' => 'UNIT TEST',
+                'type' => 'string',
+            )
+        );
+
+        array_push(
+            $data,
+            array(
+                'column' => 'type_id',
+                'value' => 110,
+                'type' => 'integer',
+            )
+        );
+
+        array_push(
+            $data,
+            array(
+                'column' => 'custom_t1',
+                'value' => 'TEST',
+                'type' => 'string',
+            )
+        );
+
+        array_push(
+            $data,
+            array(
+                'column' => 'custom_t10',
+                'value' => 'lgi@maarch.org',
+                'type' => 'string',
+            )
+        );
+
+        $aArgs = [
+            'encodedFile'   => $encodedFile,
+            'data'          => $data,
+            'collId'        => 'letterbox_coll',
+            'table'         => 'res_letterbox',
+            'fileFormat'    => 'txt',
+            'status'        => 'new',
+        ];
+
+        //TODO
+        //$response = $action->storeExtResource($aArgs);
+        
+        $this->assertGreaterThanOrEqual(0, $response);
+    }
 }
