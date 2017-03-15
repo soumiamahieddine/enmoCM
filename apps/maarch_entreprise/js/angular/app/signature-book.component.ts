5e6b1ad486dc5821e34574fef6df072c737a89ad
Damien Burel <damien.burel@maarch.org>
2017-03-15 10:35:20 +0100

    [FEAT] [PARA V2] Stay on signature Book after an action

--- apps/maarch_entreprise/js/angular/app/signature-book.component.ts
+++ apps/maarch_entreprise/js/angular/app/signature-book.component.ts
@@ -33,7 +33,8 @@ export class SignatureBookComponent implements OnInit {
         consigne                : "",
         documents               : [],
         attachments             : [],
-        histories               : []
+        //histories               : [],
+        resList                 : []
     };
 
     rightSelectedThumbnail      : number    = 0;
@@ -55,11 +56,20 @@ export class SignatureBookComponent implements OnInit {
     histViewerLink              : string    = "";
 
 
-
     constructor(public http: Http, private route: ActivatedRoute, private router: Router, private zone:NgZone) {
         window['angularSignatureBookComponent'] = {
-            componentAfterAttach: (value: string) => this.processAfterAttach(value)
+            componentAfterAttach: (value: string) => this.processAfterAttach(value),
+            componentAfterAction: () => this.processAfterAction()
         };
+    }
+
+    prepareSignatureBook() {
+        $j('#inner_content').remove();
+        $j('#header').remove();
+        $j('#viewBasketsTitle').remove();
+        $j('#homePageWelcomeTitle').remove();
+        $j('#footer').remove();
+        $j('#container').width("98%");
     }
 
     ngOnInit(): void {
@@ -104,13 +114,33 @@ export class SignatureBookComponent implements OnInit {
         });
     }
 
-    prepareSignatureBook() {
-        $j('#inner_content').remove();
-        $j('#header').remove();
-        $j('#viewBasketsTitle').remove();
-        $j('#homePageWelcomeTitle').remove();
-        $j('#footer').remove();
-        $j('#container').width("98%");
+    ngOnDestroy() : void {
+        delete window['angularSignatureBookComponent'];
+    }
+
+    processAfterAttach(mode: string) {
+        this.zone.run(() => this.refreshAttachments(mode));
+    }
+
+    processAfterAction() {
+        var idToGo = -1;
+        var c = this.signatureBook.resList.length;
+
+        for (let i = 0; i < c; i++) {
+            if (this.signatureBook.resList[i].res_id == this.resId) {
+                if (this.signatureBook.resList[i + 1]) {
+                    idToGo = this.signatureBook.resList[i + 1].res_id;
+                } else if (i > 0) {
+                    idToGo = this.signatureBook.resList[i - 1].res_id;
+                }
+            }
+        }
+
+        if (idToGo >= 0) {
+            this.zone.run(() => this.changeLocation(idToGo));
+        } else {
+            this.zone.run(() => this.backToBasket());
+        }
     }
 
     changeSignatureBookLeftContent(id: number) {
@@ -156,6 +186,51 @@ export class SignatureBookComponent implements OnInit {
                 this.rightContentWidth = "39%";
                 this.leftContentWidth = "39%";
             }
+        }
+    }
+
+    refreshAttachments(mode: string) {
+        this.http.get(this.coreUrl + 'rest/' + 'signatureBook/' + this.resId + '/attachments')
+            .map(res => res.json())
+            .subscribe((data) => {
+                this.signatureBook.attachments = data;
+                if (mode == "add") {
+                    this.changeRightViewer(this.signatureBook.attachments.length - 1);
+                } else if (mode == "del") {
+                    this.changeRightViewer(0);
+                }
+            });
+    }
+
+    addAttachmentIframe() {
+        showAttachmentsForm('index.php?display=true&module=attachments&page=attachments_content&docId=' + this.resId);
+    }
+
+    editAttachmentIframe(attachment: any) {
+        var resId: number;
+        if (attachment.res_id == 0) {
+            resId = attachment.res_id_version;
+        } else if (attachment.res_id_version == 0) {
+            resId = attachment.res_id;
+        }
+
+        modifyAttachmentsForm('index.php?display=true&module=attachments&page=attachments_content&id=' + resId + '&relation=' + attachment.relation + '&docId=' + this.resId, '98%', 'auto');
+    }
+
+    delAttachment(attachment: any) {
+        let r = confirm('Voulez-vous vraiment supprimer la pièce jointe ?');
+        if (r) {
+            var resId: number;
+            if (attachment.res_id == 0) {
+                resId = attachment.res_id_version;
+            } else if (attachment.res_id_version == 0) {
+                resId = attachment.res_id;
+            }
+
+            this.http.get('index.php?display=true&module=attachments&page=del_attachment&id=' + resId + '&relation=' + attachment.relation + '&rest=true')
+                .subscribe(() => {
+                    this.refreshAttachments('del');
+                });
         }
     }
 
@@ -244,55 +319,6 @@ export class SignatureBookComponent implements OnInit {
                 false,
                 [$j("#signatureBookActions option:selected")[0].value]
             );
-        }
-    }
-
-    refreshAttachments(mode: string) {
-        this.http.get(this.coreUrl + 'rest/' + 'signatureBook/' + this.resId + '/attachments')
-            .map(res => res.json())
-            .subscribe((data) => {
-                this.signatureBook.attachments = data;
-                if (mode == "add") {
-                    this.changeRightViewer(this.signatureBook.attachments.length - 1);
-                } else if (mode == "del") {
-                    this.changeRightViewer(0);
-                }
-            });
-    }
-
-    processAfterAttach(mode: string) {
-        this.zone.run(() => this.refreshAttachments(mode));
-    }
-
-    addAttachmentIframe() {
-        showAttachmentsForm('index.php?display=true&module=attachments&page=attachments_content&docId=' + this.resId);
-    }
-
-    editAttachmentIframe(attachment: any) {
-        var resId: number;
-        if (attachment.res_id == 0) {
-            resId = attachment.res_id_version;
-        } else if (attachment.res_id_version == 0) {
-            resId = attachment.res_id;
-        }
-
-        modifyAttachmentsForm('index.php?display=true&module=attachments&page=attachments_content&id=' + resId + '&relation=' + attachment.relation + '&docId=' + this.resId, '98%', 'auto');
-    }
-
-    delAttachment(attachment: any) {
-        let r = confirm('Voulez-vous vraiment supprimer la pièce jointe ?');
-        if (r) {
-            var resId: number;
-            if (attachment.res_id == 0) {
-                resId = attachment.res_id_version;
-            } else if (attachment.res_id_version == 0) {
-                resId = attachment.res_id;
-            }
-
-            this.http.get('index.php?display=true&module=attachments&page=del_attachment&id=' + resId + '&relation=' + attachment.relation + '&rest=true')
-                .subscribe(() => {
-                    this.refreshAttachments('del');
-                });
         }
     }
 }
