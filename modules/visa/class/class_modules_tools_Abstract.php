61acb8163a548a4d1ec95fd1877aafb38110f9d4
Alex ORLUC <alex.orluc@maarch.org>
2017-03-23 19:35:01 +0100

    fix label select visa/avis user + model

--- modules/visa/class/class_modules_tools_Abstract.php
+++ modules/visa/class/class_modules_tools_Abstract.php
@@ -490,7 +490,7 @@ abstract class visa_Abstract extends Database
                         
             //VISA USER LIST
             if($bool_modif ==true){
-                $str .= '<select data-placeholder="Ajouter un viseur / signataire" id="visaUserList" onchange="addVisaUser();">';
+                $str .= '<select data-placeholder="' . _ADD_VISA_ROLE . '" id="visaUserList" onchange="addVisaUser();">';
                 $str .= '<option value="" ></option>';
 
                 $tab_userentities = $this->getEntityVis();
@@ -518,7 +518,7 @@ abstract class visa_Abstract extends Database
                 $diff_list = new diffusion_list();
                 $listModels = $diff_list->select_listmodels($typeList);
 
-                $str .= ' <select data-placeholder="Ajouter des personnes à partir d\'un modèle" name="modelList" id="modelList" onchange="loadVisaModelUsers();">';
+                $str .= ' <select data-placeholder="'._ADD_VISA_MODEL.'" name="modelList" id="modelList" onchange="loadVisaModelUsers();">';
                 $str .= '<option value=""></option>';
                 foreach($listModels as $lm){
                         
