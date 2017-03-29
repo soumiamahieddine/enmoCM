2a7fbfabf89aca6d37edd5f5d7533d22fb5d1a2d
Alexandre Morin <alexandre.morin@maarch.org>
2017-03-29 10:17:18 +0200

    Remove example

--- modules/export_seda/example.php
+++
@@ -1,2 +0,0 @@
-require_once __DIR__.'/DOMTemplateProcessor.php';
-$DOMTemplate = new DOMDocument();$DOMTemplate->load(__DIR__.'/test.xml');$DOMTemplateProcessor = new DOMTemplateProcessor($DOMTemplate);$DOMTemplateProcessor->setSource('bar', 'bazbull');$DOMTemplateProcessor->merge();$xml = $DOMTemplate->saveXML();
