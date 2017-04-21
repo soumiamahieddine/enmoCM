b53c74989563c8553df8addf44cc80609fea27d9
Henri Queneau <henri.queneau@maarch.org>
2017-04-21 10:05:57 +0200

    jquery instead prototype

--- apps/maarch_entreprise/documents_list_copies.php
+++ apps/maarch_entreprise/documents_list_copies.php
@@ -197,7 +197,7 @@ for ($i=0;$i<count($tab);$i++)
             if($tab[$i][$j][$value]=="res_id")
             {
                 $tab[$i][$j]["res_id"]=$tab[$i][$j]['value'];
-                $tab[$i][$j]["label"]=_GED_NUM;
+                $tab[$i][$j]["label"]=_GED_NUMAAAA;
                 $tab[$i][$j]["size"]="4";
                 $tab[$i][$j]["label_align"]="left";
                 $tab[$i][$j]["align"]="left";
@@ -609,9 +609,9 @@ $status = 0;
 $content = $list->showList($tab, $paramsTab, $listKey, $_SESSION['current_basket']);
 // $debug = $list->debug(false);
 
-$content .= "<script>$$('#container')[0].setAttribute('style', 'width: 90%; min-width: 1000px;');".
-                    "$$('#content')[0].setAttribute('style', 'width: auto; min-width: 1000px;');".
-                    "$$('#inner_content')[0].setAttribute('style', 'width: auto; min-width: 1000px;');".
+$content .= "<script>$j('#container').attr('style', 'width: 90%; min-width: 1000px;');".
+                    "$j('#content').attr('style', 'width: auto; min-width: 1000px;');".
+                    "$j('#inner_content').attr('style', 'width: auto; min-width: 1000px;');".
                     // "$$('table#extended_list')[0].setAttribute('style', 'width: 100%; min-width: 900px; margin: 0;');".
             "</script>";
 
