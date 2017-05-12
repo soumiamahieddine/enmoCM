188d6ed5d624761dc8b0c97b24efd1978437ae14
Damien Burel <damien.burel@maarch.org>
2017-05-12 18:27:50 +0200

    FIX #5506 validate too #2

--- modules/attachments/attachments_content.php
+++ modules/attachments/attachments_content.php
@@ -1930,11 +1930,12 @@ $content .= '</div>';
             if (!empty($_REQUEST['docId'])) {
                 if (isset($_REQUEST['id'])) {
                     $content .= ', \'edit\'';
+                    $content .= ');simpleAjax(\'' . $_SESSION['config']['businessappurl'].'index.php?display=true&module=attachments&page=unsetTemporarySaved&mode=edit\')"/>';
                 } else {
                     $content .= ', \'add\'';
+                    $content .= ');simpleAjax(\'' . $_SESSION['config']['businessappurl'].'index.php?display=true&module=attachments&page=unsetTemporarySaved&mode=add\')"/>';
                 }
             }
-            $content .= ');simpleAjax(\'' . $_SESSION['config']['businessappurl'].'index.php?display=true&module=attachments&page=unsetTemporarySaved&mode=add\')"/>';
 
             $content .= '&nbsp;';
             $content .= '&nbsp;';
