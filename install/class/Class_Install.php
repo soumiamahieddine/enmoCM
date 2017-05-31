05f88c476aa7467d882373eaea8eb8265a398d41
Laurent Giovannoni <laurent.giovannoni@maarch.org>
2017-05-31 18:17:05 +0200

    exclude xml on update version

--- install/class/Class_Install.php
+++ install/class/Class_Install.php
@@ -2153,29 +2153,30 @@ class Install extends functions
         $db->query($query, array($sec->getPasswordHash($newPass)));
     }
 
-    public function copy_dir($dir2copy,$dir_paste)
+    function copy_dir($dir2copy, $dir_paste, $excludeExt=false)
     {
         // On vérifie si $dir2copy est un dossier
         if (is_dir($dir2copy))
         {
-
             // Si oui, on l'ouvre
             if ($dh = opendir($dir2copy))
             {
-
                 // On liste les dossiers et fichiers de $dir2copy
                 while (($file = readdir($dh)) !== false)
                 {
                     // Si le dossier dans lequel on veut coller n'existe pas, on le cree
-                    if (!is_dir($dir_paste)) mkdir ($dir_paste, 0777);
-
+                    if (!is_dir($dir_paste)) {
+                        mkdir ($dir_paste, 0777);
+                    }
                     // S'il s'agit d'un dossier, on relance la fonction recursive
-                    if(is_dir($dir2copy.$file) && $file != '..' && $file != '.') $this->copy_dir ( $dir2copy.$file.'/' , $dir_paste.$file.'/' );
-
-                    // S'il sagit d'un fichier, on le copue simplement
-                    elseif($file != '..' && $file != '.') copy ( $dir2copy.$file , $dir_paste.$file );
+                    if (is_dir($dir2copy.$file) && $file != '..' && $file != '.') {
+                        $this->copy_dir($dir2copy.$file.'/' , $dir_paste.$file.'/', $excludeExt);  
+                    } elseif ($file != '..' && $file != '.') {
+                        if (strtolower($excludeExt) <> strtolower(pathinfo($dir2copy . $file, PATHINFO_EXTENSION))) {
+                            copy($dir2copy.$file, $dir_paste.$file);
+                        }
+                    }
                 }
-
                 // On ferme $dir2copy
                 closedir($dh);
             }
