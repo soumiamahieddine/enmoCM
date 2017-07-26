3da0af91f4a9449eaa3d900b66ca875d083a06ac
Alexis Ragot <alexis.ragot@maarch.org>
2017-07-26 16:59:19 +0200

    Update docker_install_php.sh

--- ci/docker_install_php.sh
+++ ci/docker_install_php.sh
@@ -7,7 +7,7 @@ set -xe
 
 apt-get install -y libpq-dev libxml2-dev libxslt1-dev \
 && docker-php-ext-configure pgsql -with-pgsql=/usr/local/pgsql \
-&& docker-php-ext-install pdo_pgsql pgsql xsl zip
+&& docker-php-ext-install pdo_pgsql pgsql xsl zip \
 && pecl install xdebug \
 && docker-php-ext-enable xdebug
 
