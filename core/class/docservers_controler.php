668136b710ed2c568cf26d01c7558cd46b2ed5c7
Nestor <npegane@hotmail.com>
2017-09-14 10:06:41 +0200

    FIX #5923 Ajout de contrôles pour un fichier inexistant dans docserver

--- core/class/docservers_controler.php
+++ core/class/docservers_controler.php
@@ -1539,7 +1539,10 @@ class docservers_controler
                 $docserverTypeObject = $docserverTypeControler->get(
                     $docserverObject->docserver_type_id
                 );
-                if (!file_exists($file)) {
+                
+                //var_dump($file);
+                if (!file_exists($file) || empty($adr[0][$cptDocserver]['path']) || $adr[0][$cptDocserver]['filename']) {
+                    
                     $concatError .= _FILE_NOT_EXISTS_ON_THE_SERVER . ' : '
                                   . $file . '||';
                     $history->add(
