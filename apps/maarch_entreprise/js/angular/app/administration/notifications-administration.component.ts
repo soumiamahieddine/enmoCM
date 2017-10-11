5ca1a0e30c2f4fc5f7f05d047ac6f87dedade36c
Damien Burel <damien.burel@maarch.org>
2017-10-11 14:38:43 +0200

    FEAT #5802 Refactoring

--- apps/maarch_entreprise/js/angular/app/administration/notifications-administration.component.ts
+++ apps/maarch_entreprise/js/angular/app/administration/notifications-administration.component.ts
@@ -4,8 +4,6 @@ import { LANG } from '../translate.component';
 import { NotificationService } from '../notification.service';
 
 declare function $j(selector: any) : any;
-declare function successNotification(message: string) : void;
-declare function errorNotification(message: string) : void;
 
 declare var angularGlobals : any;
 
@@ -17,7 +15,8 @@ declare var angularGlobals : any;
 export class NotificationsAdministrationComponent implements OnInit {
 
     coreUrl                     : string;
-    notifications               : any[] = [];
+
+    notifications               : any[]     = [];
     loading                     : boolean   = false;
     lang                        : any       = LANG;
 
@@ -26,42 +25,36 @@ export class NotificationsAdministrationComponent implements OnInit {
     }
 
     ngOnInit(): void {
+        this.updateBreadcrumb(angularGlobals.applicationName);
+
         this.coreUrl = angularGlobals.coreUrl;
-
-        this.prepareNotifications();
-
         this.loading = true;
 
         this.http.get(this.coreUrl + 'rest/notifications')
             .subscribe((data : any) => {
                 this.notifications = data.notifications;
-                this.updateBreadcrumb(angularGlobals.applicationName);              
                 this.loading = false;
             }, (err) => {
-                errorNotification(JSON.parse(err._body).errors);
+                this.notify.error(err.error.errors);
             });
     }
 
-    prepareNotifications() {
-        $j('#inner_content').remove();
-    }
-
-    updateBreadcrumb(applicationName: string){
+    updateBreadcrumb(applicationName: string) {
         $j('#ariane')[0].innerHTML = "<a href='index.php?reinit=true'>" + applicationName + "</a> > "+
                                             "<a onclick='location.hash = \"/administration\"' style='cursor: pointer'>" + this.lang.administration + "</a> > " + this.lang.admin_notifications;
     }
 
-    deleteNotification(notification : any){
-        var resp = confirm(this.lang.deleteMsg + " ?");
-        if(resp){
-            this.http.delete(this.coreUrl + 'rest/notifications/'+notification.notification_sid)
+    deleteNotification(notification : any) {
+        let r = confirm(this.lang.deleteMsg);
+
+        if (r) {
+            this.http.delete(this.coreUrl + 'rest/notifications/' + notification.notification_sid)
                 .subscribe((data : any) => {
-                    this.notifications = data.notifications
+                    this.notifications = data.notifications;
                     this.notify.success(data.success);
                 }, (err) => {
                     this.notify.error(err.error.errors);
                 });
         }
     }
-
 }
