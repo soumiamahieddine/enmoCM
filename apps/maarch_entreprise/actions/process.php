6c46f0f1077be881f425d6dedc2fe9ee0e356a44
florian.azizian <florian.azizian@maarch.org>
2017-10-16 16:53:07 +0100

    FIX #6100 change some icons

--- apps/maarch_entreprise/actions/process.php
+++ apps/maarch_entreprise/actions/process.php
@@ -293,7 +293,7 @@ function get_form_txt($values, $path_manage_action,  $id_action, $table, $module
                     $frm_str .= '<input type="hidden" name="' . $key . '" id="' . $key
                         . '" value="' . $data[$key]['show_value']
                         . '" readonly="readonly" class="readonly" style="border:none;" />';
-                          
+
                     $frm_str .= '<div onClick="$(\'return_previsualise\').style.display=\'none\';" id="return_previsualise" style="cursor: pointer; display: none; border-radius: 10px; box-shadow: 10px 10px 15px rgba(0, 0, 0, 0.4); padding: 10px; width: auto; height: auto; position: absolute; top: 0; left: 0; z-index: 999; background-color: rgba(255, 255, 255, 0.9); border: 3px solid #459ed1;">';
                     $frm_str .= '<input type="hidden" id="identifierDetailFrame" value="" />';
                     $frm_str .= '</div>';
@@ -355,7 +355,6 @@ function get_form_txt($values, $path_manage_action,  $id_action, $table, $module
                 $frm_str .= 'style="width: 200px; max-width: 150px; border: medium none; color: rgb(102, 102, 102); height: 60px;"';
                 $frm_str .=  'title="'.$indexes[$key]['show_value'].'" alt="'.$indexes[$key]['show_value'].'" >'.$values_fields->{$key};
                 $frm_str .= '</textarea>';
-
                 $frm_str .= '</td >';
             $frm_str .= '</tr>';
         }
@@ -517,7 +516,7 @@ function get_form_txt($values, $path_manage_action,  $id_action, $table, $module
         $frm_str .= '<span onclick="loadTab(\''.$res_id.'\',\''.$coll_id.'\',\''._DOC_HISTORY.'\',\''.$pathScriptTab.'\',\'history\');return false;" '
             . 'onmouseover="this.style.cursor=\'pointer\';" class="categorie" style="width:90%;">';
         $frm_str .= '<span id="history_tab" class="tab_module" style="color:#1C99C5;"><i class="fa fa-plus-square-o"></i></span>'
-            . '&nbsp;<i class="fa fa-line-chart fa-2x" title="'._DOC_HISTORY.'"></i> <sup><span style="display:none;"></span></sup>';
+            . '&nbsp;<i class="fa fa-history fa-2x" title="'._DOC_HISTORY.'"></i> <sup><span style="display:none;"></span></sup>';
         $frm_str .= '</span>';
         $frm_str .= '</td>';
     }
@@ -574,7 +573,7 @@ function get_form_txt($values, $path_manage_action,  $id_action, $table, $module
         $frm_str .= '<span onclick="loadTab(\''.$res_id.'\',\''.$coll_id.'\',\''._DIFF_LIST_COPY.'\',\''.$pathScriptTab.'\',\'difflist\');return false;" '
             . 'onmouseover="this.style.cursor=\'pointer\';" class="categorie" style="width:90%;">';
         $frm_str .= '<span id="difflist_tab" class="tab_module" style="color:#1C99C5;"><i class="fa fa-plus-square-o"></i></span>'
-            . '&nbsp;<i class="fa fa-gear fa-2x" title="'._DIFF_LIST_COPY.'"></i> <sup><span style="display:none;"></span></sup>';
+            . '&nbsp;<i class="fa fa-share-alt fa-2x" title="'._DIFF_LIST_COPY.'"></i> <sup><span style="display:none;"></span></sup>';
         $frm_str .= '</span>';
         $frm_str .= '</td>';
     }
@@ -654,7 +653,7 @@ function get_form_txt($values, $path_manage_action,  $id_action, $table, $module
             $frm_str .= '<span onclick="loadTab(\''.$res_id.'\',\''.$coll_id.'\',\''._VISA_WORKFLOW.'\',\''.$pathScriptTab.'\',\'visa\');return false;" '
                 . 'onmouseover="this.style.cursor=\'pointer\';" class="categorie" style="width:90%;">';
             $frm_str .= '<span id="visa_tab" class="tab_module" style="color:#1C99C5;"><i class="fa fa-plus-square-o"></i></span><b>&nbsp;'
-                . '<i id="visa_tab_img" class="fa fa-certificate fa-2x" title="'._VISA_WORKFLOW.'"></i><span id="visa_tab_badge"></span>';
+                . '<i id="visa_tab_img" class="fa fa-list-ol fa-2x" title="'._VISA_WORKFLOW.'"></i><span id="visa_tab_badge"></span>';
             $frm_str .= '</span>';
             $frm_str .= '</td>';
             
@@ -677,7 +676,7 @@ function get_form_txt($values, $path_manage_action,  $id_action, $table, $module
             $frm_str .= '<span onclick="loadTab(\''.$res_id.'\',\''.$coll_id.'\',\''.urlencode(_AVIS_WORKFLOW).'\',\''.$pathScriptTab.'\',\'avis\');return false;" '
                 . 'onmouseover="this.style.cursor=\'pointer\';" class="categorie" style="width:90%;">';
             $frm_str .= '<span id="avis_tab" class="tab_module" style="color:#1C99C5;"><i class="fa fa-plus-square-o"></i></span><b>&nbsp;'
-                . '<i id="avis_tab_img" class="fa fa-check-square fa-2x" title="'._AVIS_WORKFLOW.'"></i><span id="avis_tab_badge"></span>';
+                . '<i id="avis_tab_img" class="fa fa-commenting fa-2x" title="'._AVIS_WORKFLOW.'"></i><span id="avis_tab_badge"></span>';
             $frm_str .= '</span>';   
             $frm_str .= '</td>';
             
