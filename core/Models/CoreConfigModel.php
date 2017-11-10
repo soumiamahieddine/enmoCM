e49ebdaa3a1f2b6bd6288c7c15380a8a25962809
florian.azizian <florian.azizian@maarch.org>
2017-11-10 14:55:26 +0100

    [FIX] Fix getCustomId for WebService

--- core/Models/CoreConfigModel.php
+++ core/Models/CoreConfigModel.php
@@ -25,7 +25,10 @@ class CoreConfigModel
         }
 
         $explodeUrl = explode('/', $_SERVER['SCRIPT_NAME']);
-        if (strpos($_SERVER['SCRIPT_NAME'], 'apps/maarch_entreprise') === false) {
+
+        if (strpos($_SERVER['SCRIPT_NAME'], 'ws_server') !== false) {
+            $path = $explodeUrl[count($explodeUrl) - 2];
+        } elseif (strpos($_SERVER['SCRIPT_NAME'], 'apps/maarch_entreprise') === false) {
             $path = $explodeUrl[count($explodeUrl) - 3];
         } else {
             $path = $explodeUrl[count($explodeUrl) - 4];
