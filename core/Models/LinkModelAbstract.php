37b21228c64f458d098f8ffb73ecba32d7dc8265
Damien <damien.burel@maarch.org>
2017-12-19 17:43:07 +0100

    FEAT #6757 Links number # 2

---
+++ core/Models/LinkModelAbstract.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Copyright Maarch since 2008 under licence GPLv3.
+ * See LICENCE.txt file at the root folder for more details.
+ * This file is part of Maarch software.
+ *
+ */
+
+/**
+ * @brief Link Model
+ * @author dev@maarch.org
+ * @ingroup core
+ */
+
+namespace Core\Models;
+
+
+class LinkModelAbstract
+{
+    public static function getByResId(array $aArgs)
+    {
+        ValidatorModel::notEmpty($aArgs, ['resId']);
+        ValidatorModel::intVal($aArgs, ['resId']);
+
+        $aLinks = DatabaseModel::select([
+            'select'    => ['*'],
+            'table'     => ['res_linked'],
+            'where'     => ['res_parent = ? OR res_child = ?'],
+            'data'      => [$aArgs['resId'], $aArgs['resId']]
+        ]);
+
+        return $aLinks;
+    }
+}
