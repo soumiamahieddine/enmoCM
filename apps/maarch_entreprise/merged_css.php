c45a70f314eebd21f4536c54eaec68a0ca160ef8
Damien <damien.burel@maarch.org>
2017-12-28 10:42:25 +0100

    FIX #6820 add typeahead.css

--- apps/maarch_entreprise/merged_css.php
+++ apps/maarch_entreprise/merged_css.php
@@ -62,6 +62,7 @@ readfile('apps/maarch_entreprise/css/administration.component.css');
 
 //Dependencies
 readfile('node_modules/tooltipster/dist/css/tooltipster.bundle.min.css');
+readfile('node_modules/jquery-typeahead/dist/jquery.typeahead.min.css');
 readfile('apps/maarch_entreprise/css/chosen.min.css');
 readfile('node_modules/photoswipe/dist/photoswipe.css');
 readfile('node_modules/photoswipe/dist/default-skin/default-skin.css');
