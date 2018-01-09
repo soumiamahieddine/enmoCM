abfef2f691f2e2da97cc2ea17a3b184ccffea47b
florian.azizian <florian.azizian@maarch.org>
2018-01-09 10:26:02 +0100

    FIX #6602 disable nature_id in definition_mail_categories

--- apps/maarch_entreprise/indexing_searching/details.php
+++ apps/maarch_entreprise/indexing_searching/details.php
@@ -947,8 +947,6 @@ if ((!empty($_SESSION['error']) && ! ($_SESSION['indexation'] ))  )
                                         echo 'onchange="change_doctype_details(this.options[this.options.selectedIndex].value, \''.$_SESSION['config']['businessappurl'].'index.php?display=true&dir=indexing_searching&page=change_doctype_details\' , \''._DOCTYPE.' '._MISSING.'\');"';
                                     } else if ($key == 'priority') {
                                         echo 'onchange="updateProcessDate(\'' . $_SESSION['config']['businessappurl'] . 'index.php?display=true&dir=indexing_searching&page=update_process_date\', ' . $s_id . ')"';
-                                    } else if ($key == 'nature_id') {
-                                        echo 'disabled style="background-color : #e6e6e6;"';
                                     }
                                     ?>
                                     >
