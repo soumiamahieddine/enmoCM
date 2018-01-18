8e01a1664df00ccb77e2e3470f2e5608b6fb922f
Laurent Giovannoni <laurent.giovannoni@maarch.org>
2018-01-18 11:21:11 +0100

    #10 prepare xml and shell scripts

--- modules/convert/batch/scripts/launch_convert_attachments.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-Mypath='/var/www/html/maarch_oem/modules/convert/batch'
-cd $Mypath
-ConfigPath='/var/www/html/maarch_oem/modules/convert/batch/config'
-
-php $Mypath/fill_stack.php -c $ConfigPath/config.xml -coll attachments_coll
