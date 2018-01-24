2f3de47c4f8561eaac2cacbd950dc713254096aa
florian.azizian <florian.azizian@maarch.org>
2018-01-24 12:36:01 +0100

    FEAT #25 refactoring status mvc

--- apps/maarch_entreprise/js/angular/lang/lang-fr.ts
+++ apps/maarch_entreprise/js/angular/lang/lang-fr.ts
@@ -93,7 +93,20 @@ export const LANG_FR = {
     "updateStatus"              : "Changement de statut de courrier",
     "statuses"                  : "Statut(s)",
 
-
+    //Administration Status
+    "canBeModified"             : "Modification des index",
+    "canBeSearched"             : "Recherche",
+    "imgRelated"                : "Image associée",
+    "isFolderStatus"            : "Statut de dossier",
+    "statusAdded"               : "Statut ajouté",
+    "statusCreation"            : "Création d'un statut",
+    "statusDeleted"             : "Statut supprimé",
+    "statusModification"        : "Modification du statut",
+    "statusUpdated"             : "Statut mis à jour",
+    "addStatus"                 : "Ajouter un statut",
+    "tooltipSearchStatus"       : "Si <b>coché</b>,<br/>le statut vous sera <b>proposé</b> dans le critère de recherche STATUTS de la recherche avancée",
+    "tooltipIndexStatus"        : "Si <b>coché</b>,<br/>vous pourrez <b>modifier</b> les meta-données des documents ayant ce statut",
+    "tooltipFolderStatus"       : "Si <b>coché</b>,<br/>le statut pourra être utilisé pour des <b>bannettes de dossiers</b>",
 
     //Lists
     available: "disponible",
@@ -163,21 +176,6 @@ export const LANG_FR = {
     parameterUpdated: "Paramètre modifié",
     string:"Chaine de caratère",
     type:"Type",
-
-    //Status
-    canBeModified: "Modification des index",
-    canBeSearched: "Recherche",
-    imgRelated: "Image associée",
-    isFolderStatus: "Statut de dossier",
-    statusAdded: "Statut ajouté",
-    statusCreation: "Création d'un statut",
-    statusDeleted: "Statut supprimé",
-    statusModification: "Modification du statut",
-    statusUpdated: "Statut mis à jour",
-    addStatus: "Ajouter un statut",
-    tooltipSearchStatus: "Si <b>coché</b>,<br/>le statut vous sera <b>proposé</b> dans le critère de recherche STATUTS de la recherche avancée",
-    tooltipIndexStatus: "Si <b>coché</b>,<br/>vous pourrez <b>modifier</b> les meta-données des documents ayant ce statut",
-    tooltipFolderStatus: "Si <b>coché</b>,<br/>le statut pourra être utilisé pour des <b>bannettes de dossiers</b>",
 
     //History
     dataOfMonth: "Données du mois",
