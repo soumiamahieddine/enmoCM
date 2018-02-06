f72ecaa9775a4e927a52265a161570cf2d6745a2
florian.azizian <florian.azizian@maarch.org>
2018-02-06 15:30:06 +0100

    FIX perimetre Qualiville

--- src/app/resource/controllers/ResController.php
+++ src/app/resource/controllers/ResController.php
@@ -271,6 +271,13 @@ class ResController
             }
         }
 
+        $securityClause = $_SESSION['user']['security']['letterbox_coll']['DOC']['where'];
+        if(empty($securityClause)){
+            $securityClause = '1=2';
+        }
+
+        $clause .= ' AND ' . $securityClause;
+
         $result = array();        
         $resList = ResModel::getDocsByClause(
             [
