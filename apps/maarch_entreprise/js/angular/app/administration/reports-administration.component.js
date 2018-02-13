5d43217f45051bd36b766532ed617c82c4c140cc
Alex Orluc <alex.orluc@maarch.org>
2018-02-13 16:43:37 +0100

    FIX #83 add css + material components

--- apps/maarch_entreprise/js/angular/app/administration/reports-administration.component.js
+++ apps/maarch_entreprise/js/angular/app/administration/reports-administration.component.js
@@ -22,6 +22,7 @@ var ReportsAdministrationComponent = /** @class */ (function () {
         this.reports = [];
         this.selectedGroup = "";
         this.loading = false;
+        this.loadingOptions = false;
     }
     ReportsAdministrationComponent.prototype.updateBreadcrumb = function (applicationName) {
         if ($j('#ariane')[0]) {
@@ -36,21 +37,25 @@ var ReportsAdministrationComponent = /** @class */ (function () {
         this.http.get(this.coreUrl + 'rest/reports/groups')
             .subscribe(function (data) {
             _this.groups = data['groups'];
+            _this.loadReports(0);
             _this.loading = false;
         }, function () {
             location.href = "index.php";
         });
     };
-    ReportsAdministrationComponent.prototype.loadReports = function () {
+    ReportsAdministrationComponent.prototype.loadReports = function (index) {
         var _this = this;
-        this.http.get(this.coreUrl + 'rest/reports/groups/' + this.selectedGroup)
+        this.selectedGroup = this.groups[index].group_id;
+        this.loadingOptions = true;
+        this.http.get(this.coreUrl + 'rest/reports/groups/' + this.groups[index].group_id)
             .subscribe(function (data) {
             _this.reports = data['reports'];
+            _this.loadingOptions = false;
         }, function (err) {
             _this.notify.error(err.error.errors);
         });
     };
-    ReportsAdministrationComponent.prototype.onSubmit = function () {
+    ReportsAdministrationComponent.prototype.saveReport = function () {
         var _this = this;
         this.http.put(this.coreUrl + 'rest/reports/groups/' + this.selectedGroup, this.reports)
             .subscribe(function () {
--- apps/maarch_entreprise/js/angular/app/administration/reports-administration.component.ts
+++ apps/maarch_entreprise/js/angular/app/administration/reports-administration.component.ts
@@ -22,6 +22,7 @@ export class ReportsAdministrationComponent implements OnInit {
     selectedGroup   : string    = "";
 
     loading         : boolean   = false;
+    loadingOptions  : boolean   = false;
 
 
     constructor(public http: HttpClient, private notify: NotificationService) {
@@ -42,23 +43,26 @@ export class ReportsAdministrationComponent implements OnInit {
         this.http.get(this.coreUrl + 'rest/reports/groups')
             .subscribe((data: any) => {
                 this.groups = data['groups'];
-
+                this.loadReports(0)
                 this.loading = false;
             }, () => {
                 location.href = "index.php";
             });
     }
 
-    loadReports() {
-        this.http.get(this.coreUrl + 'rest/reports/groups/' + this.selectedGroup)
+    loadReports(index:any) {
+        this.selectedGroup = this.groups[index].group_id;
+        this.loadingOptions = true;
+        this.http.get(this.coreUrl + 'rest/reports/groups/' + this.groups[index].group_id)
             .subscribe((data: any) => {
                 this.reports = data['reports'];
+                this.loadingOptions = false;
             }, (err) => {
                 this.notify.error(err.error.errors);
             });
     }
 
-    onSubmit() {
+    saveReport() {
         this.http.put(this.coreUrl + 'rest/reports/groups/' + this.selectedGroup, this.reports)
             .subscribe(() => {
                 this.notify.success(this.lang.modificationSaved);
