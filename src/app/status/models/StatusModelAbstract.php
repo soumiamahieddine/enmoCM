e40b52a277c08c1ae9c1986dd3f77766a3e22319
florian.azizian <florian.azizian@maarch.org>
2018-02-21 13:59:46 +0100

    FEAT #85 order by label_status

--- src/app/status/models/StatusModelAbstract.php
+++ src/app/status/models/StatusModelAbstract.php
@@ -25,7 +25,7 @@ class StatusModelAbstract
         $aReturn = DatabaseModel::select([
             'select'    => empty($aArgs['select']) ? ['*'] : $aArgs['select'],
             'table'     => ['status'],
-            'order_by'  => ['identifier']
+            'order_by'  => ['label_status']
         ]);
 
         return $aReturn;
