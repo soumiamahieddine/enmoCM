b22edf1768598ebc640c9b7c2162e9aa4667cdb0
florian.azizian <florian.azizian@maarch.org>
2018-03-14 15:54:34 +0100

    FEAT #202 fusion M2M refactoring

--- apps/maarch_entreprise/class/class_business_app_tools_Abstract.php
+++ apps/maarch_entreprise/class/class_business_app_tools_Abstract.php
@@ -130,6 +130,7 @@ abstract class business_app_tools_Abstract extends Database
             $_SESSION['tablename']['contact_types']         = (string) $tablename->contact_types;
             $_SESSION['tablename']['contact_purposes']      = (string) $tablename->contact_purposes;
             $_SESSION['tablename']['contact_addresses']     = (string) $tablename->contact_addresses;
+            $_SESSION['tablename']['contact_communication'] = 'contact_communication';
             $_SESSION['tablename']['tags']                  = (string) $tablename->tags;
             
             $_SESSION['config']['tmppath'] = $_SESSION['config']['corepath'] . 'apps' 
