bec3271e5a3fdda5893ad225a0510b9f60506be5
florian.azizian <florian.azizian@maarch.org>
2018-03-15 17:57:51 +0100

    FIX #211 positif number for delay

--- src/app/doctype/controllers/DoctypeController.php
+++ src/app/doctype/controllers/DoctypeController.php
@@ -352,15 +352,18 @@ class DoctypeController
             $errors[]= 'Invalid doctypes_second_level_id value';
         }
         if (!Validator::notEmpty()->validate($aArgs['process_delay']) ||
-            !Validator::intVal()->validate($aArgs['process_delay'])) {
+            !Validator::intVal()->validate($aArgs['process_delay']) ||
+            $aArgs['process_delay'] < 0) {
             $errors[]= 'Invalid process_delay value';
         }
         if (!Validator::notEmpty()->validate($aArgs['delay1']) ||
-            !Validator::intVal()->validate($aArgs['delay1'])) {
+            !Validator::intVal()->validate($aArgs['delay1']) ||
+            $aArgs['delay1'] < 0) {
             $errors[]= 'Invalid delay1 value';
         }
         if (!Validator::notEmpty()->validate($aArgs['delay2']) ||
-            !Validator::intVal()->validate($aArgs['delay2'])) {
+            !Validator::intVal()->validate($aArgs['delay2']) ||
+            $aArgs['delay2'] < 0) {
             $errors[]= 'Invalid delay2 value';
         }
 
