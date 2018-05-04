b8677e8301438fe54d1a8b249d3c566ceb446c53
Damien <damien.burel@maarch.org>
2018-05-04 10:57:09 +0200

    FEAT #7710 Remove rest session + remove test session + comment convert tests

--- core/Test/NotificationControllerTest.php
+++ core/Test/NotificationControllerTest.php
@@ -106,7 +106,7 @@ class NotificationControllerTest extends TestCase
         $fullRequest = \httpRequestCustom::addContentInBody($aArgs, $request);
         $response = $NotificationController->create($fullRequest, new \Slim\Http\Response());
         $responseBody = json_decode((string) $response->getBody());
-        $this->assertSame('identifiant déjà existant', $responseBody->errors);
+        $this->assertSame('Notification déjà existante', $responseBody->errors);
     }
 
     public function testRead()
