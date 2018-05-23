51d53f3feca946fceac58a0f8d9752940de6b3e5
Damien <damien.burel@maarch.org>
2018-05-23 10:28:43 +0200

    FEAT #7721 Fix update label

--- src/app/contact/controllers/ContactGroupController.php
+++ src/app/contact/controllers/ContactGroupController.php
@@ -105,7 +105,8 @@ class ContactGroupController
         }
 
         $data = $request->getParams();
-        $check = Validator::stringType()->notEmpty()->validate($data['label']);
+        $check = Validator::intVal()->notEmpty()->validate($data['id']);
+        $check = $check && Validator::stringType()->notEmpty()->validate($data['label']);
         $check = $check && Validator::stringType()->notEmpty()->validate($data['description']);
         $check = $check && Validator::boolType()->validate($data['public']);
         if (!$check) {
@@ -113,7 +114,7 @@ class ContactGroupController
         }
 
         $user = UserModel::getByUserId(['select' => ['id'], 'userId' => $GLOBALS['userId']]);
-        $existingGroup = ContactGroupModel::get(['select' => [1], 'where' => ['label = ?', 'owner = ?'], 'data' => [$data['label'], $user['id']]]);
+        $existingGroup = ContactGroupModel::get(['select' => [1], 'where' => ['label = ?', 'owner = ?', 'id != ?'], 'data' => [$data['label'], $user['id'], $data['id']]]);
         if (!empty($existingGroup)) {
             return $response->withStatus(400)->withJson(['errors' => _CONTACTS_GROUP_LABEL_ALREADY_EXISTS]);
         }
