65f3828df901fdebe474a4a74d505a030372c531
florian.azizian <florian.azizian@maarch.org>
2018-07-02 09:40:45 +0000

    FIX delete notification without sh

--- src/app/notification/controllers/NotificationController.php
+++ src/app/notification/controllers/NotificationController.php
@@ -244,7 +244,10 @@ class NotificationController
                 NotificationScheduleModel::saveCrontab($cronTab);
             }
 
-            unlink($pathToFolow.'modules/notifications/batch/scripts/'.$filename);
+            $filePath = $pathToFolow.'modules/notifications/batch/scripts/'.$filename;
+            if (file_exist($filePath)) {
+                unlink($filePath);
+            }
         }
 
         return $response->withJson([
