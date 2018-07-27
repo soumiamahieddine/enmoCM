5f0ba6e0e6f4e00e8be10619e6548281010aa4af
Damien <damien.burel@maarch.org>
2018-07-27 10:54:50 +0200

    [REFACTORING] Work_batch is coming back to attachments

--- sql/develop.sql
+++ sql/develop.sql
@@ -211,8 +211,6 @@ ALTER TABLE res_version_attachments DROP COLUMN IF EXISTS envelop_id;
 ALTER TABLE res_letterbox DROP COLUMN IF EXISTS approver;
 ALTER TABLE res_attachments DROP COLUMN IF EXISTS approver;
 ALTER TABLE res_version_attachments DROP COLUMN IF EXISTS approver;
-ALTER TABLE res_attachments DROP COLUMN IF EXISTS work_batch;
-ALTER TABLE res_version_attachments DROP COLUMN IF EXISTS work_batch;
 ALTER TABLE res_letterbox DROP COLUMN IF EXISTS is_ingoing;
 ALTER TABLE res_attachments DROP COLUMN IF EXISTS is_ingoing;
 ALTER TABLE res_version_attachments DROP COLUMN IF EXISTS is_ingoing;
