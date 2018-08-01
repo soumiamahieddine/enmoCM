b48afe7e0c55b9db7a545376a83f4bc2ff95f896
Damien <damien.burel@maarch.org>
2018-08-01 15:50:04 +0200

    External SB controller

--- modules/visa/class/IxbusController.php
+++ modules/visa/class/IxbusController.php
@@ -18,32 +18,33 @@ class IxbusController
     public static function getInitializeDatas()
     {
         $xmlPostString = '<?xml version="1.0" encoding="utf-8"?>
-        <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
-            <soap:Body>
-                <storeResourceRequest xmlns="http://connecting.website.com/WSDL_Service">
-                    <PRICE>tata</PRICE>
-                </storeResourceRequest>
-            </soap:Body>
-        </soap:Envelope>';
+                            <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+                              <soap:Body>
+                                <getUtilisateur xmlns="http://www.srci.fr">
+                                  <NomUtilisateur>toto</NomUtilisateur>
+                                  <MotdePasse>tata</MotdePasse>
+                                </getUtilisateur>
+                              </soap:Body>
+                            </soap:Envelope>';
 
         $opts = [
-        CURLOPT_URL => '',
+        CURLOPT_URL => 'http://parapheur.orleans.fr/ixbuswebws/Utilisateur.asmx',
         CURLOPT_HTTPHEADER => [
         'content-type:text/xml;charset=\"utf-8\"',
         'accept:text/xml',
         "Cache-Control: no-cache",
         "Pragma: no-cache",
-        "SOAPAction: \"http://tracmedia.org/InTheLife\""
+        "Content-length: ".strlen($xmlPostString),
+        "SOAPAction: \"http://www.srci.fr/getUtilisateur\""
         ],
         CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_USERPWD => 'superadmin:superadmin',
         CURLOPT_POST => true,
         CURLOPT_POSTFIELDS  => $xmlPostString
         ];
 
         $curl = curl_init();
         curl_setopt_array($curl, $opts);
-        $rawResponse[] = curl_exec($curl);
+        $rawResponse = curl_exec($curl);
 
         return $rawResponse;
     }
