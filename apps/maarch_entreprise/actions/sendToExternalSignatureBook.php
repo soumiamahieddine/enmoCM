3509ec06916661f58779d9c39a02a989884ac099
florian.azizian <florian.azizian@maarch.org>
2018-08-02 12:09:25 +0200

    start signatory book

--- apps/maarch_entreprise/actions/sendToExternalSignatureBook.php
+++ apps/maarch_entreprise/actions/sendToExternalSignatureBook.php
@@ -6,6 +6,76 @@ $etapes = ['form'];
 
 function get_form_txt($values, $path_manage_action, $id_action, $table, $module, $coll_id, $mode)
 {
+    $config = getXml();
+
+    $html = '<form name="sendToExternalSB" id="sendToExternalSB" method="post" class="forms" action="#">';
+    $html .= '<input type="hidden" name="chosen_action" id="chosen_action" value="end_action" />';
+    if (!empty($config)) {
+        if ($config['id'] == 'ixbus') {
+            include_once 'modules/visa/class/IxbusController.php';
+
+            $html .= IxbusController::getModal($config);
+        } elseif ($config['id'] == 'iParapheur') {
+            include_once 'modules/visa/class/IParapheurController.php';
+
+            $html .= IParapheurController::getModal($config);
+        } elseif ($config['id'] == 'fastParapheur') {
+            include_once 'modules/visa/class/FastParapheurController.php';
+
+            $html .= FastParapheurController::getModal($config);
+        }
+    }
+
+    $html .='<div align="center">';
+    $html .=' <input type="button" name="validate" id="validate" value="'._VALIDATE.'" class="button" ' .
+            'onclick="valid_action_form(\'sendToExternalSB\', \'' . $path_manage_action .
+            '\', \'' . $id_action . '\', \'value\', \'res_letterbox\', \'null\', \'letterbox_coll\', \'' .
+            $mode . '\');" />';
+    $html .='<input type="button" name="cancel" id="cancel" class="button" value="'._CANCEL.'" onclick="pile_actions.action_pop();destroyModal(\'modal_'.$id_action.'\');"/>';
+
+    $html .='</div>';
+    $html .='</form>';
+
+    return addslashes($html);
+}
+
+function check_form($form_id, $values)
+{
+    return true;
+}
+
+function manage_form($arr_id, $history, $id_action, $label_action, $status, $coll_id, $table, $values_form)
+{
+    $result = '';
+
+    $config = getXml();
+
+    foreach ($arr_id as $value) {
+        if (!empty($config)) {
+            if ($config['id'] == 'ixbus') {
+                include_once 'modules/visa/class/IxbusController.php';
+    
+                $html .= IxbusController::getModal($config);
+            } elseif ($config['id'] == 'iParapheur') {
+                include_once 'modules/visa/class/IParapheurController.php';
+    
+                $html .= IParapheurController::getModal($config);
+            } elseif ($config['id'] == 'fastParapheur') {
+                include_once 'modules/visa/class/FastParapheurController.php';
+    
+                $html .= FastParapheurController::getModal($config);
+            }
+        }
+
+        IxbusController::sendDatas(['config' => $config, 'resIdMaster' => $value]);
+    }
+
+    return ['result' => $result, 'history_msg' => ''];
+}
+
+
+function getXml()
+{
     if (file_exists("custom/{$_SESSION['custom_override_id']}/modules/visa/xml/remoteSignatoryBooks.xml")) {
         $path = "custom/{$_SESSION['custom_override_id']}/modules/visa/xml/remoteSignatoryBooks.xml";
     } else {
@@ -26,43 +96,6 @@ function get_form_txt($values, $path_manage_action, $id_action, $table, $module,
             }
         }
     }
-    $config['getFormData'] = [
-        'pathManageAction'  => $path_manage_action,
-        'actionId'          => $id_action,
-        'mode'              => $mode
-    ];
 
-    $html = '';
-    if (!empty($config)) {
-        if ($config['id'] == 'ixbus') {
-            include_once 'modules/visa/class/IxbusController.php';
-
-            $html = IxbusController::getModal($config);
-        } elseif ($config['id'] == 'iParapheur') {
-            include_once 'modules/visa/class/IParapheurController.php';
-
-            $html = iParapheurController::getModal($config);
-        } elseif ($config['id'] == 'fastParapheur') {
-            include_once 'modules/visa/class/FastParapheurController.php';
-
-            $html = fastParapheurController::getModal($config);
-        }
-    }
-
-    return addslashes($html);
-}
-
-function check_form($form_id,$values)
-{
-    return true;
-}
-
-function manage_form($arr_id, $history, $id_action, $label_action, $status, $coll_id, $table, $values_form)
-{
-    $result = '';
-
-    // TODO SEND DATA
-
-
-    return ['result' => $result, 'history_msg' => ''];
+    return $config;
 }
--- modules/visa/class/IxbusController.php
+++ modules/visa/class/IxbusController.php
@@ -15,22 +15,23 @@
 
 class IxbusController
 {
-    public static function getModal(array $config)
+    public static function getModal($config)
     {
         $initializeDatas = IxbusController::getInitializeDatas($config);
 
-        $html = '<form name="sendToExternalSB" id="sendToExternalSB" method="post" class="forms" action="#">';
-        $html .= '<input type="hidden" name="chosen_action" id="chosen_action" value="end_action" />';
         $html .= '<select id="nature"><option value="val1">Valeur 1</option><option value="val2">Valeur 2</option><option value="val3">Valeur 3</option></select>';
 
-        $html .='<div align="center">';
-        $html .=' <input type="button" name="validate" id="validate" value="Valider" class="button" ' .
-                'onclick="valid_action_form(\'sendToExternalSB\', \'' . $config['getFormData']['pathManageAction'] .
-                '\', \'' . $config['getFormData']['actionId'] . '\', \'value\', \'res_letterbox\', \'null\', \'letterbox_coll\', \'' .
-                $config['getFormData']['mode'] . '\');" />';
-        $html .='<input type="button" name="cancel" id="cancel" class="button" value="annuler"/>';
-        $html .='</div>';
-        $html .='</form>';
+        $initializeDatas['messagesModel'] = ['12' => 'modele courrier', '34' => 'DRH'];
+
+        $html .= '<select name="messageModel" id="messageModel">';
+        $html .= '<option value="">' . _SELECT_MESSAGE_MODEL_IXBUS . '</option>';
+        foreach ($initializeDatas['messagesModel'] as $key => $value) {
+            $html .= '<option value="';
+            $html .= $key;
+            $html .= '">';
+            $html .= $value;
+        }
+        $html .= '</select><br />';
 
         return $html;
     }
@@ -78,6 +79,7 @@ class IxbusController
         $sessionId = IxbusController::createSession($config);
         $rawResponse['natures'] = IxbusController::getNature(['config' => $config, 'sessionId' => $sessionId]);
         $rawResponse['usersList'] = IxbusController::getUsersList(['config' => $config, 'sessionId' => $sessionId]);
+        $rawResponse['messagesModel'] = IxbusController::getMessagesModel(['config' => $config, 'sessionId' => $sessionId]);
 
         return $rawResponse;
     }
@@ -113,7 +115,10 @@ class IxbusController
         curl_setopt_array($curl, $opts);
         $rawResponse = curl_exec($curl);
 
-        return $rawResponse;
+        $data = simplexml_load_string($rawResponse);
+        $response = $data->children('http://schemas.xmlsoap.org/soap/envelope/')->Body->children()->CreateSessionResponse;
+
+        return $response;
     }
 
     public static function getUsersList($aArgs)
@@ -147,6 +152,108 @@ class IxbusController
         curl_setopt_array($curl, $opts);
         $rawResponse = curl_exec($curl);
 
-        return $rawResponse;
+        $data = simplexml_load_string($rawResponse);
+        $response = $data->children('http://schemas.xmlsoap.org/soap/envelope/')->Body->children()->CreateSessionResponse;
+
+        return $response;
+    }
+
+    public static function getMessagesModel($aArgs)
+    {
+        $xmlPostString = '<?xml version="1.0" encoding="utf-8"?>
+        <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+          <soap:Body>
+            <GetMessagesModel xmlns="http://www.srci.fr">
+              <utilisateurID>123</utilisateurID>
+              <organisationID>'.$aArgs['config']['data']['organizationId'].'</organisationID>
+              <serviceID>123</serviceID>
+              <typeMessage>Test</typeMessage>
+            </GetMessagesModel>
+          </soap:Body>
+        </soap:Envelope>';
+
+        $opts = [
+        CURLOPT_URL => 'http://parapheur.orleans.fr/parapheurws/service.asmx',
+        CURLOPT_HTTPHEADER => [
+        'content-type:text/xml;charset=\"utf-8\"',
+        'accept:text/xml',
+        "Cache-Control: no-cache",
+        "Pragma: no-cache",
+        "Content-length: ".strlen($xmlPostString),
+        "Cookie:".$aArgs['sessionId'],
+        "SOAPAction: \"http://www.srci.fr/GetMessagesModel\""
+        ],
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_POST => true,
+        CURLOPT_POSTFIELDS  => $xmlPostString
+        ];
+
+        $curl = curl_init();
+        curl_setopt_array($curl, $opts);
+        $rawResponse = curl_exec($curl);
+
+        $data = simplexml_load_string($rawResponse);
+        $response = $data->children('http://schemas.xmlsoap.org/soap/envelope/')->Body->children()->CreateSessionResponse;
+
+        return $response;
+    }
+
+    public static function sendDatas($aArgs)
+    {
+        $xmlPostString = '<?xml version="1.0" encoding="utf-8"?>
+        <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+          <soap:Body>
+            <Transmettre xmlns="http://www.srci.fr">
+              <NomUtilisateur>'.$config['data']['userId'].'</NomUtilisateur>
+              <MotdePasse>'.$config['data']['password'].'</MotdePasse>
+              <IdentifiantOrganisation>'.$config['data']['organizationId'].'</IdentifiantOrganisation>
+              <attach>
+                <Document>
+                  <Attachment>
+                    <Name>string</Name>
+                    <Content>base64Binary</Content>
+                    <Size>int</Size>
+                  </Attachment>
+                  <Attachment>
+                    <Name>string</Name>
+                    <Content>base64Binary</Content>
+                    <Size>int</Size>
+                  </Attachment>
+                </Document>
+                <Description>xml</Description>
+              </attach>
+              <NomDossier>string</NomDossier>
+              <DateLimite>dateTime</DateLimite>
+              <MessageModele>int</MessageModele>
+              <IdClasseur>int</IdClasseur>
+              <Responsable>int</Responsable>
+            </Transmettre>
+          </soap:Body>
+        </soap:Envelope>';
+
+        $opts = [
+        CURLOPT_URL => 'http://parapheur.orleans.fr/parapheurws/3.21/MessagerieImprimante.asmx',
+        CURLOPT_HTTPHEADER => [
+        'content-type:text/xml;charset=\"utf-8\"',
+        'accept:text/xml',
+        "Cache-Control: no-cache",
+        "Pragma: no-cache",
+        "Content-length: ".strlen($xmlPostString),
+        "Cookie:".$aArgs['sessionId'],
+        "SOAPAction: \"http://www.srci.fr/Transmettre\""
+        ],
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_POST => true,
+        CURLOPT_POSTFIELDS  => $xmlPostString
+        ];
+
+        $curl = curl_init();
+        curl_setopt_array($curl, $opts);
+        $rawResponse = curl_exec($curl);
+
+        $data = simplexml_load_string($rawResponse);
+        $response = $data->children('http://schemas.xmlsoap.org/soap/envelope/')->Body->children()->CreateSessionResponse;
+
+        return $response;
     }
 }
