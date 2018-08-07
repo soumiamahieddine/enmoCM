f2bf53fc4f56c5a67ba5a9a14337edaf4d1c0013
florian.azizian <florian.azizian@maarch.org>
2018-08-07 15:20:00 +0000

    config

--- modules/visa/batch/scripts/retrieveMailFromExternalSignatoryBook.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd /var/www/html/maarch_v2/modules/visa/batch/
-filePath='/var/www/html/maarch_v2/modules/visa/batch/process_mailsFromSignatoryBook.php'
-php $filePath -c /var/www/html/maarch_v2/modules/visa/batch/config/config.xml
