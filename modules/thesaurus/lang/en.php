2b40afe3e2b5c17a9930bf6be71140449933818e
florian.azizian <florian.azizian@maarch.org>
2018-10-12 15:25:26 +0100

    FIX #8643 change font awesome exchange-alt

--- modules/thesaurus/lang/en.php
+++ modules/thesaurus/lang/en.php
@@ -31,80 +31,110 @@
 */
 
 /*********************** THESAURUS ***********************************/
-if (!defined("_THESAURUS"))
+if (!defined("_THESAURUS")) {
     define("_THESAURUS", "Thesaurus (keywords)");
+}
 
-if (!defined("_ADMIN_THESAURUS"))
+if (!defined("_ADMIN_THESAURUS")) {
     define("_ADMIN_THESAURUS", "Thesaurus (keywords)");
+}
 
-if (!defined("_ADMIN_THESAURUS_DESC"))
+if (!defined("_ADMIN_THESAURUS_DESC")) {
     define("_ADMIN_THESAURUS_DESC", "The thesaurus is a linguistic tool which allows to connect users' natural language and the language contained in the resources.");
+}
 
-if (!defined("_ADDED_TO_RES"))
+if (!defined("_ADDED_TO_RES")) {
     define("_ADDED_TO_RES", "added to the document");
+}
 
-if (!defined("_RESET_THESAURUS"))
+if (!defined("_RESET_THESAURUS")) {
     define("_RESET_THESAURUS", "Reset of the thesaurus list linked to the document");
+}
 
-if (!defined("_THESAURUS_NAME"))
+if (!defined("_THESAURUS_NAME")) {
     define("_THESAURUS_NAME", "Term");
+}
 
-if (!defined("_THESAURUS_PARENT_ID"))
+if (!defined("_THESAURUS_PARENT_ID")) {
     define("_THESAURUS_PARENT_ID", "Common term");
+}
 
-if (!defined("_THESAURUS_NAME_ASSOCIATE"))
+if (!defined("_THESAURUS_NAME_ASSOCIATE")) {
     define("_THESAURUS_NAME_ASSOCIATE", "Associated term(s)");
+}
 
-if (!defined("_THESAURUS_LIST"))
+if (!defined("_THESAURUS_LIST")) {
     define("_THESAURUS_LIST", "Terms list");
+}
 
-if (!defined("_ALL_THESAURUS"))
+if (!defined("_ALL_THESAURUS")) {
     define("_ALL_THESAURUS", "All the terms");
+}
 
-if (!defined("_MANAGE_THESAURUS"))
+if (!defined("_MANAGE_THESAURUS")) {
     define("_MANAGE_THESAURUS", "Manage the thesaurus");
+}
 
-if (!defined("_THESAURUS_LIST_SPECIFIC"))
+if (!defined("_THESAURUS_LIST_SPECIFIC")) {
     define("_THESAURUS_LIST_SPECIFIC", "Specific term(s))");
+}
 
-if (!defined("_MODIFY_THESAURUS"))
+if (!defined("_MODIFY_THESAURUS")) {
     define("_MODIFY_THESAURUS", "Modify the term");
+}
 
-if (!defined("_THESAURUS_NAME_IS_EMPTY"))
+if (!defined("_THESAURUS_NAME_IS_EMPTY")) {
     define("_THESAURUS_NAME_IS_EMPTY", "Term name is empty");
+}
 
-if (!defined("_THESAURUS_ADDED"))
+if (!defined("_THESAURUS_ADDED")) {
     define("_THESAURUS_ADDED", "Added term");
+}
 
-if (!defined("_THESAURUS_UPDATED"))
+if (!defined("_THESAURUS_UPDATED")) {
     define("_THESAURUS_UPDATED", "Updated term");
+}
 
-if (!defined("_THESAURUS_DELETED"))
+if (!defined("_THESAURUS_DELETED")) {
     define("_THESAURUS_DELETED", "Deleted term");
+}
 
-if (!defined("_ADD_THESAURUS"))
+if (!defined("_ADD_THESAURUS")) {
     define("_ADD_THESAURUS", "Add a term");
+}
 
-if (!defined("_CHOOSE_THESAURUS"))
+if (!defined("_CHOOSE_THESAURUS")) {
     define("_CHOOSE_THESAURUS", "*Choose a term");
+}
 
-if (!defined("_DISPLAYED"))
+if (!defined("_DISPLAYED")) {
     define("_DISPLAYED", " Displayed");
+}
 
-if (!defined("_THESAURUS_COMMENT"))
+if (!defined("_THESAURUS_COMMENT")) {
     define("_THESAURUS_COMMENT", "Thesaurus management");
+}
 
-if (!defined("_THESAURUS_VIEW"))
+if (!defined("_THESAURUS_VIEW")) {
     define("_THESAURUS_VIEW", "Display the thesaurus in the process pages");
+}
 
-if (!defined("_THESAURUS_VIEW_DESC"))
+if (!defined("_THESAURUS_VIEW_DESC")) {
     define("_THESAURUS_VIEW_DESC", "Be able to see the term(s) which are associated to different documents");
+}
 
-if (!defined("_ADD_THESAURUS_TO_RES"))
+if (!defined("_ADD_THESAURUS_TO_RES")) {
     define("_ADD_THESAURUS_TO_RES", "Be able to associate terms to different documents");
+}
 
-if (!defined("_ADD_THESAURUS_TO_RES_DESC"))
+if (!defined("_ADD_THESAURUS_TO_RES_DESC")) {
     define("_ADD_THESAURUS_TO_RES_DESC", "Allow to use thesaurus 's available terms to refer documents");
+}
 
-if (!defined("_USED_FOR"))
+if (!defined("_USED_FOR")) {
     define("_USED_FOR", "Used for");
+}
+
+if (!defined("_THESAURUSNONE")) {
+    define("_THESAURUSNONE", "No thesaurus");
+}
--- modules/thesaurus/lang/fr.php
+++ modules/thesaurus/lang/fr.php
@@ -31,80 +31,110 @@
 */
 
 /*********************** THESAURUS ***********************************/
-if (!defined("_THESAURUS"))
+if (!defined("_THESAURUS")) {
     define("_THESAURUS", "Thesaurus (Mots-clés)");
+}
 
-if (!defined("_ADMIN_THESAURUS"))
+if (!defined("_ADMIN_THESAURUS")) {
     define("_ADMIN_THESAURUS", "Thesaurus (Mots-clés)");
+}
 
-if (!defined("_ADMIN_THESAURUS_DESC"))
+if (!defined("_ADMIN_THESAURUS_DESC")) {
     define("_ADMIN_THESAURUS_DESC", "Le thésaurus est un outil linguistique qui permet de mettre en relation le langage naturel des utilisateurs et celui contenu dans les ressources.");
+}
 
-if (!defined("_ADDED_TO_RES"))
+if (!defined("_ADDED_TO_RES")) {
     define("_ADDED_TO_RES", "ajouté au document");
+}
 
-if (!defined("_RESET_THESAURUS"))
+if (!defined("_RESET_THESAURUS")) {
     define("_RESET_THESAURUS", "remise à zéro de la liste des thesaurus lié au document");
+}
 
-if (!defined("_THESAURUS_NAME"))
+if (!defined("_THESAURUS_NAME")) {
     define("_THESAURUS_NAME", "Terme");
+}
 
-if (!defined("_THESAURUS_PARENT_ID"))
+if (!defined("_THESAURUS_PARENT_ID")) {
     define("_THESAURUS_PARENT_ID", "Terme Générique");
+}
 
-if (!defined("_THESAURUS_NAME_ASSOCIATE"))
+if (!defined("_THESAURUS_NAME_ASSOCIATE")) {
     define("_THESAURUS_NAME_ASSOCIATE", "Terme(s) associé(s)");
+}
 
-if (!defined("_THESAURUS_LIST"))
+if (!defined("_THESAURUS_LIST")) {
     define("_THESAURUS_LIST", "Liste des termes");
+}
 
-if (!defined("_ALL_THESAURUS"))
+if (!defined("_ALL_THESAURUS")) {
     define("_ALL_THESAURUS", "Tous les termes");
+}
 
-if (!defined("_MANAGE_THESAURUS"))
+if (!defined("_MANAGE_THESAURUS")) {
     define("_MANAGE_THESAURUS", "Gérer le thesaurus");
+}
 
-if (!defined("_THESAURUS_LIST_SPECIFIC"))
+if (!defined("_THESAURUS_LIST_SPECIFIC")) {
     define("_THESAURUS_LIST_SPECIFIC", "Terme(s) spécifique(s)");
+}
 
-if (!defined("_MODIFY_THESAURUS"))
+if (!defined("_MODIFY_THESAURUS")) {
     define("_MODIFY_THESAURUS", "Modifier le terme");
+}
 
-if (!defined("_THESAURUS_NAME_IS_EMPTY"))
+if (!defined("_THESAURUS_NAME_IS_EMPTY")) {
     define("_THESAURUS_NAME_IS_EMPTY", "Le nom du terme est vide");
+}
 
-if (!defined("_THESAURUS_ADDED"))
+if (!defined("_THESAURUS_ADDED")) {
     define("_THESAURUS_ADDED", "Terme ajouté");
+}
 
-if (!defined("_THESAURUS_UPDATED"))
+if (!defined("_THESAURUS_UPDATED")) {
     define("_THESAURUS_UPDATED", "Terme mis à jour");
+}
 
-if (!defined("_THESAURUS_DELETED"))
+if (!defined("_THESAURUS_DELETED")) {
     define("_THESAURUS_DELETED", "Terme supprimé");
+}
 
-if (!defined("_ADD_THESAURUS"))
+if (!defined("_ADD_THESAURUS")) {
     define("_ADD_THESAURUS", "Ajouter un terme");
+}
 
-if (!defined("_CHOOSE_THESAURUS"))
+if (!defined("_CHOOSE_THESAURUS")) {
     define("_CHOOSE_THESAURUS", "Choisissez un terme");
+}
 
-if (!defined("_DISPLAYED"))
+if (!defined("_DISPLAYED")) {
     define("_DISPLAYED", "affiché(s)");
+}
 
-if (!defined("_THESAURUS_COMMENT"))
+if (!defined("_THESAURUS_COMMENT")) {
     define("_THESAURUS_COMMENT", "Gestion du thesaurus");
+}
 
-if (!defined("_THESAURUS_VIEW"))
+if (!defined("_THESAURUS_VIEW")) {
     define("_THESAURUS_VIEW", "Afficher le thésaurus dans les pages de traitement");
+}
 
-if (!defined("_THESAURUS_VIEW_DESC"))
+if (!defined("_THESAURUS_VIEW_DESC")) {
     define("_THESAURUS_VIEW_DESC", "Permet de voir le(s) terme(s) qui sont associés aux différents documents");
+}
 
-if (!defined("_ADD_THESAURUS_TO_RES"))
+if (!defined("_ADD_THESAURUS_TO_RES")) {
     define("_ADD_THESAURUS_TO_RES", "Pouvoir associer des termes aux différents documents");
+}
 
-if (!defined("_ADD_THESAURUS_TO_RES_DESC"))
+if (!defined("_ADD_THESAURUS_TO_RES_DESC")) {
     define("_ADD_THESAURUS_TO_RES_DESC", "Permet de pouvoir utiliser les termes disponibles du thésaurus afin de référencer les documents");
+}
 
-if (!defined("_USED_FOR"))
+if (!defined("_USED_FOR")) {
     define("_USED_FOR", "Utilisé pour");
+}
+
+if (!defined("_THESAURUSNONE")) {
+    define("_THESAURUSNONE", "Aucun thesaurus");
+}
