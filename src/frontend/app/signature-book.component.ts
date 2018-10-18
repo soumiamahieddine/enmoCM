555ec9bac2cf25806d3234e4f14b3c007be69baf
kevin.dezaphi <kevin.dezaphi@maarch.org>
2018-10-18 14:17:40 +0200

    remove signatory verification pop-up for visa

--- src/frontend/app/signature-book.component.ts
+++ src/frontend/app/signature-book.component.ts
@@ -505,21 +505,7 @@ export class SignatureBookComponent implements OnInit {
 
     validForm() {
         if ($j("#signatureBookActions option:selected")[0].value != "") {
-            if (this.signatureBook['listinstance']['requested_signature'] == true && this.signatureBook['isCurrentWorkflowUser'] == true) {
-                this.http.get(this.coreUrl + 'rest/listinstance/' + this.signatureBook['listinstance']['listinstance_id'])
-                    .subscribe((data: any) => {
-                        var r = true;
-                        if (data['signatory'] == false) {
-                            r = confirm("Vous n’avez signé aucun document. Êtes-vous sûr de vouloir continuer ?");
-                        }
-
-                        if (r) {
-                            this.sendActionForm();
-                        }
-                    });
-            } else {
-                this.sendActionForm();
-            }
+            this.sendActionForm();
         } else {
             alert("Aucune action choisie");
         }
