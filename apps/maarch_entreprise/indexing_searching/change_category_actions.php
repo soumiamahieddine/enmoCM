cb78aa70c64cca7b70adf6f1c8016d726945f53b
florian.azizian <florian.azizian@maarch.org>
2018-10-26 13:28:41 +0100

    FIX #8701 select action by default if there are more than 1 action

--- apps/maarch_entreprise/indexing_searching/change_category_actions.php
+++ apps/maarch_entreprise/indexing_searching/change_category_actions.php
@@ -7,34 +7,33 @@ $b = new basket();
 $_SESSION['category_id'] = $_REQUEST['category_id'];
 
 $actions = $b->get_actions_from_current_basket(
-    $_REQUEST['resId'], $_REQUEST['collId'], 'PAGE_USE', true
+    $_REQUEST['resId'],
+    $_REQUEST['collId'],
+    'PAGE_USE',
+    true
 );
 if (count($actions) > 0) {
-    //$frmStr .= '<b>' . _ACTIONS . ' : </b>';
     $frmStr .= '<select name="chosen_action" id="chosen_action">';
-
 
     $frmStr .= '<option value="">' . _CHOOSE_ACTION . '</option>';
     
     for ($indAct = 0; $indAct < count($actions); $indAct ++) {
         if ($indAct == 0 && $_SESSION['current_basket']['id'] == 'IndexingBasket' && count($actions) > 1) {
-
         } else {
             $frmStr .= '<option value="' . $actions[$indAct]['VALUE'] . '"';
-            if ($indAct == 0) {
+            if ($indAct == 0 || ($_SESSION['current_basket']['id'] == 'IndexingBasket' && $indAct == 1)) {
                 $frmStr .= 'selected="selected"';
             }
-            $frmStr .= '>' . $actions[$indAct]['LABEL'] . '</option>';            
+            $frmStr .= '>' . $actions[$indAct]['LABEL'] . '</option>';
         }
-
     }
     
     $frmStr .= '</select> ';
 } else {
     $frmStr .= _NO_AVAILABLE_ACTIONS_FOR_THIS_BASKET;
     echo "{status : 2, error_txt : '" . addslashes($frmStr) . "'}";
-    exit ();
+    exit();
 }
 
 echo "{status : 0, selectAction : '" . addslashes($frmStr) . "'}";
-exit ();
+exit();
