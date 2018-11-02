a636941928a3a1c8e64e2702214cd654568d1fff
Alex ORLUC <alex.orluc@maarch.org>
2018-11-02 18:34:31 +0100

    FIX #8788 fix display sign/visa date

--- modules/visa/class/class_modules_tools_Abstract.php
+++ modules/visa/class/class_modules_tools_Abstract.php
@@ -950,10 +950,11 @@ abstract class visa_Abstract extends Database
                         } else {
                             $vised = ' vised';
                             $link_vis = 'check';
-                            if (!empty($info_userVis['signatory'])) {
+                            
+                            if (!empty($info_userSign['signatory'])) {
                                 $info_vised = '<br/><sub>signé le : '.functions::format_date_db($info_userSign['process_date'], '', '', true).'</sub>';
     
-                                $info_vised = '<br/><select id="signRequest_'.$i.'" style="width:auto;display:none;" '.$isAllAttachementSigned;
+                                $info_vised .= '<br/><select id="signRequest_'.$i.'" style="width:auto;display:none;" '.$isAllAttachementSigned;
                                 $info_vised .= ' disabled="disabled" ';
                                 $info_vised .= '>';
                                 $info_vised .= '<option value="false">'._VISA_USER_SEARCH.'</option>';
@@ -964,7 +965,7 @@ abstract class visa_Abstract extends Database
                             } else {
                                 $info_vised = '<br/><sub>visé le : '.functions::format_date_db($info_userSign['process_date'], '', '', true).'</sub>';
     
-                                $info_vised = '<br/><select id="signRequest_'.$i.'" style="width:auto;display:none;" '.$isAllAttachementSigned;
+                                $info_vised .= '<br/><select id="signRequest_'.$i.'" style="width:auto;display:none;" '.$isAllAttachementSigned;
                                 $info_vised .= ' disabled="disabled" ';
                                 $info_vised .= '>';
                                 $info_vised .= '<option value="false" selected="selected">'._VISA_USER_SEARCH.'</option>';
