9411bf4cc4fabf6ab7111255a92bd166d9952501
Alex ORLUC <alex.orluc@maarch.org>
2018-12-20 12:54:42 +0100

    FEAT #9011 basket redirection refactoring

--- src/frontend/app/administration/user/user-administration.component.ts
+++ src/frontend/app/administration/user/user-administration.component.ts
@@ -72,7 +72,7 @@ export class UserAdministrationComponent extends AutoCompletePlugin implements O
     masterToggleBaskets(event: any) {
         if (event.checked) {  
             this.user.baskets.forEach((basket: any) => {
-                if (basket.userToDisplay == '') {
+                if (basket.userToDisplay == null) {
                     this.selectionBaskets.select(basket); 
                 }      
             });
@@ -119,6 +119,7 @@ export class UserAdministrationComponent extends AutoCompletePlugin implements O
                 this.serialId = params['id'];
                 this.http.get(this.coreUrl + "rest/users/" + this.serialId + "/details")
                     .subscribe((data: any) => {
+                        console.log(data);
                         this.user = data;
                         this.data = data.history;
                         this.userId = data.user_id;
@@ -276,14 +277,6 @@ export class UserAdministrationComponent extends AutoCompletePlugin implements O
         }
     }
 
-    showActions(basket:any){
-        $j('#'+basket.basket_id+'_'+basket.group_id).show();
-    }
-
-    hideActions(basket:any){
-        $j('#'+basket.basket_id+'_'+basket.group_id).hide();
-    }
-
     updateGroup(group: any) {
         this.http.put(this.coreUrl + "rest/users/" + this.serialId + "/groups/" + group.group_id, group)
             .subscribe((data: any) => {
@@ -439,7 +432,14 @@ export class UserAdministrationComponent extends AutoCompletePlugin implements O
         let basketsRedirect:any[] = [];
         this.user.baskets.forEach((elem: any) => {
             if (this.selectionBaskets.selected.map((e:any) => { return e.basket_id; }).indexOf(elem.basket_id) != -1 && this.selectionBaskets.selected.map((e:any) => { return e.group_id; }).indexOf(elem.group_id) != -1 && elem.allowed) {
-                basketsRedirect.push({newUser: newUser,basketId:elem.basket_id,basketOwner:this.user.user_id,virtual:elem.is_virtual})
+                basketsRedirect.push(
+                    {
+                        actual_user_id: newUser.serialId,
+                        basket_id:elem.basket_id,
+                        group_id:elem.groupSerialId,
+                        originalOwner: null
+                    }
+                )
             }
         });
         let r = confirm(this.lang.confirmAction + ' ' + this.lang.redirectBasket);
@@ -449,6 +449,7 @@ export class UserAdministrationComponent extends AutoCompletePlugin implements O
                 .subscribe((data: any) => {
                     this.userCtrl.setValue('');
                     this.user.baskets = data["baskets"];
+                    this.user.redirectedBaskets = data["redirectedBaskets"];
                     this.selectionBaskets.clear();
                     this.notify.success(this.lang.basketUpdated);
                 }, (err) => {
@@ -457,14 +458,22 @@ export class UserAdministrationComponent extends AutoCompletePlugin implements O
         }
     }
 
-    reassignBasketRedirection(newUser:any, basket: any) {
+    reassignBasketRedirection(newUser:any, basket: any, i: number) {
         let r = confirm(this.lang.confirmAction + ' ' + this.lang.redirectBasket);
 
         if (r) {
-            this.http.post(this.coreUrl + "rest/users/" + this.serialId + "/redirectedBaskets", [{"newUser" : newUser, "basketId":basket.basket_id, "basketOwner":basket.basket_owner, "virtual": basket.is_virtual}])
+            this.http.post(this.coreUrl + "rest/users/" + this.serialId + "/redirectedBaskets", [
+                {
+                    "actual_user_id": newUser.serialId, 
+                    "basket_id": basket.basket_id, 
+                    "group_id": basket.group_id,
+                    "originalOwner": basket.owner_user_id, 
+                }
+            ])
                 .subscribe((data: any) => {
                     this.userCtrl.setValue('');
                     this.user.baskets = data["baskets"];
+                    this.user.assignedBaskets.splice(i, 1);
                     this.notify.success(this.lang.basketUpdated);
                 }, (err) => {
                     this.notify.error(err.error.errors);
@@ -472,14 +481,31 @@ export class UserAdministrationComponent extends AutoCompletePlugin implements O
         }
     }
 
-    delBasketRedirection(basket: any) {
+    delBasketRedirection(basket: any, i: number) {
         let r = confirm(this.lang.confirmAction);
 
         if (r) {
-            this.http.request('DELETE', this.coreUrl + "rest/users/" + this.serialId + "/redirectedBaskets/"+ basket.basket_id, {body : {"basketOwner":basket.basket_owner}})
+            this.http.request('DELETE', this.coreUrl + "rest/users/" + this.serialId + "/redirectedBaskets/" + basket.id)
                 .subscribe((data: any) => {
                     this.userCtrl.setValue('');
                     this.user.baskets = data["baskets"];
+                    this.user.redirectedBaskets.splice(i, 1);
+                    this.notify.success(this.lang.basketUpdated);
+                }, (err) => {
+                    this.notify.error(err.error.errors);
+                });
+        }
+    }
+
+    delBasketAssignRedirection(basket: any, i: number) {
+        let r = confirm(this.lang.confirmAction);
+
+        if (r) {
+            this.http.request('DELETE', this.coreUrl + "rest/users/" + this.serialId + "/redirectedBaskets/" + basket.id)
+                .subscribe((data: any) => {
+                    this.userCtrl.setValue('');
+                    this.user.baskets = data["baskets"];
+                    this.user.assignedBaskets.splice(i, 1);
                     this.notify.success(this.lang.basketUpdated);
                 }, (err) => {
                     this.notify.error(err.error.errors);
