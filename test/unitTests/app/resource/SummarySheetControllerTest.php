9d7d749c6145824d90dff7e176772c4efc0acc55
Damien <damien.burel@maarch.org>
2019-02-04 14:19:27 +0100

    [TESTS] Add php-gd for ci 5

--- test/unitTests/app/resource/SummarySheetControllerTest.php
+++ test/unitTests/app/resource/SummarySheetControllerTest.php
@@ -31,8 +31,7 @@ class SummarySheetControllerTest extends TestCase
                 ['label' => 'Annotation(s)', 'unit' => 'notes'],
                 ['label' => 'Circuit de visa', 'unit' => 'visaWorkflow'],
                 ['label' => 'Circuit d\'avis', 'unit' => 'opinionWorkflow'],
-                ['label' => 'Commentaires', 'unit' => 'freeField'],
-                ['unit' => 'qrcode'],
+                ['label' => 'Commentaires', 'unit' => 'freeField']
             ],
         ];
         $fullRequest = \httpRequestCustom::addContentInBody($aArgs, $request);
