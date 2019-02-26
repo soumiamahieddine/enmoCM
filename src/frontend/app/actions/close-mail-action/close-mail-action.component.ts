443947eafbd464d65b7c55e50943e3fe22ba3c61
florian.azizian <florian.azizian@maarch.org>
2019-02-26 17:48:34 +0100

    FEAT #9685 close mail action V2

---
+++ src/frontend/app/actions/close-mail-action/close-mail-action.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { LANG } from '../../translate.component';
+import { NotificationService } from '../../notification.service';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+    templateUrl: "close-mail-action.component.html",
+    styleUrls: ['close-mail-action.component.scss'],
+    providers: [NotificationService],
+})
+export class CloseMailActionComponent implements OnInit {
+
+    lang: any = LANG;
+    loading: boolean = false;
+
+    constructor(public http: HttpClient, private notify: NotificationService, public dialogRef: MatDialogRef<CloseMailActionComponent>, @Inject(MAT_DIALOG_DATA) public data: any) { }
+
+    ngOnInit(): void { }
+
+    onSubmit(): void {
+        this.loading = true;
+        this.http.put('../../rest/resourcesList/users/' + this.data.currentBasketInfo.ownerId + '/groups/' + this.data.currentBasketInfo.groupId + '/baskets/' + this.data.currentBasketInfo.basketId + '/actions/' + this.data.action.id, {resources : this.data.selectedRes})
+            .subscribe((data: any) => {
+                this.loading = false;
+                this.dialogRef.close('success');
+            }, (err: any) => {
+                this.notify.handleErrors(err);
+                this.loading = false;
+            });
+    }
+}
