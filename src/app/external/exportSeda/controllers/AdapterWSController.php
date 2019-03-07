dc029dca91524037909c684b3dce93cc106541a1
florian.azizian <florian.azizian@maarch.org>
2019-03-07 14:56:14 +0100

    FEAT #9105 M2M review

--- src/app/external/exportSeda/controllers/AdapterWSController.php
+++ src/app/external/exportSeda/controllers/AdapterWSController.php
@@ -1,5 +1,17 @@
 <?php
 
+/**
+* Copyright Maarch since 2008 under licence GPLv3.
+* See LICENCE.txt file at the root folder for more details.
+* This file is part of Maarch software.
+*
+*/
+
+/**
+* @briefAdapter WS Controller
+* @author dev@maarch.org
+*/
+
 namespace ExportSeda\controllers;
 
 use MessageExchange\models\MessageExchangeModel;
--- src/app/external/exportSeda/controllers/TransferController.php
+++ src/app/external/exportSeda/controllers/TransferController.php
@@ -1,5 +1,17 @@
 <?php
 
+/**
+* Copyright Maarch since 2008 under licence GPLv3.
+* See LICENCE.txt file at the root folder for more details.
+* This file is part of Maarch software.
+*
+*/
+
+/**
+* @brief Transfer Controller
+* @author dev@maarch.org
+*/
+
 namespace ExportSeda\controllers;
 
 use SrcCore\models\CoreConfigModel;
