a3caa5e5c9a7bf56549ceb719a6a7cefd8577fcd
Vinciane <vinciane.bizet@maarch.org>
2019-03-08 10:43:12 +0100

    FIX #9511 Fix alert message on update after load an existing office

--- src/frontend/app/administration/template/template-administration.component.ts
+++ src/frontend/app/administration/template/template-administration.component.ts
@@ -282,7 +282,7 @@ export class TemplateAdministrationComponent implements OnInit {
         if (this.template.template_target != 'notifications') {
             this.template.template_datasource = 'letterbox_attachment';
         }
-        if (this.creationMode && this.template.template_style != 'uploadFile' && !this.template.jnlpUniqueId && (this.template.template_type == 'OFFICE' || (this.template.template_type == 'OFFICE_HTML' && this.template.template_style))) {
+        if (this.template.template_style != 'uploadFile' && !this.template.jnlpUniqueId && (this.template.template_type == 'OFFICE' || (this.template.template_type == 'OFFICE_HTML' && this.template.template_style))) {
             alert(this.lang.editModelFirst);
             return;
         }
