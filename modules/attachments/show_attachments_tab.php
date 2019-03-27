ac13a8f64af831dd88a0731fc41ea535c9577947
florian.azizian <florian.azizian@maarch.org>
2019-03-27 11:15:47 +0100

    FEAT #9141 restriction for document with notes

--- modules/attachments/show_attachments_tab.php
+++ modules/attachments/show_attachments_tab.php
@@ -1,8 +1,17 @@
 <?php
+
+/**
+* Copyright Maarch since 2008 under licence GPLv3.
+* See LICENCE.txt file at the root folder for more details.
+* This file is part of Maarch software.
+
+* @brief   show_attachments_tab
+* @author  dev <dev@maarch.org>
+* @ingroup attachments
+*/
+
 require_once 'core' . DIRECTORY_SEPARATOR . 'class' . DIRECTORY_SEPARATOR . 'class_request.php';
 require_once 'core' . DIRECTORY_SEPARATOR . 'class' . DIRECTORY_SEPARATOR . 'class_security.php';
-require 'modules/templates/class/templates_controler.php';
-
 
 $res_id = $_REQUEST["resId"];
 $coll_id = $_REQUEST["collId"];
@@ -10,7 +19,7 @@ $coll_id = $_REQUEST["collId"];
 $security = new security();
 $right = $security->test_right_doc($coll_id, $res_id);
 
-if(!$right){
+if (!$right) {
     exit(_NO_RIGHT_TXT);
 }
 
@@ -20,21 +29,9 @@ $core_tools->load_html();
 $core_tools->load_header('', true, false);
 $core_tools->load_js();
 
-$templatesControler = new templates_controler();
-$templates = array();
-$templates = $templatesControler->getAllTemplatesForProcess($data['destination']['value']);
-$db = new Database;
-$stmt = $db->query("SELECT res_id FROM " . $_SESSION['tablename']['attach_res_attachments']
-        . " WHERE (status = 'A_TRA' or status = 'TRA') and attachment_type <> 'converted_pdf' and attachment_type <> 'print_folder' and res_id_master = ? and coll_id = ?", array($res_id, $coll_id));
-//$req->show();
-$nb_attach = 0;
-if ($stmt->rowCount() > 0) {
-    $nb_attach = $stmt->rowCount();
-}
 $frm_str .= '<div class="ref-unit">';
 $frm_str .= '<center>';
 if ($core_tools->is_module_loaded('templates')) {
-
     $frm_str .= '<input type="button" name="attach" id="attach" class="button" value="'
             . _CREATE_PJ
             . '" onclick="showAttachmentsForm(\'' . $_SESSION['config']['businessappurl']
