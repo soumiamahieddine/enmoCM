95df3d64da1efb2143fbd203c9f0e6039f68c657
Alex ORLUC <alex.orluc@maarch.org>
2019-04-19 11:02:14 +0200

    fix order actions in admin basket

--- src/frontend/plugins/sorting.pipe.ts
+++ src/frontend/plugins/sorting.pipe.ts
@@ -1,17 +1,26 @@
 import { Pipe } from '@angular/core';
- 
+import { LatinisePipe } from "ngx-pipes";
+
 @Pipe({name: "sortBy"})
 export class SortPipe {
+
+	constructor(private latinisePipe: LatinisePipe) {}
+
+
   transform(array: Array<string>, args: string): Array<string> {
+
     array.sort((a: any, b: any) => {
-	    if ( a[args] < b[args] ){
+			const normA=this.latinisePipe.transform(a[args]).toLocaleLowerCase();
+			const normB=this.latinisePipe.transform(b[args]).toLocaleLowerCase();
+
+	    if ( normA < normB ){
 	    	return -1;
-	    }else if( a[args] > b[args] ){
+	    }else if( normA > normB ){
 	        return 1;
 	    }else{
 	    	return 0;	
 	    }
     });
     return array;
-  }
+	}
 }
