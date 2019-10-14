17d3784a21a5e979dd56ea731da624657c203fba
Guillaume Heurtier <guillaume.heurtier@maarch.org>
2019-10-14 14:50:18 +0200

    FEAT #11780 TIME 2:00 added merging tags

--- src/app/tag/models/TagModel.php
+++ src/app/tag/models/TagModel.php
@@ -119,4 +119,79 @@ class TagModel
 
         return true;
     }
+
+    public static function updateTagRes(array $args)
+    {
+        ValidatorModel::notEmpty($args, ['where']);
+        ValidatorModel::arrayType($args, ['set', 'where', 'data']);
+
+        DatabaseModel::update([
+            'table'     => 'tag_res',
+            'set'       => empty($args['set']) ? [] : $args['set'],
+            'where'     => $args['where'],
+            'data'      => empty($args['data']) ? [] : $args['data']
+        ]);
+
+        return true;
+    }
+
+    public static function deleteTagRes(array $args)
+    {
+        ValidatorModel::notEmpty($args, ['where', 'data']);
+        ValidatorModel::arrayType($args, ['where', 'data']);
+
+        DatabaseModel::delete([
+            'table' => 'tag_res',
+            'where' => $args['where'],
+            'data'  => $args['data']
+        ]);
+
+        return true;
+    }
+
+    public static function updateTagEntities(array $args)
+    {
+        ValidatorModel::notEmpty($args, ['where']);
+        ValidatorModel::arrayType($args, ['set', 'where', 'data']);
+
+        DatabaseModel::update([
+            'table'     => 'tags_entities',
+            'set'       => empty($args['set']) ? [] : $args['set'],
+            'where'     => $args['where'],
+            'data'      => empty($args['data']) ? [] : $args['data']
+        ]);
+
+        return true;
+    }
+
+    public static function deleteTagEntities(array $args)
+    {
+        ValidatorModel::notEmpty($args, ['where', 'data']);
+        ValidatorModel::arrayType($args, ['where', 'data']);
+
+        DatabaseModel::delete([
+            'table' => 'tags_entities',
+            'where' => $args['where'],
+            'data'  => $args['data']
+        ]);
+
+        return true;
+    }
+
+    public static function getTagEntities(array $aArgs)
+    {
+        ValidatorModel::arrayType($aArgs, ['select', 'where', 'data', 'orderBy']);
+        ValidatorModel::intType($aArgs, ['limit']);
+
+        $tags = DatabaseModel::select([
+            'select'    => empty($aArgs['select']) ? ['*'] : $aArgs['select'],
+            'table'     => ['tags_entities'],
+            'where'     => empty($aArgs['where']) ? [] : $aArgs['where'],
+            'data'      => empty($aArgs['data']) ? [] : $aArgs['data'],
+            'order_by'  => empty($aArgs['orderBy']) ? [] : $aArgs['orderBy'],
+            'limit'     => empty($aArgs['limit']) ? 0 : $aArgs['limit']
+        ]);
+
+        return $tags;
+    }
 }
