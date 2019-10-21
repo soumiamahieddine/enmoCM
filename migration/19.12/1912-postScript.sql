a2f7dac7a0491bf2a17188fe508d74a6e742f802
Guillaume Heurtier <guillaume.heurtier@maarch.org>
2019-10-21 11:15:06 +0200

    FEAT #11982 TIME 2:15 removed custom_* fields from res_letterbox

--- migration/19.12/1912-postScript.sql
+++ migration/19.12/1912-postScript.sql
@@ -22,3 +22,120 @@ DROP TABLE IF EXISTS indexingmodels;
 DROP TABLE IF EXISTS mlb_coll_ext;
 
 ALTER TABLE priorities DROP COLUMN IF EXISTS default_priority;
+
+ALTER TABLE res_letterbox DROP COLUMN IF EXISTS custom_t1;
+ALTER TABLE res_letterbox DROP COLUMN IF EXISTS custom_t2;
+ALTER TABLE res_letterbox DROP COLUMN IF EXISTS custom_t3;
+ALTER TABLE res_letterbox DROP COLUMN IF EXISTS custom_t4;
+ALTER TABLE res_letterbox DROP COLUMN IF EXISTS custom_t5;
+ALTER TABLE res_letterbox DROP COLUMN IF EXISTS custom_t6;
+ALTER TABLE res_letterbox DROP COLUMN IF EXISTS custom_t7;
+ALTER TABLE res_letterbox DROP COLUMN IF EXISTS custom_t8;
+ALTER TABLE res_letterbox DROP COLUMN IF EXISTS custom_t9;
+ALTER TABLE res_letterbox DROP COLUMN IF EXISTS custom_t10;
+ALTER TABLE res_letterbox DROP COLUMN IF EXISTS custom_t11;
+ALTER TABLE res_letterbox DROP COLUMN IF EXISTS custom_t12;
+ALTER TABLE res_letterbox DROP COLUMN IF EXISTS custom_t13;
+ALTER TABLE res_letterbox DROP COLUMN IF EXISTS custom_t14;
+ALTER TABLE res_letterbox DROP COLUMN IF EXISTS custom_t15;
+ALTER TABLE res_letterbox DROP COLUMN IF EXISTS custom_d1;
+ALTER TABLE res_letterbox DROP COLUMN IF EXISTS custom_d2;
+ALTER TABLE res_letterbox DROP COLUMN IF EXISTS custom_d3;
+ALTER TABLE res_letterbox DROP COLUMN IF EXISTS custom_d4;
+ALTER TABLE res_letterbox DROP COLUMN IF EXISTS custom_d5;
+ALTER TABLE res_letterbox DROP COLUMN IF EXISTS custom_d6;
+ALTER TABLE res_letterbox DROP COLUMN IF EXISTS custom_d7;
+ALTER TABLE res_letterbox DROP COLUMN IF EXISTS custom_d8;
+ALTER TABLE res_letterbox DROP COLUMN IF EXISTS custom_d9;
+ALTER TABLE res_letterbox DROP COLUMN IF EXISTS custom_d10;
+ALTER TABLE res_letterbox DROP COLUMN IF EXISTS custom_n1;
+ALTER TABLE res_letterbox DROP COLUMN IF EXISTS custom_n2;
+ALTER TABLE res_letterbox DROP COLUMN IF EXISTS custom_n3;
+ALTER TABLE res_letterbox DROP COLUMN IF EXISTS custom_n4;
+ALTER TABLE res_letterbox DROP COLUMN IF EXISTS custom_n5;
+ALTER TABLE res_letterbox DROP COLUMN IF EXISTS custom_f1;
+ALTER TABLE res_letterbox DROP COLUMN IF EXISTS custom_f2;
+ALTER TABLE res_letterbox DROP COLUMN IF EXISTS custom_f3;
+ALTER TABLE res_letterbox DROP COLUMN IF EXISTS custom_f4;
+ALTER TABLE res_letterbox DROP COLUMN IF EXISTS custom_f5;
+
+
+/* RE CREATE VIEWS */
+CREATE OR REPLACE VIEW res_view_letterbox AS
+SELECT r.res_id,
+       r.type_id,
+       r.policy_id,
+       r.cycle_id,
+       d.description AS type_label,
+       d.doctypes_first_level_id,
+       dfl.doctypes_first_level_label,
+       dfl.css_style AS doctype_first_level_style,
+       d.doctypes_second_level_id,
+       dsl.doctypes_second_level_label,
+       dsl.css_style AS doctype_second_level_style,
+       r.format,
+       r.typist,
+       r.creation_date,
+       r.modification_date,
+       r.docserver_id,
+       r.path,
+       r.filename,
+       r.fingerprint,
+       r.filesize,
+       r.scan_date,
+       r.scan_user,
+       r.scan_location,
+       r.scan_wkstation,
+       r.scan_batch,
+       r.scan_postmark,
+       r.status,
+       r.work_batch,
+       r.doc_date,
+       r.reference_number,
+       r.external_reference,
+       r.external_id,
+       r.external_link,
+       r.departure_date,
+       r.opinion_limit_date,
+       r.department_number_id,
+       r.barcode,
+       r.external_signatory_book_id,
+       r.initiator,
+       r.destination,
+       r.dest_user,
+       r.confidentiality,
+       r.category_id,
+       r.exp_contact_id,
+       r.exp_user_id,
+       r.dest_user_id,
+       r.dest_contact_id,
+       r.address_id,
+       r.alt_identifier,
+       r.admission_date,
+       r.process_limit_date,
+       r.closing_date,
+       r.alarm1_date,
+       r.alarm2_date,
+       r.flag_alarm1,
+       r.flag_alarm2,
+       r.is_multicontacts,
+       r.subject,
+       r.priority,
+       r.locker_user_id,
+       r.locker_time,
+       en.entity_label,
+       en.entity_type AS entitytype,
+       cont.contact_id,
+       cont.firstname AS contact_firstname,
+       cont.lastname AS contact_lastname,
+       cont.society AS contact_society,
+       u.lastname AS user_lastname,
+       u.firstname AS user_firstname
+FROM doctypes d,
+     doctypes_first_level dfl,
+     doctypes_second_level dsl,
+     res_letterbox r
+         LEFT JOIN entities en ON r.destination::text = en.entity_id::text
+         LEFT JOIN contacts_v2 cont ON r.exp_contact_id = cont.contact_id OR r.dest_contact_id = cont.contact_id
+         LEFT JOIN users u ON r.exp_user_id::text = u.user_id::text OR r.dest_user_id::text = u.user_id::text
+WHERE r.type_id = d.type_id AND d.doctypes_first_level_id = dfl.doctypes_first_level_id AND d.doctypes_second_level_id = dsl.doctypes_second_level_id;
