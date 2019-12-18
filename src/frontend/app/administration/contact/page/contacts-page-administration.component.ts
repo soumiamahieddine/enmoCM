4056e88bcaf7bce8218c634c9e691955f3ccc232
Alex ORLUC <alex.orluc@maarch.org>
2019-12-18 18:36:06 +0100

    FEAT #12635 TIME 3:30 begin front add contact

--- src/frontend/app/administration/contact/page/contacts-page-administration.component.ts
+++ src/frontend/app/administration/contact/page/contacts-page-administration.component.ts
@@ -14,6 +14,7 @@ import { ActivatedRoute } from '@angular/router';
 
 @Component({
     templateUrl: "contacts-page-administration.component.html",
+    styleUrls: ['contacts-page-administration.component.scss'],
     providers: [NotificationService, AppService]
 })
 export class ContactsPageAdministrationComponent implements OnInit {
@@ -27,6 +28,136 @@ export class ContactsPageAdministrationComponent implements OnInit {
     creationMode: boolean = true;
 
     contact: any = null;
+
+    contactUnit = [
+        {
+            id: 'mainInfo',
+            label: 'Denomination'
+        },
+        {
+            id: 'address',
+            label: 'Adresse'
+        },
+        {
+            id: 'complement',
+            label: 'Complement'
+        }
+    ];
+
+    contactForm = [
+        {
+            id: 'company',
+            unit: 'mainInfo',
+            label: this.lang.contactsParameters_company,
+            type: 'string',
+            control: new FormControl(),
+            default: true
+        },
+        {
+            id: 'firstname',
+            unit: 'mainInfo',
+            label: this.lang.contactsParameters_firstname,
+            type: 'string',
+            control: new FormControl(),
+            default: false
+        },
+        {
+            id: 'lastname',
+            unit: 'mainInfo',
+            label: this.lang.contactsParameters_lastname,
+            type: 'string',
+            control: new FormControl(),
+            default: false
+        },
+        {
+            id: 'function',
+            unit: 'mainInfo',
+            label: this.lang.contactsParameters_function,
+            type: 'string',
+            control: new FormControl(),
+            default: false
+        },
+        {
+            id: 'department',
+            unit: 'mainInfo',
+            label: this.lang.contactsParameters_department,
+            type: 'string',
+            control: new FormControl(),
+            default: false
+        },
+        {
+            id: 'addressAdditional1',
+            unit: 'mainInfo',
+            label: this.lang.contactsParameters_address_additional1,
+            type: 'string',
+            control: new FormControl(),
+            default: false
+        },
+        {
+            id: 'email',
+            unit: 'mainInfo',
+            label: this.lang.email,
+            type: 'string',
+            control: new FormControl(),
+            default: true
+        },
+        {
+            id: 'phone',
+            unit: 'mainInfo',
+            label: this.lang.phoneNumber,
+            type: 'string',
+            control: new FormControl(),
+            default: true
+        },
+        {
+            id: 'addressNumber',
+            unit: 'address',
+            label: this.lang.contactsParameters_address_number,
+            type: 'string',
+            control: new FormControl(),
+            default: false
+        },
+        {
+            id: 'addressStreet',
+            unit: 'address',
+            label: this.lang.contactsParameters_address_street,
+            type: 'string',
+            control: new FormControl(),
+            default: false
+        },
+        {
+            id: 'addressAdditional2',
+            unit: 'address',
+            label: this.lang.contactsParameters_address_additional2,
+            type: 'string',
+            control: new FormControl(),
+            default: false
+        },
+        {
+            id: 'addressPostcode',
+            unit: 'address',
+            label: this.lang.contactsParameters_address_postcode,
+            type: 'string',
+            control: new FormControl(),
+            default: false
+        },
+        {
+            id: 'addressTown',
+            unit: 'address',
+            label: this.lang.contactsParameters_address_town,
+            type: 'string',
+            control: new FormControl(),
+            default: false
+        },
+        {
+            id: 'addressCountry',
+            unit: 'address',
+            label: this.lang.contactsParameters_address_country,
+            type: 'string',
+            control: new FormControl(),
+            default: false
+        }
+    ];
 
     constructor(
         public http: HttpClient,
@@ -47,6 +178,29 @@ export class ContactsPageAdministrationComponent implements OnInit {
                 this.headerService.setHeader(this.lang.contactCreation);
                 this.creationMode = true;
 
+                this.http.get("../../rest/contactsCustomFields").pipe(
+                    tap((data: any) => {
+                        data.customFields.forEach((element: any) => {
+                            this.contactForm.push(
+                                {
+                                    id: `customField_${element.id}`,
+                                    unit: 'complement',
+                                    label: element.label,
+                                    type: element.type,
+                                    control: new FormControl({ value: '', disabled: false }),
+                                    default: false
+                                }
+                            );
+                        });                        
+                    }),
+                    finalize(() => this.loading = false),
+                    catchError((err: any) => {
+                        this.notify.handleErrors(err);
+                        return of(false);
+                    })
+                ).subscribe();
+
+
                 this.loading = false;
             } else {
                 window['MainHeaderComponent'].setSnav(this.sidenavLeft);
@@ -66,6 +220,33 @@ export class ContactsPageAdministrationComponent implements OnInit {
                 ).subscribe();
             }
         });
+    }
 
+    isEmptyUnit(id: string) {
+        if (this.contactForm.filter(field => field.default && field.unit === id).length === 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    initForm() {
+        this.contactForm.forEach(element => {
+            element.control = new FormControl({ value: '', disabled: false }); 
+        });
+    }
+
+    toogleAllFieldsUnit(idUnit: string) {
+        this.contactForm.filter(field => field.unit === idUnit).forEach((element: any) => {
+            element.default = true;
+        });
+    }
+
+    noField(id: string) {
+        if (this.contactForm.filter(field => !field.default && field.unit === id).length === 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
