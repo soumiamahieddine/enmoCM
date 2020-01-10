4c1a1b5e485bb3f818cc13fd8ef90cf0b646eb4a
florian.azizian <florian.azizian@maarch.org>
2020-01-10 19:46:09 +0100

    test docker

--- ci/docker_install_php.sh
+++ ci/docker_install_php.sh
@@ -6,8 +6,7 @@
 set -xe
 
 apt-get install --no-install-recommends -y libpq-dev libxml2-dev libxslt1-dev libpng-dev \
-&& docker-php-ext-configure pgsql -with-pgsql=/usr/local/pgsql \
-&& docker-php-ext-install pdo_pgsql \
+&& docker-php-ext-install pdo pgsql pdo_pgsql \
 && docker-php-ext-install xsl \
 && pecl install xdebug-2.7.0RC2 \
 && docker-php-ext-enable xdebug \
