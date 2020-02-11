a195b63e6b03a8f5659d8867141f9f8e43553698
florian.azizian <florian.azizian@maarch.org>
2020-02-11 17:10:45 +0100

    FEAT #12595 Back Message Exchange in V2

--- src/app/external/messageExchange/controllers/ReceiveMessageExchangeController.php
+++ src/app/external/messageExchange/controllers/ReceiveMessageExchangeController.php
@@ -38,7 +38,7 @@ class ReceiveMessageExchangeController
 
     public function saveMessageExchange(Request $request, Response $response)
     {
-        if (!PrivilegeController::hasPrivilege(['privilegeId' => 'save_numeric_package', 'userId' => $GLOBALS['id']])) {
+        if (!PrivilegeController::hasPrivilege(['privilegeId' => 'manage_numeric_package', 'userId' => $GLOBALS['id']])) {
             return $response->withStatus(403)->withJson(['errors' => 'Service forbidden']);
         }
 
