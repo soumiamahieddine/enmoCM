3b44fca989cd5764956b654914f72fd093a6b032
Alex ORLUC <alex.orluc@maarch.org>
2020-02-24 18:44:27 +0100

    FEAT #13120 TIME 1 front mailing edit pj

--- src/frontend/lang/lang-en.ts
+++ src/frontend/lang/lang-en.ts
@@ -1573,4 +1573,5 @@ export const LANG_EN = {
     "disableMailing" : "Disable mailing",
     "selectContact" : "select a contact",
     "mailingMsg" : "<b>Mailing enbled</b> : <br><br><p>A <b>master</b> attachment will be created without merged field <b>contact</b> (attachmentRecipient).</p><p>If you click on Mailing, the attachmenets will be generated <b>NOW</b>.<br><br>If you click on Validate, They will be generated <b>AFTER</b> <b>approver</b> validation of visa circuit.</p><p><b>One</b> attachment will be generated for <b>each contact</b> linked to the mail.</p>",
+    "attachmentGenerated" : "Attachment generated",
 };
--- src/frontend/lang/lang-fr.ts
+++ src/frontend/lang/lang-fr.ts
@@ -1611,5 +1611,6 @@ export const LANG_FR = {
     "enableMailing" : "Activer le publipostage",
     "disableMailing" : "Désactiver le publipostage",
     "selectContact" : "Sélectionner un contact",
-    "mailingMsg" : "<b>Publipostage activé</b> : <br><br><p>Un attachement <b>maître</b> sera créé sans le champ de fusion <b>contact</b> (attachmentRecipient).</p><p>Si vous cliquez sur Publipostage, les attachements seront générés <b>TOUT DE SUITE</b>.<br><br>Si vous cliquez sur Valider, ils seront générés <b>APRÈS</b> validation des <b>viseurs</b> du circuit de visa.</p><p><b>Un</b> attachement sera généré <b>par contact</b> associé au courrier.</p>",
+    "mailingMsg" : "<b>Publipostage activé</b> : <br><br><p>Un attachement <b>maître</b> sera créé sans le champ de fusion <b>contact</b> (attachmentRecipient).</p><p>Si vous cliquez sur Publipostage, les attachements seront générés <b>TOUT DE SUITE</b>.<br><br>Si vous cliquez sur Valider, ils seront générés <b>APRÈS</b> validation des <b>viseurs</b> du circuit de visa.</p><br><p><b>Un</b> attachement sera généré <b>par contact</b> associé au courrier.</p>",
+    "attachmentGenerated" : "Pièces jointes générées",
 };
