c3b0d0c7cb2bd2b629660f6d9083d66f718f1f39
Damien <damien.burel@maarch.org>
2020-03-30 17:50:19 +0200

    FEAT #13675 TIME 3:20 Add  protractor + begin test

---
+++ test/e2e/about-us-spec.js
@@ -0,0 +1,9 @@
+describe('about-us page', function() {
+    it('test page', function() {
+        browser.sleep(2000);
+        element(by.css('[title="Accueil"]')).click();
+
+        browser.sleep(2000);
+    });
+});
+
---
+++ test/e2e/login-spec.js
@@ -0,0 +1,29 @@
+describe('angular homepage todo list', function() {
+    it('should add a todo', function() {
+        browser.waitForAngularEnabled(false);
+        browser.get('http://127.0.0.1/maarch_trunk/apps/maarch_entreprise/index.php?display=true&page=login');
+
+        element(by.id('login')).sendKeys('bbain');
+        element(by.id('pass')).sendKeys('maarch');
+        element(by.css('[name="submit"]')).click();
+
+        // var todoList = element.all(by.repeater('todo in todoList.todos'));
+        // expect(todoList.count()).toEqual(3);
+        // expect(todoList.get(2).getText()).toEqual('write first protractor test');
+        //
+        // // You wrote your first test, cross it off the list
+        // todoList.get(2).element(by.css('input')).click();
+        // var completedAmount = element.all(by.css('.done-true'));
+        // expect(completedAmount.count()).toEqual(2);
+    });
+
+    it('test 2', function() {
+        browser.sleep(4000);
+        browser.waitForAngularEnabled(true);
+        var nbHeader = element.all(by.css('[class="bg-head-content"]'));
+        expect(nbHeader.count()).toEqual(1);
+
+        element(by.css('[routerLink="/about-us"]')).click();
+    });
+});
+
