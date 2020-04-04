9ce10d714516b67828356e487647ce8c025c0b95
florian.azizian <florian.azizian@maarch.org>
2020-04-04 11:24:00 +0100

    Fix TU

--- test/unitTests/app/user/UserControllerTest.php
+++ test/unitTests/app/user/UserControllerTest.php
@@ -380,12 +380,12 @@ class UserControllerTest extends TestCase
 
         $this->assertSame(self::$id, $responseBody->id);
         $this->assertIsArray($responseBody->entities);
-        $this->assertSame('DGS', $responseBody->entities[0]->entity_id);
-        $this->assertSame('Rogue', $responseBody->entities[0]->user_role);
-        $this->assertSame('N', $responseBody->entities[0]->primary_entity);
-        $this->assertSame('FIN', $responseBody->entities[1]->entity_id);
-        $this->assertSame('Hunter', $responseBody->entities[1]->user_role);
-        $this->assertSame('Y', $responseBody->entities[1]->primary_entity);
+        $this->assertSame('FIN', $responseBody->entities[0]->entity_id);
+        $this->assertSame('Hunter', $responseBody->entities[0]->user_role);
+        $this->assertSame('Y', $responseBody->entities[0]->primary_entity);
+        $this->assertSame('DGS', $responseBody->entities[1]->entity_id);
+        $this->assertSame('Rogue', $responseBody->entities[1]->user_role);
+        $this->assertSame('N', $responseBody->entities[1]->primary_entity);
     }
 
     public function testDeleteEntity()
