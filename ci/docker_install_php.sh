953754c56e64f0dc56f4c1d6541bdcbd3a1d87f1
florian.azizian <florian.azizian@maarch.org>
2020-04-24 20:54:21 +0100

    FIX #11895 Test E2E

--- ci/docker_install_php.sh
+++ ci/docker_install_php.sh
@@ -12,4 +12,10 @@ apt-get install -y libpq-dev libxml2-dev libxslt1-dev libpng-dev unoconv xpdf-ut
 && docker-php-ext-install xsl \
 && pecl install xdebug-2.9.3 \
 && docker-php-ext-enable xdebug \
-&& docker-php-ext-install gd
+&& docker-php-ext-install gd \
+&& FIREFOX_URL="https://download.mozilla.org/?product=firefox-latest-ssl&os=linux64&lang=en-US" \
+&& ACTUAL_URL=$(curl -Ls -o /dev/null -w %{url_effective} $FIREFOX_URL) \
+&& curl --silent --show-error --location --fail --retry 3 --output /tmp/firefox.tar.bz2 $ACTUAL_URL \
+&& sudo tar -xvjf /tmp/firefox.tar.bz2 -C /opt \
+&& sudo ln -s /opt/firefox/firefox /usr/local/bin/firefox \
+&& firefox --version
