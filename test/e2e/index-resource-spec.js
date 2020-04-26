a212e312e561160ad287cbbd48d0a7d9be01e6df
florian.azizian <florian.azizian@maarch.org>
2020-04-26 22:51:41 +0100

    FIX #11895 Test E2E

--- test/e2e/index-resource-spec.js
+++ test/e2e/index-resource-spec.js
@@ -2,9 +2,13 @@ var fs = require('fs');
 
 // abstract writing screen shot to a file
 function writeScreenShot(data, filename) {
-    var stream = fs.createWriteStream(filename);
+    var stream = fs.createWriteStream(filename+'.png');
     stream.write(new Buffer.from(data, 'base64'));
     stream.end();
+    var stream = fs.createWriteStream(filename+'.txt');
+    stream.write(data);
+    stream.end();
+    console.log(data);
 }
 
 describe('index resource page', function () {
@@ -13,25 +17,29 @@ describe('index resource page', function () {
         browser.get(browser.baseUrl + '/dist/index.html#/login');
         browser.sleep(500);
         browser.takeScreenshot().then(function (png) {
-            writeScreenShot(png, 'test/e2e/screenshots/login_to_app_' + browser.browserName + '.png');
+            writeScreenShot(png, 'test/e2e/screenshots/login_to_app_' + browser.browserName);
         });
         element(by.id('login')).sendKeys('bblier');
         browser.sleep(500);
         element(by.id('password')).sendKeys('maarch');
         browser.sleep(500);
         element(by.id('submit')).click();
+        browser.sleep(100);
+        browser.takeScreenshot().then(function (png) {
+            writeScreenShot(png, 'test/e2e/screenshots/submitLogin_' + browser.browserName);
+        });
     });
 
     it('index a document whitout file', function () {
         browser.sleep(4000);
         expect(browser.getCurrentUrl()).toEqual(browser.baseUrl + "/dist/index.html#/home");
         browser.takeScreenshot().then(function (png) {
-            writeScreenShot(png, 'test/e2e/screenshots/home_' + browser.browserName + '.png');
+            writeScreenShot(png, 'test/e2e/screenshots/home_' + browser.browserName);
         });
         element(by.id('indexing')).click();
         browser.sleep(500);
         browser.takeScreenshot().then(function (png) {
-            writeScreenShot(png, 'test/e2e/screenshots/index_a_document_' + browser.browserName + '.png');
+            writeScreenShot(png, 'test/e2e/screenshots/index_a_document_' + browser.browserName);
         });
         // element(by.id('doctype')).click();
         // browser.sleep(500);
