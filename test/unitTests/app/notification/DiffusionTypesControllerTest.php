7f285d98c467bf3b7cd96809cf5a287e6591e88e
florian.azizian <florian.azizian@maarch.org>
2020-04-30 09:56:43 +0100

    FIX #13676 TIME 0:30 TU for notification

--- test/unitTests/app/notification/DiffusionTypesControllerTest.php
+++ test/unitTests/app/notification/DiffusionTypesControllerTest.php
@@ -110,7 +110,7 @@ class DiffusionTypesControllerTest extends TestCase
         }
     }
 
-    public function testGetRecipientsByDestEntity()
+    public function testGetRecipientsByDestUser()
     {
         $diffusionTypesController = new \Notification\controllers\DiffusionTypesController();
 
@@ -169,6 +169,175 @@ class DiffusionTypesControllerTest extends TestCase
             $args = [
                 'notification' => [
                     'diffusion_type' => 'dest_user',
+                    'diffusion_properties' => 'NEW,COU,CLO,END,ATT,VAL,INIT'
+                ],
+                'request' => 'res_id',
+                'event' => [
+                    'record_id' => $resId,
+                    'table_name' => 'res_letterbox'
+                ]
+            ];
+
+            $response = $diffusionTypesController->getItemsToNotify($args);
+            $this->assertSame($resId, $response);
+
+            $args['event']['table_name'] = 'notes';
+            $args['event']['record_id'] = 1;
+            $response = $diffusionTypesController->getItemsToNotify($args);
+            $this->assertEmpty($response);
+
+            $args['event']['table_name'] = 'listinstance';
+            $args['event']['user_id'] = 19;
+            $response = $diffusionTypesController->getItemsToNotify($args);
+            $this->assertEmpty($response);
+        }
+    }
+
+    public function testGetRecipientsByDestEntity()
+    {
+        $diffusionTypesController = new \Notification\controllers\DiffusionTypesController();
+
+        foreach ($GLOBALS['resources'] as $resId) {
+            $args = [
+                'notification' => [
+                    'diffusion_type' => 'dest_entity'
+                ],
+                'request' => 'recipients',
+                'event' => [
+                    'record_id' => $resId
+                ]
+            ];
+
+            $response = $diffusionTypesController->getItemsToNotify($args);
+            foreach ($response as $entity) {
+                $this->assertSame('PJS', $entity['entity_id']);
+                $this->assertSame('Y', $entity['enabled']);
+                $this->assertSame('support@maarch.fr', $entity['mail']);
+            }
+        }
+
+        foreach ($GLOBALS['resources'] as $resId) {
+            $args = [
+                'notification' => [
+                    'diffusion_type' => 'dest_entity',
+                    'diffusion_properties' => 'NEW,COU,CLO,END,ATT,VAL,INIT'
+                ],
+                'request' => 'res_id',
+                'event' => [
+                    'record_id' => $resId,
+                    'table_name' => 'res_letterbox'
+                ]
+            ];
+
+            $response = $diffusionTypesController->getItemsToNotify($args);
+            $this->assertSame($resId, $response);
+
+            $args['event']['table_name'] = 'notes';
+            $args['event']['record_id'] = 1;
+            $response = $diffusionTypesController->getItemsToNotify($args);
+            $this->assertEmpty($response);
+
+            $args['event']['table_name'] = 'listinstance';
+            $args['event']['user_id'] = 19;
+            $response = $diffusionTypesController->getItemsToNotify($args);
+            $this->assertEmpty($response);
+        }
+    }
+
+    public function testGetRecipientsByDestUserSign()
+    {
+        $diffusionTypesController = new \Notification\controllers\DiffusionTypesController();
+
+        foreach ($GLOBALS['resources'] as $resId) {
+            $args = [
+                'notification' => [
+                    'diffusion_type' => 'dest_user_sign',
+                    'diffusion_properties' => 'NEW,COU,CLO,END,ATT,VAL,INIT,ESIG,EVIS'
+                ],
+                'request' => 'recipients',
+                'event' => [
+                    'record_id' => $resId,
+                    'table_name' => 'res_letterbox'
+                ]
+            ];
+
+            $response = $diffusionTypesController->getItemsToNotify($args);
+            $this->assertIsArray($response);
+
+            $args['event']['table_name'] = 'notes';
+            $args['event']['record_id'] = 1;
+            $response = $diffusionTypesController->getItemsToNotify($args);
+            $this->assertIsArray($response);
+
+            $args['event']['table_name'] = 'listinstance';
+            $args['event']['user_id'] = 19;
+            $response = $diffusionTypesController->getItemsToNotify($args);
+            $this->assertIsArray($response);
+        }
+
+        foreach ($GLOBALS['resources'] as $resId) {
+            $args = [
+                'notification' => [
+                    'diffusion_type' => 'dest_user_sign',
+                    'diffusion_properties' => 'NEW,COU,CLO,END,ATT,VAL,INIT'
+                ],
+                'request' => 'res_id',
+                'event' => [
+                    'record_id' => $resId,
+                    'table_name' => 'res_letterbox'
+                ]
+            ];
+
+            $response = $diffusionTypesController->getItemsToNotify($args);
+            $this->assertSame($resId, $response);
+
+            $args['event']['table_name'] = 'notes';
+            $args['event']['record_id'] = 1;
+            $response = $diffusionTypesController->getItemsToNotify($args);
+            $this->assertEmpty($response);
+
+            $args['event']['table_name'] = 'listinstance';
+            $args['event']['user_id'] = 19;
+            $response = $diffusionTypesController->getItemsToNotify($args);
+            $this->assertEmpty($response);
+        }
+    }
+
+    public function testGetRecipientsByDestUserVisa()
+    {
+        $diffusionTypesController = new \Notification\controllers\DiffusionTypesController();
+
+        foreach ($GLOBALS['resources'] as $resId) {
+            $args = [
+                'notification' => [
+                    'diffusion_type' => 'dest_user_visa',
+                    'diffusion_properties' => 'NEW,COU,CLO,END,ATT,VAL,INIT,ESIG,EVIS'
+                ],
+                'request' => 'recipients',
+                'event' => [
+                    'record_id' => $resId,
+                    'table_name' => 'res_letterbox'
+                ]
+            ];
+
+            $response = $diffusionTypesController->getItemsToNotify($args);
+            $this->assertIsArray($response);
+
+            $args['event']['table_name'] = 'notes';
+            $args['event']['record_id'] = 1;
+            $response = $diffusionTypesController->getItemsToNotify($args);
+            $this->assertIsArray($response);
+
+            $args['event']['table_name'] = 'listinstance';
+            $args['event']['user_id'] = 19;
+            $response = $diffusionTypesController->getItemsToNotify($args);
+            $this->assertIsArray($response);
+        }
+
+        foreach ($GLOBALS['resources'] as $resId) {
+            $args = [
+                'notification' => [
+                    'diffusion_type' => 'dest_user_visa',
                     'diffusion_properties' => 'NEW,COU,CLO,END,ATT,VAL,INIT'
                 ],
                 'request' => 'res_id',
