cda76fe1189adbb85c33c2d0ebdb726d0b1303b5
Alex ORLUC <alex.orluc@maarch.org>
2020-06-22 12:48:24 +0200

    FEAT #13671 TIME 0:10 modules refactoring

--- src/frontend/app/signature-book.module.ts
+++ src/frontend/app/signature-book.module.ts
@@ -5,13 +5,14 @@ import { SharedModule } from './app-common.module';
 import { SignatureBookRoutingModule } from './signature-book-routing.module';
 import { SignatureBookComponent } from './signature-book.component';
 import { NoteModule } from './notes/note.module';
-
+import { DocumentViewerModule } from './viewer/document-viewer.module';
 
 @NgModule({
     imports: [
         SharedModule,
         SignatureBookRoutingModule,
-        NoteModule
+        NoteModule,
+        DocumentViewerModule
     ],
     declarations: [SignatureBookComponent],
     entryComponents: [
