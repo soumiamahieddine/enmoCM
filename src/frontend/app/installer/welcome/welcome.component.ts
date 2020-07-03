3398081a547074c0706af331a4b9b3badd7dade7
Alex ORLUC <alex.orluc@maarch.org>
2020-07-03 15:30:27 +0200

    FEAT #13671 TIME 0:50 add welcome installer

--- src/frontend/app/installer/welcome/welcome.component.ts
+++ src/frontend/app/installer/welcome/welcome.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { NotificationService } from '../../../service/notification/notification.service';
+import { LANG } from '../../translate.component';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { tap } from 'rxjs/internal/operators/tap';
+import { of } from 'rxjs/internal/observable/of';
+import { catchError } from 'rxjs/internal/operators/catchError';
+import { environment } from '../../../environments/environment';
+
 
 @Component({
     selector: 'app-welcome',
@@ -7,14 +16,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomeComponent implements OnInit {
 
-    langs: string[] = [
-        'fr',
-        'en'
+    lang: any = LANG;
+
+    stepFormGroup: FormGroup;
+
+    langs: string[] = [];
+
+    appVersion: string = environment.VERSION.split('.')[0] + '.' + environment.VERSION.split('.')[1];
+
+    steps: any[] = [
+        {
+            icon : 'fas fa-check-square',
+            desc: this.lang.prerequisiteCheck
+        },
+        {
+            icon : 'fa fa-database',
+            desc: this.lang.databaseCreation
+        },
+        {
+            icon : 'fa fa-database',
+            desc: this.lang.dataSampleCreation
+        },
+        {
+            icon : 'fa fa-hdd',
+            desc: this.lang.docserverCreation
+        },
+        {
+            icon : 'fas fa-tools',
+            desc: this.lang.stepCustomizationActionDesc
+        },
+        {
+            icon : 'fa fa-user',
+            desc: this.lang.adminUserCreation
+        },
     ];
 
-    constructor() { }
+    constructor(
+        public http: HttpClient,
+        private notify: NotificationService,
+        private _formBuilder: FormBuilder
+    ) { }
 
-    ngOnInit(): void { }
+    ngOnInit(): void {
+        this.stepFormGroup = this._formBuilder.group({
+            lang: ['fr', Validators.required]
+        });
+
+        this.getLang();
+    }
+
+    getLang() {
+        this.langs = [
+            'fr',
+            'en',
+        ];
+        /*this.http.get('../rest/dev/lang').pipe(
+            tap((data: any) => {
+            }),
+            catchError((err: any) => {
+                this.notify.handleSoftErrors(err);
+                return of(false);
+            })
+        ).subscribe();*/
+    }
 
     initStep() {
         return false;
@@ -22,12 +86,18 @@ export class WelcomeComponent implements OnInit {
 
     getInfoToInstall(): any[] {
         return [];
-        /*return {
-            body : {
-                lang: this.selectedLang,
+        /*return [{
+            idStep : 'lang',
+            body: {
+                lang: this.stepFormGroup.controls['lang'].value,
             },
-            route : '/installer/lang'
-        };*/
+            route : {
+                method : 'POST',
+                url : '../rest/installer/lang'
+            },
+            description: this.lang.langSetting,
+            installPriority: 3
+        }];*/
     }
 
 }
