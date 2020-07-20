8c9dffdb00988fe7345795b3e57030b24ca03ab8
Alex ORLUC <alex.orluc@maarch.org>
2020-07-20 10:06:25 +0200

    FEAT #14213 TIME 0:05 can modify superadmin identifier in installer

--- src/frontend/app/installer/useradmin/useradmin.component.ts
+++ src/frontend/app/installer/useradmin/useradmin.component.ts
@@ -29,7 +29,7 @@ export class UseradminComponent implements OnInit {
         const valEmail: ValidatorFn[] = [Validators.pattern(/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/), Validators.required];
 
         this.stepFormGroup = this._formBuilder.group({
-            login: [{ value: 'superadmin', disabled: true }, valLogin],
+            login: ['superadmin', valLogin],
             password: ['', Validators.required],
             passwordConfirm: ['', Validators.required],
             email: ['dev@maarch.org', valEmail],
