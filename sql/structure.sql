cefbc6494396588ee70a8dc4261b2aa9b16462ba
Damien <damien.burel@maarch.org>
2020-07-21 09:44:52 +0200

    FEAT #14213 TIME 0:15 Super admin user mode structure sql

--- sql/structure.sql
+++ sql/structure.sql
@@ -206,6 +206,8 @@ CREATE TABLE usergroups_services
 )
 WITH (OIDS=FALSE);
 
+CREATE TYPE users_modes AS ENUM ('standard', 'rest', 'root_visible', 'root_invisible');
+
 CREATE TABLE users
 (
   id serial NOT NULL,
@@ -220,6 +222,7 @@ CREATE TABLE users
   status character varying(10) NOT NULL DEFAULT 'OK'::character varying,
   password_modification_date timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
   loginmode character varying(50) DEFAULT NULL::character varying,
+  mode users_modes NOT NULL DEFAULT 'standard',
   refresh_token jsonb NOT NULL DEFAULT '[]',
   reset_token text,
   failed_authentication INTEGER DEFAULT 0,
