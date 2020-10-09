5f65c4fcf8fa96081339ceef76f4f6bc2ea32155
hamza.hramchi <hamza.hramchi@xelians.fr>
2020-10-09 15:11:41 +0200

    FEAT #13271 TIME 0:01 remove conole.log

--- src/frontend/app/adv-search/result-list/search-result-list.component.ts
+++ src/frontend/app/adv-search/result-list/search-result-list.component.ts
@@ -397,8 +397,6 @@ export class SearchResultListComponent implements OnInit, OnDestroy {
 
     processPostData(data: any) {
         data.resources.forEach((element: any) => {
-            console.log(element);
-            
             // Process main datas
             Object.keys(element).forEach((key) => {
                 if (key === 'statusImage' && element[key] == null) {
