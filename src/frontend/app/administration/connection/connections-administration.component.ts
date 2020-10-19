e3f3ee13802e25699cd8ba9c3377dd50537645f8
Alex ORLUC <alex.orluc@maarch.org>
2020-10-19 10:33:41 +0200

    FEAT #13268 TIME 0:10 fix build prod

--- src/frontend/app/administration/connection/connections-administration.component.ts
+++ src/frontend/app/administration/connection/connections-administration.component.ts
@@ -1,17 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { AppService } from '@service/app.service';
 
 @Component({
-  selector: 'app-connections-administration',
-  templateUrl: './connections-administration.component.html',
-  styleUrls: ['./connections-administration.component.scss']
+    selector: 'app-connections-administration',
+    templateUrl: './connections-administration.component.html',
+    styleUrls: ['./connections-administration.component.scss']
 })
 export class ConnectionsAdministrationComponent implements OnInit {
 
-  loading: boolean = true;
+    loading: boolean = true;
 
-  constructor() { }
+    constructor(
+        public appService: AppService,
+    ) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
 }
