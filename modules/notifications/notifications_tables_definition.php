c4ded281bf7fa5fca67d08e502da19cb5d524079
florian.azizian <florian.azizian@maarch.org>
2020-11-04 16:58:55 +0100

    FEAT #15350 TIME 0:40 clean useless code (avis + notes + notifications + sendmail + templates)

--- modules/notifications/notifications_tables_definition.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if (!defined('_NOTIFICATIONS_TABLE_NAME')) {
-    define('_NOTIFICATIONS_TABLE_NAME', 'notifications');
-}
-if (!defined('_NOTIF_EVENT_STACK_TABLE_NAME')) {
-    define('_NOTIF_EVENT_STACK_TABLE_NAME', 'notif_event_stack');
-}
