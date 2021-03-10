fbe801b1b26914618e23f5857ee196a645ab2a37
florian.azizian <florian.azizian@maarch.org>
2021-03-10 10:31:31 +0100

    FIX #16417 TIME 0:25 Improve control for senders, recipients and diffusionList in controlAdjacentData

--- src/app/resource/controllers/ResourceControlController.php
+++ src/app/resource/controllers/ResourceControlController.php
@@ -295,6 +295,8 @@ class ResourceControlController
             foreach ($body['senders'] as $key => $sender) {
                 if (!Validator::arrayType()->notEmpty()->validate($sender)) {
                     return ['errors' => "Body senders[{$key}] is not an array"];
+                } elseif (!Validator::intVal()->notEmpty()->validate($sender['id'])) {
+                    return ['errors' => "Body senders[{$key}][id] is empty or not an integer"];
                 }
                 if ($sender['type'] == 'contact') {
                     $senderItem = ContactModel::getById(['id' => $sender['id'], 'select' => [1]]);
@@ -317,6 +319,8 @@ class ResourceControlController
             foreach ($body['recipients'] as $key => $recipient) {
                 if (!Validator::arrayType()->notEmpty()->validate($recipient)) {
                     return ['errors' => "Body recipients[{$key}] is not an array"];
+                } elseif (!Validator::intVal()->notEmpty()->validate($recipient['id'])) {
+                    return ['errors' => "Body recipients[{$key}][id] is empty or not an integer"];
                 }
                 if ($recipient['type'] == 'contact') {
                     $recipientItem = ContactModel::getById(['id' => $recipient['id'], 'select' => [1]]);
@@ -338,6 +342,11 @@ class ResourceControlController
             }
             $destFound = false;
             foreach ($body['diffusionList'] as $key => $diffusion) {
+                if (!Validator::arrayType()->notEmpty()->validate($diffusion)) {
+                    return ['errors' => "Body diffusionList[{$key}] is not an array"];
+                } elseif (!Validator::intVal()->notEmpty()->validate($diffusion['id'])) {
+                    return ['errors' => "Body diffusionList[{$key}][id] is empty or not an integer"];
+                }
                 if ($diffusion['mode'] == 'dest') {
                     if ($destFound) {
                         return ['errors' => "Body diffusionList has multiple dest"];
