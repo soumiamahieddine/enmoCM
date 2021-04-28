a0e9838c8368d7c347e25b3355c28a7985699ae2
hamza.hramchi <hamza.hramchi@xelians.fr>
2021-04-28 17:39:22 +0200

    FIX #16909 TIME 0:10 Fix: mail out of perimeter after action without confirmation

--- src/frontend/app/actions/actions.service.ts
+++ src/frontend/app/actions/actions.service.ts
@@ -163,7 +163,7 @@ export class ActionsService implements OnDestroy {
             if (this.lockMode) {
                 const res: any = await this.canExecuteAction(resIds);
                 if (res === true) {
-                    if (['viewDoc', 'documentDetails', 'signatureBookAction', 'processDocument'].indexOf(action.component) > -1) {
+                    if (['viewDoc', 'documentDetails', 'signatureBookAction', 'processDocument'].indexOf(action.component) > -1 || action.component === 'noConfirmAction') {
                         this[action.component](action.data);
                     } else {
                         try {
