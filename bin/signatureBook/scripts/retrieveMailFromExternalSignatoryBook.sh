7fbcc3a87ca0924ea0cfff268433cfc9dcf2408a
hamza.hramchi <hamza.hramchi@xelians.fr>
2021-09-30 12:22:45 +0200

    FIX #18115 TIME 0:02 rm script file

--- bin/signatureBook/scripts/retrieveMailFromExternalSignatoryBook.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd /var/www/html/MaarchCourrier2103/bin/signatureBook/
-filePath='/var/www/html/MaarchCourrier2103/bin/signatureBook/process_mailsFromSignatoryBook.php'
-php $filePath -c /var/www/html/MaarchCourrier2103/apps/maarch_entreprise/xml/config.json
